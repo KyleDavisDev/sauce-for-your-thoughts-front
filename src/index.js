43a7605bbca11eb27d6332c14cfb8f3af345d170
Kyle Bonar <kylewb2011@gmail.com>
2017-06-14 15:58:22 -0500

    Comments and pulling in style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+//component that handles all SPA routing
 import Router from './components/Router/Router.js'
+
+//general styles
+import './index.scss'
 ReactDOM.render(<Router />, document.getElementById('app'))
