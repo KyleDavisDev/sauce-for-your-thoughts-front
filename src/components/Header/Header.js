85260d9fb12866e64f1f86596f69f9c63ea3194b
Kyle Bonar <kylewb2011@gmail.com>
2017-06-14 15:59:00 -0500

    Init Commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react'
 import { NavLink, Link } from 'react-router-dom'
 
+//pull in logos
+import MainLogo from '../../images/icons/Logo.js'
+import StoresLogo from '../../images/icons/Store.js'
+import TagLogo from '../../images/icons/Tag.js'
+import TopLogo from '../../images/icons/Top.js'
+import AddLogo from '../../images/icons/Add.js'
+import MapLogo from '../../images/icons/Map.js'
+
+//style
+import './header.scss'
+
 class Header extends Component {
     render() {
+		const navigationItems = [
+			{ text: "Stores", img: <StoresLogo height="35" width="35" />, imgTitle: "Stores" },
+			{ text: "Tags", img: <TagLogo height="35" width="35" />, imgTitle: "Tags" },
+			{ text: "Top", img: <TopLogo height="35" width="35" />, imgTitle: "Top" },
+			{ text: "Add", img: <AddLogo height="35" width="35" />, imgTitle: "Add" },
+			{ text: "Map", img: <MapLogo height="35" width="35" />, imgTitle: "Map" },
+		]
         return(
             <div className="nav-container" >
                 <div id="navBar" >
                     <NavLink exact className="home-link" activeClassName="active" to="/" >
-                        Damn that's good
+                        <MainLogo className="home-logo" height="50" width="100" />
                     </NavLink>
 
                     <div id="nav-items-container">
 						<ul>
-							<li>
-								<NavLink className="menu-link" activeClassName="active" to="/projects">
-									Projects
-								</NavLink>
-							</li>
-							<li>
-								<a className="menu-link" target="_blank" href="/resume">
-									Resume
-								</a>
-							</li>
+							{
+								navigationItems.map( (item) => {
+									return (
+										<li key={item.text} >
+											{item.img}
+											{item.text.toUpperCase()}
+										</li>
+									)
+								})
+							}
 						</ul>
 					</div>
                 </div>
