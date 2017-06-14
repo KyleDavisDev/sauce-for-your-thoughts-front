90a2d525778820becfd1a257976e695c0eda2692
Kyle Bonar <kylewb2011@gmail.com>
2017-06-14 12:07:46 -0500

    Exploring req and res objects

--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,16 @@ const express = require('express')
 const router = express.Router();
 
 // api stuff
-router.get('/api/test', (req, res) => {
-  res.send('Hey! It works!');
-});
+// router.get('/api/test', (req, res) => {
+//   res.send('Hey! It works!');
+// });
+router.get('/api/reverse/:first', (req, res) => {
+  const reverse = req.params.first.split('').reverse().join('')
+  // req.params.name
+  res.send(reverse)
+})
 
-
-//let react handle it
+//let react handle rest
 router.get('*', (req, res) => {
   res.sendFile(`${process.cwd()}/dist/index.html`);
 });
