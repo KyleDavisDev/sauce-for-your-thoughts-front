f5575eb680cf253ae64e55268b2eb72219c1a19d
Kyle Bonar <kylewb2011@gmail.com>
2017-06-16 15:31:47 -0500

    Included Add component

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 //pull in other components for SPA
 import Header from "../Header/Header.js";
 import Holder from "../Holder/Holder.js";
+import Add from "../Add/Add.js"
 
 class Router extends Component {
   render() {
@@ -14,6 +15,7 @@ class Router extends Component {
           <Header />
           <Switch>
             <Route exact path="/" component={Holder} />
+            <Route exact path="/add" component={Add} />
             <Route
               render={function() {
                 return <p> Page not found. Sorry! </p>;
