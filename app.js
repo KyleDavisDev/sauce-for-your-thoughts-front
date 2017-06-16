e11fa8d705eb80c7498d629ce01f0bde3d4ed50c
Kyle Bonar <kylewb2011@gmail.com>
2017-06-16 15:32:03 -0500

    Included middleware

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const express = require('express')
-const expressValidator = require('express-validator')
-const mongoose = require('mongoose')
-const path = require('path')
-const routes = require('./routes/routes.js')
+const express = require("express");
+const expressValidator = require("express-validator");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const routes = require("./routes/routes.js");
 
 //create express app
-const app = express()
+const app = express();
 
 //serves up static files from distribution folder.
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, "dist")));
+
+// takes raw requests and attaches them to req.body for use later
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//populates req.cookies w/ any cookies that are w/ the req
+app.use(cookieParser());
 
 //handle routes
-app.use('/', routes)
+app.use("/", routes);
 
-module.exports = app
+module.exports = app;
