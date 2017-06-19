3b8329180cb8c84881eee2397d3b8002dcc8a4f8
Kyle Bonar <kylewb2011@gmail.com>
2017-06-19 16:10:19 -0500

    Started build out

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,13 @@
-exports.addStore = (req, res) => {
-  res.send("It works");
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
+
+exports.addStore = async (req, res) => {
+  // res.json(req.body);
+  try {
+    const store = new Store(req.body);
+    await store.save();
+    // res.redirect('/');
+  } catch (err) {
+    console.log(err);
+  }
 };
