4e887310e504e63bca320ebd07ea8daec6dd98e2
Kyle Bonar <kylewb2011@gmail.com>
2017-06-20 12:22:27 -0500

    Will no render appropriate flash component on submit

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -1,17 +1,41 @@
 import React, { Component } from "react";
 
-import StoreForm from "./StoreForm.js"
+import StoreForm from "./StoreForm.js";
+import FlashMessage from "../FlashMessage/FlashMessage.js";
 
 class Add extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      showFlashMessage: false,
+      flashMessageType: ""
+    };
+
+    this.handleStoreAdded = this.handleStoreAdded.bind(this);
+    this.closeFlashMessage = this.closeFlashMessage.bind(this);
+  }
+
+  handleStoreAdded(type) {
+    this.setState({ showFlashMessage: true, flashMessageType: type });
+  }
+
+  closeFlashMessage() {
+    this.setState({ showFlashMessage: false, flashMessageType: "" });
   }
 
   render() {
     return (
       <div className="inner">
         <h2>Add Store</h2>
-        <StoreForm />
+        <StoreForm handleStoreAdded={this.handleStoreAdded} />
+        {this.state.showFlashMessage &&
+          <FlashMessage
+            isVisible={true}
+            type={this.state.flashMessageType}
+            text={"it worked!"}
+            closeFlashMessage={this.closeFlashMessage}
+          />}
       </div>
     );
   }
