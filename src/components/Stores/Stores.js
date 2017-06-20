4ae971080e70b8c15cbb411c8a6ddec56eba2cca
Kyle Bonar <kylewb2011@gmail.com>
2017-06-20 16:01:43 -0500

    Now gets stores from DB and posts their name

--- src/components/Stores/Stores.js
+++ src/components/Stores/Stores.js
@@ -1,10 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
 class Stores extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stores: []
+    }
+  }
+
+  componentWillMount() {
+    axios
+      .get("/api/stores/get")
+      .then( (response) => {
+        //response.data is array of store objects from DB
+        this.setState({stores: response.data})
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
   render() {
-    return(
-      <div> Hello world</div>
-    )
+    return (
+      <div className="inner">
+        <h2>Stores</h2>
+          {this.state.stores.map( (store) => {
+            return store.name
+          })}
+      </div>
+    );
   }
 }
 
