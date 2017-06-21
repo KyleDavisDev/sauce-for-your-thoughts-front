9310638ae1e197498c19f8571bc6de5bdf3eb354
Kyle Bonar <kylewb2011@gmail.com>
2017-06-21 16:10:25 -0500

    Started build out for store cards

--- src/components/Stores/Stores.js
+++ src/components/Stores/Stores.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 class Stores extends Component {
@@ -7,15 +8,15 @@ class Stores extends Component {
 
     this.state = {
       stores: []
-    }
+    };
   }
 
   componentWillMount() {
     axios
       .get("/api/stores/get")
-      .then( (response) => {
+      .then(response => {
         //response.data is array of store objects from DB
-        this.setState({stores: response.data})
+        this.setState({ stores: response.data });
       })
       .catch(function(error) {
         console.log(error);
@@ -26,9 +27,27 @@ class Stores extends Component {
     return (
       <div className="inner">
         <h2>Stores</h2>
-          {this.state.stores.map( (store) => {
-            return store.name
+        <div className="stores">
+          {this.state.stores.map(store => {
+            return (
+              <div className="store" key={store.slug}>
+                <div className="store-hero">
+                  <div className="store-actions">
+                    {"<3"}
+                  </div>
+                  <div className="store-title">
+                    <Link to={`/store/${store.slug}`}>
+                      {store.name}
+                    </Link>
+                  </div>
+                </div>
+                <div className="store-details" >
+                  <p>{store.description}</p>
+                </div>
+              </div>
+            );
           })}
+        </div>
       </div>
     );
   }
