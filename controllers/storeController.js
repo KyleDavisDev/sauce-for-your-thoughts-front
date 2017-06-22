9e0c387f9346c518eafe91aeb9cf80015888017d
Kyle Bonar <kylewb2011@gmail.com>
2017-06-22 16:11:47 -0500

    Built out editStore

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -8,8 +8,8 @@ exports.addStore = async (req, res) => {
     //send back slug so we can link to it for user to rate
     res.send(store.slug);
   } catch (err) {
-    // console.log(err);
-    res.send("error");
+    console.log(err);
+    res.send(err);
   }
 };
 
@@ -17,7 +17,22 @@ exports.getStores = async (req, res) => {
   try {
     const store = await Store.find();
 
-    //TODO adjust query to not include ID maybe? 
+    //send json of stores
+    res.send(store);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+};
+
+exports.editStore = async (req, res) => {
+  try {
+    const store = await Store.findOne({ _id: req.params.id });
+
+    //TODO make sure user is actual "owner" of store
+    //check here
+
+    //send store back for user to edit
     res.send(store);
   } catch (err) {
     console.log(err);
