2ef62633f459d4329b603c11e30c0e8d82785090
Kyle Bonar <kylewb2011@gmail.com>
2017-06-22 16:14:26 -0500

    Included basic background image and link to edit specific store

--- src/components/Stores/Stores.js
+++ src/components/Stores/Stores.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 import storeImage from "../../images/photos/store.jpg";
-import Pencil from '../../images/icons/Pencil.js'
+import Pencil from "../../images/icons/Pencil.js";
 
 class Stores extends Component {
   constructor(props) {
@@ -31,35 +31,32 @@ class Stores extends Component {
       <div className="inner">
         <h2>Stores</h2>
         <div className="stores">
-          {this.state.stores.map(store => {
-            return (
-              <div className="store" key={store.slug}>
-                <div className="store-hero">
-                  <div className="store-actions">
-                    <div className="store-action-edit" > 
-                      <Link to={`/store/${store._id}/edit`} >
-                        <Pencil />
+          {this.state.stores.length > 0 &&
+            this.state.stores.map(store => {
+              return (
+                <div className="store" key={store.slug}>
+                  <div className="store-hero">
+                    <div className="store-actions">
+                      <div className="store-action store-action-edit">
+                        <Link to={`/store/${store._id}/edit`}>
+                          <Pencil />
+                        </Link>
+                      </div>
+                    </div>
+                    <img src={storeImage} title={store.name} alt={store.name} />
+                    <div className="store-title">
+                      <Link to={`/store/${store.slug}`}>
+                        {store.name}
                       </Link>
                     </div>
                   </div>
-                  <img
-                    src={storeImage}
-                    title={store.name}
-                    alt={store.name}
-                  />
-                  <div className="store-title">
-                    <Link to={`/store/${store.slug}`}>
-                      {store.name}
-                    </Link>
+                  <div className="store-details">
+                    {/*{limit description to 25 words }*/}
+                    <p>{store.description.split(" ").slice(0, 25).join(" ")}</p>
                   </div>
                 </div>
-                <div className="store-details">
-                  {/*{limit description to 25 words }*/}
-                  <p>{store.description.split(" ").slice(0, 25).join(" ")}</p>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
       </div>
     );
