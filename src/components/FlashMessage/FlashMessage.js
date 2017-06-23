4b5d5a5699dfa44cf2ff6857e62a0307d0422782
Kyle Bonar <kylewb2011@gmail.com>
2017-06-23 15:33:23 -0500

    Component can now accept object or string as text prop

--- src/components/FlashMessage/FlashMessage.js
+++ src/components/FlashMessage/FlashMessage.js
@@ -7,11 +7,25 @@ class FlashMessage extends Component {
     super(props);
 
     this.state = {
-      isVisible: this.props.isVisible,
       type: this.props.type,
       text: this.props.text,
       slug: this.props.slug
     };
+
+    this.iterateObject = this.iterateObject.bind(this);
+  }
+
+  iterateObject() {
+    // the keys of this.state.text may change later depending on
+    //the DB and this way of looping through the object will us to grab the messages
+    //reguardless of what the key is
+    const errorKeys = Object.keys(this.state.text);
+    const errorMessages = errorKeys.map(key => {
+      return this.state.text[key].message;
+    });
+    return errorMessages.map(errorMessage => {
+      return <p className="error-item">{errorMessage}</p>;
+    });
   }
 
   render() {
@@ -20,17 +34,20 @@ class FlashMessage extends Component {
       : "";
     return (
       <div className={`flash ${this.state.type}`}>
-        <p>{this.state.text}{" "}{slugLink}</p>
-        <button onClick={this.props.closeFlashMessage}>X</button>
+        {typeof this.state.text === "object" &&
+          <div className="errors-list">{this.iterateObject()}</div>}
+        {typeof this.state.text === "string" &&
+          <div className="errors-list"><p>{this.state.text}</p></div>}
+        {slugLink && <p>{slugLink}</p>}
+        <button className="close-button" onClick={this.props.closeFlashMessage}>X</button>
       </div>
     );
   }
 }
 
 FlashMessage.propTypes = {
-  isVisible: PropTypes.bool,
   type: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
+  text: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   slug: PropTypes.string.isRequired
 };
 
