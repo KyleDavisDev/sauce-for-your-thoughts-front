96c7eaddd043b77238d30e8ab69ce882d3c4a922
Kyle Bonar <kylewb2011@gmail.com>
2017-06-28 12:45:09 -0500

    Model now contains photo string

--- models/Store.js
+++ models/Store.js
@@ -33,7 +33,8 @@ const storeSchema = new mongoose.Schema({
       type: String,
       required: "You must supply an address!"
     }
-  }
+  },
+  photo: String
 });
 
 //TODO make more terse so slugs are unique
