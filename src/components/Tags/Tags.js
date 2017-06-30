db3c48bc786f1ff70b7bdbeafb1f35803ddefb6e
Kyle Bonar <kylewb2011@gmail.com>
2017-06-30 11:04:59 -0500

    Init Commit

---
+++ src/components/Tags/Tags.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Tags extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div className="inner">Hello world!</div>;
+  }
+}
+
+module.exports = Tags;
