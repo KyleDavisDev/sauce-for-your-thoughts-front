deebe86a896aa3d08b20aa1d36158c6106ac75c2
Kyle Bonar <kylewb2011@gmail.com>
2017-06-30 14:43:19 -0500

    Extracted cards to other component

--- src/components/Stores/Stores.js
+++ src/components/Stores/Stores.js
@@ -2,15 +2,14 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-import storeImage from "../../images/photos/store.jpg";
-import Pencil from "../../images/icons/Pencil.js";
+import StoreCard from "../StoreCard/StoreCard.js";
 
 class Stores extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      stores: []
+      stores: {}
     };
   }
 
@@ -32,38 +31,7 @@ class Stores extends Component {
         <h2>Stores</h2>
         <div className="stores">
           {this.state.stores.length > 0 &&
-            this.state.stores.map(store => {
-              return (
-                <div className="store" key={store.slug}>
-                  <div className="store-hero">
-                    <div className="store-actions">
-                      <div className="store-action store-action-edit">
-                        <Link to={`/store/${store._id}/edit`}>
-                          <Pencil />
-                        </Link>
-                      </div>
-                    </div>
-                    <img
-                      src={`http://localhost:7777/public/uploads/${store.photo}`}
-                      onError={e => (e.target.src = storeImage)}
-                      title={store.name}
-                      alt={store.name}
-                    />
-                    <div className="store-title">
-                      <Link to={`/store/${store.slug}`}>
-                        {store.name}
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="store-details">
-                    {/*{limit description to 25 words }*/}
-                    <p>
-                      {store.description.split(" ").slice(0, 25).join(" ")}
-                    </p>
-                  </div>
-                </div>
-              );
-            })}
+            <StoreCard stores={this.state.stores} />}
         </div>
       </div>
     );
