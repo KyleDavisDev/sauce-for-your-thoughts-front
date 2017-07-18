360be922828e7ae7e42095a1e78c5839623906d0
Kyle Bonar <kylewb2011@gmail.com>
2017-07-18 16:07:53 -0500

    Component now checks to be sure user is, in fact, logged out and is now pulling in password reset form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
 
-import LoginForm from '../LoginForm/LoginForm.js'
+import LoginForm from "../LoginForm/LoginForm.js";
+import ForgotPasswordForm from "../ForgotPasswordForm/ForgotPasswordForm.js";
 
 class Login extends Component {
   constructor(props) {
     super(props);
   }
+  componentWillMount(){
+    //make sure user is logged out
+    this.props.logUserOut();
+  }
   render() {
     return (
-        <div className="inner">
-            <LoginForm logUserIn={this.props.logUserIn} createFlashMessage={this.props.createFlashMessage}/>
-        </div>
-    )
+      <div className="inner">
+        <LoginForm
+          logUserIn={this.props.logUserIn}
+          createFlashMessage={this.props.createFlashMessage}
+        />
+        <ForgotPasswordForm
+          logUserIn={this.props.logUserIn}
+          createFlashMessage={this.props.createFlashMessage}
+        />
+      </div>
+    );
   }
 }
 
