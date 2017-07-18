a9fa7f6ca0c7763f274d1c0ebc43515138c94ef1
Kyle Bonar <kylewb2011@gmail.com>
2017-07-18 16:07:19 -0500

    Component for resetting password

---
+++ src/components/ForgotPasswordForm/ForgotPasswordForm.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import Checker from "../../helper/Checker/Checker.js";
+import Auth from "../../helper/Auth/Auth.js"
+
+class ForgotPasswordForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="form forgot-form">
+        <h2>I forgot my password!</h2>
+        <label htmlFor="forgotEmail"> Email: </label>
+        <input
+          type="email"
+          name="forgotEmail"
+          id="forgotEmail"
+          onChange={this.handleEmailChange}
+        />
+        <button type="submit" className="button">
+          SEND A RESET ->
+        </button>
+      </form>
+    );
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    if (this.state.email.length === 0) return;
+
+    axios({
+      method: "post",
+      url: "http://localhost:7777/account/reset",
+      data: {
+        email: this.state.email
+      }
+    })
+      .then(response => {
+
+        //will be object if successful else we will send fake success
+        if (Checker.isObject(response.data)) {
+          Auth
+          this.props.createFlashMessage({
+            isVisible: true,
+            type: "success",
+            text: response.data.resetURL
+          });
+        } else {
+          //set success flash
+          this.props.createFlashMessage({
+            isVisible: true,
+            type: "success",
+            text: response.data
+          });
+        }
+
+        this.setState({email: ""})
+      })
+      .catch(error => {
+        console.log(error);
+        //set error flash message
+        this.props.createFlashMessage({
+          isVisible: true,
+          type: "error",
+          text: "Oops! Try again please."
+        });
+      });
+  }
+
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  }
+}
+
+module.exports = ForgotPasswordForm;
