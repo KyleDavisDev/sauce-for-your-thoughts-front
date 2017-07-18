3be62a9e82d4cef25cb169b4dc75f2160a005186
Kyle Bonar <kylewb2011@gmail.com>
2017-07-18 16:09:29 -0500

    Lots of comments, included new function to update token

--- src/Helper/Auth/Auth.js
+++ src/Helper/Auth/Auth.js
@@ -1,21 +1,69 @@
 class Auth {
-
+  //set local storage w/ token and datetime
   static authenticateUser(token) {
-    localStorage.setItem('token', token);
+    const key = { token, timestamp: new Date().getTime() };
+    localStorage.setItem("key", JSON.stringify(key));
   }
 
+  //check if token exists and hasn't expired
   static isUserAuthenticated() {
-    return localStorage.getItem('token') !== null;
+    //if token doesn't exist, return false here
+    if (!localStorage.getItem("key")) return false;
+
+    //set timelimit for token in minutes
+    const timeLimit = 60;
+
+    //get key time or return false if timestamp doesn't exist inside of key
+    const keyTime = JSON.parse(localStorage.getItem("key")).timestamp;
+    if (!keyTime) return false;
+
+    //get current time
+    const curTime = new Date().getTime().toString();
+
+    //compare times and round to minutes
+    const diffInMinutes = Math.round(
+      (parseInt(curTime) - parseInt(keyTime)) / 1000 / 60
+    );
+
+    //token is good, update token and return true
+    if (diffInMinutes < timeLimit) {
+      this.updateUserToken();
+      return true;
+    } else {
+      //token exists but is old, should remove
+      this.deauthenticateUser();
+      return false;
+    }
   }
-  
+
+  //update existing token
+  //should ONLY be called from isUserAuthenticated so will
+  //not be doing same sanity checks as above
+  static updateUserToken() {
+    const key = {
+      token: JSON.parse(localStorage.getItem("key")).token,
+      timestamp: new Date().getTime()
+    };
+    localStorage.setItem("key", JSON.stringify(key));
+    return true;
+  }
+
+  //remove authentication token
   static deauthenticateUser() {
-    localStorage.removeItem('token');
+    //if exists, remove
+    if (localStorage.getItem("key")) {
+      localStorage.removeItem("key");
+    }
   }
 
+  //return authentication token
   static getToken() {
-    return localStorage.getItem('token');
+    //if exists, send back
+    if (localStorage.getItem("key")) {
+      return JSON.parse(localStorage.getItem("key")).token || null;
+    }
+    return null;
   }
-
 }
 
 export default Auth;
