da6babeb03f9a7605e4751522eac2fd704002dda
Kyle Bonar <kylewb2011@gmail.com>
2017-07-26 15:47:46 -0500

    Better formatted and consistant with rest of project.

--- src/components/Store/StoreEdit.js
+++ src/components/Store/StoreEdit.js
@@ -1,26 +1,31 @@
 import React, { Component } from "react";
 import axios from "axios";
+import PropTypes from "prop-types";
 
 import StoreForm from "../StoreForm/StoreForm.js";
-import FlashMessage from "../FlashMessage/FlashMessage.js";
+
+import Auth from "../../helper/Auth/Auth.js";
 
 class StoreEdit extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      store: {},
-      flashMessage: { isVisible: false, type: "", text: "", slug: "" }
+      store: {
+        name: "",
+        description: "",
+        tags: [""],
+        location: { address: "", coordinates: ["", ""] },
+        photo: ""
+      }
     };
 
-    this.updateStoreEntry = this.updateStoreEntry.bind(this);
-    this.createFlashMessage = this.createFlashMessage.bind(this);
-    this.closeFlashMessage = this.closeFlashMessage.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
-  componentWillMount() {
+  componentDidMount() {
     //TODO sanity checks to be sure the ID passed is legit
-    const storeID = this.props.match.params.id;
+    const storeID = this.props.id;
 
     axios
       .get(`http://localhost:7777/api/store/${storeID}/get`)
@@ -34,108 +39,84 @@ class StoreEdit extends Component {
       });
   }
 
-  updateStoreEntry(store) {
-    let didUpdateWork = false;
-    this.closeFlashMessage();
-    const storeID = this.props.match.params.id;
-
-    //million ways to destructure the store object but I like this one
-    const {
-      storeName: name,
-      storeDescription: description,
-      storePhoto: photo
-    } = store;
-    const tags = store.tags.filter(tag => tag.isChecked).map(tag => tag.name);
-    const address = store.location.storeAddress;
-    const coordinates = [
-      parseFloat(store.location.storeLongitude).toFixed(7) || "",
-      parseFloat(store.location.storeLatitude).toFixed(7) || ""
-    ];
-
-    const formData = new FormData();
-    formData.append("name", name);
-    formData.append("description", description);
-    formData.append("image", photo);
-    formData.append("address", address);
-    formData.append("coordinates", coordinates);
-    formData.append("tags", tags);
-
-    var options = {
-      headers: {
-        "Content-Type": "multipart/form-data"
-      }
-    };
-
-    //TODO filter/sanitize user input
-    axios
-      .post(
-        `http://localhost:7777/api/store/${storeID}/edit`,
-        formData,
-        options
-      )
-      .then(response => {
-        if (response.data.errors) {
-          this.createFlashMessage({
-            type: "error",
-            text: response.data.errors
-          });
-        } else {
-          this.setState({ store: response.data });
-          this.createFlashMessage({
-            type: "success",
-            slug: response.data.slug,
-            text: "Your store was updated!"
-          });
-          didUpdateWork = true;
-        }
-      })
-      .catch(error => {
-        this.createFlashMessage("error");
-      });
-
-    return didUpdateWork;
-  }
-
-  createFlashMessage({ type, slug = "", text }) {
-    this.setState({ flashMessage: { isVisible: true, type, text, slug } });
-  }
-
-  closeFlashMessage() {
-    const isVisible = false;
-    const type = "";
-    const text = "";
-    const slug = "";
-    this.setState({ flashMessage: { isVisible, type, text, slug } });
-  }
-
   render() {
     return (
       <div className="inner">
-        {this.state.flashMessage.isVisible &&
-          <FlashMessage
-            isVisible={this.state.flashMessage.isVisible}
-            type={this.state.flashMessage.type}
-            text={this.state.flashMessage.text}
-            slug={this.state.flashMessage.slug}
-            closeFlashMessage={this.closeFlashMessage}
-          />}
         <h2>
           Edit {this.state.store.name || "Store"}
         </h2>
         {Object.keys(this.state.store).length > 0 &&
           <StoreForm
-            onFormSubmit={this.updateStoreEntry}
+            onFormSubmit={this.handleFormSubmit}
             storeName={this.state.store.name}
             storeDescription={this.state.store.description}
+            storePhoto={this.state.store.photo}
             tags={this.state.store.tags}
             storeAddress={this.state.store.location.address}
             storeLongitude={this.state.store.location.coordinates[0]}
             storeLatitude={this.state.store.location.coordinates[1]}
-            didPostWork={false}
           />}
       </div>
     );
   }
+
+  handleFormSubmit(e, store) {
+    e.preventDefault();
+
+    const storeID = this.props.id;
+
+    //make tags an array of checked tags
+    const tags = store.tags.filter(tag => tag.isChecked).map(tag => tag.name);
+    //round location to 6 decimal places and convert to string
+    const coordinates = [
+      parseFloat(store.location.storeLongitude).toFixed(7) || "",
+      parseFloat(store.location.storeLatitude).toFixed(7) || ""
+    ];
+
+    const data = new FormData();
+    data.append("name", store.storeName);
+    data.append("description", store.storeDescription);
+    data.append("image", store.storePhoto);
+    data.append("address", store.location.storeAddress);
+    data.append("coordinates", coordinates);
+    data.append("tags", tags);
+    data.append("token", Auth.getToken());
+
+    //TODO filter/sanitize user input
+    axios({
+      method: "post",
+      url: `http://localhost:7777/api/store/${storeID}/edit`,
+      data,
+      options: { headers: { "Content-Type": "multipart/form-data" } }
+    })
+      .then(response => {
+        if (response.data.errors) {
+          this.props.createFlashMessage({
+            type: "error",
+            text: response.data.errors
+          });
+        } else {
+          this.setState({ store: response.data });
+          this.props.createFlashMessage({
+            type: "success",
+            slug: response.data.slug,
+            text: "Your store was updated!"
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        this.props.createFlashMessage({
+          type: "error",
+          text: "Try again."
+        });
+      });
+  }
 }
+
+StoreEdit.propTypes = {
+  createFlashMessage: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired
+};
 
 module.exports = StoreEdit;
