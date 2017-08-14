a0113e337ccc002f7843803c0605ce295b98c015
Kyle Bonar <kylewb2011@gmail.com>
2017-08-14 10:46:25 -0500

    Updates to lifecycle methods to get appropriate store/user information.

--- src/components/Tags/Tags.js
+++ src/components/Tags/Tags.js
@@ -4,6 +4,13 @@ import { NavLink } from "react-router-dom";
 
 import StoreCard from "../StoreCard/StoreCard.js";
 
+import Auth from "../../helper/Auth/Auth.js";
+import Checker from "../../helper/Checker/Checker.js";
+
+
+//TODO: Configure API calls such that any time the user decides to change tag filter, we do not have
+//post the token and see if person owns the store or not.
+
 class Tags extends Component {
   constructor(props) {
     super(props);
@@ -13,36 +20,29 @@ class Tags extends Component {
       tags: {},
       stores: {}
     };
+
+    this.apiGetStoresAndUserID = this.apiGetStoresAndUserID.bind(this);
   }
 
-  componentWillMount() {
-    const tag = this.props.match.params.tag;
+  componentDidMount() {
+    const tag = this.props.match.params.tag || "";
     this.setState({ title: tag });
 
-    axios
-      .get(`http://localhost:7777/api/tags/${tag}/get`)
-      .then(response => {
-        this.setState({ tags: response.data[0], stores: response.data[1] });
-      })
-      .catch(err => console.log(err));
+    this.apiGetStoresAndUserID(tag);
   }
 
   componentWillReceiveProps(nextProps) {
     const tag = nextProps.match.params.tag;
     this.setState({ title: tag });
-    axios
-      .get(`http://localhost:7777/api/tags/${tag}/get`)
-      .then(response => {
-        this.setState({ tags: response.data[0], stores: response.data[1] });
-      })
-      .catch(err => console.log(err));
+
+    this.apiGetStoresAndUserID(tag);
   }
 
   render() {
     return (
       <div className="inner">
         <h2>
-          {this.state.title || "Tags"}
+          {this.state.title ? this.state.title : "Tags"}
         </h2>
         {this.state.tags.length > 0 &&
           <ul className="tags">
@@ -70,6 +70,9 @@ class Tags extends Component {
             {this.state.stores.map(store => {
               return (
                 <StoreCard
+                  displayEditIcon={
+                    this.state.userID === store.author ? true : false
+                  }
                   ID={store._id}
                   name={store.name}
                   image={store.photo}
@@ -81,6 +84,60 @@ class Tags extends Component {
             })}
           </div>}
       </div>
+    );
+  }
+
+  getStores(tag) {
+    tag = tag === "" ? "all" : tag;
+    return axios.get(`http://localhost:7777/api/tags/${tag}/get`);
+  }
+
+  getUserID() {
+    return (
+      Auth.isUserAuthenticated() &&
+      axios({
+        method: "post",
+        url: "http://localhost:7777/account/getInfo",
+        data: { token: Auth.getToken() }
+      })
+    );
+  }
+
+  apiGetStoresAndUserID(tag) {
+    //need this slight work around because "this" is not Stores component inside the .all scope
+    let that = this;
+    axios.all([that.getStores(tag), that.getUserID()]).then(
+      axios.spread((response, user) => {
+        //we will build state
+        let stateBuilder = {};
+        //check to make sure object returned
+        if (Checker.isObject(response.data)) {
+          //success or not
+          if (response.data.isGood) {
+            stateBuilder = {
+              tags: response.data.tags,
+              stores: response.data.stores,
+              ...stateBuilder
+            };
+
+            //only if we make it here are we concerned whether or not person is logged in
+            if (Checker.isObject(user.data) && user.data.isGood) {
+              stateBuilder = { userID: user.data.user._id, ...stateBuilder };
+            }
+          } else {
+            this.props.createFlashMessage({
+              type: "error",
+              msg: stores.data.msg
+            });
+          }
+        } else {
+          this.props.createFlashMessage({
+            type: "error",
+            msg: "Something is broken! Try reloading the page."
+          });
+        }
+        this.setState(stateBuilder);
+      })
     );
   }
 }
