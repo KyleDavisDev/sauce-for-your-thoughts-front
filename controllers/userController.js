745f88719b3d81bf498e920c29afb40996139c8e
Kyle Bonar <kylewb2011@gmail.com>
2017-10-25 14:52:26 -0500

    Updated naming convention for camelCase checking.

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,24 +14,22 @@ exports.validateRegister = (req, res, next) => {
   });
   req.checkBody("password", "Password cannot be empty.").notEmpty();
   req
-    .checkBody("confirm-password", "Confirmed password cannot be empty.")
+    .checkBody("confirmPassword", "Confirmed password cannot be empty.")
     .notEmpty();
   req
-    .checkBody("confirm-password", "Oops! Your passwords do not match.")
+    .checkBody("confirmPassword", "Oops! Your passwords do not match.")
     .equals(req.body.password);
 
   const errors = req.validationErrors();
   if (errors) {
-    res.send(errors);
-    return;
+    return res.send(errors);
   }
-
   next();
 };
 
 exports.register = async (req, res, next) => {
   const user = new User({ email: req.body.email, name: req.body.name });
-  const registerWithPromise = promisify(User.register, User); //not register method can be awaited
+  const registerWithPromise = promisify(User.register, User); //now register method can be awaited
   try {
     await registerWithPromise(user, req.body.password);
   } catch (err) {
