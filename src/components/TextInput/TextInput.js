4d7065789085469920b242150fda4fd3427fc712
Kyle Bonar <kylewb2011@gmail.com>
2017-10-25 16:04:09 -0500

    Init commit.

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TextInput = props => {
+  return (
+    <div>
+      <label htmlFor={props.name}> {props.name}: </label>
+      <input
+        type={props.type || "text"}
+        id={props.id || ""}
+        name={props.name}
+        value={props.value}
+        onChange={props.onChange}
+        required={props.required || false}
+      />
+    </div>
+  );
+};
+
+TextInput.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  required: PropTypes.bool,
+  type: PropTypes.string,
+  value: PropTypes.string.isRequired
+};
+
+export default TextInput;
