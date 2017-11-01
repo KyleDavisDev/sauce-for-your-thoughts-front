8aa6c9b4ad67e5a4ed5a55b2092a29af857eb360
Kyle Bonar <kylewb2011@gmail.com>
2017-11-01 16:14:12 -0500

    Added logout action.

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,6 @@
 import Checker from "../helper/Checker/Checker";
 import api from "../api/api";
+import Auth from "../helper/Auth/Auth";
 
 export const userLoggedIn = user => ({
   type: "USER_LOGGED_IN",
@@ -7,8 +8,20 @@ export const userLoggedIn = user => ({
   text: "Successfully logged in. Thank you!"
 });
 
+export const userLoggedOut = user => ({
+  type: "USER_LOGGED_OUT"
+});
+
 export const login = credentials => dispatch => {
   return api.user.login(credentials).then(user => {
+    //save token to local storage and set timestamp
+    Auth.authenticateUser(user);
     dispatch(userLoggedIn(user));
   });
+};
+
+export const logout = () => dispatch => {
+  //remove token and dispatch action
+  Auth.deauthenticateUser();
+  dispatch(userLoggedOut());
 };
