7edd638e26f2507b8347670ebcd6fbe1e924594c
Kyle Bonar <kylewb2011@gmail.com>
2017-11-01 14:25:00 -0500

    Included several ways to manipulate the flash message component.

--- src/reducers/flashMessage.js
+++ src/reducers/flashMessage.js
@@ -1,10 +1,36 @@
 export default function flashMessage(state = {}, action) {
   switch (action.type) {
     case "USER_LOGGED_IN":
-    case "TOGGLE_TREE":
+    case "SUCCESS_FLASH":
+      return {
+        isVisible: true,
+        text: action.text,
+        type: "success",
+        slug: action.slug || null
+      };
+    case "ERROR_FLASH":
+      return {
+        isVisible: true,
+        text: action.text,
+        type: "error",
+        slug: action.slug || null
+      };
+    case "WARNING_FLASH":
+      return {
+        isVisible: true,
+        text: action.text,
+        type: "error",
+        slug: action.slug || null
+      };
+    case "TOGGLE_FLASH":
       return {
         ...state,
-        visible: true
+        isVisible: !state.isVisible
+      };
+    case "CLOSE_FLASH":
+      return {
+        ...state,
+        isVisible: false
       };
     default:
       return state;
