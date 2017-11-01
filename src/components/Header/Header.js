37dfd332d7513193eb9d6ce77b1dc758e60a976a
Kyle Bonar <kylewb2011@gmail.com>
2017-11-01 16:15:12 -0500

    Made some components into stateless functional components.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { NavLink, Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import axios from "axios";
+import { connect } from "react-redux";
+import { logout } from "../../actions/auth";
+import UserLogInLogOut from "./UserLogInLogOut";
 
 //logos
 import MainLogo from "../../images/icons/Logo.js";
@@ -17,17 +20,15 @@ import Avatar from "../../helper/Avatar/Avatar.js";
 //login/logout icon
 import Logout from "../../images/icons/Logout.js";
 
-class HomeLogo extends Component {
-  render() {
-    return (
-      <li className="nav-item">
-        <NavLink className="nav-link home-link" activeClassName="active" to="/">
-          <MainLogo className="home-logo" />
-        </NavLink>
-      </li>
-    );
-  }
-}
+const HomeLogo = () => {
+  return (
+    <li className="nav-item">
+      <NavLink className="nav-link home-link" activeClassName="active" to="/">
+        <MainLogo className="home-logo" />
+      </NavLink>
+    </li>
+  );
+};
 
 class NavItems extends Component {
   render() {
@@ -74,9 +75,7 @@ class NavItems extends Component {
                 to={`/${item.linkTo}`}
               >
                 {item.img}
-                <span>
-                  {item.text.toUpperCase()}
-                </span>
+                <span>{item.text.toUpperCase()}</span>
               </NavLink>
             </li>
           );
@@ -122,90 +121,20 @@ class SearchBar extends Component {
   }
 }
 
-class UserLogInLogOut extends Component {
-  render() {
-    return (
-      <div className="nav-section nav-user">
-        {/*Register/Update based on token*/}
-        <li className="nav-item">
-          {this.props.isUserLoggedIn
-            ? <NavLink
-                className="nav-link"
-                activeClassName="active"
-                to="/account"
-              >
-                <img src={Avatar.Boy10} className="nav-avatar" />
-                Settings
-              </NavLink>
-            : <NavLink
-                className="nav-link"
-                activeClassName="active"
-                to="/register"
-              >
-                Register
-              </NavLink>}
-        </li>
+const Header = () => {
+  return (
+    <header className="top">
+      <nav className="nav">
+        <div className="nav-section nav-pages">
+          <HomeLogo />
+          <NavItems />
+        </div>
 
-        {/*Login/Logout based on token*/}
-        <li className="nav-item">
-          {this.props.isUserLoggedIn
-            ? <NavLink
-                onClick={this.props.handleLogout}
-                to="/"
-                className="nav-link"
-                activeClassName="active"
-              >
-                <Logout />
-                Logout
-              </NavLink>
-            : <NavLink
-                className="nav-link"
-                activeClassName="active"
-                to="/login"
-              >
-                Login
-              </NavLink>}
-        </li>
-      </div>
-    );
-  }
-}
-
-UserLogInLogOut.propTypes = {
-  isUserLoggedIn: PropTypes.bool.isRequired,
-  handleLogout: PropTypes.func.isRequired
+        <SearchBar />
+        <UserLogInLogOut />
+      </nav>
+    </header>
+  );
 };
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="top">
-        <nav className="nav">
-          <div className="nav-section nav-pages">
-            {/*Home logo*/}
-            <HomeLogo />
-
-            {/*Rest of nav items*/}
-            <NavItems />
-          </div>
-
-          {/*Search bar*/}
-          <SearchBar />
-
-          {/* User section */}
-          <UserLogInLogOut
-            isUserLoggedIn={this.props.isUserLoggedIn}
-            handleLogout={this.props.handleLogout}
-          />
-        </nav>
-      </header>
-    );
-  }
-}
-
-Header.propTypes = {
-  isUserLoggedIn: PropTypes.bool.isRequired,
-  handleLogout: PropTypes.func.isRequired
-};
-
-module.exports = Header;
+export default Header;
