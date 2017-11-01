a88cec8e12248758c4b30a73280d25d753dfaed8
Kyle Bonar <kylewb2011@gmail.com>
2017-11-01 16:15:45 -0500

    Added user reducer.

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -5,6 +5,8 @@ export default function user(state = {}, action) {
         ...state,
         token: action.user
       };
+    case "USER_LOGGED_OUT":
+      return {};
     default:
       return state;
   }
