353116a1dda306721cbdd0dc3966caf1180ab8c1
Kyle Bonar <kylewb2011@gmail.com>
2017-11-02 16:12:38 -0500

    Moved api to register user here.

--- src/api/api.js
+++ src/api/api.js
@@ -12,6 +12,19 @@ export default {
             throw new Error(res.data.msg);
           }
         });
+    },
+    register: credentials => {
+      return axios
+        .post("http://localhost:7777/register", credentials)
+        .then(res => {
+          console.log(res);
+          if (res.data.isGood) {
+            return res.data.token;
+          } else {
+            throw new Error(res.data.msg);
+          }
+        });
     }
-  }
+  },
+  store: {}
 };
