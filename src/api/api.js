ca7a420c650529b8079ce66bd475218c439a5e4a
Kyle Bonar <kylewb2011@gmail.com>
2017-11-06 14:16:37 -0600

    new method for checking using logged in status.

--- src/api/api.js
+++ src/api/api.js
@@ -45,6 +45,17 @@ export default {
             throw new Error(res.data.msg);
           }
         });
+    },
+    isLoggedIn: credentials => {
+      return axios
+        .post("http://localhost:7777/user/isloggedin", credentials)
+        .then(res => {
+          if (res.status === 200 && res.data.isGood) {
+            return res.data;
+          } else {
+            throw new Error(res.data.msg);
+          }
+        });
     }
   },
   store: {}
