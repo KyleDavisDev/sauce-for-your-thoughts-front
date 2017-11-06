3acb3516d4d56349056bb9dd3b9729766e6d63be
Kyle Bonar <kylewb2011@gmail.com>
2017-11-06 14:16:22 -0600

    Improved seperation of concerns.

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,13 +1,16 @@
 import api from "../api/api";
 import Auth from "../helper/Auth/Auth";
+import { flashSuccess } from "./flash";
 
 export const userUpdated = () => ({
-  type: "USER_UPDATED",
-  text: "You're updates have been applied. Thank you!"
+  type: "USER_UPDATED"
 });
 
 export const updateUser = credentials => dispatch => {
   return api.user.update(credentials).then(res => {
     dispatch(userUpdated());
+    const text = `Your name was saved as: ${res.user
+      .name} and your email was saved as: ${res.user.email}.`;
+    dispatch(flashSuccess({ text }));
   });
 };
