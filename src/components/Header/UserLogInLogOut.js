3d13e1836205dd62e8bc35a1cc61d1dec922ccb7
Kyle Bonar <kylewb2011@gmail.com>
2017-11-07 14:50:00 -0600

    Split up component into smaller, more managable parts.

--- src/components/Header/UserLogInLogOut.js
+++ src/components/Header/UserLogInLogOut.js
@@ -7,57 +7,64 @@ import { logout } from "../../actions/auth";
 import Avatar from "../../helper/Avatar/Avatar.js";
 import Logout from "../../images/icons/Logout.js";
 
-const UserLogInLogOut = ({ isAuthenticated, logout }) => {
+const LoggedIn = ({ onClick }) => {
   return (
-    <div className="nav-section nav-user">
-      {isAuthenticated ? (
-        <div className="item-holder">
-          <li className="nav-item">
-            <NavLink
-              className="nav-link"
-              activeClassName="active"
-              to="/account"
-            >
-              <img src={Avatar.Boy10} className="nav-avatar" />
-              Settings
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              onClick={e => {
-                e.preventDefault();
-                logout();
-              }}
-              to="/"
-              className="nav-link"
-              activeClassName="active"
-            >
-              <Logout />
-              Logout
-            </NavLink>
-          </li>
-        </div>
-      ) : (
-        <div className="item-holder">
-          <li className="nav-item">
-            <NavLink
-              className="nav-link"
-              activeClassName="active"
-              to="/register"
-            >
-              Register
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" activeClassName="active" to="/login">
-              Login
-            </NavLink>
-          </li>
-        </div>
-      )}
+    <div className="item-holder">
+      <li className="nav-item">
+        <NavLink className="nav-link" activeClassName="active" to="/account">
+          <img src={Avatar.Boy10} className="nav-avatar" />
+          Settings
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          onClick={onClick}
+          className="nav-link"
+          activeClassName="active"
+        >
+          <Logout />
+          Logout
+        </NavLink>
+      </li>
     </div>
   );
 };
+LoggedIn.propType = {
+  onClick: PropTypes.func.isRequired
+};
+
+const NotLoggedIn = () => {
+  return (
+    <div className="item-holder">
+      <li className="nav-item">
+        <NavLink className="nav-link" activeClassName="active" to="/register">
+          Register
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" activeClassName="active" to="/login">
+          Login
+        </NavLink>
+      </li>
+    </div>
+  );
+};
+
+class UserLogInLogOut extends Component {
+  render() {
+    const { isAuthenticated } = this.props;
+    return (
+      <div className="nav-section nav-user">
+        {isAuthenticated ? (
+          <LoggedIn onClick={this.props.logout} />
+        ) : (
+          <NotLoggedIn />
+        )}
+      </div>
+    );
+  }
+}
 
 UserLogInLogOut.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
