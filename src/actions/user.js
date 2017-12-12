3f8668d3bf467f5f87fd3847990331f18fcf40b3
Kyle Bonar <kylewb2011@gmail.com>
2017-12-11 21:23:46 -0600

    New action for getting user info.

--- src/actions/user.js
+++ src/actions/user.js
@@ -6,12 +6,27 @@ export const userUpdated = () => ({
   type: "USER_UPDATED"
 });
 
+export const userSetInfo = ({ email, _id }) => ({
+  type: "USER_SET_INFO",
+  email,
+  _id
+});
+
 export const updateUser = credentials => dispatch => {
   return api.user.update(credentials).then(res => {
     dispatch(userUpdated());
-    const text = `Your name was saved as: ${res.user
-      .name} and your email was saved as: ${res.user.email}.`;
+    const text = `Your name was saved as: ${
+      res.user.name
+    } and your email was saved as: ${res.user.email}.`;
     dispatch(flashSuccess({ text }));
+    return res.user;
+  });
+};
+
+export const getInfo = credentials => dispatch => {
+  return api.user.getInfo(credentials).then(res => {
+    const { email, _id } = res.user;
+    dispatch(userSetInfo({ email, _id }));
     return res.user;
   });
 };
