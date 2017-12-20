b339ee7a8c42fac1226254a922cbea00e70c25ce
Kyle Bonar <kylewb2011@gmail.com>
2017-12-20 14:35:40 -0600

    Code cleanup, added email and name to update user, built out userSetInfo and getInfo.

--- src/actions/user.js
+++ src/actions/user.js
@@ -2,16 +2,33 @@ import api from "../api/api";
 import Auth from "../helper/Auth/Auth";
 import { flashSuccess } from "./flash";
 
-export const userUpdated = () => ({
-  type: "USER_UPDATED"
+export const userUpdated = ({ email, name }) => ({
+  type: "USER_UPDATED",
+  email,
+  name
+});
+
+export const userSetInfo = ({ email, _id, name }) => ({
+  type: "USER_SET_INFO",
+  email,
+  _id,
+  name
 });
 
 export const updateUser = credentials => dispatch => {
   return api.user.update(credentials).then(res => {
-    dispatch(userUpdated());
-    const text = `Your name was saved as: ${res.user
-      .name} and your email was saved as: ${res.user.email}.`;
+    const { email, name } = res.user;
+    dispatch(userUpdated({ email, name }));
+    const text = `Your name was saved as: ${name} and your email was saved as: ${email}.`;
     dispatch(flashSuccess({ text }));
+    return res.user;
+  });
+};
+
+export const getInfo = credentials => dispatch => {
+  return api.user.getInfo(credentials).then(res => {
+    const { email, _id, name } = res.user;
+    dispatch(userSetInfo({ email, _id, name }));
     return res.user;
   });
 };
