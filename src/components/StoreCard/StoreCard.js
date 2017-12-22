88df29e36ea46774335b4510ea32cf01dd9bb405
Kyle Bonar <kylewb2011@gmail.com>
2017-12-22 09:23:19 -0600

    Switched to stateless functional component.

--- src/components/StoreCard/StoreCard.js
+++ src/components/StoreCard/StoreCard.js
@@ -5,41 +5,41 @@ import PropTypes from "prop-types";
 import FillerImage from "../../images/photos/store.jpg";
 import Pencil from "../../images/icons/Pencil.js";
 
-class StoreCard extends Component {
-  render() {
-    return (
-      <div className="store">
-        <div className="store-hero">
-          <div className="store-actions">
-            {this.props.displayEditIcon &&
-              <div className="store-action store-action-edit">
-                <Link to={`/store/${this.props.ID}/edit`}>
-                  <Pencil />
-                </Link>
-              </div>}
-          </div>
-          <img
-            src={`http://localhost:7777/public/uploads/${this.props.image}`}
-            onError={e => (e.target.src = FillerImage)}
-            title={this.props.name}
-            alt={this.props.name}
-          />
-          <div className="store-title">
-            <Link to={`/store/${this.props.slug}`}>
-              {this.props.name}
-            </Link>
-          </div>
+const StoreCard = store => {
+  return (
+    <div className="store">
+      <div className="store-hero">
+        <div className="store-actions">
+          {store.displayEditIcon && (
+            <div className="store-action store-action-edit">
+              <Link to={`/store/${store.ID}/edit`}>
+                <Pencil />
+              </Link>
+            </div>
+          )}
         </div>
-        <div className="store-details">
-          {/*{limit description to 25 words }*/}
-          <p>
-            {this.props.description.split(" ").slice(0, 25).join(" ")}
-          </p>
+        <img
+          src={`http://localhost:7777/public/uploads/${store.image}`}
+          onError={e => (e.target.src = FillerImage)}
+          title={store.name}
+          alt={store.name}
+        />
+        <div className="store-title">
+          <Link to={`/store/${store.slug}`}>{store.name}</Link>
         </div>
       </div>
-    );
-  }
-}
+      <div className="store-details">
+        {/*{limit description to 25 words }*/}
+        <p>
+          {store.description
+            .split(" ")
+            .slice(0, 25)
+            .join(" ")}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 StoreCard.propTypes = {
   ID: PropTypes.string.isRequired,
