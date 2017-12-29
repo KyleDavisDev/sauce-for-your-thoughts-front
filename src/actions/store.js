53b6c6c0f609def30b0a67a4c4e8179768d0a32b
Kyle Bonar <kylewb2011@gmail.com>
2017-12-29 14:34:03 -0600

    Init commit of store found action.

--- src/actions/store.js
+++ src/actions/store.js
@@ -6,6 +6,11 @@ export const storeAdded = () => ({
   type: "STORE_ADDED"
 });
 
+export const storeFound = (store) => ({
+  type: "STORE_FOUND",
+  store
+})
+
 export const addStore = data => dispatch => {
   return api.store.add(data).then(res => {
     // dispatch(storeAdded())
@@ -13,3 +18,10 @@ export const addStore = data => dispatch => {
     return res;
   });
 };
+
+export const getStore = data => dispatch => {
+  return api.store.getStore(data).then(res => {
+    dispatchEvent(foundStore(res.store));
+    return res;
+  })
+}
