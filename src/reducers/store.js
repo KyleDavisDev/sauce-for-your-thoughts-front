f44460b6e8dfdf41beb521c360b7197d65ceab49
Kyle Bonar <kylewb2011@gmail.com>
2017-12-30 22:37:38 -0600

    Fixed typo and state is now empty object.

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -2,8 +2,8 @@ export default function store(state = {}, action) {
   switch (action.type) {
     case "STORE_FOUND":
       return action.store;
-    case "STORE_UPDATES":
-      return null;
+    case "STORE_UPDATED":
+      return {};
     default:
       return state;
   }
