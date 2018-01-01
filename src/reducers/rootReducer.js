5d8852f50473412fdf37c1851940556b1374fb7f
Kyle Bonar <kylewb2011@gmail.com>
2017-12-31 18:59:09 -0600

    Added tags action

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,6 +3,7 @@ import { combineReducers } from "redux";
 import user from "./user";
 import flashMessage from "./flashMessage";
 import stores from "./stores";
-import store from "./store"
+import store from "./store";
+import tags from "./tags";
 
-export default combineReducers({ user, flashMessage, stores, store });
+export default combineReducers({ user, flashMessage, stores, store, tags });
