875ca195cd77df034274592fc45a925006b43463
Kyle Bonar <kylewb2011@gmail.com>
2018-01-01 12:59:13 -0600

    Moved SearchBar to seperate file.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,10 @@ import { connect } from "react-redux";
 import { logout } from "../../actions/auth";
 import UserLogInLogOut from "./UserLogInLogOut";
 import NavItems from "./NavItems";
+import SearchBar from "./SearchBar";
 
 import MainLogo from "../../images/icons/Logo.js";
+
 //avatar for logged in users
 import Avatar from "../../helper/Avatar/Avatar.js";
 
@@ -23,42 +25,6 @@ const HomeLogo = () => {
     </li>
   );
 };
-
-class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchValue: ""
-    };
-
-    this.handleSearchChange = this.handleSearchChange.bind(this);
-  }
-  render() {
-    return (
-      <div className="nav-section nav-search">
-        <div className="search">
-          <input
-            type="text"
-            placeholder="Coffee, beer..."
-            name="search"
-            onChange={this.handleSearchChange}
-            value={this.state.searchValue}
-          />
-        </div>
-      </div>
-    );
-  }
-
-  handleSearchChange(e) {
-    const temp = e.target.value;
-    this.setState({ searchValue: e.target.value });
-    axios({
-      method: "get",
-      url: `http://localhost:7777/api/stores/search/${temp}`
-    }).then(response => console.log(response));
-  }
-}
 
 const Header = () => {
   return (
