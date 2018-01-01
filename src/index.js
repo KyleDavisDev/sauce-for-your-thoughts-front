d679753ec154be55202e68d5cc79e15d21af629a
Kyle Bonar <kylewb2011@gmail.com>
2017-12-31 23:51:52 -0600

    Removed reference to unused file.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import RootReducer from "./reducers/rootReducer";
-import registerServiceWorker from "./registerServiceWorker";
 import App from "./components/App/App.js";
 import "./scss/style.scss";
 import { isLoggedIn } from "./actions/auth";
@@ -40,4 +39,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("app")
 );
-registerServiceWorker();
