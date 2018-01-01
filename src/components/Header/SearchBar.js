32fa08240808be2dc4afe6f4ddc999edb088b03a
Kyle Bonar <kylewb2011@gmail.com>
2018-01-01 14:11:45 -0600

    Basic buildout of search dropdown feature.

--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -3,18 +3,20 @@ import axios from "axios";
 import { connect } from "react-redux";
 import { getStoresBySearch as getStores } from "../../actions/stores";
 import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      results: [],
       searchValue: ""
     };
-
-    // this.handleSearchChange = this.handleSearchChange.bind(this);
   }
+
   render() {
+    const { results } = this.state;
     return (
       <div className="nav-section nav-search">
         <div className="search">
@@ -24,7 +26,24 @@ class SearchBar extends Component {
             name="search"
             onChange={this.handleSearchChange}
             value={this.state.searchValue}
+            onKeyDown={this.handleKeyPress}
           />
+          <div className="search-results">
+            {results.length > 0 &&
+              results.map(result => {
+                return (
+                  <NavLink
+                    className="search-result"
+                    to={`/store/${result.slug}`}
+                    key={result.slug}
+                    activeClassName="active"
+                    onClick={this.handleClick}
+                  >
+                    <strong>{result.name}</strong>
+                  </NavLink>
+                );
+              })}
+          </div>
         </div>
       </div>
     );
@@ -36,12 +55,45 @@ class SearchBar extends Component {
     this.props
       .getStores(searchValue)
       .then(res => {
-        console.log(res);
+        //When found a store
+        const results = res.stores.map(store => {
+          store.focus = false;
+          return store;
+        });
+        this.setState({ results });
       })
       .catch(err => {
+        this.setState({ results: [] });
         //err.message hold human-readable text
       });
   };
+
+  handleKeyPress = e => {
+    //13 = enter
+    //38 = up arrow
+    //40 = down arrow
+    if (![13, 38, 40].includes(e.keyCode)) return;
+
+    const index = this.state.results.find(store => store.focus === true);
+    if (index === undefined) {
+      if (e.keyCode === 40) {
+      }
+    }
+    // if(e.keyCode === 40) {
+    //   const results = this.state.results.map((result,ind) => {
+    //     if(ind === index) {
+    //       result.focus = false
+    //     }
+    //   })
+    //   this.setState({ results:   })
+    // }
+  };
+
+  handleClick = () =>
+    this.setState({
+      searchValue: "",
+      results: []
+    });
 }
 SearchBar.propTypes = {
   getStores: PropTypes.func.isRequired
