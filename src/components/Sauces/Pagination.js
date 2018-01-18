2eb1e4fd781a0ad729aaabae5fd7d54b764b99cc
Kyle Bonar <kylewb2011@gmail.com>
2018-01-17 22:48:18 -0600

    Fixed placement of words.

--- src/components/Sauces/Pagination.js
+++ src/components/Sauces/Pagination.js
@@ -5,7 +5,7 @@ const Pagination = ({ total, saucePerPage, page }) => {
   const totalPages = Math.floor(total / saucePerPage)
   return (
     <div className="pagination">
-      {page != 1 && <div className="pagination__prev"><Link to={`/sauces/page/${page - 1}`} >Prev</Link></div>}
+      <div className="pagination__prev">{page != 1 ? <Link to={`/sauces/page/${page - 1}`} >Prev</Link> : ""}</div>
       <div className="pagination__text">{`Page ${page} of ${totalPages}`}</div>
       {page != totalPages && <div className="pagination__next"><Link to={`/sauces/page/${page + 1}`} >Next</Link></div>}
     </div>
