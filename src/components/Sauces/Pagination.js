13400925afc0d3b1f75bdb77226e159204d76703
Kyle Bonar <kylewb2011@gmail.com>
2018-01-17 22:55:20 -0600

    Fixed styling related issues.

--- src/components/Sauces/Pagination.js
+++ src/components/Sauces/Pagination.js
@@ -7,7 +7,7 @@ const Pagination = ({ total, saucePerPage, page }) => {
     <div className="pagination">
       <div className="pagination__prev">{page != 1 ? <Link to={`/sauces/page/${page - 1}`} >Prev</Link> : ""}</div>
       <div className="pagination__text">{`Page ${page} of ${totalPages}`}</div>
-      {page != totalPages && <div className="pagination__next"><Link to={`/sauces/page/${page + 1}`} >Next</Link></div>}
+      <div className="pagination__next">{page != totalPages ? <Link to={`/sauces/page/${page + 1}`} >Next</Link> : ""}</div>
     </div>
   )
 }
