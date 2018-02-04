3d6febfc384e0ac3b36ef8cc503444b46805bee4
Kyle Bonar <kylewb2011@gmail.com>
2018-02-04 17:31:05 -0600

    Formatting update, resolved math issue.

--- src/components/Sauces/Pagination.js
+++ src/components/Sauces/Pagination.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 const Pagination = ({ total, saucePerPage, page }) => {
-  const totalPages = Math.floor(total / saucePerPage)
+  const totalPages = Math.ceil(total / saucePerPage);
   return (
     <div className="pagination">
-      <div className="pagination__prev">{page != 1 ? <Link to={`/sauces/page/${page - 1}`} >Prev</Link> : ""}</div>
+      <div className="pagination__prev">
+        {page != 1 ? <Link to={`/sauces/page/${page - 1}`}>Prev</Link> : ""}
+      </div>
       <div className="pagination__text">{`Page ${page} of ${totalPages}`}</div>
-      <div className="pagination__next">{page != totalPages ? <Link to={`/sauces/page/${page + 1}`} >Next</Link> : ""}</div>
+      <div className="pagination__next">
+        {page != totalPages ? (
+          <Link to={`/sauces/page/${page + 1}`}>Next</Link>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Pagination;
