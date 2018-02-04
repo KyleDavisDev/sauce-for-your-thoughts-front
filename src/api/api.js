683421bb3a02bfb5e9edfa482e05e9a8680e3f4a
Kyle Bonar <kylewb2011@gmail.com>
2018-02-03 23:59:19 -0600

    abstracted host string.

--- src/api/api.js
+++ src/api/api.js
@@ -1,54 +1,48 @@
 import axios from "axios";
 
+const host = "http://localhost:7777";
+
 export default {
   user: {
     login: credentials => {
-      return axios
-        .post("http://localhost:7777/api/user/login", credentials)
-        .then(res => {
-          if (res.data.isGood) {
-            return res.data;
-          } else {
-            throw new Error(res.data.msg);
-          }
-        });
+      return axios.post(`${host}/api/user/login`, credentials).then(res => {
+        if (res.data.isGood) {
+          return res.data;
+        } else {
+          throw new Error(res.data.msg);
+        }
+      });
     },
     register: credentials => {
-      return axios
-        .post("http://localhost:7777/api/user/register", credentials)
-        .then(res => {
-          if (res.data.isGood) {
-            return res.data;
-          } else {
-            throw new Error(res.data.msg);
-          }
-        });
+      return axios.post(`${host}/api/user/register`, credentials).then(res => {
+        if (res.data.isGood) {
+          return res.data;
+        } else {
+          throw new Error(res.data.msg);
+        }
+      });
     },
     getInfo: credentials => {
-      return axios
-        .post("http://localhost:7777/api/user/getInfo", credentials)
-        .then(res => {
-          if (res.status === 200 && res.data.isGood) {
-            return res.data;
-          } else {
-            throw new Error(res.data.msg);
-          }
-        });
+      return axios.post(`${host}/api/user/getInfo`, credentials).then(res => {
+        if (res.status === 200 && res.data.isGood) {
+          return res.data;
+        } else {
+          throw new Error(res.data.msg);
+        }
+      });
     },
     update: credentials => {
-      return axios
-        .post("http://localhost:7777/api/user/update", credentials)
-        .then(res => {
-          if (res.status === 200 && res.data.isGood) {
-            return res.data;
-          } else {
-            throw new Error(res.data.msg);
-          }
-        });
+      return axios.post(`${host}/api/user/update`, credentials).then(res => {
+        if (res.status === 200 && res.data.isGood) {
+          return res.data;
+        } else {
+          throw new Error(res.data.msg);
+        }
+      });
     },
     isLoggedIn: credentials => {
       return axios
-        .post("http://localhost:7777/api/user/isloggedin", credentials)
+        .post(`${host}/api/user/isloggedin`, credentials)
         .then(res => {
           if (res.status === 200 && res.data.isGood) {
             return res.data;
@@ -58,32 +52,28 @@ export default {
         });
     },
     heartSauce: data => {
-      return axios
-        .post("http://localhost:7777/api/user/heartSauce", data)
-        .then(res => {
-          if (res.data.isGood && res.status === 200) {
-            return res.data;
-          } else {
-            throw new Error(res.data.msg);
-          }
-        });
+      return axios.post(`${host}/api/user/heartSauce`, data).then(res => {
+        if (res.data.isGood && res.status === 200) {
+          return res.data;
+        } else {
+          throw new Error(res.data.msg);
+        }
+      });
     },
     unHeartSauce: data => {
-      return axios
-        .post("http://localhost:7777/api/user/unHeartSauce", data)
-        .then(res => {
-          if (res.data.isGood && res.status === 200) {
-            return res.data;
-          } else {
-            throw new Error(res.data.msg);
-          }
-        });
+      return axios.post(`${host}/api/user/unHeartSauce`, data).then(res => {
+        if (res.data.isGood && res.status === 200) {
+          return res.data;
+        } else {
+          throw new Error(res.data.msg);
+        }
+      });
     }
   },
   sauce: {
     add: data => {
       return axios
-        .post("http://localhost:7777/api/sauce/add", data, {
+        .post(`${host}/api/sauce/add`, data, {
           headers: { "Content-Type": "multipart/form-data" }
         })
         .then(res => {
@@ -95,30 +85,26 @@ export default {
         });
     },
     getById: data => {
-      return axios
-        .post("http://localhost:7777/api/sauce/get/id", data)
-        .then(res => {
-          if (res.data.isGood) {
-            return res.data;
-          } else {
-            throw new Error(res.data.msg);
-          }
-        });
+      return axios.post(`${host}/api/sauce/get/id`, data).then(res => {
+        if (res.data.isGood) {
+          return res.data;
+        } else {
+          throw new Error(res.data.msg);
+        }
+      });
     },
     getBySlug: data => {
-      return axios
-        .get(`http://localhost:7777/api/sauce/get/${data}`)
-        .then(res => {
-          if (res.data.isGood) {
-            return res.data;
-          } else {
-            throw new Error(res.data.msg);
-          }
-        });
+      return axios.get(`${host}/api/sauce/get/${data}`).then(res => {
+        if (res.data.isGood) {
+          return res.data;
+        } else {
+          throw new Error(res.data.msg);
+        }
+      });
     },
     update: data => {
       return axios
-        .post("http://localhost:7777/api/sauce/update", data, {
+        .post(`${host}/api/sauce/update`, data, {
           headers: { "Content-Type": "multipart/form-data" }
         })
         .then(res => {
@@ -132,43 +118,37 @@ export default {
   },
   sauces: {
     get: credentials => {
-      return axios
-        .post("http://localhost:7777/api/sauces/get", credentials)
-        .then(res => {
-          if (res.data.isGood && res.status === 200) {
-            return res.data;
-          } else {
-            throw new Error(res.data.msg);
-          }
-        });
+      return axios.post(`${host}/api/sauces/get`, credentials).then(res => {
+        if (res.data.isGood && res.status === 200) {
+          return res.data;
+        } else {
+          throw new Error(res.data.msg);
+        }
+      });
     },
     getSaucesByTag: data => {
-      return axios
-        .post(`http://localhost:7777/api/sauces/get/by/tag`, data)
-        .then(res => {
-          if (res.data.isGood) {
-            return res.data;
-          } else {
-            throw new Error(res.data.msg);
-          }
-        });
+      return axios.post(`${host}/api/sauces/get/by/tag`, data).then(res => {
+        if (res.data.isGood) {
+          return res.data;
+        } else {
+          throw new Error(res.data.msg);
+        }
+      });
     },
     search: searchValue => {
-      return axios
-        .get(`http://localhost:7777/api/sauces/search/${searchValue}`)
-        .then(res => {
-          if (res.data.isGood) {
-            return res.data;
-          } else {
-            //this false error will be handled in SearchBar component
-            throw new Error(res.data.msg);
-          }
-        });
+      return axios.get(`${host}/api/sauces/search/${searchValue}`).then(res => {
+        if (res.data.isGood) {
+          return res.data;
+        } else {
+          //this false error will be handled in SearchBar component
+          throw new Error(res.data.msg);
+        }
+      });
     }
   },
   tags: {
     getList: () => {
-      return axios.get("http://localhost:7777/api/tags/get").then(res => {
+      return axios.get(`${host}/api/tags/get`).then(res => {
         if (res.data.isGood) {
           return res.data;
         } else {
