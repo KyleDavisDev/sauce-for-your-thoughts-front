09de30e429007a90453136fceae817fce5302f01
Kyle Bonar <kylewb2011@gmail.com>
2018-02-09 13:21:31 -0600

    Moved Cards into component, now has proper dismount method, removed requirement to be logged in.

--- src/components/Tags/Tags.js
+++ src/components/Tags/Tags.js
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getInfo, toggleSauce, getHearts } from "../../redux/actions/user";
-import { getSaucesByTag } from "../../redux/actions/sauces";
+import { getSaucesByTag, cleanUpSauces } from "../../redux/actions/sauces";
 import { flashError } from "../../redux/actions/flash";
-import { getTagsList } from "../../redux/actions/tags";
+import { getTagsList, cleanUpTags } from "../../redux/actions/tags";
 import Card from "../Card/Card.js";
 
 const Title = ({ title }) => {
@@ -14,41 +14,6 @@ const Title = ({ title }) => {
 };
 Title.proptypes = {
   title: PropTypes.string.isRequired
-};
-
-const Cards = ({ sauces, email, toggleSauce, hearts }) => {
-  return (
-    <div className="sauces">
-      {sauces.map(sauce => {
-        return (
-          <Card
-            displayEditIcon={email === sauce.author ? true : false}
-            heart={hearts.includes(sauce._id)}
-            toggleSauce={toggleSauce}
-            ID={sauce._id}
-            name={sauce.name}
-            image={sauce.photo}
-            slug={sauce.slug}
-            description={sauce.description}
-            key={sauce.slug}
-          />
-        );
-      })}
-    </div>
-  );
-};
-Cards.proptypes = {
-  sauces: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-      photo: PropTypes.string.isRequired,
-      tags: PropTypes.arrayOf(PropTypes.string).isRequired
-    }).isRequired
-  ).isRequired,
-  email: PropTypes.string.isRequired,
-  heartSauce: PropTypes.func.isRequired,
-  unHeartSauce: PropTypes.func.isRequired
 };
 
 const TagsList = ({ tags, count }) => {
@@ -91,11 +56,7 @@ TagsList.proptypes = {
 };
 
 class Tags extends Component {
-  componentWillMount() {
-    if (!this.props.user.token) this.props.history.push("/login");
-  }
   componentDidMount() {
-    if (!this.props.user.token) return;
     const tag = this.props.match.params.tag || "All";
     axios
       .all([
@@ -107,29 +68,23 @@ class Tags extends Component {
       .catch(err => console.log(err.response));
   }
 
-  componentWillReceiveProps(nextProps) {
-    const tag = nextProps.match.params.tag;
-    if (tag && tag !== this.props.match.params.tag) {
-      this.getSaucesByTag(tag);
-    }
+  componentWillUnmount() {
+    this.props.cleanUpSauces();
+    this.props.cleanUpTags();
   }
 
   render() {
     const title = this.props.match.params.tag || "All";
     const { tags = [], sauces = [] } = this.props;
-    const { email = [], hearts = [] } = this.props.user;
+    const { email = "", hearts = [] } = this.props.user;
     return (
       <div className="inner">
         <Title title={title} />
         {tags.length > 0 && <TagsList tags={tags} count={sauces.length} />}
-        {sauces.length > 0 && (
-          <Cards
-            sauces={sauces}
-            email={email}
-            toggleSauce={this.toggleSauce}
-            hearts={hearts}
-          />
-        )}
+        <div className="sauces">
+          {sauces.length > 0 &&
+            sauces.map(sauce => this.renderCards({ sauce, email }))}
+        </div>
       </div>
     );
   }
@@ -143,10 +98,12 @@ class Tags extends Component {
 
   //this will pass token to api and store email/name into redux store on success
   getUserInfo = () => {
+    //make sure user is logged in
+    if (!this.props.user.token) return;
     //check if email already passed to component to save api call
     if (this.props.user.email) return;
-    const data = { token: this.props.user.token };
 
+    const data = { token: this.props.user.token };
     return this.props.getInfo(data);
   };
 
@@ -168,6 +125,25 @@ class Tags extends Component {
       .toggleSauce(data)
       .catch(err => this.props.flashError({ text: err.response }));
   };
+
+  renderCards = ({ sauce, email }) => {
+    const hearts = this.props.user.hearts || [];
+    const displayHeartIcon = !!this.props.user.token;
+    return (
+      <Card
+        displayEditIcon={email === sauce.author ? true : false}
+        displayHeartIcon={displayHeartIcon}
+        heart={hearts.includes(sauce._id)}
+        toggleSauce={this.toggleSauce}
+        ID={sauce._id}
+        name={sauce.name}
+        image={sauce.photo}
+        slug={sauce.slug}
+        description={sauce.description}
+        key={sauce.slug}
+      />
+    );
+  };
 }
 Tags.propTypes = {
   sauces: PropTypes.arrayOf(
@@ -187,6 +163,8 @@ Tags.propTypes = {
   }),
   flashError: PropTypes.func.isRequired,
   getSaucesByTag: PropTypes.func.isRequired,
+  cleanUpSauces: PropTypes.func.isRequired,
+  cleanUpTags: PropTypes.func.isRequired,
   getInfo: PropTypes.func.isRequired,
   getTagsList: PropTypes.func.isRequired,
   toggleSauce: PropTypes.func.isRequired
@@ -207,6 +185,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = {
   flashError,
   getSaucesByTag,
+  cleanUpSauces,
+  cleanUpTags,
   getInfo,
   getTagsList,
   toggleSauce,
