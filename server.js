11875aa1ed5d7e5ae6a1d8863d71414bc8815dde
Kyle Bonar <kylewb2011@gmail.com>
2018-02-28 03:33:09 +0000

    Serve static files.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-
+app.use(express.static('dist'));
 //rest will fall here and be handled by react app
 app.get("*", function(req, res) {
   res.sendFile(`${process.cwd()}/dist/index.html`);
