5c8ad8c891cf19976ff77daf84874ea8ed73e460
Kyle Bonar <kylewb2011@gmail.com>
2018-03-25 14:36:36 -0500

    Added method for adding review.

--- src/redux/actions/review.js
+++ src/redux/actions/review.js
@@ -1,5 +1,10 @@
 import api from "../../api/api";
 
+export const addReviews = ({ reviews }) => ({
+  type: "REVIEWS_ADDED",
+  reviews
+});
+
 export const addReview = data => dispatch => {
   return api.review.add(data).then(res => {
     //update store here...?
