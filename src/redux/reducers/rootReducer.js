28c332143d724a24418d00be8e6c78efed213baa
Kyle Bonar <kylewb2011@gmail.com>
2018-03-25 14:54:13 -0500

    Added two reducers.

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -5,5 +5,15 @@ import flashMessage from "./flashMessage";
 import sauces from "./sauces";
 import sauce from "./sauce";
 import tags from "./tags";
+import reviews from "./reviews";
+import users from "./users";
 
-export default combineReducers({ user, flashMessage, sauces, sauce, tags });
+export default combineReducers({
+  user,
+  flashMessage,
+  sauces,
+  sauce,
+  tags,
+  reviews,
+  users
+});
