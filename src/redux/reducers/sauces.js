8666a6496cc80e9c6bd937cf0d891834f0c8b3af
Kyle Bonar <kylewb2011@gmail.com>
2018-03-25 01:58:40 -0500

    Added function to flatten sauce objects.

--- src/redux/reducers/sauces.js
+++ src/redux/reducers/sauces.js
@@ -1,7 +1,31 @@
+export const flattenSauce = ({ sauce }) => {
+  //TODO figure out flattening sauce
+  const key = sauce._id;
+  const temp = {
+    key: {
+      name: sauce.name,
+      description: sauce.description,
+      photo: sauce.photo,
+      tags: sauce.tags,
+      author: { _id: sauce.author._id },
+      reviews: sauce.rewiews.map(x => x._id)
+    }
+  };
+  console.log("i");
+  return temp;
+};
+
 export default function sauces(state = [], action) {
   switch (action.type) {
     case "SAUCES_FOUND":
       return action.sauces;
+
+    case "SAUCE_ADDED":
+      console.log("sauce found");
+      console.log(action);
+      action.sauce = flattenSauce({ sauce: action.sauce });
+      console.log("yo");
+      console.log(action);
     case "UPDATED_SAUCES_ITEM":
       //update single sauces item if sauces is already set
       return state
