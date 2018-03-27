9b5220a63283b29c57cffeb9a87728f3bda06d28
Kyle Bonar <kylewb2011@gmail.com>
2018-03-27 15:02:31 -0500

    Applied eslint rules.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,43 +1,32 @@
-import React, { Component } from "react";
-import { NavLink, Link } from "react-router-dom";
-import PropTypes from "prop-types";
-import axios from "axios";
-import { connect } from "react-redux";
-import { logout } from "../../redux/actions/auth";
+import React from "react";
+import { NavLink } from "react-router-dom";
 import UserLogInLogOut from "./UserLogInLogOut";
 import NavItems from "./NavItems";
 import SearchBar from "./SearchBar";
-import MainLogo from "../../images/icons/Logo.js";
-import Logout from "../../images/icons/Logout.js";
+
 import LogoSFYT from "../../images/icons/LogoSFYT.js";
 // const LogoSFYT = require("../../images/icons/LogoSFYT.png");
 
-const HomeLogo = () => {
-  return (
-    <li className="nav-item">
-      <NavLink className="nav-link home-link" activeClassName="active" to="/">
-        {/* <MainLogo className="home-logo" /> */}
-        <LogoSFYT />
-        {/* <img src={LogoSFYT} className="home-logo" /> */}
-      </NavLink>
-    </li>
-  );
-};
+const HomeLogo = () => (
+  <li className="nav-item">
+    <NavLink className="nav-link home-link" activeClassName="active" to="/">
+      <LogoSFYT />
+    </NavLink>
+  </li>
+);
 
-const Header = () => {
-  return (
-    <header className="top">
-      <nav className="nav">
-        <div className="nav-section nav-pages">
-          <HomeLogo />
-          <NavItems />
-        </div>
+const Header = () => (
+  <header className="top">
+    <nav className="nav">
+      <div className="nav-section nav-pages">
+        <HomeLogo />
+        <NavItems />
+      </div>
 
-        <SearchBar />
-        <UserLogInLogOut />
-      </nav>
-    </header>
-  );
-};
+      <SearchBar />
+      <UserLogInLogOut />
+    </nav>
+  </header>
+);
 
 export default Header;
