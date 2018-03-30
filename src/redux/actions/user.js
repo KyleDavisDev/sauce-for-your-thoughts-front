1aea38937ee7734b95113bf09eaa428b882582b7
Kyle Bonar <kylewb2011@gmail.com>
2018-03-30 12:50:34 -0500

    Migrated hearts to users

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -13,42 +13,28 @@ export const userGotInfo = ({ email, name }) => ({
   name
 });
 
-export const gotHearts = ({ hearts }) => ({
-  type: "GOT_HEARTS",
-  hearts
-});
-
 export const toggledHeart = ({ sauce }) => ({
   type: "TOGGLE_HEARTED",
   sauce
 });
 
-export const updateUser = credentials => dispatch => {
-  return api.user.update(credentials).then(res => {
+export const updateUser = credentials => dispatch =>
+  api.user.update(credentials).then(res => {
     const { email, name } = res.user;
     dispatch(userUpdated({ email, name }));
     const text = `Your name was saved as: ${name} and your email was saved as: ${email}.`;
     dispatch(flashSuccess({ text }));
     return res.user;
   });
-};
 
-export const getInfo = credentials => dispatch => {
-  return api.user.getInfo(credentials).then(res => {
+export const getInfo = credentials => dispatch =>
+  api.user.getInfo(credentials).then(res => {
     const { email, name } = res.user;
     dispatch(userGotInfo({ email, name }));
     return res.user;
   });
-};
 
-export const toggleSauce = data => dispatch => {
-  return api.user.toggleSauce(data).then(res => {
+export const toggleSauce = data => dispatch =>
+  api.user.toggleSauce(data).then(res => {
     dispatch(toggledHeart({ sauce: res.data.sauce }));
   });
-};
-
-export const getHearts = credentials => dispatch => {
-  return api.user.getHearts(credentials).then(res => {
-    dispatch(gotHearts({ hearts: res.data.hearts }));
-  });
-};
--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -4,3 +4,13 @@ export const addUsers = ({ users }) => ({
   type: "USERS_ADDED",
   users
 });
+
+export const gotHearts = ({ hearts }) => ({
+  type: "GOT_HEARTS",
+  hearts
+});
+
+export const getHearts = credentials => dispatch =>
+  api.user.getHearts(credentials).then(res => {
+    dispatch(gotHearts({ hearts: res.data.hearts }));
+  });
