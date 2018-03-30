62eab74cf057687fa79e61168be6999f5005c14d
Kyle Bonar <kylewb2011@gmail.com>
2018-03-30 13:37:32 -0500

    added comments.

--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -10,6 +10,12 @@ export const gotHearts = ({ hearts }) => ({
   hearts
 });
 
+/** @description Grab all sauces available (will limit this to only set amount at a time in future)
+ *  @param {Object} user - API expects a specific format
+ *    @param {String} user.token - JWT string
+ *  @param {function} dispatch - dispatch action
+ *  @return {NULL}
+ */
 export const getHearts = credentials => dispatch =>
   api.user.getHearts(credentials).then(res => {
     dispatch(gotHearts({ hearts: res.data.hearts }));
