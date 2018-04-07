c844766f8d229c2992105c33a8c07beda6c77457
Kyle Bonar <kylewb2011@gmail.com>
2018-04-07 17:46:40 -0500

    added comments.

--- src/redux/reducers/reviews.js
+++ src/redux/reducers/reviews.js
@@ -1,21 +1,22 @@
 export default function sauce(state = {}, action) {
   switch (action.type) {
+    /** @description Expects action.reviews.allIds[] and action.reviews.byID{} */
     case "REVIEWS_ADDED":
       // construct return object
       return {
         byId:
-          "byId" in state && Object.keys(state.byId).length > 0
-            ? Object.assign({}, state.byId, action.reviews.byId)
-            : Object.assign({}, action.reviews.byId),
+          "byId" in state && Object.keys(state.byId).length > 0 // sanity check
+            ? { ...state.byId, ...action.reviews.byId } // concat new reviews to old
+            : { ...action.reviews.byId }, // set new reviews to be entire object
         allIds:
-          "allIds" in state && state.allIds.length > 0
+          "allIds" in state && state.allIds.length > 0 // sanity check
             ? [
-                ...state.allIds,
+                ...state.allIds, // old ids
                 ...action.reviews.allIds.filter(
-                  x => state.allIds.indexOf(x) === -1
+                  x => state.allIds.indexOf(x) === -1 // concat only id that are not already in the array
                 )
               ]
-            : [...action.reviews.allIds]
+            : [...action.reviews.allIds] // set new reviews to be entire array
       };
 
     default:
