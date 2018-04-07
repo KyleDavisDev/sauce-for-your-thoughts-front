a91645d67b497101bcfdd05d5580c3089f5a64f5
Kyle Bonar <kylewb2011@gmail.com>
2018-04-07 17:40:47 -0500

    Added comments, dipatches event to add reviews to store.

--- src/redux/actions/reviews.js
+++ src/redux/actions/reviews.js
@@ -5,10 +5,35 @@ export const addReviews = ({ reviews }) => ({
   reviews
 });
 
+/** @description add a single review to the DB
+ *  @param {Object} data - all encompasing object
+ *    @param {Object} data.user - holds user information
+ *      @param {String} data.user.token - unique user identifier
+ *    @param {Object} data.sauce - hold sauce information
+ *      @param {String} data.sauce._id - unique suace identifier
+ *    @param {Object} data.review - holds review information
+ *      @param {String} data.review.text - user review to be saved
+ *      @param {Number} data.review.rating - 0-10 value of the sauce
+ *  @returns {Promise}
+ *    @returns {NULL}
+ */
 export const addReview = data => dispatch =>
-  api.review.add(data).then(
-    res =>
-    //add review to redux store
-      dispatch(addReviews({reviews: [res.data.review]}))
-      res.data
-  );
+  api.review.add(data).then(res => {
+    console.log(res.data);
+
+    // format review so reducer can understand it
+    const review = {
+      byId: {
+        [res.data.review._id]: {
+          author: { _id: res.data.review.author },
+          sauce: { _id: res.data.review.sauce },
+          rating: res.data.review.rating,
+          text: res.data.review.text
+        }
+      },
+      allIds: [res.data.review._id]
+    };
+
+    // add review to redux store
+    dispatch(addReviews({ reviews: review }));
+  });
