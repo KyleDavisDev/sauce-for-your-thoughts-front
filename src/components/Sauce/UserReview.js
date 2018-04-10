b78559272481f1611f9eab7ca8054075c737543c
Kyle Bonar <kylewb2011@gmail.com>
2018-04-09 22:59:31 -0500

    Added propTypes and matchStateToProps

--- src/components/Sauce/UserReview.js
+++ src/components/Sauce/UserReview.js
@@ -1,27 +1,48 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-const UserReview = props => (
-  <div className="review">
-    <div className="review__header">
-      <div className="review__author">
-        <img
-          className="avatar"
-          alt="submitter icon"
-          src="https://gravatar.com/avatar/9efd86dfb66394fae773919df6a9c0fb?s=200"
-        />
-        <p>Wes Bos</p>
+const UserReview = props => {
+  const { _id } = props.review;
+  return (
+    <div className="review">
+      <div className="review__header">
+        <div className="review__author">
+          <img
+            className="avatar"
+            alt="submitter's icon"
+            src="https://gravatar.com/avatar/9efd86dfb66394fae773919df6a9c0fb?s=200"
+          />
+          <p>Wes Bos</p>
+        </div>
+        <div className="review__stars" title="Rated 5 our of 5 stars">
+          ★★★★★
+        </div>
+        <time className="review__time" dateTime="2017-03-08T22:36:48.575Z">
+          a year ago
+        </time>
       </div>
-      <div className="review__stars" title="Rated 5 our of 5 stars">
-        ★★★★★
+      <div className="review__body">
+        <p>Always a great spot to grab a coffee with a friend. </p>
       </div>
-      <time className="review__time" dateTime="2017-03-08T22:36:48.575Z">
-        a year ago
-      </time>
     </div>
-    <div className="review__body">
-      <p>Always a great spot to grab a coffee with a friend. </p>
-    </div>
-  </div>
-);
+  );
+};
 
-export default UserReview;
+UserReview.propTypes = {
+  review: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    sauce: PropTypes.shape({ _id: PropTypes.string.isRequired }).isRequired,
+    author: PropTypes.shape({ _id: PropTypes.string.isRequired }).isRequired
+  }).isRequired
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const review = ownProps._id ? state.reviews.byId[ownProps._id] : {};
+  return {
+    review
+  };
+};
+export default connect(mapStateToProps, null)(UserReview);
