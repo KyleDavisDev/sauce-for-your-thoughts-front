1996e6244605cb0230f43851fc915141c1f8f28a
Kyle Bonar <kylewb2011@gmail.com>
2018-04-15 18:59:27 -0500

    linting

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -3,11 +3,23 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import AccountForm from "./AccountForm";
-import api from "../../api/api";
 import { flashError, flashClose } from "../../redux/actions/flash";
 import { getInfo, updateUser } from "../../redux/actions/user";
 
 class Account extends Component {
+  static propTypes = {
+    user: {
+      email: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      token: PropTypes.string.isRequired
+    }.isRequired,
+    history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
+    getInfo: PropTypes.func.isRequired,
+    updateUser: PropTypes.func.isRequired,
+    flashError: PropTypes.func.isRequired,
+    flashClose: PropTypes.func.isRequired
+  };
+
   componentWillMount() {
     if (!this.props.user.token) this.props.history.push("/login");
   }
@@ -18,8 +30,8 @@ class Account extends Component {
   }
 
   render() {
-    //check if state exists at all first
-    const { name = "", email = "" } = this.props.user;
+    // check if state exists at all first
+    const { name, email } = this.props.user;
     return (
       <div className="inner">
         <AccountForm name={name} email={email} onSubmit={this.onSubmit} />
@@ -27,24 +39,11 @@ class Account extends Component {
     );
   }
 
-  getUserInfo = () => {
-    const data = { user: { token: this.props.user.token } };
-
-    this.props
-      .getInfo(data)
-      .then(res => {
-        //do something here...?
-      })
-      .catch(err => {
-        this.props.flashError({ text: err.response.data.msg });
-      });
-  };
-
   onSubmit = e => {
-    //close flash
+    // close flash
     this.props.flashClose();
 
-    //add token to data so we can look up user
+    // add token to data so we can look up user
     const data = {
       update: { name: e.name, email: e.email },
       user: { token: this.props.user.token }
@@ -53,26 +52,28 @@ class Account extends Component {
       this.props.flashError({ text: err.response.data.msg });
     });
   };
+
+  getUserInfo = () => {
+    const data = { user: { token: this.props.user.token } };
+
+    this.props
+      .getInfo(data)
+      .then(res => {
+        // do something here...?
+      })
+      .catch(err => {
+        this.props.flashError({ text: err.response.data.msg });
+      });
+  };
 }
 
-Account.propType = {
-  user: {
-    email: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    token: PropTypes.string.isRequired
-  },
-  getInfo: PropTypes.func.isRequired,
-  updateUser: PropTypes.func.isRequired,
-  flashError: PropTypes.func.isRequired,
-  flashClose: PropTypes.func.isRequired
-};
-
+// TODO figure out if setting values here is anti-pattern
 function mapStateToProps(state) {
   return {
     user: {
-      email: state.user.email,
-      name: state.user.name,
-      token: state.users.self.token
+      email: state.user.email || "",
+      name: state.user.name || "",
+      token: state.users.self.token || ""
     }
   };
 }
