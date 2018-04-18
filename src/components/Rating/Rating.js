d11be1c0b72398d66a823f4a8aeb379a1e9b8dac
Kyle Bonar <kylewb2011@gmail.com>
2018-04-18 13:08:50 -0500

    Mostly starting over.

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -4,9 +4,16 @@ import Star from "../../images/icons/Star";
 
 class Rating extends Component {
   static propTypes = {
-    rating: PropTypes.number.isRequired,
+    value: PropTypes.number,
     onClick: PropTypes.func.isRequired,
-    height: PropTypes.number
+    height: PropTypes.number,
+    total: PropTypes.number
+  };
+
+  static defaultProps = {
+    value: 0,
+    total: 10,
+    height: 50
   };
 
   constructor(props) {
@@ -14,44 +21,23 @@ class Rating extends Component {
 
     // create array of length 10, w/ each index having a <Star /> value
     this.state = {
-      hold: false,
-      rating: this.props.rating,
-      starArray: Array(...Array(10)).map((x, ind) => {
-        const classVal = ind < this.props.rating ? "filled" : "empty";
-        return { logo: <Star height={this.props.height || 50} />, classVal };
-      })
+      interacting: false
     };
   }
 
-  // TODO: Clean up this mess
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.rating === 0) return;
-    console.log("nextporps", nextProps);
-    console.log("ownprops", this.state);
-
-    const rating =
-      nextProps.rating === this.state.rating ? 0 : nextProps.rating;
-    const hold = nextProps.rating !== this.state.rating;
-    const starArray = this.state.starArray.map((x, ind) => {
-      const classVal = ind < nextProps.rating ? "filled" : "empty";
-      return { logo: <Star height={nextProps.height || 50} />, classVal };
-    });
-
-    console.log(rating, hold);
-    console.log("____________");
-    this.setState(prevState => ({
-      rating,
-      hold,
-      starArray
-    }));
-
-    // this will reset the value next to "Rating:" in Form component
-    // will also cause componentWillReiveProps to trigger again which should possibly be looked into later
-    if (rating === 0) this.props.onClick(0);
-  }
+  componentWillReceiveProps(nextProps) {}
 
   render() {
-    return <div className="star--container">{this.createTenStars()}</div>;
+    // grab from props
+    const { value, height, total } = this.props;
+    // grab from state
+    const { interacting } = this.state;
+
+    // 1. init array of undefined the size of total number needed
+    // 2. map over array
+    const symbols = [...Array(total)].map(x => console.log("hi"));
+
+    return <div className="star--container">yo</div>;
   }
 
   onClick = rating => {
@@ -59,39 +45,13 @@ class Rating extends Component {
     this.props.onClick(rating);
   };
 
-  onMouseLeave = ind => {
-    if (this.state.hold) return;
-    this.toggleStars(0);
+  onMouseEnter = () => {
+    this.setState({ interacting: true });
   };
 
-  onMouseEnter = ind => {
-    if (this.state.hold) return;
-    this.toggleStars(ind);
+  onMouseLeave = () => {
+    this.setState({ interacting: false, value: this.props.value });
   };
-
-  toggleStars = ind => {
-    this.setState(prevState => ({
-      rating: ind,
-      starArray: prevState.starArray.map((star, i) => {
-        const classVal = i < ind ? "filled" : "empty";
-        return { ...star, classVal };
-      })
-    }));
-  };
-
-  createTenStars = () =>
-    this.state.starArray.map((star, ind) => (
-      <button
-        type="button"
-        className={`star star--${star.classVal}`}
-        key={ind}
-        onClick={() => this.onClick(ind + 1)}
-        onMouseEnter={() => this.onMouseEnter(ind)}
-        onMouseLeave={() => this.onMouseLeave(ind)}
-      >
-        {star.logo}
-      </button>
-    ));
 }
 
 export default Rating;
