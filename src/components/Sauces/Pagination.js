11f8408c9b375aaafd8d4e145e2acbf348fc3c45
Kyle Bonar <kylewb2011@gmail.com>
2018-05-10 15:13:55 -0500

    Fixed typo.

--- src/components/Sauces/Pagination.js
+++ src/components/Sauces/Pagination.js
@@ -7,18 +7,18 @@ const Pagination = ({ total, page, limit }) => {
     <div className="pagination">
       <div className="pagination__prev">
         {page !== 1 ? (
-          <Link to={`/sauces/page/?${page - 1}&limit=${limit}`}>Prev</Link>
+          <Link to={`/sauces/?page=${page - 1}&limit=${limit}`}>Prev</Link>
         ) : (
-            ""
-          )}
+          ""
+        )}
       </div>
       <div className="pagination__text">{`Page ${page} of ${totalPages}`}</div>
       <div className="pagination__next">
         {page !== totalPages ? (
           <Link to={`/sauces/?page=${page + 1}&limit=${limit}`}>Next</Link>
         ) : (
-            ""
-          )}
+          ""
+        )}
       </div>
     </div>
   );
