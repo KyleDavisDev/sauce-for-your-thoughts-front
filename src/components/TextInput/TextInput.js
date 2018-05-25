7089531d8e7e40563a411605f0f5e17b80f81015
Kyle Bonar <kylewb2011@gmail.com>
2018-05-24 23:16:54 -0500

    Turned to class to make use of passing reference

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,61 +1,69 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const TextInput = ({
-  id,
-  name,
-  onChange,
-  required,
-  type,
-  value,
-  placeholder
-}) => (
-  <div>
-    <label htmlFor={name} className="text__upper text__grey">
-      {name}
-      {required && "*"}
-    </label>
-    {type.toLowerCase() === "textarea" ? (
-      <textarea
-        id={id}
-        name={name}
-        cols="30"
-        rows="10"
-        placeholder={placeholder}
-        onChange={onChange}
-        value={value}
-        required={required}
-      />
-    ) : (
-      <input
-        type={type}
-        id={id}
-        name={name}
-        value={value}
-        placeholder={placeholder}
-        onChange={onChange}
-        required={required}
-      />
-    )}
-  </div>
-);
+class TextInput extends React.Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    required: PropTypes.bool,
+    type: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    placeholder: PropTypes.string,
+    parentRef: PropTypes.func
+  };
+  static defaultProps = {
+    required: false,
+    type: "text",
+    value: "",
+    placeholder: "",
+    parentRef: () => {}
+  };
+  render() {
+    const {
+      id,
+      name,
+      onChange,
+      required,
+      type,
+      value,
+      placeholder,
+      parentRef
+    } = this.props;
 
-TextInput.propTypes = {
-  id: PropTypes.string,
-  name: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  required: PropTypes.bool,
-  type: PropTypes.string,
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  placeholder: PropTypes.string
-};
-
-TextInput.defaultProps = {
-  id: "",
-  required: false,
-  type: "text",
-  value: "",
-  placeholder: ""
-};
+    return (
+      <div>
+        <label htmlFor={name} className="text__upper text__grey">
+          {name}
+          {required && "*"}
+        </label>
+        {type.toLowerCase() === "textarea" ? (
+          <textarea
+            id={id}
+            name={name}
+            cols="30"
+            rows="10"
+            placeholder={placeholder}
+            onChange={onChange}
+            value={value}
+            required={required}
+            ref={parentRef}
+          />
+        ) : (
+          <input
+            type={type}
+            id={id}
+            name={name}
+            value={value}
+            placeholder={placeholder}
+            onChange={onChange}
+            required={required}
+            ref={parentRef}
+          />
+        )}
+      </div>
+    );
+  }
+}
 
 export default TextInput;
