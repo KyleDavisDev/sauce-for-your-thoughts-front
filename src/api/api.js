6ba302485b7a3befcf3fd8a46a4e70dc25e10a2a
Kyle Bonar <kylewb2011@gmail.com>
2018-05-30 22:42:14 -0500

    Added comments

--- src/api/api.js
+++ src/api/api.js
@@ -189,6 +189,30 @@ const api = {
       })
   },
   review: {
+    /** @description add a single review to the DB
+     *  @param {Object} data - all encompasing object
+     *    @param {Object} data.user - holds user information
+     *      @param {String} data.user.token - unique user identifier
+     *    @param {Object} data.sauce - hold sauce information
+     *      @param {String} data.sauce.token - unique sauce string
+     *    @param {String} data.review.taste - taste obj
+     *      @param {String} data.review.taste.txt - description of taste
+     *      @param {Number} data.review.taste.rating - 1-5 rating
+     *    @param {String} data.review.heat - heat obj
+     *      @param {String} data.review.heat.txt - description of heat
+     *      @param {Number} data.review.heat.rating - 1-5 rating
+     *    @param {String} data.review.aroma - aroma obj
+     *      @param {String} data.review.aroma.txt - description of aroma
+     *      @param {Number} data.review.aroma.rating - 1-5 rating
+     *    @param {String} data.review.overall - overall obj
+     *      @param {String} data.review.overall.txt - description of overall
+     *      @param {Number} data.review.overall.rating - 1-5 rating
+     *    @param {String} data.review.label - label obj
+     *      @param {String} data.review.label.txt - description of label
+     *      @param {Number} data.review.label.rating - 1-5 rating
+     *    @param {String?} data.review.note - who made the sauce
+     *  @returns {Promise}
+     */
     add: data =>
       axios.post(`${host}/api/review/add`, data).then(res => {
         if (res.data.isGood && res.status === 200) {
