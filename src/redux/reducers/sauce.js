893382e5f72218497fb2dfa290c31dbc6c028bae
Kyle Bonar <kylewb2011@gmail.com>
2018-05-31 19:27:59 -0500

    removed disrupting code.

--- src/redux/reducers/sauce.js
+++ src/redux/reducers/sauce.js
@@ -6,10 +6,8 @@ export default function sauce(state = {}, action) {
       return {};
     case "REVIEWS_FOUND":
       return { ...state, reviews: action.reviews };
-    case "REVIEW_ADDED":
-      return { ...state, reviews: [action.review, ...state.reviews] };
     case "CLEANED_UP_SAUCE":
-      return {}; //clear sauce from redux store
+      return {}; // clear sauce from redux store
     default:
       return state;
   }
