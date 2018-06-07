ec4722d4f32ac8b9135937479f51ed9c409793fc
Kyle Bonar <kylewb2011@gmail.com>
2018-06-07 09:03:21 -0500

    Updated component to be more modular

--- src/components/CheckboxList/CheckboxList.js
+++ src/components/CheckboxList/CheckboxList.js
@@ -1,28 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
+import shortid from "shortid";
 
 const CheckBoxList = ({ tags, onChange, name }) => (
   <ul className="tags">
-    {tags.map(tag => (
-      <div key={tag._id} className="tag tag-choice">
-        <input
-          type="checkbox"
-          id={tag._id}
-          name={name || tag.name}
-          value={tag.name}
-          checked={tag.isChecked}
-          onChange={onChange}
-        />
-        <label htmlFor={tag._id}>{tag.name}</label>
-      </div>
-    ))}
+    {tags.map(tag => {
+      const _id = tag._id || shortid.generate();
+      const isChecked = tag.isChecked || false;
+      return (
+        <div key={_id} className="tag tag-choice">
+          <input
+            type="checkbox"
+            id={_id}
+            name={name || tag.name}
+            value={tag.name}
+            checked={isChecked}
+            onChange={onChange}
+          />
+          <label htmlFor={_id}>{tag.name}</label>
+        </div>
+      );
+    })}
   </ul>
 );
 CheckBoxList.propTypes = {
   tags: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string.isRequired,
-      _id: PropTypes.string.isRequired
+      _id: PropTypes.string,
+      isChecked: PropTypes.bool
     })
   ).isRequired,
   onChange: PropTypes.func.isRequired,
