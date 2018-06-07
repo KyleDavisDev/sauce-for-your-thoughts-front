4cfcbadff5f49044f4b41ed790e6af50b432e304
Kyle Bonar <kylewb2011@gmail.com>
2018-06-07 12:06:56 -0500

    removed console log, updated where data is found

--- src/redux/reducers/reviews.js
+++ src/redux/reducers/reviews.js
@@ -1,15 +1,13 @@
 export default function sauce(state = {}, action) {
   switch (action.type) {
     /** @description Expects action.reviews.allIds[] and action.reviews.byID{} */
-    case "REVIEW_ADDED":
-      console.log(state);
+    case "REVIEWS_ADDED":
       // construct return object
       return {
-        byId: { ...state.byId, ...action.review.byId }, // concat new review to old
-
+        byId: { ...state.byId, ...action.reviews.byId }, // concat new review to old
         allIds: [
           ...state.allIds, // old ids
-          ...action.review.allIds.filter(
+          ...action.reviews.allIds.filter(
             x => state.allIds.indexOf(x) === -1 // concat only id that are not already in the array
           )
         ]
