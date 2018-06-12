505c3a37faa670138c34b79af40bc92bccaf784a
Kyle Bonar <kylewb2011@gmail.com>
2018-06-12 10:26:56 -0500

    Building out component, updated props

--- src/screens/sauce/screens/sauceSingle/components/singleHero/SingleHero.js
+++ src/screens/sauce/screens/sauceSingle/components/singleHero/SingleHero.js
@@ -6,22 +6,29 @@ import { host } from "../../../../../../utils/api/api";
 import ComingSoon from "../../../../../../images/photos/ComingSoon.png";
 
 const SingleHero = ({ sauce }) => (
-  <div className="single-hero">
-    <img
-      alt={`User-submitted background for ${sauce.name}`}
-      className="single-image"
-      src={`${host}/public/uploads/${sauce.photo}`}
-      onError={e => (e.target.src = ComingSoon)}
-    />
-    <h2 className="title title-single">
-      <Link to={sauce.slug || "#"}>{sauce.name || "Loading..."}</Link>
-    </h2>
+  <div className="single__container--tilted">
+    {sauce && (
+      <div className="single__container">
+        <div className="single__hero">
+          <h3 className="single__title">
+            <Link to={sauce.slug || "#"}>{sauce.name || "Loading..."}</Link>
+          </h3>
+          <img
+            alt={`User-submitted background for ${sauce.name}`}
+            className="single__image"
+            src={`${host}/public/uploads/${sauce.photo}`}
+            onError={e => (e.target.src = ComingSoon)}
+          />
+        </div>
+        <div className="single__desc">description stuff</div>
+      </div>
+    )}
   </div>
 );
 SingleHero.propTypes = {
   sauce: PropTypes.shape({
     name: PropTypes.string.isRequired,
-    photo: PropTypes.string.isRequired,
+    photo: PropTypes.string,
     slug: PropTypes.string.isRequired
   })
 };
