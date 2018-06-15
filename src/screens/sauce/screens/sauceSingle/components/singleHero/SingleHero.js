f0c76b4d4458f8ec1559a2105fed3b561a79f3a7
Kyle Bonar <kylewb2011@gmail.com>
2018-06-15 14:51:18 -0500

    seperated title from image, started to build out information section

--- src/screens/sauce/screens/sauceSingle/components/singleHero/SingleHero.js
+++ src/screens/sauce/screens/sauceSingle/components/singleHero/SingleHero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Fire from "react-icons/lib/fa/fire";
 
 import { host } from "../../../../../../utils/api/api";
 import ComingSoon from "../../../../../../images/photos/ComingSoon.png";
@@ -9,18 +10,32 @@ const SingleHero = ({ sauce }) => (
   <div className="single__container--tilted">
     {sauce && (
       <div className="single__container">
-        <div className="single__hero">
+        <div className="single__header">
           <h3 className="single__title">
             <Link to={sauce.slug || "#"}>{sauce.name || "Loading..."}</Link>
           </h3>
-          <img
-            alt={`User-submitted background for ${sauce.name}`}
-            className="single__image"
-            src={`${host}/public/uploads/${sauce.photo}`}
-            onError={e => (e.target.src = ComingSoon)}
-          />
         </div>
-        <div className="single__desc">description stuff</div>
+        <div className="single__content">
+          <div className="single__image__container">
+            <img
+              alt={`User-submitted background for ${sauce.name}`}
+              className="single__image"
+              src={`${host}/public/uploads/${sauce.photo}`}
+              onError={e => (e.target.src = ComingSoon)}
+            />
+          </div>
+          <div className="single__information">
+            <div className="single__item">
+              <div className="item__icon">
+                <Fire />
+              </div>
+              <div className="item__body">
+                <div className="item__title">Heat</div>
+                <div className="item__desc">description stuff</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )}
   </div>
