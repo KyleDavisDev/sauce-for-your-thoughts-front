e61ffab532ac88a096a43a1485c4c7966c869a02
Kyle Bonar <kylewb2011@gmail.com>
2018-09-03 15:12:27 -0500

    Using flashmessage interface.

--- src/interface/initialState.ts
+++ src/interface/initialState.ts
@@ -1,26 +1,22 @@
 import { ISauce } from "../interface/sauce";
 import { IUser } from "../interface/user";
 import { IReview } from "../interface/review";
+import { IFlashMessage } from "../interface/flashMessage";
 
 export interface IState {
-  flashMessage: {
-    isVisible: boolean;
-    type: null;
-    text: null | string;
-    slug: null;
-  };
-  sauces: {
+  flashMessage: IFlashMessage;
+  sauces?: {
     allIds: number[];
-    byId: { [key: string]: ISauce };
-    total: number;
-    query: {};
+    byId?: { [key: string]: ISauce };
+    total?: number;
+    query?: {};
   };
-  sauce: ISauce; // Remove this...?
-  tags: string[]; // Remove this..?
-  users: {
+  sauce?: ISauce; // Remove this...?
+  tags?: string[]; // Remove this..?
+  users?: {
     allIds: number[];
     byId: { [key: string]: IUser };
     self: { token: null | string };
   };
-  reviews: { allIds: number[]; byId: { [key: string]: IReview } };
+  reviews?: { allIds: number[]; byId: { [key: string]: IReview } };
 }
