43efe6a17ee015252446d4784b47ae35e46de79c
Kyle Bonar <kylewb2011@gmail.com>
2018-09-03 20:34:10 -0500

    Building out for testing

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,5 +1,23 @@
 import * as React from "react";
 
-const Home = ({}) => <h1>Hello from world!!</h1>;
+import { TextInput } from "../../components/TextInput/TextInput";
+
+const Home = ({}) => (
+  <h1>
+    Hello from world!!
+    <TextInput
+      id={"5"}
+      name={"this is a test"}
+      value={"this shows up in input"}
+      title={"Title"}
+      onChange={() => {
+        console.log("hi");
+      }}
+      parentRef={() => {
+        console.log("hi");
+      }}
+    />
+  </h1>
+);
 
 export default Home;
