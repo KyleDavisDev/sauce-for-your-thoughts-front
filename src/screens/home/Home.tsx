a57892b58597fe15652a0633ffdf5ac97eb2fd72
Kyle Bonar <kylewb2011@gmail.com>
2018-09-08 12:44:24 -0500

    Updating filler content to actual home page

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,23 +1,11 @@
 import * as React from "react";
 
-import { TextInput } from "../../components/TextInput/TextInput";
+import LandingImage from "./components/landingImage/LandingImage";
 
 const Home = ({}) => (
-  <h1>
-    Hello from world!!
-    <TextInput
-      id={"5"}
-      name={"this is a test"}
-      value={"this shows up in input"}
-      title={"Title"}
-      onChange={() => {
-        console.log("hi");
-      }}
-      parentRef={() => {
-        console.log("hi");
-      }}
-    />
-  </h1>
+  <div>
+    <LandingImage />
+  </div>
 );
 
 export default Home;
