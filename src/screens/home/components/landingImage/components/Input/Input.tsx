c91f2dea2141d25b6024f93a053f4a9ee4ba586f
Kyle Bonar <kylewb2011@gmail.com>
2018-09-08 18:52:07 -0500

    Init commit.

---
+++ src/screens/home/components/landingImage/components/Input/Input.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import styled from "../../../../../../theme/styled-components";
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  max-width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 1px solid #ababab;
+  margin-top: 5px;
+  margin-bottom: 15px;
+`;
+
+interface InputProps {
+  id: string;
+  name: string;
+  value: string;
+  onChange(): void;
+}
+
+const Input: React.SFC<InputProps> = props => {
+  return (
+    <Div>
+      <StyledInput
+        type={"text"}
+        id={props.id}
+        name={props.name}
+        value={props.value}
+        placeholder="Search..."
+        onChange={props.onChange}
+      />
+    </Div>
+  );
+};
+
+export default Input;
