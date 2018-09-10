77e431360c31f09e08dc94c9e7377e5339caba59
Kyle Bonar <kylewb2011@gmail.com>
2018-09-09 21:57:59 -0500

    Moved component to be more usable across the application

--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -1,21 +1,14 @@
 import * as React from "react";
 import * as shortid from "shortid";
 
-import styled from "../../../../../../theme/styled-components";
+import styled from "../../theme/styled-components";
 
 const SelectContainer = styled.div`
   border: 1px solid #e1e1e1;
-  border-radius: 4px;
   color: #4b4b4b;
   display: inline-block;
   position: relative;
   background-color: #f5f5f5;
-  border-color: #eaeaea;
-  border-bottom: 0;
-  border-bottom-right-radius: 0;
-  border-left: 0;
-  border-top: 0;
-  border-top-right-radius: 0;
 
   &:after {
     position: absolute;
@@ -27,36 +20,22 @@ const SelectContainer = styled.div`
   }
 `;
 
-const Select = styled.select`
-  background-color: transparent;
-  appearance: none;
-  border: 0;
-  cursor: pointer;
-  display: inline-block;
-  min-height: 30px;
-  position: relative;
-  transition: background-color 0.3s, color 0.3s, border 0.3s;
-  vertical-align: middle;
-  height: 58px;
-  padding: 0px 40px 0 15px;
-  font-size: 1rem;
-`;
-
 interface DropDownProps {
   id?: string;
-
   options: string[];
   selectedValue: string;
+  className?: string;
   onSelect(event: React.ChangeEvent<HTMLSelectElement>): void;
 }
 
 const DropDown: React.SFC<DropDownProps> = props => {
   return (
     <SelectContainer>
-      <Select
+      <select
         id={props.id}
         onChange={props.onSelect}
         value={props.selectedValue}
+        className={props.className}
       >
         {props.options.map(opt => {
           return (
@@ -65,7 +44,7 @@ const DropDown: React.SFC<DropDownProps> = props => {
             </option>
           );
         })}
-      </Select>
+      </select>
     </SelectContainer>
   );
 };
