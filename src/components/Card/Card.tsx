83f24084e7176bf86b61413c763724162bcb337e
Kyle Bonar <kylewb2011@gmail.com>
2018-09-10 22:54:32 -0500

    Styling component. Removed alt param

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,21 +1,40 @@
 import * as React from "react";
+import styled from "../../theme/styled-components";
+
+const Div = styled.div`
+  max-width: 350px;
+  margin: 0 1em;
+  background-color: ${props => props.theme.cardBackgroundColor};
+  display: flex;
+  flex-direction: column;
+`;
+
+const Image = styled.img`
+  width: 100%;
+`;
+
+const Body = styled.div`
+  padding: 1em;
+`;
 
 interface CardProps {
   imageLink: string;
-  imageAlt: string;
   title: string;
   description: string;
+  className?: string;
 }
 
 const Card: React.SFC<CardProps> = props => {
   return (
-    <div>
-      <img src={props.imageLink} alt={props.imageAlt} />
-      <div>
+    <Div className={props.className}>
+      <Image src={props.imageLink} alt={props.description} />
+      <Body>
         <h4>{props.title}</h4>
-        <p>{props.description}</p>
-      </div>
-    </div>
+        {props.description.length > 25
+          ? props.description.substring(0, 24) + "..."
+          : props.description}
+      </Body>
+    </Div>
   );
 };
 
