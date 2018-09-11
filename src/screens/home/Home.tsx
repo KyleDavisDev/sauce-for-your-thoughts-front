634d38a4ac2bc0ba830ed959d166ba8b1858dd6b
Kyle Bonar <kylewb2011@gmail.com>
2018-09-10 22:55:50 -0500

    Bringin in Cards, adding style and test data

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,10 +1,29 @@
 import * as React from "react";
+import styled from "../../theme/styled-components";
 
 import LandingImage from "./components/landingImage/LandingImage";
+import Card from "../../components/Card/Card";
+
+const Article = styled.article`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const StyledCard = styled(Card)`
+  width: 33%;
+`;
 
 const Home = ({}) => (
   <div>
     <LandingImage />
+    <h1>New submissions</h1>
+    <Article>
+      <StyledCard
+        title="test"
+        imageLink="https://as.ftcdn.net/r/v1/pics/2fd8819a419c4245e5429905770db4b570661f48/home/discover_collections/Images.jpg"
+        description="description here"
+      />
+    </Article>
   </div>
 );
 
