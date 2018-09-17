751586f6fd0cb9ee72ed14af3f4f2c11ddedfbcb
Kyle Bonar <kylewb2011@gmail.com>
2018-09-17 15:28:52 -0500

    Updated style

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -3,13 +3,33 @@ import styled from "../../theme/styled-components";
 
 const StyledDiv = styled.div`
   width: 100%;
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const StyledH2 = styled.h2`
   margin-top: 1em;
   margin-bottom: 0.5em;
-  color: ${props => props.theme.black};
+  padding: 5px 15px;
+  color: ${props => props.theme.white};
+  background-color: ${props => props.theme.primaryThemeColor};
+  position: relative;
+
+  &:after {
+    content: "";
+    width: 0;
+    height: 0;
+    position: absolute;
+    top: 0;
+    right: -20px;
+    border-style: solid;
+    border-width: 38px 20px 0 0;
+    border-color: ${props => props.theme.primaryThemeColor} transparent
+      transparent transparent;
+    z-index: 3;
+  }
 `;
 
 const StyledH6 = styled.h6`
