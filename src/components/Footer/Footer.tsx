26c7f03f620c620b5d1302c87a2dee528b6c9809
Kyle Bonar <kylewb2011@gmail.com>
2018-09-17 17:32:38 -0500

    Adding styling and more explicit containers

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,20 +3,33 @@ import * as React from "react";
 import styled from "../../theme/styled-components";
 import FooterNavigation from "./components/FooterNavigation/FooterNavigation";
 
-const StyledDiv = styled.div`
+const StyledFooter = styled.footer`
   width: 100%;
   background-color: ${props => props.theme.primaryThemeColor};
   display: flex;
   flex-direction: row;
 `;
 
+const StyledDiv = styled.div`
+  max-width: ${props => props.theme.maxPageWidth};
+  margin: 0 auto;
+`;
+
+const StyledFooterNavigation = styled(FooterNavigation)`
+  width: 25%;
+  box-sizing: border-box;
+  padding: 0 1em;
+`;
+
 interface FooterProps {}
 
 const Footer: React.SFC<FooterProps> = props => {
   return (
-    <StyledDiv>
-      <FooterNavigation />
-    </StyledDiv>
+    <StyledFooter>
+      <StyledDiv>
+        <FooterNavigation />
+      </StyledDiv>
+    </StyledFooter>
   );
 };
 
