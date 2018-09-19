0aef7bc7f7a2eca8dff984a5a1d25d2806ccd000
Kyle Bonar <kylewb2011@gmail.com>
2018-09-19 10:40:10 -0500

    Added another item, updated style

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,24 +1,25 @@
 import * as React from "react";
 
 import styled from "../../theme/styled-components";
-import FooterNavigation from "./components/FooterNavigation/FooterNavigation";
+import Navigation from "./components/Navigation/Navigation";
+import Peppers from "./components/Peppers/Peppers";
 
 const StyledFooter = styled.footer`
   width: 100%;
   background-color: ${props => props.theme.primaryThemeColor};
-  display: flex;
-  flex-direction: row;
 `;
 
 const StyledDiv = styled.div`
   max-width: ${props => props.theme.maxPageWidth};
   margin: 0 auto;
-`;
+  display: flex;
+  flex-direction: row;
 
-const StyledFooterNavigation = styled(FooterNavigation)`
-  width: 25%;
-  box-sizing: border-box;
-  padding: 0 1em;
+  > div {
+    width: 25%;
+    box-sizing: border-box;
+    padding: 0 1em;
+  }
 `;
 
 interface FooterProps {}
@@ -27,7 +28,8 @@ const Footer: React.SFC<FooterProps> = props => {
   return (
     <StyledFooter>
       <StyledDiv>
-        <FooterNavigation />
+        <Navigation />
+        <Peppers />
       </StyledDiv>
     </StyledFooter>
   );
