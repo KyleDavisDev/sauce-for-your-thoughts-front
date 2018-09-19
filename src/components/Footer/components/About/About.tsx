119bce8e8bf6a233b78d790fd656ce5e1cb2855e
Kyle Bonar <kylewb2011@gmail.com>
2018-09-19 11:57:24 -0500

    Added styles, added base layout

--- src/components/Footer/components/About/About.tsx
+++ src/components/Footer/components/About/About.tsx
@@ -1,9 +1,39 @@
 import * as React from "react";
 
-interface TypesProps {}
+import styled from "../../../../theme/styled-components";
+
+const StyledDiv = styled.div`
+  max-width: ${props => props.theme.footerMaxWidth};
+`;
+
+const StyledH5 = styled.h5`
+  margin: 0.5em 0;
+  font-weight: 400;
+  padding: 0;
+  text-transform: uppercase;
+  color: ${props => props.theme.white};
+  font-size: ${props => props.theme.scaleH5};
+`;
+
+const StyledP = styled.p`
+  margin: 0.5em 0;
+  padding: 0;
+  color: ${props => props.theme.white};
+  font-size: ${props => props.theme.scaleP};
+`;
+
+interface TypesProps {
+  className?: string;
+}
 
 const Types: React.SFC<TypesProps> = props => {
-  return <div> hello!</div>;
+  return (
+    <StyledDiv className={props.className}>
+      <StyledH5>About</StyledH5>
+
+      <StyledP>Text here</StyledP>
+    </StyledDiv>
+  );
 };
 
 export default Types;
