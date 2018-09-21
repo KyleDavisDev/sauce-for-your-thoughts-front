dd6f44a81dd97582c5e24ca56575904a90d3bfa2
Kyle Bonar <kylewb2011@gmail.com>
2018-09-20 21:14:21 -0500

    Init commit

--- __tests__/components/Button/Button.test.tsx
+++ __tests__/components/Button/Button.test.tsx
@@ -1,5 +1,35 @@
 import * as React from "react";
-import { render } from "react-testing-library";
+import { render, cleanup } from "react-testing-library";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 import Button from "../../../src/components/Button/Button";
 
-render(<div />);
+function renderWithRouter(
+  ui: React.ReactElement<any>,
+  {
+    route = "/",
+    history = createMemoryHistory({ initialEntries: [route] })
+  } = {}
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    // adding `history` to the returned utilities to allow us
+    // to reference it in our tests (just try to avoid using
+    // this to test implementation details).
+    history
+  };
+}
+
+afterEach(cleanup);
+
+test("<Button /> renders", () => {
+  renderWithRouter(<Button text="test" />);
+});
+
+test("<Button /> has proper text", () => {
+  const { container, debug } = renderWithRouter(
+    <Button text="test">text here</Button>
+  );
+  debug();
+  expect(container.innerHTML).toMatch("test");
+});
