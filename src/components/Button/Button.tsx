24ba6434e560e906cf59caaea6957c647befd9b1
Kyle Bonar <kylewb2011@gmail.com>
2018-09-21 16:01:58 -0500

    Split component into two components, code cleanup

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
 import styled from "../../theme/styled-components";
 
 interface ButtonProps {
   children?: string;
   type?: "outline" | "solid";
-  isLink?: boolean;
   linkTo?: string;
   className?: string;
-  onClick?(event: any): void;
+  onClick(event: any): void;
 }
 
 const Div = styled.div`
@@ -17,37 +15,20 @@ const Div = styled.div`
 `;
 
 const Button: React.SFC<ButtonProps> = props => {
-  const onClick = (event: any) => {
-    event.preventDefault();
-    props.onClick
-      ? props.onClick(event)
-      : // tslint:disable-next-line:no-console
-        console.error("Please provide a onClick!");
-  };
-
   return (
     <Div className={props.className}>
-      {props.isLink ? (
-        <Link to={props.linkTo || "#"} className={props.className}>
-          {props.children}
-        </Link>
-      ) : (
-        <button className={props.className} onClick={onClick}>
-          {props.children}
-        </button>
-      )}
+      <button onClick={props.onClick}>{props.children}</button>
     </Div>
   );
 };
 
 Button.defaultProps = {
   type: "outline",
-  isLink: true,
   linkTo: "#"
 };
 
 const StyledButton = styled(Button)`
-  a {
+  button {
     text-decoration: none;
     padding: 0.5em 1em;
     transition: all 0.2s ease;
