4983b61afe7961ae47f7f54adc4b7cdd3ec70aa5
Kyle Bonar <kylewb2011@gmail.com>
2018-09-20 22:20:21 -0500

    Added more scale values

--- src/theme/styled-components.ts
+++ src/theme/styled-components.ts
@@ -22,7 +22,10 @@ export interface ThemeInterface {
   maxPageWidth: string;
   footerMaxWidth: string;
   scaleP: string;
+  scaleH1: string;
   scaleH2: string;
+  scaleH3: string;
+  scaleH4: string;
   scaleH5: string;
   scaleH6: string;
   defaultFontSize: string;
@@ -45,7 +48,10 @@ const enum scale {
   maxPageWidth = "75rem",
   footerMaxWidth = "300px",
   scaleP = "1rem",
-  scaleH2 = "1.875rem",
+  scaleH1 = "2.5rem",
+  scaleH2 = "2.25rem",
+  scaleH3 = "2rem",
+  scaleH4 = "1.875rem",
   scaleH5 = "1.125rem",
   scaleH6 = "1rem"
 }
@@ -67,7 +73,10 @@ export const enum theme {
   defaultFontSize = scale.defaultFontSize,
   footerMaxWidth = scale.footerMaxWidth,
   scaleP = scale.scaleP,
+  scaleH1 = scale.scaleH1,
   scaleH2 = scale.scaleH2,
+  scaleH3 = scale.scaleH3,
+  scaleH4 = scale.scaleH4,
   scaleH5 = scale.scaleH5,
   scaleH6 = scale.scaleH6
 }
