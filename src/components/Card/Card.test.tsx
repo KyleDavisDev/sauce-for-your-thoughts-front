b45370b270e01cfa45980eea2ff5b14866d9946e
Kyle Bonar <kylewb2011@gmail.com>
2018-09-25 09:51:36 -0500

    Init commit

---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as enzyme from "enzyme";
+import { MemoryRouter } from "react-router-dom";
+import Card from "./Card";
+
+describe("<Card>", () => {
+  it("renders", () => {
+    const wrapper = enzyme.shallow(
+      <Card title="" description="" anchorLink="" />
+    );
+    expect(wrapper).toBeTruthy();
+  });
+
+  it("renders correct content", () => {
+    let wrapper = enzyme.render(
+      <MemoryRouter>
+        <Card title="Title1" description="Desc1" anchorLink="" />
+      </MemoryRouter>
+    );
+    expect(wrapper.find("h4").text()).toEqual("Title1");
+    expect(wrapper.find("p").text()).toEqual("Desc1");
+
+    wrapper = enzyme.render(
+      <MemoryRouter>
+        <Card title="Title2" description="Desc2" anchorLink="" />
+      </MemoryRouter>
+    );
+    expect(wrapper.find("h4").text()).toEqual("Title2");
+    expect(wrapper.find("p").text()).toEqual("Desc2");
+  });
+});
