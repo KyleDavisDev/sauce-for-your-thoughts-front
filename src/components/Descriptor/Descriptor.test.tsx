eb36aed9a69417478b15494c65662ab208d3b9e9
Kyle Bonar <kylewb2011@gmail.com>
2018-10-01 09:50:52 -0500

    Init commit

---
+++ src/components/Descriptor/Descriptor.test.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import * as enzyme from "enzyme";
+import Descriptor from "./Descriptor";
+
+describe("<Descriptor>", () => {
+  const title = "Test title";
+  const children = "Test description";
+  const wrapper = enzyme.shallow(
+    <Descriptor title={title}>{children}</Descriptor>
+  );
+
+  it("renders", () => {
+    expect(wrapper).toBeTruthy();
+  });
+
+  it("Renders correct title", () => {
+    expect(wrapper.find("h5").text()).toEqual(title);
+
+    // Test an updated title
+    wrapper.setProps({ title: "Another title here!!" });
+    expect(wrapper.find("h5").text()).toEqual("Another title here!!");
+  });
+
+  it("Renders correct description", () => {
+    expect(
+      wrapper
+        .render()
+        .find("p")
+        .text()
+    ).toEqual(children);
+
+    // Test an updated description
+    wrapper.setProps({ children: "Second description test" });
+    expect(
+      wrapper
+        .render()
+        .find("p")
+        .text()
+    ).toEqual("Second description test");
+  });
+});
