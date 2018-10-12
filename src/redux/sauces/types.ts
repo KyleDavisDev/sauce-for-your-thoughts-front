b70bcddf2ba360ebcce52a7f153b07ba94335c0f
Kyle Bonar <kylewb2011@gmail.com>
2018-10-12 15:54:56 -0500

    Added action

--- src/redux/sauces/types.ts
+++ src/redux/sauces/types.ts
@@ -1,5 +1,15 @@
 import { ISauce } from "../sauce/types";
 
+export interface IAction {
+  type: string;
+  sauces: {
+    byId: null | { [key: string]: ISauce };
+    allIds: null | number[];
+  };
+  query: null | { [key: string]: { sauces: number[] } };
+  total: number;
+}
+
 // Declare state types with `readonly` modifier to get compile time immutability.
 // https://github.com/piotrwitek/react-redux-typescript-guide#state-with-type-level-immutability
 export interface ISauces {
