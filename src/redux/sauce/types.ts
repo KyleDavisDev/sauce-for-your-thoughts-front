d36e6f13d7eedc8f745a31de6c0e15dd503ca4c0
Kyle Bonar <kylewb2011@gmail.com>
2018-10-12 15:54:43 -0500

    Updated comment

--- src/redux/sauce/types.ts
+++ src/redux/sauce/types.ts
@@ -1,4 +1,5 @@
-// Sauce shape
+// Declare state types with `readonly` modifier to get compile time immutability.
+// https://github.com/piotrwitek/react-redux-typescript-guide#state-with-type-level-immutability
 export interface ISauce {
   _id: number;
   maker: string;
