8ab4312f4651300bd2972332841adf8d1b5ffc77
Kyle Bonar <kylewb2011@gmail.com>
2018-10-12 15:54:35 -0500

    Added action

--- src/redux/flashMessage/types.ts
+++ src/redux/flashMessage/types.ts
@@ -1,6 +1,12 @@
+export interface IAction {
+  type: string;
+  slug: string;
+  text: string;
+}
+
 // Declare state types with `readonly` modifier to get compile time immutability.
 // https://github.com/piotrwitek/react-redux-typescript-guide#state-with-type-level-immutability
-export interface IFlashMessage {
+export interface IFlashState {
   readonly isVisible: boolean;
   readonly type: null | string;
   readonly text: null | string;
