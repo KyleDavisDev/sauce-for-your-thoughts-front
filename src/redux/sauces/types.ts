c9a658f4a819c5760e91151e94265fdc941e3137
Kyle Bonar <kylewb2011@gmail.com>
2018-10-15 21:30:49 -0500

    allIds must be passed, more explicit in query type

--- src/redux/sauces/types.ts
+++ src/redux/sauces/types.ts
@@ -13,8 +13,8 @@ export interface IAction {
 // Declare state types with `readonly` modifier to get compile time immutability.
 // https://github.com/piotrwitek/react-redux-typescript-guide#state-with-type-level-immutability
 export interface ISauces {
-  readonly allIds: null | number[];
+  readonly allIds: number[];
   readonly byId: null | { [key: string]: ISauce };
   readonly total: null | number;
-  readonly query: null | {};
+  readonly query: null | { [key: string]: { sauces: number[] } };
 }
