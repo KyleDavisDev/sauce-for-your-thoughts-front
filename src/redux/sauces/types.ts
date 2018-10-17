865b0b80653c6a2534088caf68a13c32b08bd0ba
Kyle Bonar <kylewb2011@gmail.com>
2018-10-17 08:57:44 -0500

    Abstracted some interfaces, added interfaces for action emitters to utilize

--- src/redux/sauces/types.ts
+++ src/redux/sauces/types.ts
@@ -10,11 +10,26 @@ export interface IAction {
   total: number;
 }
 
+export interface IbyID {
+  [key: string]: ISauce;
+}
+
+export interface IQuery {
+  [key: string]: { sauces: number[] };
+}
+
 // Declare state types with `readonly` modifier to get compile time immutability.
 // https://github.com/piotrwitek/react-redux-typescript-guide#state-with-type-level-immutability
 export interface ISauces {
   readonly allIds: number[];
-  readonly byId: null | { [key: string]: ISauce };
+  readonly byId: null | IbyID;
   readonly total: null | number;
-  readonly query: null | { [key: string]: { sauces: number[] } };
+  readonly query: null | IQuery;
+}
+
+export interface IActionSauces {
+  readonly type: string;
+  readonly sauces: ISauce[];
+  readonly total: null | number;
+  readonly query: null | IQuery;
 }
