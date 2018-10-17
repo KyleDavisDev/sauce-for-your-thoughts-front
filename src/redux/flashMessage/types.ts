3f66a5fcfcbd26122d0ff517eeb264191eb040d5
Kyle Bonar <kylewb2011@gmail.com>
2018-10-17 14:25:31 -0500

    Added enum of the different FlashMessageTypes

--- src/redux/flashMessage/types.ts
+++ src/redux/flashMessage/types.ts
@@ -4,6 +4,20 @@ export interface IAction {
   text: string;
 }
 
+// Use const enums for better autocompletion of action type names. These will
+// be compiled away leaving only the final value in your compiled code.
+//
+// Define however naming conventions you'd like for your action types, but
+// personally, I use the `@@context/ACTION_TYPE` convention, to follow the convention
+// of Redux's `@@INIT` action.
+export const enum FlashMessageActionTypes {
+  SUCCESS_FLASH = "@@flashMessage/SUCCESS_FLASH",
+  ERROR_FLASH = "@@flashMessage/ERROR_FLASH",
+  WARNING_FLASH = "@@flashMessage/WARNING_FLASH",
+  TOGGLE_FLASH = "@@flashMessage/TOGGLE_FLASH",
+  USER_LOGGED_OUT = "@@flashMessage/USER_LOGGED_OUT",
+  CLOSE_FLASH = "@@flashMessage/CLOSE_FLASH"
+}
 // Declare state types with `readonly` modifier to get compile time immutability.
 // https://github.com/piotrwitek/react-redux-typescript-guide#state-with-type-level-immutability
 export interface IFlashState {
