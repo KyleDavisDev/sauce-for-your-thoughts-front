b1eab89bf69cfcee74612dcf7594879901ddc414
Kyle Bonar <kylewb2011@gmail.com>
2018-10-19 09:17:14 -0500

    Added interface for actions, enum of action types, and a standard expected state for reviews

--- src/redux/reviews/types.ts
+++ src/redux/reviews/types.ts
@@ -6,8 +6,23 @@ export interface IReviewSection {
   txt: string;
 }
 
-// Declare state types with `readonly` modifier to get compile time immutability.
-// https://github.com/piotrwitek/react-redux-typescript-guide#state-with-type-level-immutability
+// All actions emitted must follow this form
+export interface IReviewsAction {
+  type: string;
+  allIds: number[];
+  byId: { [key: string]: IReview };
+}
+
+// Use const enums for better autocompletion of action type names. These will
+// be compiled away leaving only the final value in your compiled code.
+//
+// Define however naming conventions you'd like for your action types, but
+// personally, I use the `@@context/ACTION_TYPE` convention, to follow the convention
+// of Redux's `@@INIT` action.
+export const enum ReviewsActionTypes {
+  REVIEWS_ADDED = "@@reviews/REVIEWS_ADDED"
+}
+
 export interface IReview {
   _id: number;
   author: IUserRef; // User reference
@@ -19,6 +34,14 @@ export interface IReview {
   taste?: IReviewSection;
   heat?: IReviewSection;
   note?: IReviewSection;
+}
+
+// Used for state
+// Declare state types with `readonly` modifier to get compile time immutability.
+// https://github.com/piotrwitek/react-redux-typescript-guide#state-with-type-level-immutability
+export interface IReviewsState {
+  readonly allIds: number[];
+  readonly byId: null | { [key: string]: IReview };
 }
 
 // Trimmed down for reference only
