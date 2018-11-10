1d3bb295aea3e0ccb104c8415e7e98f921501f9a
Kyle Bonar <kylewb2011@gmail.com>
2018-11-10 10:58:00 -0600

    Injecting global styles - will be useful for fonts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,10 +4,17 @@ import { Provider } from "react-redux";
 
 import Screens from "../screens/Screens"; // Routing
 import { configureStore } from "../redux/configureStore"; // redux store
-import { ThemeProvider, theme } from "../theme/styled-components"; // theme
+import { ThemeProvider, theme, injectGlobal } from "../theme/styled-components"; // theme
 
 // Configure/initialize redux store
 const store = configureStore();
+
+// tslint:disable-next-line:no-unused-expression
+injectGlobal`
+  h1,h2,h3,h4,h5,h6 {
+    margin: 0px;
+  }
+`;
 
 class App extends React.Component<{}, {}> {
   public render(): JSX.Element {
