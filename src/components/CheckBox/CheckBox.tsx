34540694d0851ef13d29ffb90b86d1867bafb0c0
Kyle Bonar <kylewb2011@gmail.com>
2018-11-11 10:11:20 -0600

    Label now wraps input, updated style, adding onclick

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -6,34 +6,38 @@ interface CheckBoxProps {
   id: string;
   value: string;
   label: string;
+  onClick: (event: React.MouseEvent<HTMLInputElement>) => void;
   className?: string;
 }
 
 const CheckBox: React.SFC<CheckBoxProps> = props => {
   return (
     <div className={props.className}>
-      <input type="checkbox" value={props.value} id={props.id} />
-      <label htmlFor={props.id}>{props.label}</label>
+      <label htmlFor={props.id}>
+        <input
+          defaultChecked={props.checked}
+          type="checkbox"
+          value={props.value}
+          id={props.id}
+          onClick={props.onClick}
+        />
+        {props.label}
+      </label>
     </div>
   );
 };
 
 const StyledCheckbox = styled(CheckBox)`
-  position: relative;
-  left: 25px;
+  display: inline-block;
+  padding: 1rem 1rem 1rem 1rem;
+  box-sizing: border-box;
 
   input {
-    z-index: 5;
-    position: absolute;
-    top: 10px;
-    left: -10px;
+    margin-right: 5px;
   }
 
   label {
-    position: absolute;
-    padding: 10px 10px 10px 30px;
-    left: -20px;
-
+    padding: 10px 10px 10px 10px;
     background-color: ${props =>
       props.checked
         ? props.theme.checkBoxActiveBackgroundColor
