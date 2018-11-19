4a2ecda8b71666677ecbfcc7530877cc509607a4
Kyle Bonar <kylewb2011@gmail.com>
2018-11-18 21:29:59 -0600

    build out initial state interface

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -6,6 +6,11 @@ import flashMessage from "./flashMessage/reducer";
 import sauces from "./sauces/reducer";
 import reviews from "./reviews/reducer";
 import users from "./users/reducer";
+import { ISauce } from "./sauce/types";
+import { IQuery } from "./sauces/types";
+import { IUser } from "./users/types";
+import { IReview } from "./reviews/types";
+import { IFlashState } from "./flashMessage/types";
 
 const rootReducer = combineReducers({
   flashMessage,
@@ -13,6 +18,25 @@ const rootReducer = combineReducers({
   reviews,
   users
 });
+
+// interface IinitialSate {
+//   sauces: {
+//     allIds?: string[];
+//     byId?: { [key: string]: ISauce };
+//     total?: number;
+//     query?: IQuery;
+//   };
+//   users: {
+//     self: { token?: string };
+//     byId?: { [key: string]: IUser };
+//     allIds?: string[];
+//   };
+//   reviews: {
+//     byId?: { [key: string]: IReview };
+//     allIds?: string[];
+//   };
+//   flashMessage: IFlashState;
+// }
 
 export const configureStore = () => {
   const initialState: object = {
@@ -23,7 +47,8 @@ export const configureStore = () => {
       query: {}
     },
     users: { self: { token: "123" }, byId: {}, allIds: [] },
-    reviews: { byId: {}, allIds: [] }
+    reviews: { byId: {}, allIds: [] },
+    flashMessage: { isVisible: false, type: null, text: null, slug: null }
   };
 
   if (process.env.NODE_ENV === "production") {
