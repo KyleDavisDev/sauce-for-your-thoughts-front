8020c1466e4ba0bec65a01386c89201dcbe5ab87
Kyle Bonar <kylewb2011@gmail.com>
2018-12-24 12:47:07 -0600

    Using filter bar

--- src/screens/sauces/Sauces.tsx
+++ src/screens/sauces/Sauces.tsx
@@ -4,6 +4,7 @@ import PageTitle from "../../components/PageTitle/PageTitle";
 import TopBar from "../../components/TopBar/TopBar";
 import Navigation from "../../components/Navigation/Navigation";
 import Footer from "../../components/Footer/Footer";
+import FilterBar from "./components/FilterBar/FilterBar";
 
 export interface SaucesProps {}
 
@@ -23,6 +24,7 @@ export default class Sauces extends React.Component<SaucesProps, SaucesState> {
         <Navigation />
         <Article>
           <PageTitle>Sauces</PageTitle>
+          <FilterBar />
         </Article>
         <Footer />
       </div>
