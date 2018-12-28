ffe2ba1837fce1912f38b23448bc667e9fa98896
Kyle Bonar <kylewb2011@gmail.com>
2018-12-28 16:52:35 -0600

    Style updates, comment updates, reduced complexity

--- src/screens/sauces/components/pagination/Pagination.tsx
+++ src/screens/sauces/components/pagination/Pagination.tsx
@@ -1,6 +1,19 @@
 import * as React from "react";
 import { Link } from "../../../../components/Link/Link";
 import { Button } from "../../../../components/Button/Button";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  > a,
+  > span {
+    margin: 0 0.5em;
+  }
+`;
 
 interface PaginationProps {
   total: number;
@@ -15,19 +28,19 @@ const Pagination: React.SFC<PaginationProps> = props => {
   const lowerBound = page - range > 1 ? page - range : 1; // Get the lower bound. We don't want negative value so stop at 0
   const upperBound = page + range < totalPages ? page + range : totalPages; // Get the upper bound. We don't want value too high so stop at max # of pages
   return (
-    <div>
-      {/* Go Back */}
-      {page !== 1 ? (
-        <div>
-          <Link to={`/sauces/?page=${1}&limit=${limit}`}>
-            <Button displayType="outline">First</Button>
-          </Link>
-          <Link to={`/sauces/?page=${page - 1}&limit=${limit}`}>
-            <Button displayType="outline">Prev</Button>
-          </Link>
-        </div>
-      ) : (
-        ""
+    <StyledDiv>
+      {/* First Button */}
+      {page !== 1 && (
+        <Link to={`/sauces/?page=${1}&limit=${limit}`}>
+          <Button displayType="outline">First</Button>
+        </Link>
+      )}
+
+      {/* Previous Button */}
+      {page !== 1 && (
+        <Link to={`/sauces/?page=${page - 1}&limit=${limit}`}>
+          <Button displayType="outline">Prev</Button>
+        </Link>
       )}
 
       {/* Show past few pages */}
@@ -40,31 +53,31 @@ const Pagination: React.SFC<PaginationProps> = props => {
       })}
 
       {/* Center */}
-      <div>{page}</div>
+      <span>{page}</span>
 
       {/* Show future few button */}
       {new Array(upperBound - page).fill(undefined).map((x, ind) => {
         return (
-          <Link to={`/sauces/?page=${page + ind}&limit=${limit}`}>
-            {(page + ind).toString()}
+          <Link to={`/sauces/?page=${page + ind + 1}&limit=${limit}`}>
+            {(page + ind + 1).toString()}
           </Link>
         );
       })}
 
-      {/* Go Forward */}
-      {page !== totalPages ? (
-        <div>
-          <Link to={`/sauces/?page=${page + 1}&limit=${limit}`}>
-            <Button displayType="outline">Next</Button>
-          </Link>
-          <Link to={`/sauces/?page=${totalPages}&limit=${limit}`}>
-            <Button displayType="outline">Last</Button>
-          </Link>
-        </div>
-      ) : (
-        ""
+      {/* Next Button */}
+      {page !== totalPages && (
+        <Link to={`/sauces/?page=${page + 1}&limit=${limit}`}>
+          <Button displayType="outline">Next</Button>
+        </Link>
       )}
-    </div>
+
+      {/* Last Button */}
+      {page !== totalPages && (
+        <Link to={`/sauces/?page=${totalPages}&limit=${limit}`}>
+          <Button displayType="outline">Last</Button>
+        </Link>
+      )}
+    </StyledDiv>
   );
 };
 
