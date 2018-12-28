3199a9a1bb1d90aaa2af122b228edf3c22f49251
Kyle Bonar <kylewb2011@gmail.com>
2018-12-28 13:00:49 -0600

    Updated style, added placeholder cards

--- src/screens/sauces/Sauces.tsx
+++ src/screens/sauces/Sauces.tsx
@@ -5,6 +5,26 @@ import TopBar from "../../components/TopBar/TopBar";
 import Navigation from "../../components/Navigation/Navigation";
 import Footer from "../../components/Footer/Footer";
 import FilterBar from "./components/FilterBar/FilterBar";
+import Card from "../../components/Card/Card";
+import styled from "styled-components";
+
+const StyledCardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
+
+const StyledCardHolder = styled.div`
+  padding: 1em;
+  width: 100%;
+  box-sizing: border-box;
+  max-width: 300px;
+`;
+
+const StyledCard = styled(Card)`
+  margin: 0;
+`;
 
 export interface SaucesProps {}
 
@@ -23,6 +43,20 @@ export default class Sauces extends React.Component<SaucesProps, SaucesState> {
         <Article>
           <PageTitle>Sauces</PageTitle>
           <FilterBar />
+          <StyledCardContainer>
+            {new Array(9).fill(undefined).map(x => {
+              return (
+                <StyledCardHolder>
+                  <StyledCard
+                    anchorLink="#"
+                    title="test"
+                    imageLink="https://as.ftcdn.net/r/v1/pics/2fd8819a419c4245e5429905770db4b570661f48/home/discover_collections/Images.jpg"
+                    description="description here"
+                  />
+                </StyledCardHolder>
+              );
+            })}
+          </StyledCardContainer>
         </Article>
         <Footer />
       </div>
