14847b62b9ddaebae25bd9091e476cd30c130c37
Kyle Bonar <kylewb2011@gmail.com>
2018-12-29 16:20:45 -0600

    Style update, added review title

--- src/screens/sauce/screens/SauceSpotlight/SauceSpotlight.tsx
+++ src/screens/sauce/screens/SauceSpotlight/SauceSpotlight.tsx
@@ -7,10 +7,9 @@ import Article from "../../../../components/Article/Article";
 import PageTitle from "../../../../components/PageTitle/PageTitle";
 import Navigation from "../../../../components/Navigation/Navigation";
 import styled from "../../../../theme/styled-components";
-import { Link } from "../../../../components/Link/Link";
-import { Button } from "../../../../components/Button/Button";
 import Footer from "../../../../components/Footer/Footer";
 import SpotlightHero from "./components/SauceContainer/SpotlightHero";
+import Descriptor from "../../../../components/Descriptor/Descriptor";
 
 const StyledArticle = styled(Article)`
   max-width: 1200px;
@@ -25,6 +24,13 @@ const StyledLeftContainer = styled.div`
   padding: 0 1em;
 `;
 
+const StyledDescriptor = styled(Descriptor)`
+  > p {
+    margin-top: 0;
+    font-style: italic;
+  }
+`;
+
 export interface SauceSingleProps {}
 
 class SauceSingle extends React.Component<SauceSingleProps, any> {
@@ -37,6 +43,10 @@ class SauceSingle extends React.Component<SauceSingleProps, any> {
           <StyledLeftContainer>
             <PageTitle>Chalulu hot sauce</PageTitle>
             <SpotlightHero id={"5"} />
+
+            <StyledDescriptor title="Reviews">
+              The opinions expressed are soley those of the author.
+            </StyledDescriptor>
           </StyledLeftContainer>
         </StyledArticle>
         <Footer />
