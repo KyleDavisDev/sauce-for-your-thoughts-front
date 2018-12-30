958b23f384d2ce4e3e0dae82a8f5a3fdb853f7d0
Kyle Bonar <kylewb2011@gmail.com>
2018-12-30 17:26:59 -0600

    Init commit.

---
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/dummyData.tsx
@@ -0,0 +1,112 @@
+export const dummyData = [
+  {
+    _id: "58c6a8aafb4d61115815f8b4",
+    date: "2017-03-13T14:11:54.848Z",
+    sauce: "5",
+    author: "1",
+    overall: {
+      rating: 4,
+      txt:
+        "Ive had some umami hot sauces that are too complex in flavor profile and stumble over themselves.... this is not it."
+    },
+    label: {
+      rating: 4,
+      txt:
+        "Ive had some umami hot sauces that are too complex in flavor profile and stumble over themselves.... this is not it."
+    },
+    aroma: {
+      rating: 4,
+      txt:
+        "Deep rich smokey aroma with a sweetness of caramalized onions. Really enjoyable on the nose. Not overly complex or simple Deep rich smokey aroma with a sweetness of caramalized onions. Really enjoyable on the nose. Not overly complex or simple "
+    },
+    taste: { rating: 4, txt: "here is some test text" }
+  },
+  {
+    _id: "58c6a8b8fb4d61115815f8b5",
+    date: "2017-03-13T14:12:08.255Z",
+    sauce: "5",
+    author: "2",
+    overall: { rating: 4, txt: "here is some test text" },
+    label: { rating: 4, txt: "here is some test text" },
+    aroma: { rating: 4, txt: "On the mild side, but balanced well" },
+    taste: { rating: 4, txt: "here is some test text" },
+    heat: { rating: 4, txt: "On the mild side, but balanced well" },
+    note: {
+      rating: 4,
+      txt:
+        "Ive had some umami hot sauces that are too complex in flavor profile and stumble over themselves.... this is not it."
+    }
+  },
+  {
+    _id: "58c6a8cefb4d61115815f8b6",
+    date: "2017-03-13T14:12:30.578Z",
+    sauce: "5",
+    author: "3",
+    overall: { rating: 4, txt: "On the mild side, but balanced well" },
+    label: {
+      rating: 4,
+      txt:
+        "Ive had some umami hot sauces that are too complex in flavor profile and stumble over themselves.... this is not it."
+    },
+    aroma: { rating: 4, txt: "here is some test text" },
+    taste: {
+      rating: 4,
+      txt:
+        "Deep rich smokey aroma with a sweetness of caramalized onions. Really enjoyable on the nose. Not overly complex or simple Deep rich smokey aroma with a sweetness of caramalized onions. Really enjoyable on the nose. Not overly complex or simple "
+    },
+    heat: { rating: 4, txt: "here is some test text" },
+    note: { rating: 4, txt: "On the mild side, but balanced well" }
+  },
+  {
+    _id: "58c6a8e5fb4d61115815f8b7",
+    date: "2017-03-13T14:12:53.559Z",
+    sauce: "5",
+    author: "4",
+    overall: { rating: 4, txt: "here is some test text" },
+    label: {
+      rating: 4,
+      txt:
+        "Ive had some umami hot sauces that are too complex in flavor profile and stumble over themselves.... this is not it."
+    },
+    aroma: {
+      rating: 4,
+      txt:
+        "Deep rich smokey aroma with a sweetness of caramalized onions. Really enjoyable on the nose. Not overly complex or simple Deep rich smokey aroma with a sweetness of caramalized onions. Really enjoyable on the nose. Not overly complex or simple "
+    },
+    taste: {
+      rating: 4,
+      txt:
+        "Ive had some umami hot sauces that are too complex in flavor profile and stumble over themselves.... this is not it."
+    },
+    heat: {
+      rating: 4,
+      txt:
+        "Deep rich smokey aroma with a sweetness of caramalized onions. Really enjoyable on the nose. Not overly complex or simple Deep rich smokey aroma with a sweetness of caramalized onions. Really enjoyable on the nose. Not overly complex or simple "
+    },
+    note: { rating: 4, txt: "here is some test text" }
+  },
+  {
+    _id: "58c6a8f6fb4d61115815f8b8",
+    date: "2017-03-13T14:13:10.681Z",
+    sauce: "5",
+    author: "122",
+    overall: { rating: 4, txt: "here is some test text" },
+    label: {
+      rating: 4,
+      txt:
+        "Ive had some umami hot sauces that are too complex in flavor profile and stumble over themselves.... this is not it."
+    },
+    aroma: {
+      rating: 4,
+      txt:
+        "Ive had some umami hot sauces that are too complex in flavor profile and stumble over themselves.... this is not it."
+    },
+    taste: { rating: 4, txt: "here is some test text" },
+    heat: { rating: 4, txt: "here is some test text" },
+    note: {
+      rating: 4,
+      txt:
+        "Deep rich smokey aroma with a sweetness of caramalized onions. Really enjoyable on the nose. Not overly complex or simple Deep rich smokey aroma with a sweetness of caramalized onions. Really enjoyable on the nose. Not overly complex or simple "
+    }
+  }
+];
