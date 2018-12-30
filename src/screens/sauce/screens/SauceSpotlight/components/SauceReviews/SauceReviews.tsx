db29ff235144cd6bc7bd8d3df419db768c3bf7f4
Kyle Bonar <kylewb2011@gmail.com>
2018-12-30 11:17:39 -0600

    Updated component to get reviews from redux store

--- src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/SauceReviews.tsx
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/SauceReviews.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { IinitialState } from "../../../../../../redux/configureStore";
 import styled from "styled-components";
+import { IReview } from "../../../../../../redux/reviews/types";
 
 const StyledSauceContainer = styled.div`
   background-color: ${props => props.theme.formContainerBackgroundColor};
@@ -14,6 +15,8 @@ const StyledSauceContainer = styled.div`
 
 export interface SauceReviewsProps {
   id: string; // This is sauce's id that will have to be looked up
+  reviews: IReview[];
+  unfoundReviews?: number[];
 }
 
 class SauceReviews extends React.Component<SauceReviewsProps, any> {
@@ -26,8 +29,36 @@ class SauceReviews extends React.Component<SauceReviewsProps, any> {
   }
 }
 
-const mapState2Props = (state: IinitialState) => {
-  return {};
+const mapState2Props = (state: IinitialState, ownProps: SauceReviewsProps) => {
+  // Make sure we actually have sauces and reviews in the state
+  const sauces = state.sauces.byId;
+  const reviews = state.reviews.byId;
+  if (!sauces || !reviews) {
+    return {};
+  }
+
+  // Grab the array of ids
+  const sauceReviews: string[] | undefined = sauces[ownProps.id].reviews;
+
+  // Maybe no reviews on the sauce yet.
+  if (!sauceReviews || sauceReviews.length === 0) {
+    return { reviews: [] };
+  }
+
+  // Push any unfound ids into this arr so we can try, once more, to look for them.
+  // This is a small safety net only
+  const unfoundReviews: string[] = [];
+
+  // Loop through list of review ids and find the entire review from redux state
+  // TODO: Add pagination if there becomes too many reviews on the page
+  const reviewsToRender = sauceReviews.map(ReviewID => {
+    if (reviews[ReviewID] === undefined) {
+      unfoundReviews.push(ReviewID);
+    }
+    return reviews[ReviewID];
+  });
+
+  return { reviews: reviewsToRender };
 };
 
 export default connect(mapState2Props)(SauceReviews);
