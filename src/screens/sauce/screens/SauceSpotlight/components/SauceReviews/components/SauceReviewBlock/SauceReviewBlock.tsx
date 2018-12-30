10f4acba797625ae18aaad3c901598ba691e2044
Kyle Bonar <kylewb2011@gmail.com>
2018-12-30 17:27:21 -0600

    Style updates, connecting to redux, adding toggle to state

--- src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/components/SauceReviewBlock/SauceReviewBlock.tsx
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/components/SauceReviewBlock/SauceReviewBlock.tsx
@@ -1,20 +1,68 @@
 import * as React from "react";
+import { connect } from "react-redux";
+import { IReview } from "../../../../../../../../redux/reviews/types";
+import styled from "styled-components";
+import { IinitialState } from "Users/kylebonar/Documents/projects/sites/now-thats-delicious-front/src/redux/configureStore";
 
-export interface SauceReviewBlockProps {}
+const StyledContainer = styled.div`
+  background-color: ${props => props.theme.formContainerBackgroundColor};
+  font-family: AvenirNextReg;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-bottom: 1em;
+`;
 
-export interface SauceReviewBlockState {}
+const StyledToggleContainer = styled.div`
+  background-color: blue;
+  padding: 0.75em;
 
-export default class SauceReviewBlock extends React.Component<
+  &:hover,
+  &:focus {
+    background-color: yellow;
+    cursor: pointer;
+  }
+`;
+
+const StyledContentContainer = styled.div`
+  padding: 0.75em;
+`;
+
+export interface SauceReviewBlockProps {
+  review: IReview;
+}
+
+export interface SauceReviewBlockState {
+  isOpen: boolean; // Will be our toggle
+}
+
+class SauceReviewBlock extends React.Component<
   SauceReviewBlockProps,
   SauceReviewBlockState
 > {
   constructor(props: SauceReviewBlockProps) {
     super(props);
 
-    this.state = {};
+    this.state = { isOpen: true };
   }
 
   public render() {
-    return <div>I'm a review!</div>;
+    return (
+      <StyledContainer>
+        <StyledToggleContainer>
+          {this.state.isOpen ? "+" : "-"}
+        </StyledToggleContainer>
+        <StyledContentContainer>I'm a review!</StyledContentContainer>
+      </StyledContainer>
+    );
   }
 }
+
+const mapState2Props = (
+  state: IinitialState,
+  ownProps: SauceReviewBlockProps
+) => {
+  return {};
+};
+
+export default connect(mapState2Props)(SauceReviewBlock);
