39ba4e315d8feb556f512165634e53f660ae4209
Kyle Bonar <kylewb2011@gmail.com>
2018-12-30 22:13:48 -0600

    Seperating top titles, adjusting imports, managing styles

--- src/screens/sauce/screens/SauceSpotlight/SauceSpotlight.tsx
+++ src/screens/sauce/screens/SauceSpotlight/SauceSpotlight.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
 import { connect } from "react-redux";
+
 import { IinitialState } from "../../../../redux/configureStore";
 import TopBar from "../../../../components/TopBar/TopBar";
-
 import Article from "../../../../components/Article/Article";
 import PageTitle from "../../../../components/PageTitle/PageTitle";
 import Navigation from "../../../../components/Navigation/Navigation";
 import styled from "../../../../theme/styled-components";
 import Footer from "../../../../components/Footer/Footer";
+import Descriptor from "../../../../components/Descriptor/Descriptor";
 import SauceSpotlight from "./components/SauceSpotlight/SauceSpotlight";
 import SauceReviews from "./components/SauceReviews/SauceReviews";
-import Descriptor from "../../../../components/Descriptor/Descriptor";
+import SauceAuthor from "./components/SauceAuthor/SauceAuthor";
 
 const StyledArticle = styled(Article)`
   max-width: 1200px;
@@ -19,8 +20,22 @@ const StyledArticle = styled(Article)`
   align-items: flex-start;
 `;
 
+const StyledTitleContainer = styled.div`
+  max-width: 1200px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  margin: 0 auto;
+`;
+
 const StyledLeftContainer = styled.div`
   width: 66%;
+  box-sizing: border-box;
+  padding: 0 1em;
+`;
+
+const StyledRightContainer = styled.div`
+  width: 33%;
   box-sizing: border-box;
   padding: 0 1em;
 `;
@@ -35,21 +50,41 @@ const StyledDescriptor = styled(Descriptor)`
 export interface SauceSingleProps {}
 
 class SauceSingle extends React.Component<SauceSingleProps, any> {
+  public componentDidMount() {
+    window.scrollTo(0, 0); // Move screen to top
+  }
   public render() {
     return (
       <div>
         <TopBar />
         <Navigation />
-        <StyledArticle>
+
+        {/* Titles */}
+        <StyledTitleContainer>
           <StyledLeftContainer>
             <PageTitle>Chalulu hot sauce</PageTitle>
+          </StyledLeftContainer>
+
+          <StyledRightContainer>
+            <StyledDescriptor title="Author" />
+          </StyledRightContainer>
+        </StyledTitleContainer>
+
+        {/* Actual page content */}
+        <StyledArticle>
+          <StyledLeftContainer>
+            {/* Spotlight */}
             <SauceSpotlight id={"5"} />
 
+            {/* Reviews */}
             <StyledDescriptor title="Reviews">
               The opinions expressed are soley those of the author.
             </StyledDescriptor>
             <SauceReviews id={"5"} />
           </StyledLeftContainer>
+          <StyledRightContainer>
+            <SauceAuthor />
+          </StyledRightContainer>
         </StyledArticle>
         <Footer />
       </div>
