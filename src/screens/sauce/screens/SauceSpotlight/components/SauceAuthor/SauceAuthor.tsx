ba0f9583dde9b871567c128bf2cdcb7d986d1e1d
Kyle Bonar <kylewb2011@gmail.com>
2018-12-30 22:32:48 -0600

    Style updates, get author from redux

--- src/screens/sauce/screens/SauceSpotlight/components/SauceAuthor/SauceAuthor.tsx
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceAuthor/SauceAuthor.tsx
@@ -1,17 +1,41 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import { IinitialState } from "../../../../../../redux/configureStore";
+import styled from "../../../../../../theme/styled-components";
+import { IUser } from "../../../../../../redux/users/types";
 
-export interface SauceAuthorProps {}
+const StyledContainer = styled.div`
+  background-color: ${props => props.theme.formContainerBackgroundColor};
+  font-family: AvenirNextReg;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  margin-bottom: 1em;
+  max-height: auto;
+`;
+
+export interface SauceAuthorProps {
+  id: string; // this is sauce's id
+  author?: IUser; // Will have to find this through redux
+}
 
 class SauceAuthor extends React.Component<SauceAuthorProps, any> {
   public render() {
-    return <div>I'm an author!!</div>;
+    return <StyledContainer>I'm an author!!</StyledContainer>;
   }
 }
 
-const mapState2Props = (state: IinitialState) => {
-  return {};
+const mapState2Props = (state: IinitialState, ownProps: SauceAuthorProps) => {
+  // Want to check that we have sauce, the sauce for the page we are on, and users to find the author
+  const sauces = state.sauces.byId;
+  const users = state.users.byId;
+  if (!sauces || !sauces[ownProps.id] || !users) {
+    return {};
+  }
+
+  // Return the author of the sauce
+  const authorID = sauces[ownProps.id].author;
+  return { author: users[authorID] };
 };
 
 export default connect(mapState2Props)(SauceAuthor);
