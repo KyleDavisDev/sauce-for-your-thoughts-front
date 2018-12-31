9afbdebdf95fe7e5f0950eef9d41f2a0333c83c3
Kyle Bonar <kylewb2011@gmail.com>
2018-12-30 18:30:51 -0600

    style update, added taste, aroma, overall, note, label sections

--- src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/components/SauceReviewBlock/SauceReviewBlock.tsx
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/components/SauceReviewBlock/SauceReviewBlock.tsx
@@ -42,7 +42,7 @@ const StyledCategoryContainer = styled.div`
   padding: 0 1em;
 
   > i {
-    padding: 0 1em 0 0;
+    padding: 0 0.5em 0 0;
   }
 `;
 
@@ -103,19 +103,116 @@ class SauceReviewBlock extends React.Component<
           <i>Reviewer:</i> {this.props.author} on{" "}
           {new Date(review.created).toLocaleDateString("en-US", dateOptions)}
           <StyledReviewContainer>
+            {/* Aroma */}
             {review.aroma && (
               <div>
                 <StyledCategoryContainer>
-                  <i>Aroma: </i>{" "}
+                  <i>Aroma:</i>
                   <ReactRating
                     initialRating={review.aroma.rating}
                     readonly={true}
                     emptySymbol={<StyledEmptyStar height={20} />}
                     fullSymbol={<StyledFullStar height={20} />}
-                  />
+                  />{" "}
+                  <small>({review.aroma.rating.toString()})</small>
                 </StyledCategoryContainer>
                 <StyledCategoryDescription>
                   {review.aroma.txt}
+                </StyledCategoryDescription>
+              </div>
+            )}
+
+            {/* Taste */}
+            {review.taste && (
+              <div>
+                <StyledCategoryContainer>
+                  <i>Taste:</i>
+                  <ReactRating
+                    initialRating={review.taste.rating}
+                    readonly={true}
+                    emptySymbol={<StyledEmptyStar height={20} />}
+                    fullSymbol={<StyledFullStar height={20} />}
+                  />{" "}
+                  <small>({review.taste.rating.toString()})</small>
+                </StyledCategoryContainer>
+                <StyledCategoryDescription>
+                  {review.taste.txt}
+                </StyledCategoryDescription>
+              </div>
+            )}
+
+            {/* Label */}
+            {review.label && (
+              <div>
+                <StyledCategoryContainer>
+                  <i>Label:</i>
+                  <ReactRating
+                    initialRating={review.label.rating}
+                    readonly={true}
+                    emptySymbol={<StyledEmptyStar height={20} />}
+                    fullSymbol={<StyledFullStar height={20} />}
+                  />{" "}
+                  <small>({review.label.rating.toString()})</small>
+                </StyledCategoryContainer>
+                <StyledCategoryDescription>
+                  {review.label.txt}
+                </StyledCategoryDescription>
+              </div>
+            )}
+
+            {/* Heat */}
+            {review.heat && (
+              <div>
+                <StyledCategoryContainer>
+                  <i>Heat:</i>
+                  <ReactRating
+                    initialRating={review.heat.rating}
+                    readonly={true}
+                    emptySymbol={<StyledEmptyStar height={20} />}
+                    fullSymbol={<StyledFullStar height={20} />}
+                  />{" "}
+                  <small>({review.heat.rating.toString()})</small>
+                </StyledCategoryContainer>
+                <StyledCategoryDescription>
+                  {review.heat.txt}
+                </StyledCategoryDescription>
+              </div>
+            )}
+
+            {/* Note */}
+            {review.note && (
+              <div>
+                <StyledCategoryContainer>
+                  <i>Note:</i>
+                  <ReactRating
+                    initialRating={review.note.rating}
+                    readonly={true}
+                    emptySymbol={<StyledEmptyStar height={20} />}
+                    fullSymbol={<StyledFullStar height={20} />}
+                  />{" "}
+                  <small>({review.note.rating.toString()})</small>
+                </StyledCategoryContainer>
+                <StyledCategoryDescription>
+                  {review.note.txt}
+                </StyledCategoryDescription>
+              </div>
+            )}
+
+            {/* Overall */}
+            {review.overall && (
+              <div>
+                <StyledCategoryContainer>
+                  <i>Overall:</i>
+                  <ReactRating
+                    initialRating={review.overall.rating}
+                    readonly={true}
+                    emptySymbol={<StyledEmptyStar height={20} />}
+                    fullSymbol={<StyledFullStar height={20} />}
+                  />{" "}
+                  <small>({review.overall.rating.toString()})</small>
+                </StyledCategoryContainer>
+                <StyledCategoryDescription>
+                  {review.overall.txt}
                 </StyledCategoryDescription>
               </div>
             )}
