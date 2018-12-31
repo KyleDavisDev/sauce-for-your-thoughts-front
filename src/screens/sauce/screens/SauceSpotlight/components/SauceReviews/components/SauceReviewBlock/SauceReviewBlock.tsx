8fd651dbc738108bfdb3de77ff67ce65d6c6d662
Kyle Bonar <kylewb2011@gmail.com>
2018-12-30 18:17:38 -0600

    Style updates, using react-rating, formatting date, adding aroma section

--- src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/components/SauceReviewBlock/SauceReviewBlock.tsx
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/components/SauceReviewBlock/SauceReviewBlock.tsx
@@ -1,15 +1,17 @@
 import * as React from "react";
 import { connect } from "react-redux";
+import ReactRating from "react-rating";
 import { IReview } from "../../../../../../../../redux/reviews/types";
 import styled from "styled-components";
 import { IinitialState } from "Users/kylebonar/Documents/projects/sites/now-thats-delicious-front/src/redux/configureStore";
+import Star from "../../../../../../../../images/icons/Star";
 
 const StyledContainer = styled.div`
   background-color: ${props => props.theme.formContainerBackgroundColor};
   font-family: AvenirNextReg;
   display: flex;
   flex-direction: row;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
   margin-bottom: 1em;
 `;
 
@@ -26,6 +28,44 @@ const StyledToggleContainer = styled.div`
 
 const StyledContentContainer = styled.div`
   padding: 0.75em;
+`;
+
+const StyledReviewContainer = styled.div`
+  margin: 0.5em;
+`;
+
+const StyledCategoryContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: nowrap;
+  padding: 0 1em;
+
+  > i {
+    padding: 0 1em 0 0;
+  }
+`;
+
+const StyledEmptyStar = styled(Star)`
+  .border {
+    fill: ${props => props.theme.primaryThemeColor};
+  }
+
+  .center {
+    fill: transparent;
+  }
+`;
+
+const StyledCategoryDescription = styled.p`
+  margin-top: 0px;
+  padding: 0 2em;
+`;
+
+const StyledFullStar = styled(Star)`
+  .border,
+  .center {
+    fill: ${props => props.theme.primaryThemeColor};
+  }
 `;
 
 export interface SauceReviewBlockProps {
@@ -48,13 +88,38 @@ class SauceReviewBlock extends React.Component<
   }
 
   public render() {
+    const dateOptions = {
+      weekday: "long",
+      year: "numeric",
+      month: "long"
+    };
+    const review: IReview = this.props.review;
     return (
       <StyledContainer>
         <StyledToggleContainer>
           {this.state.isOpen ? "+" : "-"}
         </StyledToggleContainer>
         <StyledContentContainer>
-          <i>Reviewer:</i> {this.props.author}
+          <i>Reviewer:</i> {this.props.author} on{" "}
+          {new Date(review.created).toLocaleDateString("en-US", dateOptions)}
+          <StyledReviewContainer>
+            {review.aroma && (
+              <div>
+                <StyledCategoryContainer>
+                  <i>Aroma: </i>{" "}
+                  <ReactRating
+                    initialRating={review.aroma.rating}
+                    readonly={true}
+                    emptySymbol={<StyledEmptyStar height={20} />}
+                    fullSymbol={<StyledFullStar height={20} />}
+                  />
+                </StyledCategoryContainer>
+                <StyledCategoryDescription>
+                  {review.aroma.txt}
+                </StyledCategoryDescription>
+              </div>
+            )}
+          </StyledReviewContainer>
         </StyledContentContainer>
       </StyledContainer>
     );
