d2b27c0cb74ffe80a4d4e8b9f0384b656dfa9a86
Kyle Bonar <kylewb2011@gmail.com>
2018-12-30 18:55:14 -0600

    Using Button component, style updates, init toggle method

--- src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/components/SauceReviewBlock/SauceReviewBlock.tsx
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/components/SauceReviewBlock/SauceReviewBlock.tsx
@@ -3,8 +3,9 @@ import { connect } from "react-redux";
 import ReactRating from "react-rating";
 import { IReview } from "../../../../../../../../redux/reviews/types";
 import styled from "styled-components";
-import { IinitialState } from "Users/kylebonar/Documents/projects/sites/now-thats-delicious-front/src/redux/configureStore";
+import { IinitialState } from "../../../../../../../../redux/configureStore";
 import Star from "../../../../../../../../images/icons/Star";
+import { Button } from "../../../../../../../../components/Button/Button";
 
 const StyledContainer = styled.div`
   background-color: ${props => props.theme.formContainerBackgroundColor};
@@ -13,11 +14,28 @@ const StyledContainer = styled.div`
   flex-direction: row;
   flex-wrap: nowrap;
   margin-bottom: 1em;
+
+  max-height: auto;
 `;
 
-const StyledToggleContainer = styled.div`
-  background-color: blue;
-  padding: 0.75em;
+const StyledButton = styled(Button)`
+  background-color: ${props => props.theme.lightGrey};
+  padding: 0.25em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5em;
+
+  > button {
+    background-color: inherit;
+    transition: none;
+    padding: 0;
+    margin: 0;
+
+    &:hover {
+      background-color: inherit;
+    }
+  }
 
   &:hover,
   &:focus {
@@ -27,11 +45,7 @@ const StyledToggleContainer = styled.div`
 `;
 
 const StyledContentContainer = styled.div`
-  padding: 0.75em;
-`;
-
-const StyledReviewContainer = styled.div`
-  margin: 0.5em;
+  padding: 1em;
 `;
 
 const StyledCategoryContainer = styled.div`
@@ -46,6 +60,11 @@ const StyledCategoryContainer = styled.div`
   }
 `;
 
+const StyledCategoryDescription = styled.p`
+  margin-top: 0px;
+  padding: 0 2em;
+`;
+
 const StyledEmptyStar = styled(Star)`
   .border {
     fill: ${props => props.theme.primaryThemeColor};
@@ -54,11 +73,6 @@ const StyledEmptyStar = styled(Star)`
   .center {
     fill: transparent;
   }
-`;
-
-const StyledCategoryDescription = styled.p`
-  margin-top: 0px;
-  padding: 0 2em;
 `;
 
 const StyledFullStar = styled(Star)`
@@ -96,131 +110,141 @@ class SauceReviewBlock extends React.Component<
     const review: IReview = this.props.review;
     return (
       <StyledContainer>
-        <StyledToggleContainer>
+        <StyledButton onClick={this.onToggleClick}>
           {this.state.isOpen ? "+" : "-"}
-        </StyledToggleContainer>
-        <StyledContentContainer>
-          <i>Reviewer:</i> {this.props.author} on{" "}
-          {new Date(review.created).toLocaleDateString("en-US", dateOptions)}
-          <StyledReviewContainer>
-            {/* Aroma */}
-            {review.aroma && (
-              <div>
-                <StyledCategoryContainer>
-                  <i>Aroma:</i>
-                  <ReactRating
-                    initialRating={review.aroma.rating}
-                    readonly={true}
-                    emptySymbol={<StyledEmptyStar height={20} />}
-                    fullSymbol={<StyledFullStar height={20} />}
-                  />{" "}
-                  <small>({review.aroma.rating.toString()})</small>
-                </StyledCategoryContainer>
-                <StyledCategoryDescription>
-                  {review.aroma.txt}
-                </StyledCategoryDescription>
-              </div>
-            )}
+        </StyledButton>
 
-            {/* Taste */}
-            {review.taste && (
-              <div>
-                <StyledCategoryContainer>
-                  <i>Taste:</i>
-                  <ReactRating
-                    initialRating={review.taste.rating}
-                    readonly={true}
-                    emptySymbol={<StyledEmptyStar height={20} />}
-                    fullSymbol={<StyledFullStar height={20} />}
-                  />{" "}
-                  <small>({review.taste.rating.toString()})</small>
-                </StyledCategoryContainer>
-                <StyledCategoryDescription>
-                  {review.taste.txt}
-                </StyledCategoryDescription>
-              </div>
-            )}
+        {/* Show content if state allows it otherwise show empty */}
+        {this.state.isOpen ? (
+          <StyledContentContainer>
+            <i>Reviewer:</i> {this.props.author} on{" "}
+            {new Date(review.created).toLocaleDateString("en-US", dateOptions)}
+            <div>
+              {/* Aroma */}
+              {review.aroma && (
+                <div>
+                  <StyledCategoryContainer>
+                    <i>Aroma:</i>
+                    <ReactRating
+                      initialRating={review.aroma.rating}
+                      readonly={true}
+                      emptySymbol={<StyledEmptyStar height={20} />}
+                      fullSymbol={<StyledFullStar height={20} />}
+                    />{" "}
+                    <small>({review.aroma.rating.toString()})</small>
+                  </StyledCategoryContainer>
+                  <StyledCategoryDescription>
+                    {review.aroma.txt}
+                  </StyledCategoryDescription>
+                </div>
+              )}
 
-            {/* Label */}
-            {review.label && (
-              <div>
-                <StyledCategoryContainer>
-                  <i>Label:</i>
-                  <ReactRating
-                    initialRating={review.label.rating}
-                    readonly={true}
-                    emptySymbol={<StyledEmptyStar height={20} />}
-                    fullSymbol={<StyledFullStar height={20} />}
-                  />{" "}
-                  <small>({review.label.rating.toString()})</small>
-                </StyledCategoryContainer>
-                <StyledCategoryDescription>
-                  {review.label.txt}
-                </StyledCategoryDescription>
-              </div>
-            )}
+              {/* Taste */}
+              {review.taste && (
+                <div>
+                  <StyledCategoryContainer>
+                    <i>Taste:</i>
+                    <ReactRating
+                      initialRating={review.taste.rating}
+                      readonly={true}
+                      emptySymbol={<StyledEmptyStar height={20} />}
+                      fullSymbol={<StyledFullStar height={20} />}
+                    />{" "}
+                    <small>({review.taste.rating.toString()})</small>
+                  </StyledCategoryContainer>
+                  <StyledCategoryDescription>
+                    {review.taste.txt}
+                  </StyledCategoryDescription>
+                </div>
+              )}
 
-            {/* Heat */}
-            {review.heat && (
-              <div>
-                <StyledCategoryContainer>
-                  <i>Heat:</i>
-                  <ReactRating
-                    initialRating={review.heat.rating}
-                    readonly={true}
-                    emptySymbol={<StyledEmptyStar height={20} />}
-                    fullSymbol={<StyledFullStar height={20} />}
-                  />{" "}
-                  <small>({review.heat.rating.toString()})</small>
-                </StyledCategoryContainer>
-                <StyledCategoryDescription>
-                  {review.heat.txt}
-                </StyledCategoryDescription>
-              </div>
-            )}
+              {/* Label */}
+              {review.label && (
+                <div>
+                  <StyledCategoryContainer>
+                    <i>Label:</i>
+                    <ReactRating
+                      initialRating={review.label.rating}
+                      readonly={true}
+                      emptySymbol={<StyledEmptyStar height={20} />}
+                      fullSymbol={<StyledFullStar height={20} />}
+                    />{" "}
+                    <small>({review.label.rating.toString()})</small>
+                  </StyledCategoryContainer>
+                  <StyledCategoryDescription>
+                    {review.label.txt}
+                  </StyledCategoryDescription>
+                </div>
+              )}
 
-            {/* Note */}
-            {review.note && (
-              <div>
-                <StyledCategoryContainer>
-                  <i>Note:</i>
-                  <ReactRating
-                    initialRating={review.note.rating}
-                    readonly={true}
-                    emptySymbol={<StyledEmptyStar height={20} />}
-                    fullSymbol={<StyledFullStar height={20} />}
-                  />{" "}
-                  <small>({review.note.rating.toString()})</small>
-                </StyledCategoryContainer>
-                <StyledCategoryDescription>
-                  {review.note.txt}
-                </StyledCategoryDescription>
-              </div>
-            )}
+              {/* Heat */}
+              {review.heat && (
+                <div>
+                  <StyledCategoryContainer>
+                    <i>Heat:</i>
+                    <ReactRating
+                      initialRating={review.heat.rating}
+                      readonly={true}
+                      emptySymbol={<StyledEmptyStar height={20} />}
+                      fullSymbol={<StyledFullStar height={20} />}
+                    />{" "}
+                    <small>({review.heat.rating.toString()})</small>
+                  </StyledCategoryContainer>
+                  <StyledCategoryDescription>
+                    {review.heat.txt}
+                  </StyledCategoryDescription>
+                </div>
+              )}
 
-            {/* Overall */}
-            {review.overall && (
-              <div>
-                <StyledCategoryContainer>
-                  <i>Overall:</i>
-                  <ReactRating
-                    initialRating={review.overall.rating}
-                    readonly={true}
-                    emptySymbol={<StyledEmptyStar height={20} />}
-                    fullSymbol={<StyledFullStar height={20} />}
-                  />{" "}
-                  <small>({review.overall.rating.toString()})</small>
-                </StyledCategoryContainer>
-                <StyledCategoryDescription>
-                  {review.overall.txt}
-                </StyledCategoryDescription>
-              </div>
-            )}
-          </StyledReviewContainer>
-        </StyledContentContainer>
+              {/* Note */}
+              {review.note && (
+                <div>
+                  <StyledCategoryContainer>
+                    <i>Note:</i>
+                    <ReactRating
+                      initialRating={review.note.rating}
+                      readonly={true}
+                      emptySymbol={<StyledEmptyStar height={20} />}
+                      fullSymbol={<StyledFullStar height={20} />}
+                    />{" "}
+                    <small>({review.note.rating.toString()})</small>
+                  </StyledCategoryContainer>
+                  <StyledCategoryDescription>
+                    {review.note.txt}
+                  </StyledCategoryDescription>
+                </div>
+              )}
+
+              {/* Overall */}
+              {review.overall && (
+                <div>
+                  <StyledCategoryContainer>
+                    <i>Overall:</i>
+                    <ReactRating
+                      initialRating={review.overall.rating}
+                      readonly={true}
+                      emptySymbol={<StyledEmptyStar height={20} />}
+                      fullSymbol={<StyledFullStar height={20} />}
+                    />{" "}
+                    <small>({review.overall.rating.toString()})</small>
+                  </StyledCategoryContainer>
+                  <StyledCategoryDescription>
+                    {review.overall.txt}
+                  </StyledCategoryDescription>
+                </div>
+              )}
+            </div>
+          </StyledContentContainer>
+        ) : (
+          <StyledContentContainer />
+        )}
       </StyledContainer>
     );
   }
+
+  public onToggleClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    console.log("I was clicked!");
+  };
 }
 
 const mapState2Props = (
