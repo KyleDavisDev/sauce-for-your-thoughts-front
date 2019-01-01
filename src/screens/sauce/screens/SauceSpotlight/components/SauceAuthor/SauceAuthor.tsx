08b7fed878d008d71e278c0a018d21db75b01bb7
Kyle Bonar <kylewb2011@gmail.com>
2019-01-01 12:28:38 -0600

    extended dummy data, style updates, added member since and # of reviews

--- src/screens/sauce/screens/SauceSpotlight/components/SauceAuthor/SauceAuthor.tsx
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceAuthor/SauceAuthor.tsx
@@ -16,6 +16,10 @@ const StyledContentContainer = styled.div`
   justify-content: space-between;
 `;
 
+const StyledHR = styled.hr`
+  border-top: 1px ${props => props.theme.grey};
+`;
+
 export interface SauceAuthorProps {
   id: string; // this is sauce's id
   author?: IUser; // Will have to find this through redux
@@ -23,15 +27,35 @@ export interface SauceAuthorProps {
 
 class SauceAuthor extends React.Component<SauceAuthorProps, any> {
   public render() {
+    const dateOptions = {
+      weekday: "long",
+      year: "numeric",
+      month: "long"
+    };
     return (
-      <StyledContainer>
+      <div>
         {this.props.author && (
-          <StyledContentContainer>
-            <i>Author:</i>
-            <span>{this.props.author.name}</span>
-          </StyledContentContainer>
+          <StyledContainer>
+            <StyledContentContainer>
+              <i>Author:</i> <span>{this.props.author.name}</span>
+            </StyledContentContainer>
+            <StyledHR />
+            <StyledContentContainer>
+              <i>Reviews:</i> <span>{this.props.author.reviews.length}</span>
+            </StyledContentContainer>
+            <StyledHR />
+            <StyledContentContainer>
+              <i>Member Since:</i>{" "}
+              <span>
+                {this.props.author.created.toLocaleDateString(
+                  "en-US",
+                  dateOptions
+                )}
+              </span>
+            </StyledContentContainer>
+          </StyledContainer>
         )}
-      </StyledContainer>
+      </div>
     );
   }
 }
@@ -42,7 +66,13 @@ const mapState2Props = (state: IinitialState, ownProps: SauceAuthorProps) => {
   const users = state.users.byId;
   if (!sauces || !sauces[ownProps.id] || !users) {
     // Return test data
-    return { author: { name: "Testy Davox", reviews: 8 } };
+    return {
+      author: {
+        name: "Testy Davox",
+        reviews: ["string1", "string2", "3"],
+        created: new Date("2017-03-13T14:12:08.255Z")
+      }
+    };
   }
 
   // Return the author of the sauce
