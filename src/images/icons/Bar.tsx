b6542cc700a12cb4ad64b6ca5b08a7169e35e743
Kyle Bonar <kylewb2011@gmail.com>
2019-01-03 10:45:20 -0600

    Init commit

---
+++ src/images/icons/Bar.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+const Bar: React.SFC<{}> = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="-5 -7 24 24"
+      width="24"
+      height="24"
+      preserveAspectRatio="xMinYMin"
+    >
+      <path d="M1 0h12a1 1 0 0 1 0 2H1a1 1 0 1 1 0-2zm0 8h12a1 1 0 0 1 0 2H1a1 1 0 1 1 0-2zm0-4h12a1 1 0 0 1 0 2H1a1 1 0 1 1 0-2z" />
+    </svg>
+  );
+};
+
+export default Bar;
