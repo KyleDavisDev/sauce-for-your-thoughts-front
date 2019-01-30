a3fead40dfe4f911fa6094c662c4f81d14e784af
Kyle Bonar <kylewb2011@gmail.com>
2019-01-29 22:36:06 -0600

    Using types

--- src/redux/flashMessage/actions.ts
+++ src/redux/flashMessage/actions.ts
@@ -1,45 +1,27 @@
-import { IAction } from "./types";
+import { IAction, IParam, FlashMessageActionTypes } from "./types";
 
-export const flashSuccess = ({
-  text,
-  slug = null
-}: {
-  text: string;
-  slug: null | string;
-}): IAction => ({
-  type: "SUCCESS_FLASH",
+export const flashSuccess = ({ text, slug = null }: IParam): IAction => ({
+  type: FlashMessageActionTypes.SUCCESS_FLASH,
   text,
   slug
 });
 
-export const flashError = ({
-  text,
-  slug = null
-}: {
-  text: string;
-  slug: null | string;
-}): IAction => ({
-  type: "ERROR_FLASH",
+export const flashError = ({ text, slug = null }: IParam): IAction => ({
+  type: FlashMessageActionTypes.ERROR_FLASH,
   text,
   slug
 });
 
-export const flashWarning = ({
-  text,
-  slug = null
-}: {
-  text: string;
-  slug: null | string;
-}): IAction => ({
-  type: "WARNING_FLASH",
+export const flashWarning = ({ text, slug = null }: IParam): IAction => ({
+  type: FlashMessageActionTypes.WARNING_FLASH,
   text,
   slug
 });
 
 export const flashToggle = (): IAction => ({
-  type: "TOGGLE_FLASH"
+  type: FlashMessageActionTypes.TOGGLE_FLASH
 });
 
 export const flashClose = (): IAction => ({
-  type: "CLOSE_FLASH"
+  type: FlashMessageActionTypes.CLOSE_FLASH
 });
