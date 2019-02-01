0e56c9da6bf8d5f7c19a485546109d0b0104bf68
Kyle Bonar <kylewb2011@gmail.com>
2019-01-31 23:18:30 -0600

    types for redux action and state

--- src/redux/users/types.ts
+++ src/redux/users/types.ts
@@ -12,6 +12,19 @@ export interface IUserRef {
   _id: string;
 }
 
+// Used for redux reducer
+export interface IUserAction {
+  type: string;
+  token?: string;
+}
+
+// Used for redux state
+export interface IUserState {
+  self: { token?: string };
+  byId?: { [key: string]: IUser };
+  allIds?: string[];
+}
+
 // Register user
 export interface IRegisterUser {
   user: {
@@ -21,4 +34,13 @@ export interface IRegisterUser {
     confirmPassword: string;
     displayName: string;
   };
+}
+
+// Use const enums for better autocompletion of action type names. These will
+// be compiled away leaving only the final value in your compiled code.
+export const enum UsersActionTypes {
+  USER_LOGGED_IN = "@@users/USER_LOGGED_IN",
+  USER_GOT_INFO = "@@users/USER_GOT_INFO",
+  USER_UPDATED = "@@users/USER_UPDATED",
+  USER_LOGGED_OUT = "@@users/USER_LOGGED_OUT"
 }
