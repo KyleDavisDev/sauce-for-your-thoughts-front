6dd5f9cf07b61f330d5e915d972f742213921f5f
Kyle Bonar <kylewb2011@gmail.com>
2019-02-03 23:43:58 -0600

    Init commit

---
+++ src/components/TopBar/components/Body/Body.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+interface BodyProps {}
+
+const Body: React.SFC<BodyProps> = props => {
+  return <div>Something</div>;
+};
+Body.displayName = "Body";
+
+export default Body;
