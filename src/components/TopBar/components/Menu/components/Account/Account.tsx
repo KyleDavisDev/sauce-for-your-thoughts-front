fdbc542a175a00342c9e3b03c4f9b5404cc392d7
Kyle Bonar <kylewb2011@gmail.com>
2019-02-08 15:31:32 -0600

    Will be pure component

---
+++ src/components/TopBar/components/Menu/components/Account/Account.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import styled from "styled-components";
+
+export interface AccountProps {}
+
+class Account extends React.PureComponent<AccountProps, any> {
+  public render() {
+    return <div>here is some stuff!</div>;
+  }
+}
+
+export default Account;
