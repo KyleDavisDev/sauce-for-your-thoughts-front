6f1eaa27ad2acd98cc563196c6a68f2e9d6543eb
Kyle Bonar <kylewb2011@gmail.com>
2019-02-09 17:07:22 -0600

    Added more items, removed styled

--- src/components/TopBar/components/Menu/components/Account/Account.tsx
+++ src/components/TopBar/components/Menu/components/Account/Account.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import styled from "styled-components";
 import Title from "../Title/Title";
+import Item from "../Item/Item";
 
 export interface AccountProps {}
 
@@ -9,6 +9,9 @@ class Account extends React.PureComponent<AccountProps, any> {
     return (
       <div>
         <Title>Account</Title>
+        <Item to="#settings">Settings</Item>
+        <Item to="#settings">Uploads</Item>
+        <Item to="#settings">Uploads</Item>
       </div>
     );
   }
