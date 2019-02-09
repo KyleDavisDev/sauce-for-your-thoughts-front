2934d59e0d9100fffa9285dfddb6fd18b450fa66
Kyle Bonar <kylewb2011@gmail.com>
2019-02-09 17:07:28 -0600

    Init commit

---
+++ src/components/TopBar/components/Menu/components/Item/Item.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { Link } from "../../../../../Link/Link";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: inline-block;
+  width: 100%;
+
+  a {
+    padding: 8px 12px;
+    width: 100%;
+    display: block;
+    color: ${x => x.theme.primaryThemeColor};
+    &:hover,
+    &:focus {
+      background-color: ${x => x.theme.primaryLightThemeColor};
+      color: ${x => x.theme.primaryThemeColor};
+    }
+  }
+`;
+
+interface ItemProps {
+  to: string;
+  children: string;
+}
+
+const Item: React.SFC<ItemProps> = props => {
+  return (
+    <StyledDiv>
+      <Link to={props.to}>{props.children}</Link>
+    </StyledDiv>
+  );
+};
+
+export default Item;
