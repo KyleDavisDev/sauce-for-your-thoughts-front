5c7d74c1337e51c59d41f83b38f05a35fa6deb2e
Kyle Bonar <kylewb2011@gmail.com>
2019-02-09 16:42:27 -0600

    Render title component

--- src/components/TopBar/components/Menu/components/Account/Account.tsx
+++ src/components/TopBar/components/Menu/components/Account/Account.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import styled from "styled-components";
+import Title from "../Title/Title";
 
 export interface AccountProps {}
 
 class Account extends React.PureComponent<AccountProps, any> {
   public render() {
-    return <div>here is some stuff!</div>;
+    return (
+      <div>
+        <Title>Account</Title>
+      </div>
+    );
   }
 }
 
