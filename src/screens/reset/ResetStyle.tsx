6fe6000b4e7318e47245278d57afd3a35178b077
Kyle Bonar <kylewb2011@gmail.com>
2019-02-10 14:22:49 -0600

    Init commit

---
+++ src/screens/reset/ResetStyle.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import Article from "../../components/Article/Article";
+import { Button } from "../../components/Button/Button";
+
+export const StyledDiv = styled.div`
+  height: 100vh;
+`;
+
+export const StyledLogoContainer = styled.div`
+  max-width: 150px;
+  margin: 0 auto;
+  padding: 1em;
+`;
+
+export const StyledArticle = styled(Article)`
+  max-width: 600px;
+`;
+export const StyledFormContainer = styled.div`
+  background-color: ${props => props.theme.formContainerBackgroundColor};
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+export const StyledText = styled.p`
+  margin: 0 auto 1em;
+`;
+
+export const StyledButton = styled(Button)`
+  text-align: center;
+`;
