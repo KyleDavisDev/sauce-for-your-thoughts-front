0d2753c5997d1f281d9bb4105c3295de8e927cc0
Kyle Bonar <kylewb2011@gmail.com>
2019-02-17 10:54:24 -0600

    removed unused code

--- src/redux/users/actions.ts
+++ src/redux/users/actions.ts
@@ -25,31 +25,6 @@ export const userLoggedIn = ({
 //   name
 // });
 
-/** @description Grab all sauces available (will limit this to only set amount at a time in future)
- *  @param {Object} user - API expects a specific format
- *    @param {String} user.token - JWT string
- *  @param {function} dispatch - dispatch action
- *  @return {NULL}
- */
-// export const getHearts = credentials => dispatch =>
-//   api.user.getHearts(credentials).then(res => {
-//     const hearts = res.data.hearts.map(heart => heart._id);
-//     dispatch(gotHearts({ hearts }));
-//   });
-
-/** @description add/remove sauce from user's array of hearted sauces
- *  @param {Object} data - API expects specific format
- *    @param {Object} user - user object
- *      @param {String} token - JWT string
- *    @param {Object} sauce - sauce object
- *      @param {String} _id - sauce identifier
- *  @returns {NULL}
- */
-// export const toggleHeart = data => dispatch =>
-//   api.user.toggleSauce(data).then(res => {
-//     dispatch(toggledHeart({ sauce: res.data.sauce }));
-//   });
-
 /** @description update a specific user's DB info
  *  @param {Object} credentials - data container
  *    @param {Object} credentials.user - user container
@@ -69,20 +44,6 @@ export const userLoggedIn = ({
 //     const text = `Your name was saved as: ${name} and your email was saved as: ${email}.`;
 //     dispatch(flashSuccess({ text }));
 //     return res.user;
-//   });
-
-/** @description gets basic information on a specifc user
- *  @param {Object} credentials - container
- *  @param {Object} credentials.user - user information container
- *    @param {String} credentials.user.token - unique user identifier
- *  @fires users#gotUserInfo - set user.self properties
- *  @returns {Promise}
- *    @returns {NULL}
- */
-// export const getInfo = credentials => dispatch =>
-//   api.user.getInfo(credentials).then(res => {
-//     const { _id, email, name } = res.data.user;
-//     dispatch(gotUserInfo({ _id, email, name }));
 //   });
 
 /** @description pass credentials to server to register user
