f4d947804999cb2bb1c3e58f418ae85d84bc3227
Kyle Bonar <kylewb2011@gmail.com>
2019-02-17 16:13:22 -0600

    Seperated styles to own file

--- src/screens/home/components/landingImage/LandingImage.tsx
+++ src/screens/home/components/landingImage/LandingImage.tsx
@@ -1,78 +1,16 @@
 import * as React from "react";
 
-import styled from "../../../../theme/styled-components";
-import DropDown from "../../../../components/DropDown/DropDown";
-import TextInput from "../../../../components/TextInput/TextInput";
-import { Link } from "../../../../components/Link/Link";
 import { Button } from "../../../../components/Button/Button";
-
-const HeroContainer = styled.div`
-  background: #000;
-  position: relative;
-  margin-bottom: 1em;
-`;
-
-const HeroImage = styled.div`
-  background-image: url("../../../../images/photos/Sauces.jpg");
-  background-size: cover;
-  background-position-y: 45%;
-  opacity: 0.45;
-  height: 400px;
-`;
-
-const HeroBody = styled.div`
-  position: absolute;
-  z-index: 1;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  top: 0;
-  padding: 0 2em;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  justify-content: center;
-`;
-
-const HeroTitle = styled.h1`
-  color: ${x => x.theme.landingHeroTextColor};
-  font-family: FuturaMedium;
-`;
-
-const StyledDiv = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: stretch;
-`;
-
-const StyledDropDown = styled(DropDown)`
-  min-height: 30px;
-`;
-
-const StyledInput = styled(TextInput)`
-  flex-direction: row;
-  align-items: stretch;
-
-  input {
-    border: 0px;
-    margin-top: 0px;
-    margin-bottom: 0px;
-    width: 100%;
-    min-width: 20em;
-    max-width: 100%;
-    padding: 0.66em;
-    box-sizing: border-box;
-    border: 0px;
-    font-size: 1em;
-  }
-`;
-
-const StyledLink = styled(Link)`
-  display: flex;
-  div {
-    display: flex;
-  }
-`;
+import {
+  HeroContainer,
+  HeroImage,
+  HeroBody,
+  HeroTitle,
+  StyledDiv,
+  StyledDropDown,
+  StyledInput,
+  StyledLink
+} from "./LandingImageStyle";
 
 export interface LandingImageProps {
   className?: string;
---
+++ src/screens/home/components/landingImage/LandingImageStyle.tsx
@@ -0,0 +1,72 @@
+import styled from "../../../../theme/styled-components";
+import DropDown from "../../../../components/DropDown/DropDown";
+import TextInput from "../../../../components/TextInput/TextInput";
+import { Link } from "../../../../components/Link/Link";
+
+export const HeroContainer = styled.div`
+  background: #000;
+  position: relative;
+  margin-bottom: 1em;
+`;
+
+export const HeroImage = styled.div`
+  background-image: url("../../../../images/photos/Sauces.jpg");
+  background-size: cover;
+  background-position-y: 45%;
+  opacity: 0.45;
+  height: 400px;
+`;
+
+export const HeroBody = styled.div`
+  position: absolute;
+  z-index: 1;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  padding: 0 2em;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+`;
+
+export const HeroTitle = styled.h1`
+  color: ${x => x.theme.landingHeroTextColor};
+  font-family: FuturaMedium;
+`;
+
+export const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
+`;
+
+export const StyledDropDown = styled(DropDown)`
+  min-height: 30px;
+`;
+
+export const StyledInput = styled(TextInput)`
+  flex-direction: row;
+  align-items: stretch;
+
+  input {
+    border: 0px;
+    margin-top: 0px;
+    margin-bottom: 0px;
+    width: 100%;
+    min-width: 20em;
+    max-width: 100%;
+    padding: 0.66em;
+    box-sizing: border-box;
+    border: 0px;
+    font-size: 1em;
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  display: flex;
+  div {
+    display: flex;
+  }
+`;
