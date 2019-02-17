8e188385e0c309ef751c066955d068b0d5fd5fab
Kyle Bonar <kylewb2011@gmail.com>
2019-02-17 11:21:40 -0600

    handle logout action, removed unused code

--- src/redux/users/reducer.ts
+++ src/redux/users/reducer.ts
@@ -13,24 +13,20 @@ const userReducer: Reducer<IUserState> = (
 ): IUserState => {
   switch (action.type) {
     case UsersActionTypes.USER_LOGGED_IN:
+      // Set user.self info
       return {
         ...state,
         self: { token: action.token, displayName: action.displayName }
       };
-    // case UsersActionTypes.USER_GOT_INFO:
-    //   return {
-    //     ...state,
-    //     email: action.email,
-    //     name: action.name
-    //   };
+    case UsersActionTypes.USER_LOGGED_OUT:
+      // remove all user.self stuff
+      return { ...state, self: {} };
     // case UsersActionTypes.USER_UPDATED:
     //   return {
     //     ...state,
     //     email: action.email,
     //     name: action.name
     //   };
-    // case UsersActionTypes.USER_LOGGED_OUT:
-    //   return {};
 
     default:
       return state;
