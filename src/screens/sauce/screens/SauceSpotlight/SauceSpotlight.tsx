5d5d3b8f33965970ee8712161c58dffb6788f2ed
Kyle Bonar <kylewb2011@gmail.com>
2019-03-12 09:46:55 -0500

    Grab slug from URL, render dummy data

--- src/screens/sauce/screens/SauceSpotlight/SauceSpotlight.tsx
+++ src/screens/sauce/screens/SauceSpotlight/SauceSpotlight.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { connect } from "react-redux";
+import queryString, { OutputParams } from "query-string";
 
 import { IinitialState } from "../../../../redux/configureStore";
 import TopBar from "../../../../components/TopBar/TopBar";
@@ -16,13 +17,31 @@ import {
   StyledDescriptor,
   StyledH2
 } from "./SauceSpotlightStyle";
+import { dummyData } from "./dummyData";
 
-export interface SauceSingleProps {}
+export interface SauceSingleProps {
+  location: { search: string };
+  history: { push: (location: string) => any };
+}
 
 class SauceSingle extends React.Component<SauceSingleProps, any> {
+  constructor(props: SauceSingleProps) {
+    super(props);
+  }
   public componentDidMount() {
+    // Get slug from URL
+    const slug: string | null = this.getPageFromPath(
+      this.props.location.search // this value is what react-router assings to '?s=....'
+    );
+
+    // Sauce slug is whack, redirect user
+    if (slug === null) {
+      this.props.history.push("/");
+      // Maybe display banner too?
+    }
     window.scrollTo(0, 0); // Move screen to top
   }
+
   public render() {
     return (
       <div>
@@ -39,7 +58,7 @@ class SauceSingle extends React.Component<SauceSingleProps, any> {
             <StyledDescriptor title="Reviews">
               The opinions expressed are soley those of the author.
             </StyledDescriptor>
-            <SauceReviews id={"5"} />
+            <SauceReviews slug={"5"} reviews={dummyData} />
           </StyledLeftContainer>
           <StyledRightContainer>
             <StyledH2>Author</StyledH2>
@@ -49,6 +68,18 @@ class SauceSingle extends React.Component<SauceSingleProps, any> {
         <Footer />
       </div>
     );
+  }
+
+  private getPageFromPath(path: string): string | null {
+    // Get s from string
+    const values: OutputParams = queryString.parse(path);
+
+    // Make sure s is defined, not an array
+    if (!values.s || Array.isArray(values.s)) {
+      return null;
+    }
+
+    return values.s;
   }
 }
 
