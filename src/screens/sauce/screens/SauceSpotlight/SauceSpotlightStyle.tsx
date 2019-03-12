a7e9be267d9a0c3bcac505be823850e2be7d9a6e
Kyle Bonar <kylewb2011@gmail.com>
2019-03-12 09:27:31 -0500

    Move styles to seperate file

---
+++ src/screens/sauce/screens/SauceSpotlight/SauceSpotlightStyle.tsx
@@ -0,0 +1,47 @@
+import Article from "../../../../components/Article/Article";
+import styled from "../../../../theme/styled-components";
+import Descriptor from "../../../../components/Descriptor/Descriptor";
+
+export const StyledArticle = styled(Article)`
+  max-width: 1200px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  flex-wrap: wrap;
+`;
+
+export const StyledLeftContainer = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0 1em;
+
+  @media (min-width: ${props => props.theme.smToMd}) {
+    width: 66%;
+  }
+`;
+
+export const StyledRightContainer = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0 1em;
+
+  @media (min-width: ${props => props.theme.smToMd}) {
+    width: 33%;
+  }
+`;
+
+export const StyledDescriptor = styled(Descriptor)`
+  > p {
+    margin-top: 0;
+    font-style: italic;
+  }
+`;
+
+export const StyledH2 = styled.h2`
+  margin-top: 16px;
+  margin-bottom: 16px;
+
+  @media (min-width: ${props => props.theme.smToMd}) {
+    margin-top: 51px;
+  }
+`;
