e15c8addebd55e75c62cb55c4cc349df7c9a2f73
Kyle Bonar <kylewb2011@gmail.com>
2019-03-17 21:38:43 -0500

    Updated comments

--- src/redux/sauce/actions.ts
+++ src/redux/sauce/actions.ts
@@ -1,4 +1,5 @@
 import { API } from "../../utils/api/Api";
+import { addedSauces } from "../sauces/actions";
 
 /** @description Add sauce to DB
  *  @param {Object} data - Form Data that has been JSONified
@@ -19,14 +20,30 @@ export const addSauce = ({ formData }: { formData: FormData }) => async (
 };
 
 /** @description grab single sauce related to slug
- *  @param {String} slug - keyword to lookup
+ *  @param {Object} data - object containing slug we are interested in
+ *    @param {Object} data.sauce  - sauce container
+ *      @param {String} data.sauce.slug - unique sauce slug
+ *
  *  @returns {Promise}
- *    @returns {String}
+ *  @fires sauces.addsauce - add sauce to redux store
+ *  @resolves {Object} res.data - relevant info to request
+ *
+ *  {Boolean} res.data.isGood - whether request was good or not
+ *
+ *  {ISauce} res.data.sauce - sauce data
+ *
+ *  @reject {String} error message
  */
 export const getSauceBySlug = ({
   data
 }: {
   data: { sauce: { slug: string } };
 }) => async (dispatch: any): Promise<null> => {
-  return API.sauce.getBySlug(data).then(res => {});
+  return API.sauce.getBySlug({ data }).then((res: any) => {
+    const { slug } = res.data.sauce;
+
+    // Push sauce into redux store
+    // dispatch(addedSauces({}))
+    return slug;
+  });
 };
