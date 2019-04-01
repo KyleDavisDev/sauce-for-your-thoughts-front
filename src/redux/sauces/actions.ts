c08c403a65c69c3aa2fcc3513adf06c30ff745ba
Kyle Bonar <kylewb2011@gmail.com>
2019-03-31 20:51:47 -0500

    Move actions to sauces

--- src/redux/sauces/actions.ts
+++ src/redux/sauces/actions.ts
@@ -45,6 +45,53 @@ export const updatedSaucesItems = ({
   byId
 });
 
+/** @description grab single sauce related to slug
+ *  @param {Object} data - object containing slug we are interested in
+ *    @param {Object} data.sauce  - sauce container
+ *      @param {String} data.sauce.slug - unique sauce slug
+ *
+ *  @returns {Promise}
+ *  @fires sauces.addsauce - add sauce to redux store
+ *  @resolves {Object} res.data - relevant info to request
+ *
+ *  {Boolean} res.data.isGood - whether request was good or not
+ *
+ *  {ISauce} res.data.sauce - sauce data
+ *
+ *  @reject {String} error message
+ */
+export const getSauceBySlug = ({
+  data
+}: {
+  data: { sauce: { slug: string } };
+}) => async (dispatch: any): Promise<null> => {
+  return API.sauce.getBySlug({ data }).then((res: any) => {
+    const { slug } = res.data.sauce;
+
+    // Push sauce into redux store
+    // dispatch(addedSauces({}))
+    return slug;
+  });
+};
+
+/** @description Add sauce to DB
+ *  @param {FormData} formdata - Form Data that has been JSONified
+ *    @param {Object} formdata.user - author of the sauce
+ *      @param {String} formdata.user.token - unique string
+ *    @param {ISauce} formdata.sauce - sauce object
+ *  @returns {Promise}
+ *    @returns {String} slug - unique sauce slug
+ */
+export const addSauce = ({ formData }: { formData: FormData }) => async (
+  dispatch: any
+): Promise<string> => {
+  return API.sauce.add({ formData }).then((res: any) => {
+    const { slug } = res.data.sauce;
+
+    return slug;
+  });
+};
+
 // This may not be used anymore
 // export const saucesByTagFound = ({ sauces }) => ({
 //   type: SaucesActionTypes.SAUCES_BY_TAG_FOUND,
