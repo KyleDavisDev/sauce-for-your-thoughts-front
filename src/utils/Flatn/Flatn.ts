bd0104f09bb501a3b2016c0fa2954bafde5f0d48
Kyle Bonar <kylewb2011@gmail.com>
2019-04-06 16:25:12 -0500

    Added flatten users method

--- src/utils/Flatn/Flatn.ts
+++ src/utils/Flatn/Flatn.ts
@@ -1,10 +1,17 @@
+import moment from "moment";
+
 import { IReview, IReviewsState } from "../../redux/reviews/types";
+import { IUser, IUserState } from "../../redux/users/types";
 
 class Flatn {
   // flatten array of reviews
   public static reviews({ reviews }: { reviews: IReview[] }): IReviewsState {
     const allHashIDs: string[] = [];
     const byHashID: { [key: string]: IReview } = {};
+
+    // Will assign this to reviews if need to.
+    // Creating it once here will save computing time and give all reviews same value
+    const addedToStore: number = moment().unix();
 
     for (let i = 0, len = reviews.length; i < len; i++) {
       const hashID: string | undefined = reviews[i].hashID;
@@ -17,9 +24,41 @@ class Flatn {
 
       // Add to obj
       byHashID[hashID] = reviews[i];
+
+      // If review doesn't have _addedToStore prop, we will add it
+      if (!byHashID[hashID]._addedToStore) {
+        byHashID[hashID]._addedToStore = addedToStore;
+      }
     }
 
     return { allHashIDs, byHashID };
+  }
+
+  // flatten array of users
+  public static users({ users }: { users: IUser[] }): IUserState {
+    const allDisplayNames: string[] = [];
+    const byDisplayName: { [key: string]: IUser } = {};
+
+    // Will assign this to users if need to.
+    // Creating it once here will save computing time and give all users same value
+    const addedToStore: number = moment().unix();
+
+    for (let i = 0, len = users.length; i < len; i++) {
+      const displayName: string = users[i].displayName;
+
+      // Push into array
+      allDisplayNames.push(displayName);
+
+      // Add to obj
+      byDisplayName[displayName] = users[i];
+
+      // If review doesn't have _addedToStore prop, we will add it
+      if (!byDisplayName[displayName]._addedToStore) {
+        byDisplayName[displayName]._addedToStore = addedToStore;
+      }
+    }
+
+    return { allDisplayNames, byDisplayName };
   }
 }
 
