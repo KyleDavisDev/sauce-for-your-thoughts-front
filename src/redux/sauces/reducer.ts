a164b34f97258a802d9d8438b36d89a0ad9036eb
Kyle Bonar <kylewb2011@gmail.com>
2019-04-07 11:27:05 -0500

    Compare _full values, updated comments, return object

--- src/redux/sauces/reducer.ts
+++ src/redux/sauces/reducer.ts
@@ -1,9 +1,14 @@
 import { Reducer } from "redux";
-import { ISaucesState, SaucesActionTypes, ISaucesAction } from "./types";
+import {
+  ISaucesState,
+  SaucesActionTypes,
+  ISaucesAction,
+  ISauce
+} from "./types";
 
 const initialState: ISaucesState = {
-  byId: {},
-  allIds: [],
+  bySlug: {},
+  allSlugs: [],
   query: {},
   total: 0
 };
@@ -14,36 +19,62 @@ const sauceReducer: Reducer<ISaucesState> = (
 ): ISaucesState => {
   switch (action.type) {
     case SaucesActionTypes.SAUCES_ADDED:
-      // Look for null cases first
-      if (!action.allIds || !action.query) {
-        return state;
-      }
+      // Make sure we are not using undefined objects
+      const stateBySlug = state.bySlug || {};
+      const actionBySlug = action.bySlug || {};
+      // Will replace state with new action ones unless
+      // state entry has _full === true and action entry has _full === false
+      Object.keys(actionBySlug).map(slug => {
+        // if state already contains slug, we need to compare _full
+        if (stateBySlug[slug]) {
+          // If new one is full, we want this record since it will be newest
+          if (actionBySlug[slug]._full) {
+            return;
+          }
 
+          // If state has _full, we will go with this since it is more full
+          if (stateBySlug[slug]._full) {
+            actionBySlug[slug] = stateBySlug[slug];
+            return;
+          }
+        }
+
+        // Return
+        return;
+      });
+
+      const bySlug = {
+        ...stateBySlug,
+        ...actionBySlug
+      };
+
+      // Make sure we aren't using undefined arrays
+      const stateAllSlugs = state.allSlugs || [];
+      const actionAllSlugs = action.allSlugs || [];
       // This will create an array of unique ID's only.
-      // This is O(n^2), maybe optimize later?
-      const uniqueIDs: number[] = [
-        ...state.allIds,
-        ...action.allIds.filter((id: number) => {
-          return state.allIds.indexOf(id) === 0; // indexOf === 0 if it does not find 'id' in 'state.allIds'
+      const allSlugs: string[] = [
+        ...stateAllSlugs,
+        ...actionAllSlugs.filter((slug: string) => {
+          return stateAllSlugs.indexOf(slug) === -1; // indexOf === 0 if it does not find 'Slug' in 'state.allIds'
         })
       ];
 
       // Return new state.
       return {
         ...state,
-        byId: { ...state.byId, ...action.byId },
-        allIds: uniqueIDs,
+        bySlug,
+        allSlugs,
         query: { ...state.query, ...action.query },
         total: action.total || state.total
       };
 
     case SaucesActionTypes.UPDATE_SAUCE:
-      if (!action.allIds) return state;
+      if (!action.allSlugs) return state;
       // update single sauces item if sauces is already set
       return {
         ...state,
-        byId: { ...state.byId, ...action.byId },
-        allIds: [...state.allIds, ...action.allIds]
+        bySlug: { ...state.bySlug, ...action.bySlug },
+        allSlugs: [...state.allSlugs, ...action.allSlugs]
       };
     case SaucesActionTypes.SAUCES_BY_TAG_FOUND:
       return state; // Will come back to this
