af7a426894bf6555d9052f94bf29be0b24fe4d4b
Kyle Bonar <kylewb2011@gmail.com>
2019-04-19 09:40:15 -0500

    Moved styles out to own file

--- src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/components/SauceReviewBlock/SauceReviewBlock.tsx
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/components/SauceReviewBlock/SauceReviewBlock.tsx
@@ -2,83 +2,15 @@ import * as React from "react";
 import { connect } from "react-redux";
 import ReactRating from "react-rating";
 import { IReview } from "../../../../../../../../redux/reviews/types";
-import styled from "styled-components";
 import { IinitialState } from "../../../../../../../../redux/configureStore";
-import Star from "../../../../../../../../images/icons/Star";
-import { Button } from "../../../../../../../../components/Button/Button";
+import {StyledContainer,
+  StyledButton,
+  StyledContentContainer,
+  StyledCategoryContainer,
+  StyledCategoryDescription,
+  StyledEmptyStar,
+  StyledFullStar} from "./SauceReviewBlockStyle"
 
-const StyledContainer = styled.div`
-  background-color: ${props => props.theme.formContainerBackgroundColor};
-  font-family: AvenirNextReg;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  margin-bottom: 1em;
-  max-height: auto;
-`;
-
-const StyledButton = styled(Button)`
-  background-color: ${props => props.theme.lightGrey};
-  display: flex;
-  align-items: stretch;
-  font-size: 1.5em;
-
-  > button {
-    background-color: inherit;
-    transition: none;
-    padding: 0.25em;
-    margin: 0;
-
-    &:hover,
-    &:focus {
-      background-color: inherit;
-    }
-  }
-
-  &:hover,
-  &:focus {
-    background-color: ${props => props.theme.secondaryThemeColor};
-    cursor: pointer;
-  }
-`;
-
-const StyledContentContainer = styled.div`
-  padding: 1em;
-`;
-
-const StyledCategoryContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  flex-wrap: nowrap;
-  padding: 0 1em;
-
-  > i {
-    padding: 0 0.5em 0 0;
-  }
-`;
-
-const StyledCategoryDescription = styled.p`
-  margin-top: 0px;
-  padding: 0 2em;
-`;
-
-const StyledEmptyStar = styled(Star)`
-  .border {
-    fill: ${props => props.theme.primaryThemeColor};
-  }
-
-  .center {
-    fill: transparent;
-  }
-`;
-
-const StyledFullStar = styled(Star)`
-  .border,
-  .center {
-    fill: ${props => props.theme.primaryThemeColor};
-  }
-`;
 
 export interface SauceReviewBlockProps {
   review: IReview;
---
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/components/SauceReviewBlock/SauceReviewBlockStyle.tsx
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+import Star from "../../../../../../../../images/icons/Star";
+import { Button } from "../../../../../../../../components/Button/Button";
+
+export const StyledContainer = styled.div`
+  background-color: ${props => props.theme.formContainerBackgroundColor};
+  font-family: AvenirNextReg;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  margin-bottom: 1em;
+  max-height: auto;
+`;
+
+export const StyledButton = styled(Button)`
+  background-color: ${props => props.theme.lightGrey};
+  display: flex;
+  align-items: stretch;
+  font-size: 1.5em;
+
+  > button {
+    background-color: inherit;
+    transition: none;
+    padding: 0.25em;
+    margin: 0;
+
+    &:hover,
+    &:focus {
+      background-color: inherit;
+    }
+  }
+
+  &:hover,
+  &:focus {
+    background-color: ${props => props.theme.secondaryThemeColor};
+    cursor: pointer;
+  }
+`;
+
+export const StyledContentContainer = styled.div`
+  padding: 1em;
+`;
+
+export const StyledCategoryContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: nowrap;
+  padding: 0 1em;
+
+  > i {
+    padding: 0 0.5em 0 0;
+  }
+`;
+
+export const StyledCategoryDescription = styled.p`
+  margin-top: 0px;
+  padding: 0 2em;
+`;
+
+export const StyledEmptyStar = styled(Star)`
+  .border {
+    fill: ${props => props.theme.primaryThemeColor};
+  }
+
+  .center {
+    fill: transparent;
+  }
+`;
+
+export const StyledFullStar = styled(Star)`
+  .border,
+  .center {
+    fill: ${props => props.theme.primaryThemeColor};
+  }
+`;
