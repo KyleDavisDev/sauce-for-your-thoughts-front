f2d5b1229a09791aad2759c813568201ed2f0a93
Kyle Bonar <kylewb2011@gmail.com>
2019-04-20 15:03:15 -0500

    Updated background to use border instead, abstracted styles to own file

--- src/screens/sauce/screens/SauceSpotlight/components/SauceHero/SauceHero.tsx
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceHero/SauceHero.tsx
@@ -1,35 +1,14 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import { IinitialState } from "../../../../../../redux/configureStore";
-import styled from "styled-components";
-import { Link } from "../../../../../../components/Link/Link";
-import { Button } from "../../../../../../components/Button/Button";
 import { ISauce } from "../../../../../../redux/sauces/types";
 
-const StyledSauceContainer = styled.div`
-  background-color: ${props => props.theme.formContainerBackgroundColor};
-  padding: 1.5rem;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  margin-bottom: 3em;
-`;
-
-const StyledImageContainer = styled.div`
-  max-width: 33%;
-`;
-
-const StyleImg = styled.img`
-  width: 100%;
-  box-sizing: border-box;
-`;
-
-const StyledSauceInfoContainer = styled.div`
-  font-family: AvenirNextReg;
-  width: 66%;
-  box-sizing: border-box;
-  padding: 0 1em;
-`;
+import {
+  StyledSauceContainer,
+  StyledImageContainer,
+  StyleImg,
+  StyledSauceInfoContainer
+} from "./SauceHeroStyle";
 
 export interface SauceHeroProps {
   sauce?: ISauce; // This is sauce's slug that will have to be looked up
@@ -69,11 +48,6 @@ class SauceHero extends React.PureComponent<SauceHeroProps, any> {
           <p>
             <i>Made in:</i> {(sauce && sauce.country) || "Loading..."}
           </p>
-          {sauce && (
-            <Link to={`/review/add/?s=${sauce.slug}`}>
-              <Button displayType="solid">Add Review</Button>
-            </Link>
-          )}
         </StyledSauceInfoContainer>
       </StyledSauceContainer>
     );
---
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceHero/SauceHeroStyle.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const StyledSauceContainer = styled.div`
+  background-color: ${props => props.theme.formContainerBackgroundColor};
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-bottom: 3em;
+`;
+
+export const StyledImageContainer = styled.div`
+  max-width: 100%;
+
+  @media (min-width: ${props => props.theme.smToMd}) {
+    width: 50%;
+  }
+`;
+
+export const StyleImg = styled.img`
+  width: 100%;
+  box-sizing: border-box;
+`;
+
+export const StyledSauceInfoContainer = styled.div`
+  font-family: AvenirNextReg;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0 1em;
+
+  @media (min-width: ${props => props.theme.smToMd}) {
+    width: 50%;
+  }
+`;
