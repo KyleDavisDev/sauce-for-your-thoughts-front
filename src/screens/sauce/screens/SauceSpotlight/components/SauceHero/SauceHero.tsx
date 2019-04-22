fcfb2fe4af049704cf09eff25fc2600fb05cc971
Kyle Bonar <kylewb2011@gmail.com>
2019-04-22 14:04:58 -0500

    Render sauce name, find correct place to get image

--- src/screens/sauce/screens/SauceSpotlight/components/SauceHero/SauceHero.tsx
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceHero/SauceHero.tsx
@@ -7,8 +7,14 @@ import {
   StyledSauceContainer,
   StyledImageContainer,
   StyleImg,
-  StyledSauceInfoContainer
+  StyledSauceInfoContainer,
+  StyledH2
 } from "./SauceHeroStyle";
+
+const host =
+  process.env.API_ENV === "prod"
+    ? "https://sauceforyourthoughts.com"
+    : "http://localhost:7777";
 
 export interface SauceHeroProps {
   sauce?: ISauce; // This is sauce's slug that will have to be looked up
@@ -26,12 +32,13 @@ class SauceHero extends React.PureComponent<SauceHeroProps, any> {
       <StyledSauceContainer>
         <StyledImageContainer>
           {sauce && sauce.photo ? (
-            <StyleImg src={`${sauce.photo}`} />
+            <StyleImg src={`${host}/public/uploads/${sauce.photo}`} />
           ) : (
             <StyleImg src="https://www.snhrc.com/wp-content/uploads/2018/09/Image-Coming-Soon.png" />
           )}
         </StyledImageContainer>
         <StyledSauceInfoContainer>
+          <StyledH2>{(sauce && sauce.name) || "Loading..."}</StyledH2>
           <p>
             <i>Maker:</i> {(sauce && sauce.maker) || "Loading..."}
           </p>
