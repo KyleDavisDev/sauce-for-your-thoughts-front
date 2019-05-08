991bb0f9e7242f27ab4caa6843b581ec4f28a501
Kyle Bonar <kylewb2011@gmail.com>
2019-05-08 09:07:00 -0500

    Add border

--- src/screens/reviews/ReviewAddStyle.tsx
+++ src/screens/reviews/ReviewAddStyle.tsx
@@ -4,7 +4,7 @@ import Descriptor from "../../components/Descriptor/Descriptor";
 import { Button } from "../../components/Button/Button";
 
 export const StyledFormContainer = styled.div`
-  background-color: ${props => props.theme.formContainerBackgroundColor};
+  border: ${props => props.theme.border};
   padding: 1.5rem;
   display: flex;
   flex-direction: row;
