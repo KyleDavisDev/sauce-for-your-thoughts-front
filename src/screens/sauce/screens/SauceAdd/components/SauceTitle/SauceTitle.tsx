f806709eba22db8218132fd03757ca4c37e8d18e
Kyle Bonar <kylewb2011@gmail.com>
2019-06-14 14:57:35 -0500

    stateless function to pure component

--- src/screens/sauce/screens/SauceAdd/components/SauceTitle/SauceTitle.tsx
+++ src/screens/sauce/screens/SauceAdd/components/SauceTitle/SauceTitle.tsx
@@ -13,32 +13,34 @@ export interface ISauceTitleProps {
   maker: string;
 }
 
-export function SauceTitle(props: ISauceTitleProps) {
-  return (
-    <StyledRow>
-      <StyledDescriptor title="Title">
-        What is the name of the sauce? Who is the maker? This is required.
-      </StyledDescriptor>
-      <StyledRightSide>
-        <StyledTextInput
-          onChange={props.onTextChange}
-          label="Name"
-          name="name"
-          id="name"
-          showLabel={true}
-          value={props.name}
-          required={true}
-        />
-        <StyledTextInput
-          onChange={props.onTextChange}
-          label="Maker"
-          name="maker"
-          id="maker"
-          showLabel={true}
-          value={props.maker}
-          required={true}
-        />
-      </StyledRightSide>
-    </StyledRow>
-  );
+export class SauceTitle extends React.PureComponent<ISauceTitleProps, any> {
+  public render() {
+    return (
+      <StyledRow>
+        <StyledDescriptor title="Title">
+          What is the name of the sauce? Who is the maker? This is required.
+        </StyledDescriptor>
+        <StyledRightSide>
+          <StyledTextInput
+            onChange={this.props.onTextChange}
+            label="Name"
+            name="name"
+            id="name"
+            showLabel={true}
+            value={this.props.name}
+            required={true}
+          />
+          <StyledTextInput
+            onChange={this.props.onTextChange}
+            label="Maker"
+            name="maker"
+            id="maker"
+            showLabel={true}
+            value={this.props.maker}
+            required={true}
+          />
+        </StyledRightSide>
+      </StyledRow>
+    );
+  }
 }
