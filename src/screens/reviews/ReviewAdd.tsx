59dafc08b3e572c8aa6cd1e9bedeab1e02e8c2dc
Kyle Bonar <kylewb2011@gmail.com>
2019-06-18 14:25:15 -0500

    Cover up form if user cannot submit

--- src/screens/reviews/ReviewAdd.tsx
+++ src/screens/reviews/ReviewAdd.tsx
@@ -31,6 +31,7 @@ import {
   FlashMessageProps,
   FlashMessage
 } from "../../components/FlashMessage/FlashMessage";
+import { Overlay } from "../../components/Overlay/Overlay";
 
 export interface ReviewAddProps {
   addReview: ({
@@ -136,173 +137,175 @@ class ReviewAdd extends React.Component<ReviewAddProps, ReviewAddState> {
                   {this.state.flashMessage.text}
                 </FlashMessage>
               )}
-              {/* Taste */}
-              <StyledRow>
-                <StyledDescriptor title="Taste">
-                  Can you taste garlic? Are there hints of thyme? Describe what
-                  you taste in the sauce.
-                </StyledDescriptor>
-                <StyledRightSide>
-                  <Label>Taste Rating</Label>
-                  <Rating
-                    emptySymbol={<StyledEmptyStar />}
-                    fullSymbol={<StyledFullStar />}
-                    onClick={e => this.onStarClick(e, "taste")}
-                    initialRating={this.state.taste.rating}
-                    readonly={!this.state.enabled}
-                  />
-                  <StyledTextArea
-                    onChange={this.onTextChange}
-                    label="Description"
-                    name="taste"
-                    id="taste"
-                    showLabel={true}
-                    value={this.state.taste.txt}
-                    disabled={!this.state.enabled}
-                    readonly={!this.state.enabled}
-                  />
-                </StyledRightSide>
-              </StyledRow>
+              <Overlay enabled={this.state.enabled}>
+                {/* Taste */}
+                <StyledRow>
+                  <StyledDescriptor title="Taste">
+                    Can you taste garlic? Are there hints of thyme? Describe
+                    what you taste in the sauce.
+                  </StyledDescriptor>
+                  <StyledRightSide>
+                    <Label>Taste Rating</Label>
+                    <Rating
+                      emptySymbol={<StyledEmptyStar />}
+                      fullSymbol={<StyledFullStar />}
+                      onClick={e => this.onStarClick(e, "taste")}
+                      initialRating={this.state.taste.rating}
+                      readonly={!this.state.enabled}
+                    />
+                    <StyledTextArea
+                      onChange={this.onTextChange}
+                      label="Description"
+                      name="taste"
+                      id="taste"
+                      showLabel={true}
+                      value={this.state.taste.txt}
+                      disabled={!this.state.enabled}
+                      readonly={!this.state.enabled}
+                    />
+                  </StyledRightSide>
+                </StyledRow>
 
-              {/* Aroma */}
-              <StyledRow>
-                <StyledDescriptor title="Aroma">
-                  What can you smell in the sauce? Try closing your eyes and
-                  wafting the aroma towards your nose? What accents do you pick
-                  up?
-                </StyledDescriptor>
-                <StyledRightSide>
-                  <Label>Aroma Rating</Label>
-                  <Rating
-                    emptySymbol={<StyledEmptyStar />}
-                    fullSymbol={<StyledFullStar />}
-                    onClick={e => this.onStarClick(e, "aroma")}
-                    initialRating={this.state.aroma.rating}
-                    readonly={!this.state.enabled}
-                  />
-                  <StyledTextArea
-                    onChange={this.onTextChange}
-                    label="Description"
-                    name="aroma"
-                    id="aroma"
-                    showLabel={true}
-                    value={this.state.aroma ? this.state.aroma.txt : ""}
-                    disabled={!this.state.enabled}
-                    readonly={!this.state.enabled}
-                  />
-                </StyledRightSide>
-              </StyledRow>
+                {/* Aroma */}
+                <StyledRow>
+                  <StyledDescriptor title="Aroma">
+                    What can you smell in the sauce? Try closing your eyes and
+                    wafting the aroma towards your nose? What accents do you
+                    pick up?
+                  </StyledDescriptor>
+                  <StyledRightSide>
+                    <Label>Aroma Rating</Label>
+                    <Rating
+                      emptySymbol={<StyledEmptyStar />}
+                      fullSymbol={<StyledFullStar />}
+                      onClick={e => this.onStarClick(e, "aroma")}
+                      initialRating={this.state.aroma.rating}
+                      readonly={!this.state.enabled}
+                    />
+                    <StyledTextArea
+                      onChange={this.onTextChange}
+                      label="Description"
+                      name="aroma"
+                      id="aroma"
+                      showLabel={true}
+                      value={this.state.aroma ? this.state.aroma.txt : ""}
+                      disabled={!this.state.enabled}
+                      readonly={!this.state.enabled}
+                    />
+                  </StyledRightSide>
+                </StyledRow>
 
-              {/* Label */}
-              <StyledRow>
-                <StyledDescriptor title="Label">
-                  How do you feel about the design? Does it speak to you? Does
-                  it remind you of anything? How effective does the design
-                  convey what the sauce is/is not.
-                </StyledDescriptor>
-                <StyledRightSide>
-                  <Label>Label Rating</Label>
-                  <Rating
-                    emptySymbol={<StyledEmptyStar />}
-                    fullSymbol={<StyledFullStar />}
-                    onClick={e => this.onStarClick(e, "label")}
-                    initialRating={this.state.label.rating}
-                    readonly={!this.state.enabled}
-                  />
-                  <StyledTextArea
-                    onChange={this.onTextChange}
-                    label="Description"
-                    name="label"
-                    id="label"
-                    showLabel={true}
-                    value={this.state.label ? this.state.label.txt : ""}
-                    disabled={!this.state.enabled}
-                    readonly={!this.state.enabled}
-                  />
-                </StyledRightSide>
-              </StyledRow>
+                {/* Label */}
+                <StyledRow>
+                  <StyledDescriptor title="Label">
+                    How do you feel about the design? Does it speak to you? Does
+                    it remind you of anything? How effective does the design
+                    convey what the sauce is/is not.
+                  </StyledDescriptor>
+                  <StyledRightSide>
+                    <Label>Label Rating</Label>
+                    <Rating
+                      emptySymbol={<StyledEmptyStar />}
+                      fullSymbol={<StyledFullStar />}
+                      onClick={e => this.onStarClick(e, "label")}
+                      initialRating={this.state.label.rating}
+                      readonly={!this.state.enabled}
+                    />
+                    <StyledTextArea
+                      onChange={this.onTextChange}
+                      label="Description"
+                      name="label"
+                      id="label"
+                      showLabel={true}
+                      value={this.state.label ? this.state.label.txt : ""}
+                      disabled={!this.state.enabled}
+                      readonly={!this.state.enabled}
+                    />
+                  </StyledRightSide>
+                </StyledRow>
 
-              {/* Heat */}
-              <StyledRow>
-                <StyledDescriptor title="Heat">
-                  How spicy is this sauce? Did you have to run for water? Was it
-                  the perfect amount of heat?
-                </StyledDescriptor>
-                <StyledRightSide>
-                  <Label>Heat Rating</Label>
-                  <Rating
-                    emptySymbol={<StyledEmptyStar />}
-                    fullSymbol={<StyledFullStar />}
-                    onClick={e => this.onStarClick(e, "heat")}
-                    initialRating={this.state.heat.rating}
-                    readonly={!this.state.enabled}
-                  />
-                  <StyledTextArea
-                    onChange={this.onTextChange}
-                    label="Description"
-                    name="heat"
-                    id="heat"
-                    showLabel={true}
-                    value={this.state.heat ? this.state.heat.txt : ""}
-                    disabled={!this.state.enabled}
-                    readonly={!this.state.enabled}
-                  />
-                </StyledRightSide>
-              </StyledRow>
+                {/* Heat */}
+                <StyledRow>
+                  <StyledDescriptor title="Heat">
+                    How spicy is this sauce? Did you have to run for water? Was
+                    it the perfect amount of heat?
+                  </StyledDescriptor>
+                  <StyledRightSide>
+                    <Label>Heat Rating</Label>
+                    <Rating
+                      emptySymbol={<StyledEmptyStar />}
+                      fullSymbol={<StyledFullStar />}
+                      onClick={e => this.onStarClick(e, "heat")}
+                      initialRating={this.state.heat.rating}
+                      readonly={!this.state.enabled}
+                    />
+                    <StyledTextArea
+                      onChange={this.onTextChange}
+                      label="Description"
+                      name="heat"
+                      id="heat"
+                      showLabel={true}
+                      value={this.state.heat ? this.state.heat.txt : ""}
+                      disabled={!this.state.enabled}
+                      readonly={!this.state.enabled}
+                    />
+                  </StyledRightSide>
+                </StyledRow>
 
-              {/* Overall */}
-              <StyledRow>
-                <StyledDescriptor title="Overall">
-                  What are you overall impressions? What did the sauce get
-                  right? What can it improve on?
-                </StyledDescriptor>
-                <StyledRightSide>
-                  <Label>Overall Rating</Label>
-                  <Rating
-                    emptySymbol={<StyledEmptyStar />}
-                    fullSymbol={<StyledFullStar />}
-                    onClick={e => this.onStarClick(e, "overall")}
-                    initialRating={this.state.overall.rating}
-                    readonly={!this.state.enabled}
-                  />
-                  <StyledTextArea
-                    onChange={this.onTextChange}
-                    required={true}
-                    label="Description"
-                    name="overall"
-                    id="overall"
-                    showLabel={true}
-                    value={this.state.overall ? this.state.overall.txt : ""}
-                    disabled={!this.state.enabled}
-                    readonly={!this.state.enabled}
-                  />
-                </StyledRightSide>
-              </StyledRow>
+                {/* Overall */}
+                <StyledRow>
+                  <StyledDescriptor title="Overall">
+                    What are you overall impressions? What did the sauce get
+                    right? What can it improve on?
+                  </StyledDescriptor>
+                  <StyledRightSide>
+                    <Label>Overall Rating</Label>
+                    <Rating
+                      emptySymbol={<StyledEmptyStar />}
+                      fullSymbol={<StyledFullStar />}
+                      onClick={e => this.onStarClick(e, "overall")}
+                      initialRating={this.state.overall.rating}
+                      readonly={!this.state.enabled}
+                    />
+                    <StyledTextArea
+                      onChange={this.onTextChange}
+                      required={true}
+                      label="Description"
+                      name="overall"
+                      id="overall"
+                      showLabel={true}
+                      value={this.state.overall ? this.state.overall.txt : ""}
+                      disabled={!this.state.enabled}
+                      readonly={!this.state.enabled}
+                    />
+                  </StyledRightSide>
+                </StyledRow>
 
-              {/* Note */}
-              <StyledRow>
-                <StyledDescriptor title="Note">
-                  Have any fleeting thoughts that you would like to add? Include
-                  it here!
-                </StyledDescriptor>
-                <StyledRightSide>
-                  <StyledTextArea
-                    onChange={this.onTextChange}
-                    label="Description"
-                    name="note"
-                    id="note"
-                    showLabel={true}
-                    value={this.state.note ? this.state.note.txt : ""}
-                    disabled={!this.state.enabled}
-                    readonly={!this.state.enabled}
-                  />
-                </StyledRightSide>
-              </StyledRow>
+                {/* Note */}
+                <StyledRow>
+                  <StyledDescriptor title="Note">
+                    Have any fleeting thoughts that you would like to add?
+                    Include it here!
+                  </StyledDescriptor>
+                  <StyledRightSide>
+                    <StyledTextArea
+                      onChange={this.onTextChange}
+                      label="Description"
+                      name="note"
+                      id="note"
+                      showLabel={true}
+                      value={this.state.note ? this.state.note.txt : ""}
+                      disabled={!this.state.enabled}
+                      readonly={!this.state.enabled}
+                    />
+                  </StyledRightSide>
+                </StyledRow>
 
-              <StyledButton onClick={() => {}} type="submit">
-                Submit
-                <ArrowRight />
-              </StyledButton>
+                <StyledButton onClick={() => {}} type="submit">
+                  Submit
+                  <ArrowRight />
+                </StyledButton>
+              </Overlay>
             </form>
           </StyledFormContainer>
         </Article>
