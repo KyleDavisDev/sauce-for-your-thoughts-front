4698bd3d015ee7cac39e897dd66436c352769b7c
Kyle Bonar <kylewb2011@gmail.com>
2019-06-26 10:04:36 -0500

    Styles to own file

--- src/screens/home/components/FeaturedSauces/FeaturedSauces.tsx
+++ src/screens/home/components/FeaturedSauces/FeaturedSauces.tsx
@@ -1,20 +1,12 @@
 import * as React from "react";
 
-import styled from "../../../../theme/styled-components";
-import Card from "../../../../components/Card/Card";
 import SectionTitle from "../../../../components/SectionTitle/SectionTitle";
-
-const StyledDiv = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  margin-bottom: 1em;
-  justify-content: center;
-`;
-
-const StyledCard = styled(Card)`
-  width: 33%;
-`;
+import {
+  StyledDiv,
+  StyledCard,
+  StyledCardHolder,
+  StyledCardContainer
+} from "./FeaturedSaucesStyles";
 
 interface FeaturedSaucesProps {
   className?: string;
@@ -27,24 +19,7 @@ const FeaturedSauces: React.SFC<FeaturedSaucesProps> = props => {
         title="Featured Sauces"
         description="Check out some of these unique sauces. Discover flavors you've never tasted before!"
       />
-      <StyledCard
-        to={`/sauce/?s=${5}`}
-        title="test"
-        imageLink="https://as.ftcdn.net/r/v1/pics/2fd8819a419c4245e5429905770db4b570661f48/home/discover_collections/Images.jpg"
-        description="description here"
-      />
-      <StyledCard
-        to={`/sauce/?s=${5}`}
-        title="test"
-        imageLink="https://as.ftcdn.net/r/v1/pics/2fd8819a419c4245e5429905770db4b570661f48/home/discover_collections/Images.jpg"
-        description="description here"
-      />
-      <StyledCard
-        to={`/sauce/?s=${5}`}
-        title="test"
-        imageLink="https://as.ftcdn.net/r/v1/pics/2fd8819a419c4245e5429905770db4b570661f48/home/discover_collections/Images.jpg"
-        description="description here"
-      />
+      <StyledCardContainer />
     </StyledDiv>
   );
 };
