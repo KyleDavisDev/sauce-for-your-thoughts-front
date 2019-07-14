96bcce017a7c9bee8f58d29b5b911cab676c1576
Kyle Bonar <kylewb2011@gmail.com>
2019-07-14 14:43:38 -0500

    Create edit link if user logged in and has a submission

--- src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/SauceReviews.tsx
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/SauceReviews.tsx
@@ -5,6 +5,8 @@ import { IReview } from "../../../../../../redux/reviews/types";
 import { Button } from "../../../../../../components/Button/Button";
 import { Link } from "../../../../../../components/Link/Link";
 import SauceReviewBlock from "./components/SauceReviewBlock/SauceReviewBlock";
+import { connect } from "react-redux";
+import { AppState } from "../../../../../../redux/configureStore";
 
 const StyledContainer = styled.div`
   font-family: AvenirNextReg;
@@ -28,6 +30,7 @@ const StyledLink = styled(Link)`
 export interface SauceReviewsProps {
   slug?: string;
   reviews?: IReview[];
+  displayEditLink: boolean;
 }
 
 class SauceReviews extends React.PureComponent<SauceReviewsProps, any> {
@@ -36,7 +39,8 @@ class SauceReviews extends React.PureComponent<SauceReviewsProps, any> {
   }
 
   public render() {
-    const { reviews } = this.props;
+    const { reviews, displayEditLink, slug } = this.props;
+
     return (
       <div>
         {reviews && reviews.length > 0 ? (
@@ -50,12 +54,44 @@ class SauceReviews extends React.PureComponent<SauceReviewsProps, any> {
             </p>
           </StyledContainer>
         )}
-        <StyledLink to={`/review/add?s=${this.props.slug}`}>
-          <Button>Add Review</Button>
-        </StyledLink>
+        {displayEditLink ? (
+          <StyledLink to={`/review/edit?s=${slug}`}>
+            <Button>Edit Your Review</Button>
+          </StyledLink>
+        ) : (
+          <StyledLink to={`/review/add?s=${slug}`}>
+            <Button>Add Review</Button>
+          </StyledLink>
+        )}
       </div>
     );
   }
 }
 
-export default SauceReviews;
+const mapState2Props = (state: AppState, ownProps: SauceReviewsProps) => {
+  // Grab user from store
+  const user = state.users.self.displayName;
+  // If no user, return normal props
+  if (!user) return { ...ownProps };
+
+  // Grab reviews
+  const reviews = ownProps.reviews;
+  // If no reviews, return
+  if (!reviews) return { ...ownProps };
+
+  let displayEditLink: boolean = false;
+  const len = reviews.length;
+  for (let i = 0; i < len; i++) {
+    if (reviews[i].author === user) {
+      displayEditLink = true;
+      break;
+    }
+  }
+
+  return { ...ownProps, displayEditLink };
+};
+
+export default connect(
+  mapState2Props,
+  null
+)(SauceReviews);
