4fc0ca8b698599fba483c8738a140fbe09ea043a
Kyle Bonar <kylewb2011@gmail.com>
2019-07-17 08:46:27 -0500

    Find login inside of account folder

--- src/screens/Screens.tsx
+++ src/screens/Screens.tsx
@@ -73,7 +73,7 @@ const Register = Loadable({
 
 // Login
 const Login = Loadable({
-  loader: () => import("./login/Login"),
+  loader: () => import("./account/login/Login"),
   loading: () => (
     <div>
       <Loading />
@@ -122,7 +122,7 @@ const Screens = (
     <Route path="/review/edit" component={ReviewEdit} />
     <Route path="/sauces" component={Sauces} />
     <Route path="/account/register" component={Register} />
-    <Route path="/login" component={Login} />
+    <Route path="/acount/login" component={Login} />
     <Route path="/reset" component={Reset} />
     <Route exact path="/sauce" component={SauceSpotlight} />
 
