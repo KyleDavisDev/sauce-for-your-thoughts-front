2ef963c1c715529f55a75bd60c1477bb77fdb4a7
Kyle Bonar <kylewb2011@gmail.com>
2019-07-17 13:21:38 -0500

    removed unused imports/methods/etc. Added link to profile settings

--- src/screens/account/settings/Settings.tsx
+++ src/screens/account/settings/Settings.tsx
@@ -5,7 +5,6 @@ import { Settings } from "../../../redux/users/actions";
 import { ISettingsUser } from "../../../redux/users/types";
 import LogoSFYT from "../../../images/icons/LogoSFYT";
 import PageTitle from "../../../components/PageTitle/PageTitle";
-import TextInput from "../../../components/TextInput/TextInput";
 import { Link } from "../../../components/Link/Link";
 import { AppState } from "../../../redux/configureStore";
 import {
@@ -13,15 +12,12 @@ import {
   StyledLogoContainer,
   StyledArticle,
   StyledFormContainer,
-  StyledText,
   StyledButton
 } from "./SettingsStyle";
 import {
   FlashMessage,
   FlashMessageProps
 } from "../../../components/FlashMessage/FlashMessage";
-import Err from "../../../utils/Err/Err";
-import Auth from "../../../utils/Auth/Auth";
 
 export interface SettingsProps {
   Settings: any;
@@ -66,161 +62,23 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
         <StyledArticle>
           <PageTitle>Settings</PageTitle>
           <StyledFormContainer>
-            {this.state.flashMessage.isVisible && (
-              <FlashMessage type={this.state.flashMessage.type} isVisible>
-                {this.state.flashMessage.text}
-              </FlashMessage>
-            )}
-            <form onSubmit={this.onSubmit} style={{ width: "100%" }}>
-              <TextInput
-                type="email"
-                onChange={this.onTextChange}
-                showLabel={true}
-                label={"Email"}
-                name={"email"}
-                value={this.state.email}
-                required={true}
-              />
-              <TextInput
-                type="email"
-                onChange={this.onTextChange}
-                showLabel={true}
-                label={"Confirm Email"}
-                name={"confirmEmail"}
-                value={this.state.confirmEmail}
-                required={true}
-              />
-              <TextInput
-                type="password"
-                onChange={this.onTextChange}
-                showLabel={true}
-                label={"Password"}
-                name={"password"}
-                value={this.state.password}
-                required={true}
-              />
-              <TextInput
-                type="password"
-                onChange={this.onTextChange}
-                showLabel={true}
-                label={"Confirm Password"}
-                name={"confirmPassword"}
-                value={this.state.confirmPassword}
-                required={true}
-              />
-              <TextInput
-                type="text"
-                onChange={this.onTextChange}
-                showLabel={true}
-                label={"Display Name"}
-                name={"displayName"}
-                value={this.state.displayName}
-                required={true}
-              />
+            <p>Edit Profile</p>
 
-              <StyledButton type="submit">Update</StyledButton>
-            </form>
+            <Link to="/account/settings/profile">
+              <StyledButton type="button">Edit Profile</StyledButton>
+            </Link>
           </StyledFormContainer>
         </StyledArticle>
       </StyledDiv>
     );
   }
-
-  private onTextChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    if (!event || !event.target) {
-      return;
-    }
-    // Grab the name and value
-    const { name, value }: { name: string; value: string } = event.target;
-
-    // Update local state
-    this.setState({
-      ...this.state,
-      [name]: value
-    });
-  };
-
-  private onSubmit = async (event: React.FormEvent): Promise<any> => {
-    event.preventDefault();
-
-    if (this.state.email !== this.state.confirmEmail) {
-      this.setState({
-        flashMessage: {
-          isVisible: true,
-          text: "Your emails do not match. Please fix this before continuing.",
-          type: "alert"
-        }
-      });
-      return;
-    }
-
-    if (this.state.password !== this.state.confirmPassword) {
-      this.setState({
-        flashMessage: {
-          isVisible: true,
-          text:
-            "Your passwords do not match. Please fix this before continuing.",
-          type: "alert"
-        }
-      });
-      return;
-    }
-
-    if (this.state.password.length < 8) {
-      this.setState({
-        flashMessage: {
-          isVisible: true,
-          text:
-            "Your password is too weak! Please make your password over 8 characters long.",
-          type: "alert"
-        }
-      });
-      return;
-    }
-
-    // Grab values from state
-    const {
-      email,
-      confirmEmail,
-      password,
-      confirmPassword,
-      displayName
-    } = this.state;
-
-    // Create credentials obj
-    const credentials: ISettingsUser = {
-      user: { email, confirmEmail, password, confirmPassword, displayName }
-    };
-    try {
-      // dispatch action which calls API to Settings user
-      const { token } = await this.props.Settings({ credentials });
-
-      // Set user to be remembered
-      Auth.authenticateUser({ token, displayName });
-
-      // Redirect user to sauces page -- Maybe take them to user home page instead?
-      this.props.history.push("/sauces");
-    } catch (err) {
-      const text = Err.getError({ err });
-      // Create warning flash
-      this.setState({
-        flashMessage: {
-          isVisible: true,
-          text,
-          type: "warning"
-        }
-      });
-    }
-  };
 }
 
 const mapState2Props = (state: AppState) => {
   return {};
 };
 
-const mapDispatch2Props = {
-  Settings
-};
+const mapDispatch2Props = {};
 
 export default connect(
   mapState2Props,
