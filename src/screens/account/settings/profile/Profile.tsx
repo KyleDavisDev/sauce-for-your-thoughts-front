18cdd0059633b25eb483168f08b6171513691e0e
Kyle Bonar <kylewb2011@gmail.com>
2019-07-21 18:33:46 -0500

    Removed unused code, commented out code for testing, connect compont to redux, call redux dispatch on load, split state into original and updated

--- src/screens/account/settings/profile/Profile.tsx
+++ src/screens/account/settings/profile/Profile.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 import { connect } from "react-redux";
+import validator from "validator";
 
-import { Profile } from "../../../../redux/users/actions";
-import { IProfileUser } from "../../../../redux/users/types";
+// import { Profile } from "../../../../redux/users/actions";
+// import { IRegisterUser } from "../../../../redux/users/types";
 import LogoSFYT from "../../../../images/icons/LogoSFYT";
 import PageTitle from "../../../../components/PageTitle/PageTitle";
 import { TextInput } from "../../../../components/TextInput/TextInput";
 import { Link } from "../../../../components/Link/Link";
-import { AppState } from "../../../../redux/configureStore";
+import { AppState, MyThunkDispatch } from "../../../../redux/configureStore";
+import { getInfo } from "../../../../redux/users/actions";
 import {
   StyledDiv,
   StyledLogoContainer,
@@ -19,21 +21,32 @@ import {
   FlashMessage,
   FlashMessageProps
 } from "../../../../components/FlashMessage/FlashMessage";
-import Err from "../../../../utils/Err/Err";
-import Auth from "../../../../utils/Auth/Auth";
 
 export interface ProfileProps {
-  Profile: any;
   history: { push: (location: string) => null };
+  getInfo: ({
+    data
+  }: {
+    data: { user: { token: string }; displayName: string };
+  }) => Promise<null>;
+  user: { token: string; displayName: string };
 }
 
 export interface ProfileState {
-  email: string;
-  confirmEmail: string;
-  disabledConfirmEmail: boolean;
+  original: {
+    email: string;
+    displayName: string;
+  };
+  updated: {
+    email: string;
+    confirmEmail: string;
+    displayName: string;
+  };
   password: string;
-  disabledPassword: boolean;
-  displayName: string;
+  enabled: {
+    password: boolean;
+    email: boolean;
+  };
   flashMessage: FlashMessageProps;
 }
 
@@ -43,16 +56,30 @@ class Profile extends React.Component<ProfileProps, ProfileState> {
 
     // Init state
     this.state = {
-      email: "",
-      confirmEmail: "",
-      disabledConfirmEmail: true,
+      original: {
+        email: "",
+        displayName: ""
+      },
+      updated: {
+        email: "",
+        confirmEmail: "",
+        displayName: ""
+      },
       password: "",
-      disabledPassword: true,
-      displayName: "",
+      enabled: {
+        email: false,
+        password: false
+      },
       flashMessage: {
         isVisible: false
       }
     };
+  }
+
+  public async componentDidMount() {
+    const { token, displayName } = this.props.user;
+    const data = { user: { token }, displayName };
+    await this.props.getInfo({ data });
   }
 
   public render() {
@@ -79,27 +106,27 @@ class Profile extends React.Component<ProfileProps, ProfileState> {
                 showLabel={true}
                 label={"Email"}
                 name={"email"}
-                value={this.state.email}
+                value={this.state.updated.email}
                 required={true}
               />
               <TextInput
                 type="email"
                 onChange={this.onTextChange}
-                disabled={this.state.disabledConfirmEmail}
+                disabled={!this.state.enabled.email}
                 showLabel={true}
                 label={"Confirm Email"}
                 name={"confirmEmail"}
-                value={this.state.confirmEmail}
+                value={this.state.updated.confirmEmail}
                 required={true}
               />
               <TextInput
                 type="password"
                 onChange={this.onTextChange}
-                disabled={true}
+                disabled={!this.state.enabled.password}
                 showLabel={true}
                 label={"Password"}
                 name={"password"}
-                value={this.state.password}
+                value={this.state.enabled.password ? this.state.password : ""}
                 required={true}
               />
               <TextInput
@@ -108,7 +135,7 @@ class Profile extends React.Component<ProfileProps, ProfileState> {
                 showLabel={true}
                 label={"Display Name"}
                 name={"displayName"}
-                value={this.state.displayName}
+                value={this.state.updated.displayName}
                 required={true}
               />
 
@@ -127,94 +154,122 @@ class Profile extends React.Component<ProfileProps, ProfileState> {
     // Grab the name and value
     const { name, value }: { name: string; value: string } = event.target;
 
+    // Check if we should enable 'confirm email' box
+    const enabledEmail = this.toggleConfirmEmail(name, value);
+
+    // Check if we should enable 'confirm password' box
+    const enabledPassword = enabledEmail;
     // Update local state
     this.setState({
       ...this.state,
-      [name]: value
+      [name]: value,
+      enabled: { ...this.state.enabled, email: enabledEmail }
     });
   };
 
-  private onSubmit = async (event: React.FormEvent): Promise<any> => {
-    event.preventDefault();
-
-    if (this.state.email !== this.state.confirmEmail) {
-      this.setState({
-        flashMessage: {
-          isVisible: true,
-          text: "Your emails do not match. Please fix this before continuing.",
-          type: "alert"
-        }
-      });
-      return;
+  private toggleConfirmEmail = (name: string, value: string): boolean => {
+    // Will only consider when the email textbox is being used
+    if (name === "email") {
+      // If textbox has valid email, enable confirm textbox.
+      return validator.isEmail(value) ? true : false;
     }
 
-    if (this.state.password !== this.state.confirmPassword) {
-      this.setState({
-        flashMessage: {
-          isVisible: true,
-          text:
-            "Your passwords do not match. Please fix this before continuing.",
-          type: "alert"
-        }
-      });
-      return;
-    }
-
-    if (this.state.password.length < 8) {
-      this.setState({
-        flashMessage: {
-          isVisible: true,
-          text:
-            "Your password is too weak! Please make your password over 8 characters long.",
-          type: "alert"
-        }
-      });
-      return;
-    }
-
-    // Grab values from state
-    const {
-      email,
-      confirmEmail,
-      password,
-      confirmPassword,
-      displayName
-    } = this.state;
-
-    // Create credentials obj
-    const credentials: IProfileUser = {
-      user: { email, confirmEmail, password, confirmPassword, displayName }
-    };
-    try {
-      // dispatch action which calls API to Profile user
-      const { token } = await this.props.Profile({ credentials });
-
-      // Set user to be remembered
-      Auth.authenticateUser({ token, displayName });
-
-      // Redirect user to sauces page -- Maybe take them to user home page instead?
-      this.props.history.push("/sauces");
-    } catch (err) {
-      const text = Err.getError({ err });
-      // Create warning flash
-      this.setState({
-        flashMessage: {
-          isVisible: true,
-          text,
-          type: "warning"
-        }
-      });
-    }
+    // Return whichever value is already there
+    return this.state.enabled.email;
   };
+
+  private toggleConfirmPassword = (): boolean => {
+    return true;
+  };
+
+  private onSubmit = async (event: React.FormEvent): Promise<any> => {
+    return;
+  };
+  //   event.preventDefault();
+
+  //   if (this.state.email !== this.state.confirmEmail) {
+  //     this.setState({
+  //       flashMessage: {
+  //         isVisible: true,
+  //         text: "Your emails do not match. Please fix this before continuing.",
+  //         type: "alert"
+  //       }
+  //     });
+  //     return;
+  //   }
+
+  //   if (this.state.password !== this.state.confirmPassword) {
+  //     this.setState({
+  //       flashMessage: {
+  //         isVisible: true,
+  //         text:
+  //           "Your passwords do not match. Please fix this before continuing.",
+  //         type: "alert"
+  //       }
+  //     });
+  //     return;
+  //   }
+
+  //   if (this.state.password.length < 8) {
+  //     this.setState({
+  //       flashMessage: {
+  //         isVisible: true,
+  //         text:
+  //           "Your password is too weak! Please make your password over 8 characters long.",
+  //         type: "alert"
+  //       }
+  //     });
+  //     return;
+  //   }
+
+  //   // Grab values from state
+  //   const {
+  //     email,
+  //     confirmEmail,
+  //     password,
+  //     confirmPassword,
+  //     displayName
+  //   } = this.state;
+
+  //   // Create credentials obj
+  //   const credentials: IRegisterUser = {
+  //     user: { email, confirmEmail, password, confirmPassword, displayName }
+  //   };
+  //   try {
+  //     // dispatch action which calls API to Profile user
+  //     const { token } = await this.props.Profile({ credentials });
+
+  //     // Set user to be remembered
+  //     Auth.authenticateUser({ token, displayName });
+
+  //     // Redirect user to sauces page -- Maybe take them to user home page instead?
+  //     this.props.history.push("/sauces");
+  //   } catch (err) {
+  //     const text = Err.getError({ err });
+  //     // Create warning flash
+  //     this.setState({
+  //       flashMessage: {
+  //         isVisible: true,
+  //         text,
+  //         type: "warning"
+  //       }
+  //     });
+  //   }
+  // };
 }
 
 const mapState2Props = (state: AppState) => {
-  return {};
+  return { user: state.users.self };
 };
 
-const mapDispatch2Props = {
-  Profile
-};
+// For TS w/ redux-thunk: https://github.com/reduxjs/redux-thunk/issues/213#issuecomment-428380685
+const mapDispatch2Props = (dispatch: MyThunkDispatch) => ({
+  getInfo: ({
+    data
+  }: {
+    data: { user: { token: string }; displayName: string };
+  }) => dispatch(getInfo({ data }))
+});
 
 export default connect(
   mapState2Props,
