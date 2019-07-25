70f4d06e8909c085f36357988e7bcf5a7a6fbe78
Kyle Bonar <kylewb2011@gmail.com>
2019-07-25 11:15:48 -0500

    Class now simple function to standardize errors

--- src/utils/Err/Err.ts
+++ src/utils/Err/Err.ts
@@ -1,23 +1,25 @@
-const DEFAULT_MSG =
-  "Something wasn't configured correctly. Please make sure your form is filled out entirely and try again.";
-
-class Err {
-  // set local storage w/ token and datetime
-  public static getError({ err }: { err: any }): string {
-    if (err.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      return err.response.status === 401 ? err.response.data.msg : DEFAULT_MSG;
-    } else if (err.request) {
-      // The request was made but no response was received
-      // `err.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      return "Network error - Please try again";
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      return DEFAULT_MSG;
-    }
-  }
+// Return interface
+interface IErrReturn {
+  response: { data: { msg: string; status: number } };
 }
+
+// Params interface
+interface IErrParams {
+  msg: string;
+  status: number;
+}
+
+// Function designed to resemble how a network error would look like.
+// This will help the catch(err) only have to handle a single format
+const Err = ({ msg, status }: IErrParams): IErrReturn => {
+  return {
+    response: {
+      data: {
+        status,
+        msg
+      }
+    }
+  };
+};
 
 export default Err;
