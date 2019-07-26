c04ec111e074ce9a465b4760a26fe21fc86ee020
Kyle Bonar <kylewb2011@gmail.com>
2019-07-26 08:33:50 -0500

    Init commit

---
+++ src/screens/account/settings/UpdateDisplayName/UpdateDisplayNameStyle.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import Article from "../../../../components/Article/Article";
+
+export const StyledDiv = styled.div`
+  height: 100vh;
+`;
+
+export const StyledLogoContainer = styled.div`
+  max-width: 150px;
+  margin: 0 auto;
+  padding: 1em;
+`;
+
+export const StyledArticle = styled(Article)`
+  max-width: 600px;
+`;
+export const StyledFormContainer = styled.div`
+  border: ${props => props.theme.border};
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+export const StyledButtonHolder = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: stretch;
+`;
