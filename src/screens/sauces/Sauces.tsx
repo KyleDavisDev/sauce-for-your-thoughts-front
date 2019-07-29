06927153c801f8ac9df13dd18d96f45a6f573a43
Kyle Bonar <kylewb2011@gmail.com>
2019-07-29 00:46:14 -0500

    new state items, brought util function in, update page on submit, fixed redux bug

--- src/screens/sauces/Sauces.tsx
+++ src/screens/sauces/Sauces.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import queryString, { OutputParams } from "query-string";
 
 import { getSaucesByQuery } from "../../redux/sauces/actions";
 import FilterBar from "./components/FilterBar/FilterBar";
@@ -16,7 +17,6 @@ import {
 import { connect } from "react-redux";
 import { AppState, MyThunkDispatch } from "../../redux/configureStore";
 import { ISauce, SaucesParams } from "../../redux/sauces/types";
-import Utils from "../../utils/Utils/Utils";
 
 export interface SaucesProps {
   location: { search: string };
@@ -24,21 +24,35 @@ export interface SaucesProps {
   getSaucesByQuery: ({ query }: { query?: string }) => Promise<null>;
   count?: number;
   page?: number;
+  order?: string;
+  history: { push: (location: string) => null };
 }
 
 export interface SaucesState {
   page: number;
+  limit: number;
+  order: string;
+  type: string;
   minPage: number;
   maxPage: number;
   total: number;
 }
+
+// Constants
+const DEFAULT_LIMIT_COUNT = 15;
+const DEFAULT_PAGE = 1;
+const DEFAULT_TYPE = "all";
+const DEFAULT_ORDER = "newest";
 
 class Sauces extends React.Component<SaucesProps, SaucesState> {
   constructor(props: SaucesProps) {
     super(props);
 
     this.state = {
-      page: 1,
+      page: DEFAULT_PAGE,
+      limit: DEFAULT_LIMIT_COUNT,
+      order: DEFAULT_ORDER,
+      type: DEFAULT_TYPE,
       minPage: 1,
       maxPage: 10, // Will update this value from API
       total: 52 // Will update this value from API
@@ -46,34 +60,35 @@ class Sauces extends React.Component<SaucesProps, SaucesState> {
   }
 
   public componentDidMount() {
-    const params: SaucesParams = Utils.getParamsFromPath({
+    const { page, limit, order, type }: SaucesParams = getParamsFromPath({
       path: this.props.location.search
     });
 
-    this.setState({ ...this.state, page: params.page });
+    // this.setState({ ...this.state, page: page });
 
     window.scrollTo(0, 0); // Move screen to top
 
     // If we don't have sauces, go look for them!
     if (!this.props.sauces) {
       // Construct query string
-      const query = `lim=${params.limit}&order=${params.order}&page=${
-        params.page
-      }&type=${params.type}`;
+      const query = `lim=${limit}&order=${order}&page=${page}&type=${type}`;
       // Call API
       this.props.getSaucesByQuery({ query }).catch(err => console.log(err));
+
+      this.setState({ ...this.state, page, limit, order, type });
     }
   }
 
   public componentWillReceiveProps(props: SaucesProps) {
     // Going to compare current page vs page in URL
-    const params: SaucesParams = Utils.getParamsFromPath({
+    const { page, limit, order, type }: SaucesParams = getParamsFromPath({
       path: props.location.search
     });
     const { page: pageFromState } = this.state;
 
-    if (params.page !== pageFromState) {
-      this.setState({ ...this.state, page: params.page });
+    // Update state if page has changed
+    if (page !== pageFromState) {
+      this.setState({ ...this.state, page, limit, order, type });
     }
 
     window.scrollTo(0, 0); // Move screen to top
@@ -85,13 +100,18 @@ class Sauces extends React.Component<SaucesProps, SaucesState> {
     const count = this.props.count || 0;
     const page = this.props.page || 1;
 
+    console.log(sauces);
     return (
       <div>
         <TopBar />
         <Navigation />
         <StyledArticle>
           <PageTitle>Sauces</PageTitle>
-          <FilterBar onSubmit={this.onSubmit} />
+          <FilterBar
+            onSubmit={this.onSubmit}
+            orderSelected={this.state.order}
+            typeSelected={this.state.type}
+          />
           <StyledCardContainer>
             {sauces.length > 0 &&
               sauces.map((sauce, ind) => {
@@ -117,20 +137,33 @@ class Sauces extends React.Component<SaucesProps, SaucesState> {
   }
 
   private onSubmit = ({ type, order }: { type: string; order: string }) => {
-    console.log(type, order);
+    // Get any params from path
+    const params: SaucesParams = getParamsFromPath({
+      path: this.props.location.search
+    });
+
+    // Construct query string
+    const query = `/sauces?lim=${params.limit}&order=${order}&page=${
+      params.page
+    }&type=${type}`;
+
+    // Go to new page
+    this.props.history.push(query);
   };
 }
 
 function mapStateToProps(state: AppState, myProps: any): any {
-  const { limit, order, page, type } = Utils.getParamsFromPath({
-    path: myProps.match.params
+  // Get path params
+  const { limit, order, page, type } = getParamsFromPath({
+    path: myProps.location.search
   });
-  // Construct query string
-  const queryString = `lim=${limit}&order=${order}&page=${page}&type=${type}`;
+
+  // Construct key string
+  const key = `lim=${limit}&order=${order}&page=${page}&type=${type}`;
 
   // Find the sauces we will render by first getting the array of slugs
   const sauceSlugs2Render: string[] | undefined = state.sauces.query
-    ? state.sauces.query[queryString]
+    ? state.sauces.query[key]
     : [];
 
   // Make sure we have something to work with
@@ -167,3 +200,46 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(Sauces);
+
+/** @description Parse the path location string into components we can comprehend
+ *  @param {String} path string with parsable params
+ *  @returns {Number} limit - # of sauces per page
+ *  @returns {String} order - how the sauces should be sorted
+ *  @returns {Number} page - current page
+ *  @returns {type} type - which sauces should be returned
+ */
+function getParamsFromPath({ path }: { path: string }): SaucesParams {
+  // Get values from string
+  const values: OutputParams = queryString.parse(path);
+
+  let page: number;
+  // Make sure page is not undefined or an array
+  if (values.page && !Array.isArray(values.page)) {
+    // Make sure it's a valid number
+    page = parseInt(values.page, 10);
+  } else {
+    page = DEFAULT_PAGE; // set default
+  }
+
+  let limit: number;
+  // Make sure limit is not undefined or an array
+  if (values.limit && !Array.isArray(values.limit)) {
+    // Make sure it's a valid number
+    limit = parseInt(values.limit, 10);
+    limit = limit < 0 ? 1 : limit;
+  } else {
+    limit = DEFAULT_LIMIT_COUNT; // set default
+  }
+
+  const type: string =
+    values.type && !Array.isArray(values.type)
+      ? values.type.toLowerCase()
+      : DEFAULT_TYPE;
+
+  const order: string =
+    values.order && !Array.isArray(values.order)
+      ? values.order.toLowerCase()
+      : DEFAULT_ORDER;
+
+  return { limit, order, page, type };
+}
