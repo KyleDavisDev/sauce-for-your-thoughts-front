5106ea8981aed4c64c040352d7b968f8a7205a8c
Kyle Bonar <kylewb2011@gmail.com>
2019-08-07 13:27:15 -0500

    init commit

---
+++ src/components/TopBar/TopBarStyle.tsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { Link } from "../Link/Link";
+import { Dropdown, Trigger, Body } from "./components/Dropdown/Dropdown";
+
+export const StyledDiv = styled.div`
+  background-color: ${props => props.theme.white};
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  width: 100%;
+  padding: 0.5em 0em;
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  margin-right: 2em;
+  color: ${x => x.theme.grey};
+  fill: ${x => x.theme.grey};
+
+  &:hover,
+  &:focus {
+    text-decoration: none;
+    color: ${x => x.theme.primaryThemeColor};
+    fill: ${x => x.theme.primaryThemeColor};
+  }
+`;
+
+export const StyledDropDown = styled(Dropdown)`
+  margin-right: 2em;
+`;
+
+export const StyledTrigger = styled(Trigger)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-family: FuturaMedium;
+  color: ${x => x.theme.grey};
+  fill: ${x => x.theme.grey};
+
+  &:hover,
+  &:focus {
+    text-decoration: none;
+    color: ${x => x.theme.primaryThemeColor};
+    fill: ${x => x.theme.primaryThemeColor};
+  }
+`;
+
+export const StyledBody = styled(Body)`
+  right: 0;
+`;
+
+export const StyledAvatar = styled.img`
+  max-width: 35px;
+  margin-left: 5px;
+`;
