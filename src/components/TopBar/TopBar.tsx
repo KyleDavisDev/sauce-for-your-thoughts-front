bf80477fa462d23debb6eba101e383a2809230c4
Kyle Bonar <kylewb2011@gmail.com>
2019-08-07 13:27:23 -0500

    get styles from sister file

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,70 +1,18 @@
 import * as React from "react";
-import { Link } from "../Link/Link";
 import { connect } from "react-redux";
-
-import styled from "../../theme/styled-components";
-import { Dropdown, Trigger, Body } from "./components/Dropdown/Dropdown";
+import ChevronDown from "../../images/icons/ChevronDown";
+import LoginIcon from "../../images/icons/LoginIcon";
+import UserIcon from "../../images/icons/UserIcon";
 import { AppState } from "../../redux/configureStore";
 import Menu from "./components/Menu/Menu";
-
-// SVG icons
-import UserIcon from "../../images/icons/UserIcon";
-import LoginIcon from "../../images/icons/LoginIcon";
-import ChevronDown from "../../images/icons/ChevronDown";
-
-const StyledDiv = styled.div`
-  background-color: ${props => props.theme.white};
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  width: 100%;
-  padding: 0.5em 0em;
-`;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  display: flex;
-  align-items: center;
-  margin-right: 2em;
-  color: ${x => x.theme.grey};
-  fill: ${x => x.theme.grey};
-
-  &:hover,
-  &:focus {
-    text-decoration: none;
-    color: ${x => x.theme.primaryThemeColor};
-    fill: ${x => x.theme.primaryThemeColor};
-  }
-`;
-
-const StyledDropDown = styled(Dropdown)`
-  margin-right: 2em;
-`;
-
-const StyledTrigger = styled(Trigger)`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  font-family: FuturaMedium;
-  color: ${x => x.theme.grey};
-  fill: ${x => x.theme.grey};
-
-  &:hover,
-  &:focus {
-    text-decoration: none;
-    color: ${x => x.theme.primaryThemeColor};
-    fill: ${x => x.theme.primaryThemeColor};
-  }
-`;
-
-const StyledBody = styled(Body)`
-  right: 0;
-`;
-
-const StyledAvatar = styled.img`
-  max-width: 35px;
-  margin-left: 5px;
-`;
+import {
+  StyledAvatar,
+  StyledBody,
+  StyledDiv,
+  StyledDropDown,
+  StyledLink,
+  StyledTrigger
+} from "./TopBarStyle";
 
 export interface TopBarProps {
   isLoggedIn?: boolean;
