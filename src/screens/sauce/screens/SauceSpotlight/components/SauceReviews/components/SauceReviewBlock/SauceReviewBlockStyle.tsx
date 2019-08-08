e8a8f96ca5b9c90a972df13d296012f2eb588413
Kyle Bonar <kylewb2011@gmail.com>
2019-08-08 09:34:20 -0500

    New components for avatar

--- src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/components/SauceReviewBlock/SauceReviewBlockStyle.tsx
+++ src/screens/sauce/screens/SauceSpotlight/components/SauceReviews/components/SauceReviewBlock/SauceReviewBlockStyle.tsx
@@ -82,3 +82,22 @@ export const StyledFullStar = styled(Star)`
     fill: ${props => props.theme.primaryThemeColor};
   }
 `;
+
+export const StyledAvatar = styled.img`
+  max-width: 35px;
+  margin-left: 5px;
+  margin-right: 5px;
+`;
+
+export const StyledReviewerContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const StyledReviewer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 5px;
+`;
