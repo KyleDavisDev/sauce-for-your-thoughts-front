e0314d403c13bc284fdd97c688a9883d37a3459d
Kyle Bonar <kylewb2011@gmail.com>
2019-08-09 09:22:33 -0500

    Update a display name

--- src/redux/users/reducer.ts
+++ src/redux/users/reducer.ts
@@ -4,7 +4,8 @@ import {
   USER_LOGGED_IN,
   USER_LOGGED_OUT,
   USER_ADDED,
-  USER_CLEARED
+  USER_CLEARED,
+  USER_UPDATE_DISPLAYNAME
 } from "./types";
 import { Reducer } from "redux";
 
@@ -59,6 +60,37 @@ const userReducer: Reducer<IUserState> = (
     }
     case USER_CLEARED: {
       return initialState;
+    }
+    case USER_UPDATE_DISPLAYNAME: {
+      // Grab variables from action.
+      const { displayName, oldDisplayName } = action;
+      // If cannot find then return immediately.
+      if (!displayName || !oldDisplayName) return state;
+      // If the same, return immediately
+      if (displayName === oldDisplayName) return state;
+
+      // Make sure we have full state.self
+      if (!state.self) return state;
+      // Make sure we have content in byDisplayName
+      if (!state.byDisplayName) return state;
+
+      // Update in self
+      const self = { ...state.self, displayName };
+      // remove old from array, push into array
+      const allDisplayNames = state.allDisplayNames
+        ? state.allDisplayNames
+            .filter(name => {
+              return name !== oldDisplayName;
+            })
+            .concat([displayName])
+        : [displayName];
+      // update key and property in obj
+      const byDisplayName = {
+        ...state.byDisplayName,
+        [displayName]: state.byDisplayName[oldDisplayName]
+      };
+
+      return { self, allDisplayNames, byDisplayName };
     }
 
     default:
