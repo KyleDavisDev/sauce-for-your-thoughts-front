d85433a130a8f8c77fab338ab3c7cb905f1109b5
Kyle Bonar <kylewb2011@gmail.com>
2019-08-09 08:08:19 -0500

    Pass only necessary info to redux action

--- src/screens/account/settings/UpdateEmail/UpdateEmail.tsx
+++ src/screens/account/settings/UpdateEmail/UpdateEmail.tsx
@@ -27,17 +27,7 @@ import Auth from "../../../../utils/Auth/Auth";
 export interface UpdateEmailProps {
   history: { push: (location: string) => null };
   user: { token: string; displayName: string; avatarURL: string };
-  updateEmail: ({
-    data,
-    token,
-    displayName,
-    avatarURL
-  }: {
-    data: IUserUpdateEmail;
-    token: string;
-    displayName: string;
-    avatarURL: string;
-  }) => Promise<null>;
+  updateEmail: ({ data }: { data: IUserUpdateEmail }) => Promise<null>;
   logout: () => null;
 }
 
@@ -222,19 +212,12 @@ class UpdateEmail extends React.Component<UpdateEmailProps, UpdateEmailState> {
       return;
     }
 
-    // Get displayName and avatarURL or else redirect
-    const { displayName, avatarURL } = this.props.user;
-    if (!displayName || !avatarURL) {
-      this.props.history.push("/account/login?return=/account/settings/email");
-      return;
-    }
-
     // Construct data
     const data: IUserUpdateEmail = {
       user: { token, email, confirmEmail, password }
     };
     try {
-      await this.props.updateEmail({ data, displayName, avatarURL, token });
+      await this.props.updateEmail({ data });
 
       // clear input and display flash
       this.setState({
@@ -280,16 +263,13 @@ const mapState2Props = (state: AppState) => {
 // For TS w/ redux-thunk: https://github.com/reduxjs/redux-thunk/issues/213#issuecomment-428380685
 const mapDispatch2Props = (dispatch: MyThunkDispatch) => ({
   updateEmail: ({
-    data,
-    token,
-    displayName,
-    avatarURL
+    data
   }: {
     data: IUserUpdateEmail;
     token: string;
     displayName: string;
     avatarURL: string;
-  }) => dispatch(updateEmail({ data, token, displayName, avatarURL })),
+  }) => dispatch(updateEmail({ data })),
   logout: () => dispatch(logout())
 });
 
