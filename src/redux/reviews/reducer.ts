4887aee32bae353bd7bebf1382527566a8511bd2
Kyle Bonar <kylewb2011@gmail.com>
2019-08-12 09:20:47 -0500

    update a single user's display name

--- src/redux/reviews/reducer.ts
+++ src/redux/reviews/reducer.ts
@@ -3,7 +3,8 @@ import {
   IReviewsState,
   IReviewsAction,
   REVIEWS_ADDED,
-  REVIEWS_UPDATED
+  REVIEWS_UPDATED,
+  REVIEWS_UPDATED_DISPLAYNAME
 } from "./types";
 
 const initialState: IReviewsState = {
@@ -43,6 +44,45 @@ const reviewReducer: Reducer<IReviewsState> = (
       const byReviewID = { ...state.byReviewID, ...action.byReviewID };
 
       // return state
+      return { ...state, byReviewID };
+    }
+
+    case REVIEWS_UPDATED_DISPLAYNAME: {
+      // Grab variables from action.
+      const { displayName, oldDisplayName } = action;
+      // If cannot find then return immediately.
+      if (!displayName || !oldDisplayName) return state;
+      // If the same, return immediately
+      if (displayName === oldDisplayName) return state;
+      // init
+      let byReviewID = {};
+
+      // make sure we have .bySlug
+      if (state.byReviewID && Object.keys(state.byReviewID).length > 0) {
+        // Go through items and update name
+        byReviewID = Object.keys(state.byReviewID).map(slug => {
+          // make sure we have a review -- Here bc TS gets upset otherwise
+          if (!state.byReviewID) return;
+
+          // If the review doesn't have an author, can return now
+          if (!state.byReviewID[slug].author) {
+            return { ...state.byReviewID[slug] };
+          }
+
+          // Find old display name
+          if (state.byReviewID[slug].author === oldDisplayName) {
+            // update display name
+            state.byReviewID[slug].author = displayName;
+          }
+
+          return { ...state.byReviewID[slug] };
+        });
+      } else {
+        // Nothing to update since displayNAme is only in bySlug
+        return state;
+      }
+
+      // Return
       return { ...state, byReviewID };
     }
 
