a34c3e44a27c781b8856350231426ca8d1b4636a
Kyle Bonar <kylewb2011@gmail.com>
2019-08-12 09:09:14 -0500

    Updated type

--- src/redux/sauces/types.ts
+++ src/redux/sauces/types.ts
@@ -44,6 +44,8 @@ export interface IAddSaucesAction {
 // Used for action emitters and reducer
 export interface ISaucesReturnAction extends IAddSaucesAction {
   type: string;
+  oldDisplayName?: string;
+  displayName?: string;
 }
 
 // Used for redux state
@@ -74,6 +76,7 @@ export const SAUCES_ADDED = "SAUCES_ADDED";
 export const SAUCES_UPDATE = "SAUCES_UPDATE";
 export const SAUCES_CLEARED = "SAUCES_CLEARED";
 export const SAUCES_REMOVED = "SAUCES_REMOVED";
+export const SAUCES_UPDATE_DISPLAYNAME = "SAUCES_UPDATE_DISPLAYNAME";
 export const SAUCE_FOUND = "SAUCE_FOUND";
 export const SAUCE_UPDATE = "SAUCE_UPDATE";
 
