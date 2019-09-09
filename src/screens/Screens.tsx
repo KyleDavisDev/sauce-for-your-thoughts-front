bde47eb893998591a84b27b761e8f34d3576e0aa
Kyle Bonar <kylewb2011@gmail.com>
2019-09-09 15:43:23 -0500

    Does not need to be exact route

--- src/screens/Screens.tsx
+++ src/screens/Screens.tsx
@@ -177,11 +177,7 @@ const Screens = (
     <Route path="/account/settings/password" component={UpdatePassword} />
     <Route path="/account/settings/displayname" component={UpdateDisplayName} />
     <Route path="/account/settings/avatar" component={UpdateAvatar} />
-    <Route
-      exact
-      path="/account/confirm/email/:email"
-      component={ConfirmEmail}
-    />
+    <Route path="/account/confirm/email/:email" component={ConfirmEmail} />
     <Route path="/account/login" component={Login} />
     <Route path="/reset" component={Reset} />
     <Route exact path="/sauce" component={SauceSpotlight} />
