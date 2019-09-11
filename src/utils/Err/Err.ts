399a85a3d0b9e9d32574d9efce919fbdac3e258f
Kyle Bonar <kylewb2011@gmail.com>
2019-09-11 11:07:08 -0500

    Accept isGood if passed

--- src/utils/Err/Err.ts
+++ src/utils/Err/Err.ts
@@ -1,22 +1,24 @@
 // Return interface
 export interface IErrReturn {
-  response: { data: { msg: string; status: number } };
+  response: { data: { msg: string; status: number; isGood: boolean } };
 }
 
 // Params interface
 interface IErrParams {
   msg: string;
   status: number;
+  isGood?: boolean;
 }
 
 // Function designed to resemble how a network error would look like.
 // This will help the catch(err) only have to handle a single format
-const Err = ({ msg, status }: IErrParams): IErrReturn => {
+const Err = ({ msg, status, isGood = false }: IErrParams): IErrReturn => {
   return {
     response: {
       data: {
         status,
-        msg
+        msg,
+        isGood
       }
     }
   };
