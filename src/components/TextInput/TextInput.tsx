23f50744327dcf3867b91299af567f455f613c26
Kyle Bonar <kylewb2011@gmail.com>
2019-09-12 09:58:57 -0500

    Display a bit of helper text under input if passed

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -16,6 +16,7 @@ interface TextInputProps {
   value?: string | number;
   className?: string;
   disabled?: boolean;
+  requirementText?: string;
   onChange(
     event:
       | React.ChangeEvent<HTMLInputElement>
@@ -46,24 +47,46 @@ class TextInput extends React.PureComponent<TextInputProps, TextInputState> {
         {this.props.showLabel && this.props.label && (
           <Label htmlFor={this.state.id}>
             {this.props.label}
-            {this.props.required ? "*" : ""}
+            {this.props.required ? (
+              <span style={{ color: "#B20000" }}>*</span>
+            ) : (
+              ""
+            )}
           </Label>
         )}
 
         {/* Make sure prop passed and is either 'text' or 'password' */}
         {this.props.type && (
-          <StyledInput
-            type={this.props.type}
-            id={this.state.id}
-            name={this.props.name}
-            value={this.props.value}
-            placeholder={this.props.placeholder}
-            onChange={this.props.onChange}
-            required={this.props.required}
-            aria-required={this.props.required}
-            disabled={this.props.disabled}
-            aria-disabled={this.props.disabled}
-          />
+          <div>
+            <StyledInput
+              type={this.props.type}
+              id={this.state.id}
+              name={this.props.name}
+              value={this.props.value}
+              placeholder={this.props.placeholder}
+              onChange={this.props.onChange}
+              required={this.props.required}
+              aria-required={this.props.required}
+              disabled={this.props.disabled}
+              aria-disabled={this.props.disabled}
+              style={{
+                marginBottom: this.props.requirementText ? "0px" : "15px"
+              }}
+            />
+            {this.props.requirementText ? (
+              <p
+                style={{
+                  fontSize: ".85rem",
+                  marginTop: "1px",
+                  marginBottom: "15px"
+                }}
+              >
+                {this.props.requirementText}
+              </p>
+            ) : (
+              ""
+            )}
+          </div>
         )}
       </StyledDiv>
     );
@@ -73,6 +96,7 @@ class TextInput extends React.PureComponent<TextInputProps, TextInputState> {
 const StyledTextInput = styled(TextInput)`
   input {
     background-color: ${props => (props.disabled ? "#eee" : props.theme.white)};
+    box-shadow: none;
 
     :hover,
     :focus {
