fe389a9bfc56b95e07314b741f3b30970b5cb955
Kyle Bonar <kylewb2011@gmail.com>
2019-09-13 14:43:08 -0500

    Update refresh logic

--- src/utils/Auth/Auth.ts
+++ src/utils/Auth/Auth.ts
@@ -175,40 +175,17 @@ class Auth {
     const sfytKey: string | null = localStorage.getItem("sfytKey");
 
     // Make sure we have key
-    if (sfytKey === null) return false;
-
-    // grab values from key
-    const {
-      token,
-      displayName,
-      avatarURL
-    }: { token: string; displayName: string; avatarURL: string } = JSON.parse(
-      sfytKey
-    );
-
-    // Make sure we have token
-    if (token === null) return false;
-
-    // make sure token is string and not empty
-    if (
-      Object.prototype.toString.call(token) === "[object String]" &&
-      token.length > 0
-    ) {
-      // apply new time to key
-      const key = {
-        token,
-        timestamp: new Date().getTime(),
-        displayName,
-        avatarURL
-      };
-      // set key
-      localStorage.setItem("sfytKey", JSON.stringify(key));
-      return true;
-    } else {
+    if (sfytKey === null) {
       // remove local storage item
       this.deauthenticateUser();
       return false;
     }
+
+    // apply new time to key
+    const key = { ...JSON.parse(sfytKey), timestamp: new Date().getTime() };
+    // set key
+    localStorage.setItem("sfytKey", JSON.stringify(key));
+    return true;
   }
 }
 
