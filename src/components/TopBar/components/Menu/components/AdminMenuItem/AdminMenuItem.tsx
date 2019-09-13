8393c9548c3142310ca3645543d21320151ebb45
Kyle Bonar <kylewb2011@gmail.com>
2019-09-13 14:55:49 -0500

    Init commit

---
+++ src/components/TopBar/components/Menu/components/AdminMenuItem/AdminMenuItem.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import styled from "styled-components";
+
+import Title from "../Title/Title";
+import Item from "../Item/Item";
+
+const StyledTitle = styled(Title)`
+  padding-bottom: 0px;
+`;
+
+export interface AdminMenuItemProps {}
+
+class AdminMenuItem extends React.PureComponent<AdminMenuItemProps, any> {
+  public render() {
+    return (
+      <div>
+        <StyledTitle>Admin</StyledTitle>
+        <Item to="#">Approve Submissions</Item>
+      </div>
+    );
+  }
+}
+
+export default AdminMenuItem;
