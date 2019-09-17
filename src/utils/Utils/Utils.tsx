aa5b206bdf3e84f3638a7732ff84e1e9788f7a97
Kyle Bonar <kylewb2011@gmail.com>
2019-09-17 15:48:38 -0500

    New method to convert PascalCase to camelCase

--- src/utils/Utils/Utils.tsx
+++ src/utils/Utils/Utils.tsx
@@ -81,6 +81,49 @@ class Utils {
   public static isNumber(input: any) {
     return Object.prototype.toString.call(input) === "[object Number]";
   }
+
+  /** @description Converts PascalCase JSON to camelCase
+   *  @param {any} o - array/obj/string/etc
+   *  @return {any} o - camelCased'd o
+   */
+  public static toCamel(o: any): any {
+    let newO: any;
+    let origKey: any;
+    let newKey: any;
+    let value: any;
+    if (o instanceof Array) {
+      return o.map(val => {
+        if (typeof val === "object") {
+          return Utils.toCamel(val);
+        }
+        return val;
+      });
+    } else {
+      newO = {};
+      for (origKey in o) {
+        if (o.hasOwnProperty(origKey)) {
+          // This will allow a key that is ALL uppercase to remain ALL uppercase
+          if (origKey === origKey.toUpperCase()) {
+            newKey = origKey.toString();
+          } else {
+            // Else camel case
+            newKey = (
+              origKey.charAt(0).toLowerCase() + origKey.slice(1) || origKey
+            ).toString();
+          }
+          value = o[origKey];
+          if (
+            value instanceof Array ||
+            (value !== null && value.constructor === Object)
+          ) {
+            return Utils.toCamel(value);
+          }
+          newO[newKey] = value;
+        }
+      }
+    }
+    return newO;
+  }
 }
 
 export default Utils;
