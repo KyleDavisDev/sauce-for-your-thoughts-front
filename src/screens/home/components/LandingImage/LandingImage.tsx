eb3132b209715b33de54373497485091e113c139
Kyle Bonar <kylewb2011@gmail.com>
2019-10-21 17:24:04 -0500

    Update folder name

---
+++ src/screens/home/components/LandingImage/LandingImage.tsx
@@ -0,0 +1,128 @@
+import * as React from "react";
+import { connect } from "react-redux";
+
+import { AppState } from "../../../../redux/configureStore";
+import {
+  HeroContainer,
+  HeroImage,
+  HeroBody,
+  HeroTitle,
+  StyledDiv,
+  StyledDropDown,
+  StyledInput,
+  StyledButton
+} from "./LandingImageStyle";
+
+export interface LandingImageProps {
+  className?: string;
+  types: string[];
+  history: { push: (location: string) => any };
+}
+
+export interface LandingImageState {
+  search: {
+    value: string;
+  };
+  filter: {
+    types: string[];
+    selectedValue: string;
+  };
+}
+
+class LandingImage extends React.Component<
+  LandingImageProps,
+  LandingImageState
+> {
+  constructor(props: LandingImageProps) {
+    super(props);
+
+    this.state = {
+      search: { value: "" },
+      filter: { types: this.props.types, selectedValue: "all" }
+    };
+  }
+
+  public render() {
+    return (
+      <HeroContainer className={this.props.className}>
+        <HeroImage />
+        <HeroBody>
+          <HeroTitle>Find your perfect sauce</HeroTitle>
+          <form onSubmit={this.onSubmit}>
+            <StyledDiv>
+              <StyledDropDown
+                options={this.state.filter.types}
+                selectedValue={this.state.filter.selectedValue}
+                onSelect={this.onSelect}
+              />
+              <StyledInput
+                type="text"
+                id="Hero__Search"
+                name="Hero__Search"
+                onChange={this.onTextChange}
+                value={this.state.search.value}
+                placeholder="Search..."
+              />
+
+              <StyledButton type="submit">Search</StyledButton>
+            </StyledDiv>
+          </form>
+        </HeroBody>
+      </HeroContainer>
+    );
+  }
+
+  private onSubmit = async (event: React.FormEvent): Promise<null> => {
+    event.preventDefault();
+
+    let query = `/sauces?srch=${this.state.search.value}`;
+    if (this.state.filter.selectedValue.toLowerCase() !== "all") {
+      query += `&type=${this.state.filter.selectedValue}`;
+    }
+
+    // take person to sauces page w/ prefilled search query
+    this.props.history.push(query);
+    return null;
+  };
+
+  private onSelect = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+    if (!event || !event.target || !event.target.value) {
+      return;
+    }
+
+    // Grab the value
+    const selectedValue: string = event.target.value;
+
+    // Update local state
+    this.setState({
+      ...this.state,
+      filter: {
+        ...this.state.filter,
+        selectedValue
+      }
+    });
+  };
+
+  private onTextChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    if (!event || !event.target || !event.target.value) {
+      return;
+    }
+
+    // Grab the value
+    const value: string = event.target.value;
+
+    // Update local state
+    this.setState({
+      ...this.state,
+      search: {
+        value
+      }
+    });
+  };
+}
+
+const mapState2Props = (state: AppState) => {
+  return { types: state.sauces.types };
+};
+
+export default connect(mapState2Props)(LandingImage);
---
+++ src/screens/home/components/LandingImage/LandingImageStyle.tsx
@@ -0,0 +1,84 @@
+import styled from "../../../../theme/styled-components";
+import DropDown from "../../../../components/DropDown/DropDown";
+import { TextInput } from "../../../../components/TextInput/TextInput";
+import { Button } from "../../../../components/Button/Button";
+
+export const HeroContainer = styled.div`
+  background: #000;
+  position: relative;
+  margin-bottom: 1em;
+`;
+
+export const HeroImage = styled.div`
+  background-image: url("../../../../images/photos/Sauces.jpg");
+  background-size: cover;
+  background-position-y: 45%;
+  opacity: 0.45;
+  height: 400px;
+`;
+
+export const HeroBody = styled.div`
+  position: absolute;
+  z-index: 1;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  padding: 0 2em;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+`;
+
+export const HeroTitle = styled.h1`
+  color: ${x => x.theme.landingHeroTextColor};
+  font-family: FuturaMedium;
+`;
+
+export const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
+`;
+
+export const StyledDropDown = styled(DropDown)`
+  min-height: 30px;
+  border: 0px;
+  box-sizing: border-box;
+
+  > div {
+    width: auto;
+  }
+
+  select {
+    width: auto;
+    border: 0;
+  }
+`;
+
+export const StyledInput = styled(TextInput)`
+  flex-direction: row;
+  align-items: stretch;
+
+  input {
+    border: 0px;
+    margin-top: 0px;
+    margin-bottom: 0px;
+    width: 100%;
+    min-width: auto;
+    max-width: 100%;
+    padding: 0.66em;
+    box-sizing: border-box;
+    border: 0px;
+    font-size: 1em;
+
+    @media (min-width: ${props => props.theme.smToMd}) {
+      min-width: 20em;
+    }
+  }
+`;
+
+export const StyledButton = styled(Button)`
+  display: flex;
+`;
