caab388662373ecf75e450216b4ac86f1fd3bc29
Kyle Bonar <kylewb2011@gmail.com>
2019-12-05 09:56:20 -0600

    updated import

--- src/screens/sauce/screens/SauceAdd/components/SaucePhoto/SaucePhoto.tsx
+++ src/screens/sauce/screens/SauceAdd/components/SaucePhoto/SaucePhoto.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import DropNCrop from "@synapsestudios/react-drop-n-crop";
+import { DropNCrop } from "@synapsestudios/react-drop-n-crop";
 
 import { Button } from "../../../../../../components/Button/Button";
 import {
