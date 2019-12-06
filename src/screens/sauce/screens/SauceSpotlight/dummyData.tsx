72a92c3e5fa5f29bdb74c2259e04e9384645744f
Kyle Bonar <kylewb2011@gmail.com>
2019-12-06 08:34:34 -0600

    Update to match IReview type

--- src/screens/sauce/screens/SauceSpotlight/dummyData.tsx
+++ src/screens/sauce/screens/SauceSpotlight/dummyData.tsx
@@ -3,9 +3,9 @@ import { IReview } from "../../../../redux/reviews/types";
 export const dummyData: IReview[] = [
   {
     _id: "58c6a8aafb4d61115815f8b4",
-    created: "2017-03-13T14:11:54.848Z",
-    sauce: { slug: "5" },
-    author: { displayName: "1" },
+    created: 1575642773,
+    sauce: "5",
+    author: "1",
     overall: {
       rating: 3,
       txt:
@@ -25,9 +25,9 @@ export const dummyData: IReview[] = [
   },
   {
     _id: "58c6a8b8fb4d61115815f8b5",
-    created: "2017-03-13T14:12:08.255Z",
-    sauce: { slug: "5" },
-    author: { displayName: "2" },
+    created: 1575642773,
+    sauce: "5",
+    author: "2",
     overall: { rating: 1, txt: "here is some test text" },
     label: { rating: 1, txt: "here is some test text" },
     taste: { rating: 5, txt: "here is some test text" },
@@ -35,9 +35,9 @@ export const dummyData: IReview[] = [
   },
   {
     _id: "58c6a8cefb4d61115815f8b6",
-    created: "2017-03-13T14:12:30.578Z",
-    sauce: { slug: "5" },
-    author: { displayName: "3" },
+    created: 1575642773,
+    sauce: "5",
+    author: "3",
     overall: { rating: 4, txt: "On the mild side, but balanced well" },
 
     aroma: { rating: 4, txt: "here is some test text" },
@@ -51,9 +51,9 @@ export const dummyData: IReview[] = [
   },
   {
     _id: "58c6a8e5fb4d61115815f8b7",
-    created: "2017-03-13T14:12:53.559Z",
-    sauce: { slug: "5" },
-    author: { displayName: "4" },
+    created: 1575642773,
+    sauce: "5",
+    author: "4",
     overall: { rating: 1, txt: "here is some test text" },
     label: {
       rating: 1,
@@ -74,9 +74,9 @@ export const dummyData: IReview[] = [
   },
   {
     _id: "58c6a8f6fb4d61115815f8b8",
-    created: "2017-03-13T14:13:10.681Z",
-    sauce: { slug: "5" },
-    author: { displayName: "122" },
+    created: 1575642773,
+    sauce: "5",
+    author: "122",
     overall: { rating: 2, txt: "here is some test text" },
     label: {
       rating: 3,
