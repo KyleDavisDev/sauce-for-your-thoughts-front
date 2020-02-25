5ddbc2036a3675f906a6b89564ab936b8623ccf2
Kyle Bonar <kylewb2011@gmail.com>
2020-02-25 08:46:44 -0600

    init commit

---
+++ pages/sauces.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface ISaucesProps {}
+
+const Sauces: React.FunctionComponent<ISaucesProps> = props => {
+  return <p>hello</p>;
+};
+
+export default Sauces;
