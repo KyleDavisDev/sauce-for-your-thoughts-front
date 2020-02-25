d8fb833e5ed5c7ada0137454ef640ca5a8e21172
Kyle Bonar <kylewb2011@gmail.com>
2020-02-25 08:47:08 -0600

    Improved custom hook logic, abstracted render logic

--- src/screens/home/components/FeaturedSauces/FeaturedSauces.tsx
+++ src/screens/home/components/FeaturedSauces/FeaturedSauces.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState, Dispatch } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import SectionTitle from "../../../../components/SectionTitle/SectionTitle";
 import { ISauce } from "../../../../redux/sauces/types";
-import { AppState, ISaucesState } from "../../../../redux/configureStore";
+import { AppState } from "../../../../redux/configureStore";
 import { getSaucesByFeatured } from "../../../../redux/sauces/actions";
 
 import {
@@ -30,68 +30,68 @@ const FeaturedSauces: React.SFC<FeaturedSaucesProps> = props => {
         description="Check out some of these unique sauces. Discover flavors you've never tasted before!"
       />
       <StyledCardContainer>
-        {loading ? (
-          <p>Loading...</p>
-        ) : featuredSauces && featuredSauces.length > 0 ? (
-          featuredSauces.map((sauce, ind) => {
-            return (
-              <StyledCardHolder key={ind}>
-                <StyledCard
-                  title={sauce.name}
-                  imageLink={`${sauce.photo}`}
-                  description={sauce.description}
-                  to={`/sauce?s=${sauce.slug}`}
-                />
-              </StyledCardHolder>
-            );
-          })
-        ) : (
-          "No sauces found..."
-        )}
+        {renderContent(loading, featuredSauces)}
       </StyledCardContainer>
     </StyledDiv>
   );
+
+  function renderContent(loading: boolean, featuredSauces: ISauce[] | null) {
+    if (loading) {
+      return <p>Loading...</p>;
+    }
+
+    if (featuredSauces && featuredSauces.length > 0) {
+      return featuredSauces.map((sauce, ind) => {
+        return (
+          <StyledCardHolder key={ind}>
+            <StyledCard
+              title={sauce.name}
+              imageLink={`${sauce.photo}`}
+              description={sauce.description}
+              to={`/sauce?s=${sauce.slug}`}
+            />
+          </StyledCardHolder>
+        );
+      });
+    }
+
+    return <p> "No sauces found..." </p>;
+  }
 };
 
 function useFeatedSauces(): [ISauce[] | null, boolean] {
   // get sauces from redux store
   const { sauces } = useSelector((state: AppState) => state);
   // assign results
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<ISauce[]>([]);
   // assign loading
   const [loading, setLoading] = useState(false);
-  console.log(loading);
+
   // assign dispatch
   const dispatch = useDispatch();
-  console.log(1);
 
-  // check if we need to dispatch redux action or not
-  if (sauces.featured.length === 0 && !loading) {
-    console.log(2);
-    // emmit redux action
-    dispatch(getSaucesByFeatured());
-  }
+  // run once
+  useEffect(() => {
+    // check if we need to dispatch redux action or not
+    if (sauces.featured.length === 0 && !loading) {
+      // emmit redux action to populate sauces.featured
+      dispatch(getSaucesByFeatured());
 
+      // update loading
+      setLoading(true);
+    }
+  }, []);
+
+  // run once and when sauces.featured changes
   useEffect(() => {
     async function getData() {
       try {
-        console.log(4);
-        // set loading
-        setLoading(true);
-
         // Find the sauces we will render by first getting the array of slugs
-        const sauceSlugs2Render: string[] | undefined = sauces.featured
-          ? sauces.featured
-          : [];
-        console.log(5);
-        // Make sure we have something to work with
-        if (!sauceSlugs2Render || sauceSlugs2Render.length === 0) {
-          return null;
-        }
-        console.log(6);
+        const sauceSlugs2Render = sauces.featured;
+
         // Make sure our store has content
         const bySlug = sauces.bySlug ? sauces.bySlug : {};
-        if (!bySlug) return null;
+        if (!bySlug) return [null, loading];
 
         // Find actual sauces
         const featured = sauceSlugs2Render
@@ -101,21 +101,18 @@ function useFeatedSauces(): [ISauce[] | null, boolean] {
           : [];
 
         // Make sure we found the sauces
-        if (featured.length === 0) return null;
-        console.log(7);
+        if (featured.length === 0) return [null, loading];
+
         setResults(featured);
-        return null;
       } catch {
       } finally {
-        console.log(8);
         // finish loading
-        setLoading(true);
+        setLoading(false);
       }
-      return null;
+      return [null, loading];
     }
 
-    if (results.length === 0) {
-      console.log(3);
+    if (results.length === 0 && sauces.featured.length > 0) {
       getData();
     }
   }, [sauces.featured]);
@@ -123,9 +120,4 @@ function useFeatedSauces(): [ISauce[] | null, boolean] {
   return [results, loading];
 }
 
-// For TS w/ redux-thunk: https://github.com/reduxjs/redux-thunk/issues/213#issuecomment-428380685
-// const mapDispatchToProps = (dispatch: MyThunkDispatch) => ({
-//   getSaucesByFeatured: () => dispatch(getSaucesByFeatured())
-// });
 export default FeaturedSauces;
-// export default connect(mapStateToProps, mapDispatchToProps)(FeaturedSauces);
