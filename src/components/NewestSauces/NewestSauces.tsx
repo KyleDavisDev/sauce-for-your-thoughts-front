5b4e8078f982ac81f188c4e12fa03eb34219c683
Kyle Bonar <kylewb2011@gmail.com>
2020-02-27 08:17:52 -0600

    Class component to functional component

--- src/components/NewestSauces/NewestSauces.tsx
+++ src/components/NewestSauces/NewestSauces.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import { connect } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import SectionTitle from "../SectionTitle/SectionTitle";
-import { MyThunkDispatch, AppState } from "../../redux/configureStore";
+import { AppState } from "../../redux/configureStore";
 import { ISauce } from "../../redux/sauces/types";
 import { getSaucesByNewest } from "../../redux/sauces/actions";
 import {
@@ -14,91 +14,103 @@ import {
 
 export interface NewestSaucesProps {
   className?: string;
-  sauces: { newest?: ISauce[] };
-  getSaucesByNewest: () => Promise<null>;
 }
 
 export interface NewestSaucesState {}
 
-class NewestSauces extends React.PureComponent<
-  NewestSaucesProps,
-  NewestSaucesState
-> {
-  constructor(props: NewestSaucesProps) {
-    super(props);
-  }
+const NewestSauces: React.SFC<NewestSaucesProps> = props => {
+  // find our suaces
+  const [newestSauces, loading] = useNewestSauces();
 
-  public componentDidMount() {
-    window.scrollTo(0, 0); // Move screen to top
-    // If we don't have sauces, go look for them!
-    if (!this.props.sauces || !this.props.sauces.newest) {
-      // Call API
-      this.props.getSaucesByNewest().catch(err => console.log(err));
+  return (
+    <StyledDiv className={props.className}>
+      <SectionTitle
+        title="Newest Sauces"
+        description="We are always adding new sauces to our knowledgebase!"
+      />
+      <StyledCardContainer>
+        {loading ? (
+          <p>Loading...</p>
+        ) : newestSauces && newestSauces.length > 0 ? (
+          newestSauces.map((sauce, ind) => {
+            return (
+              <StyledCardHolder key={ind}>
+                <StyledCard
+                  title={sauce.name}
+                  imageLink={`${sauce.photo}`}
+                  description={sauce.description}
+                  to={`/sauce?s=${sauce.slug}`}
+                />
+              </StyledCardHolder>
+            );
+          })
+        ) : (
+          "No sauces found..."
+        )}
+      </StyledCardContainer>
+    </StyledDiv>
+  );
+};
+
+export function useNewestSauces(): any {
+  // get sauces from redux store
+  const { sauces } = useSelector((state: AppState) => state);
+  // assign results
+  const [results, setResults] = useState<ISauce[]>([]);
+  // assign loading
+  const [loading, setLoading] = useState(false);
+
+  // assign dispatch
+  const dispatch = useDispatch();
+
+  // run once
+  useEffect(() => {
+    // check if we need to dispatch redux action or not
+    if (sauces.newest.length === 0 && !loading) {
+      // emmit redux action to populate sauces.featured
+      dispatch(getSaucesByNewest());
+
+      // update loading
+      setLoading(true);
     }
-  }
+  }, []);
 
-  public render() {
-    const { sauces } = this.props;
-    return (
-      <StyledDiv className={this.props.className}>
-        <SectionTitle
-          title="Newest Sauces"
-          description="We are always adding new sauces to our knowledgebase!"
-        />
-        <StyledCardContainer>
-          {sauces.newest && sauces.newest.length > 0
-            ? sauces.newest.map((sauce, ind) => {
-                return (
-                  <StyledCardHolder key={ind}>
-                    <StyledCard
-                      title={sauce.name}
-                      imageLink={`${sauce.photo}`}
-                      description={sauce.description}
-                      to={`/sauce?s=${sauce.slug}`}
-                    />
-                  </StyledCardHolder>
-                );
-              })
-            : "No sauces found..."}
-        </StyledCardContainer>
-      </StyledDiv>
-    );
-  }
+  // run when sauces.featured changes
+  useEffect(() => {
+    async function getData() {
+      try {
+        // Find the sauces we will render by first getting the array of slugs
+        const sauceSlugs2Render = sauces.featured;
+
+        // Make sure our store has content
+        const bySlug = sauces.bySlug ? sauces.bySlug : {};
+        if (!bySlug) return [null, loading];
+
+        // Find actual sauces
+        const featured = sauceSlugs2Render
+          ? sauceSlugs2Render.map(slug => {
+              return bySlug[slug];
+            })
+          : [];
+
+        // Make sure we found the sauces
+        if (featured.length === 0) return [null, loading];
+
+        setResults(featured);
+      } catch {
+      } finally {
+        // finish loading
+        setLoading(false);
+      }
+      return [null, loading];
+    }
+
+    if (results.length === 0 && sauces.featured.length > 0) {
+      getData();
+    }
+  }, [sauces.featured]);
+
+  return [results, loading];
 }
 
-function mapStateToProps(state: AppState, myProps: any): any {
-  // Find the sauces we will render by first getting the array of slugs
-  const sauceSlugs2Render: string[] | undefined = state.sauces.newest
-    ? state.sauces.newest
-    : [];
-
-  // Make sure we have something to work with
-  if (!sauceSlugs2Render || sauceSlugs2Render.length === 0) {
-    return { sauces: {} };
-  }
-
-  // Make sure our store has content
-  const bySlug = state.sauces.bySlug ? state.sauces.bySlug : {};
-  if (!bySlug) return { sauces: {} };
-
-  // Find actual sauces
-  const newest = sauceSlugs2Render
-    ? sauceSlugs2Render.map(slug => {
-        return bySlug[slug];
-      })
-    : [];
-
-  // Make sure we found the sauces
-  if (newest.length === 0) return { sauces: {} };
-
-  return {
-    sauces: { newest }
-  };
-}
-
-// For TS w/ redux-thunk: https://github.com/reduxjs/redux-thunk/issues/213#issuecomment-428380685
-const mapDispatchToProps = (dispatch: MyThunkDispatch) => ({
-  getSaucesByNewest: () => dispatch(getSaucesByNewest())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(NewestSauces);
+export default NewestSauces;
