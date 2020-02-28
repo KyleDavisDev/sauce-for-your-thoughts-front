6a4022b00f72ef9f87f0ac9c00d10601c8381a9f
Kyle Bonar <kylewb2011@gmail.com>
2020-02-28 10:48:43 -0600

    Class component to SFC

--- src/components/LoginUser/LoginUser.tsx
+++ src/components/LoginUser/LoginUser.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/router";
 import validator from "validator";
 import queryString from "query-string";
 
@@ -21,175 +22,124 @@ import {
   StyledFooterDivs
 } from "./LoginUserStyle";
 
-export interface LoginProps {
-  login: ({ credentials }: { credentials: ILoginUser }) => any;
-  history: { push: (location: string) => null };
-  location: { search: string };
-}
+export interface LoginProps {}
 
-export interface LoginState {
-  email: string;
-  password: string;
-  flashMessage: FlashMessageProps;
-}
+export interface LoginState {}
 
-class Login extends React.Component<LoginProps, LoginState> {
-  constructor(props: LoginProps) {
-    super(props);
+const LoginUser: React.SFC<LoginProps> = () => {
+  // assign state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [flashMessage, setFlashMessage] = useState<FlashMessageProps>({
+    isVisible: false,
+    text: "",
+    type: undefined
+  });
 
-    // Init state
-    this.state = {
-      email: "",
-      password: "",
-      flashMessage: {
-        isVisible: false
-      }
-    };
-  }
+  // assign dispatch
+  const dispatch = useDispatch();
 
-  public render() {
-    return (
-      <StyledDiv>
-        <StyledLogoContainer>
-          <Link to="/">
-            <LogoSFYT />
-          </Link>
-        </StyledLogoContainer>
-        <hr />
-        <StyledArticle>
-          <PageTitle>Login</PageTitle>
-          <StyledFormContainer>
-            {this.state.flashMessage.isVisible && (
-              <FlashMessage type={this.state.flashMessage.type} isVisible>
-                {this.state.flashMessage.text}
-              </FlashMessage>
-            )}
-            <form onSubmit={this.onSubmit} style={{ width: "100%" }}>
-              <TextInput
-                type="email"
-                onChange={this.onTextChange}
-                showLabel={true}
-                label={"Email"}
-                name={"email"}
-                value={this.state.email}
-                required={true}
-              />
-              <TextInput
-                type="password"
-                onChange={this.onTextChange}
-                showLabel={true}
-                label={"Password"}
-                name={"password"}
-                value={this.state.password}
-                required={true}
-              />
-              <StyledFooterDivs>
-                <StyledButton type="submit">Login</StyledButton>
-              </StyledFooterDivs>
-              <StyledText>
-                <Link to="/reset">Forgot your username or password?</Link>
-              </StyledText>
+  // assign NextJS router
+  const router = useRouter();
 
-              <StyledText>
-                Don't have an account yet?{" "}
-                <Link to="/account/register">Sign up!</Link>
-              </StyledText>
-            </form>
-          </StyledFormContainer>
-        </StyledArticle>
-      </StyledDiv>
-    );
-  }
-
-  private onTextChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    if (!event || !event.target) {
-      return;
-    }
-    // Grab the name and value
-    const { name, value }: { name: string; value: string } = event.target;
-
-    // Update local state
-    this.setState({
-      ...this.state,
-      [name]: value
-    });
-  };
-
-  private onSubmit = async (event: React.FormEvent): Promise<any> => {
+  const onSubmit = async (event: React.FormEvent): Promise<any> => {
     event.preventDefault();
 
     // If not email don't even send network request
-    if (!validator.isEmail(this.state.email)) {
-      this.setState({
-        flashMessage: {
-          isVisible: true,
-          text: "An email must be used.",
-          type: "alert"
-        }
+    if (!validator.isEmail(email)) {
+      setFlashMessage({
+        isVisible: true,
+        text: "An email must be used.",
+        type: "alert"
       });
       return;
     }
 
     // If password too short, don't send network request
-    if (this.state.password.length < 8) {
-      this.setState({
-        flashMessage: {
-          isVisible: true,
-          text: "Password must be longer than 8 characters",
-          type: "alert"
-        }
+    if (password.length < 8) {
+      setFlashMessage({
+        isVisible: true,
+        text: "Password must be longer than 8 characters",
+        type: "alert"
       });
       return;
     }
 
     const credentials: ILoginUser = {
-      user: { email: this.state.email, password: this.state.password }
+      user: { email, password }
     };
     try {
       // dispatch action which calls API to login user
-      await this.props.login({ credentials });
-
-      const returnURL: string | null = this.getReturnFromPath(
-        this.props.location.search
-      );
+      const tmp = dispatch(login({ credentials }));
+      console.log(tmp);
 
       // Redirect user to where they were or to sauces page
-      if (returnURL) {
-        this.props.history.push(returnURL);
+      if (router.query.return && !Array.isArray(router.query.return)) {
+        router.push(router.query.return);
       } else {
-        this.props.history.push("/sauces");
+        router.push("/sauces");
       }
     } catch (err) {
       // Create warning flash
-      this.setState({
-        flashMessage: {
-          isVisible: true,
-          text: err.response.data.msg,
-          type: "warning"
-        }
+      setFlashMessage({
+        isVisible: true,
+        text: err.response.data.msg,
+        type: "warning"
       });
     }
   };
 
-  private getReturnFromPath(path: string): string | null {
-    // Get s from string
-    const values = queryString.parse(path);
+  return (
+    <StyledDiv>
+      <StyledLogoContainer>
+        <Link to="/">
+          <LogoSFYT />
+        </Link>
+      </StyledLogoContainer>
+      <hr />
+      <StyledArticle>
+        <PageTitle>Login</PageTitle>
+        <StyledFormContainer>
+          {flashMessage.isVisible && (
+            <FlashMessage type={flashMessage.type} isVisible>
+              {flashMessage.text}
+            </FlashMessage>
+          )}
+          <form onSubmit={e => onSubmit(e)} style={{ width: "100%" }}>
+            <TextInput
+              type="email"
+              onChange={e => setEmail(e.target.value)}
+              showLabel={true}
+              label={"Email"}
+              name={"email"}
+              value={email}
+              required={true}
+            />
+            <TextInput
+              type="password"
+              onChange={e => setPassword(e.target.value)}
+              showLabel={true}
+              label={"Password"}
+              name={"password"}
+              value={password}
+              required={true}
+            />
+            <StyledFooterDivs>
+              <StyledButton type="submit">Login</StyledButton>
+            </StyledFooterDivs>
+            <StyledText>
+              <Link to="/reset">Forgot your username or password?</Link>
+            </StyledText>
 
-    // Make sure return is defined, not an array
-    if (!values.return || Array.isArray(values.return)) {
-      return null;
-    }
-
-    return values.return;
-  }
-}
-
-const mapState2Props = (state: AppState) => {
-  return {};
+            <StyledText>
+              Don't have an account yet?{" "}
+              <Link to="/account/register">Sign up!</Link>
+            </StyledText>
+          </form>
+        </StyledFormContainer>
+      </StyledArticle>
+    </StyledDiv>
+  );
 };
 
-const mapDispatch2Props = {
-  login
-};
-
-export default connect(mapState2Props, mapDispatch2Props)(Login);
+export default LoginUser;
