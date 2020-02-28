1292aa3dfbdfd2e72b131355b97dce05c89a8e6b
Kyle Bonar <kylewb2011@gmail.com>
2020-02-28 14:38:01 -0600

    If person has localStorage items, call redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import App from "next/app";
 import React from "react";
+import { Provider } from "react-redux";
 import Page from "../src/components/Page/Page";
 import withReduxStore from "../src/redux/with-redux-store";
-import { Provider } from "react-redux";
+import Auth from "../src/utils/Auth/Auth";
+import { getPersonFromStorage } from "../src/redux/users/actions";
 
 class MyApp extends App<any, any> {
+  componentDidMount() {
+    // Check if we need to update our store with localStorage info
+    if (Auth.isUserAuthenticated()) {
+      this.props.reduxStore.dispatch(getPersonFromStorage());
+    }
+  }
   public render() {
     const { Component, pageProps, reduxStore } = this.props;
     return (
