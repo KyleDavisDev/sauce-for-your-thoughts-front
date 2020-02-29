d6f3a2878855261583f9e0049f5398ee6472d53e
Kyle Bonar <kylewb2011@gmail.com>
2020-02-29 11:56:40 -0600

    Updated style

--- src/components/TopBar/components/Trigger/Trigger.tsx
+++ src/components/TopBar/components/Trigger/Trigger.tsx
@@ -3,14 +3,24 @@ import * as React from "react";
 import styled from "../../../../theme/styled-components";
 
 const StyledButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-family: FuturaMedium;
+  color: ${x => x.theme.grey};
+  fill: ${x => x.theme.grey};
   background: none;
   border: 0px;
   padding: 5px;
   outline: none;
 
-  &:hover {
+  &:hover,
+  &:focus {
     outline: none;
     cursor: pointer;
+    text-decoration: none;
+    color: ${x => x.theme.primaryThemeColor};
+    fill: ${x => x.theme.primaryThemeColor};
   }
 `;
 
@@ -19,7 +29,7 @@ interface TriggerProps {
     | string
     | Element
     | JSX.Element
-    | Array<string | Element | JSX.Element|undefined>;
+    | Array<string | Element | JSX.Element | undefined>;
   className?: string;
   onClick?: () => any;
 }
