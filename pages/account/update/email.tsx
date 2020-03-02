d5742467f3e2b08bc2b3268855d1aa92f189845c
Kyle Bonar <kylewb2011@gmail.com>
2020-03-02 11:19:41 -0600

    init commit

---
+++ pages/account/update/email.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import UpdateEmail from "../../../src/components/UpdateEmail/UpdateEmail";
+
+export interface EmailPageProps {}
+
+const EmailPage: React.SFC<EmailPageProps> = () => {
+  return <UpdateEmail />;
+};
+
+export default EmailPage;
