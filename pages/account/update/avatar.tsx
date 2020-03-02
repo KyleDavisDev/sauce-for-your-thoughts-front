e414b65b1961af8cdf7cf11a7681d3e3315ee2c8
Kyle Bonar <kylewb2011@gmail.com>
2020-03-02 09:53:14 -0600

    Init commit

---
+++ pages/account/update/avatar.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import UpdateAvatar from "../../../src/components/UpdateAvatar/UpdateAvatar";
+
+export interface AvatarPageProps {}
+
+const AvatarPage: React.SFC<AvatarPageProps> = () => {
+  return <UpdateAvatar />;
+};
+
+export default AvatarPage;
