4ecf713da4e0158fa5cb2528b9167d42a6600231
Kyle Bonar <kylewb2011@gmail.com>
2020-03-02 22:38:11 -0600

    init commit

---
+++ pages/account/update/displayName.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import UpdateDisplayName from "../../../src/components/UpdateDisplayName/UpdateDisplayName";
+
+export interface DisplayNamePageProps {}
+
+const DisplayNamePage: React.SFC<DisplayNamePageProps> = () => {
+  return <UpdateDisplayName />;
+};
+
+export default DisplayNamePage;
