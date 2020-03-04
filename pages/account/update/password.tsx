b50fd9129e92c8d343c6defb965d88b6b1856fad
Kyle Bonar <kylewb2011@gmail.com>
2020-03-04 08:09:38 -0600

    init commit

---
+++ pages/account/update/password.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import UpdatePassword from "../../../src/components/UpdatePassword/UpdatePassword";
+
+export interface PasswordPageProps {}
+
+const PasswordPage: React.SFC<PasswordPageProps> = () => {
+  return <UpdatePassword />;
+};
+
+export default PasswordPage;
