5f03e1867de245bbaa547703f69a83610456459e
Kyle Bonar <kylewb2011@gmail.com>
2020-03-10 14:35:32 -0500

    init commit

---
+++ pages/sauce/add.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import SauceAdd from "../../src/components/SauceAdd/SauceAdd";
+
+interface AddPageProps {}
+
+const AddPage: React.FunctionComponent<AddPageProps> = props => {
+  return <SauceAdd />;
+};
+
+export default AddPage;
