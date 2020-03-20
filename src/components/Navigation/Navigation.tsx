9f509af8f87a40b8562d5574330b53e0aa76af80
Kyle Bonar <kylewb2011@gmail.com>
2020-03-20 11:10:36 -0500

    Class component to FC

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -10,55 +10,40 @@ import {
   StyledLink
 } from "./NavigationStyle";
 
-export interface NavigationState {
-  isOpen: boolean;
-}
+const Navigation: React.FunctionComponent<{}> = props => {
+  const [isOpen, setIsOpen] = React.useState(false);
 
-class Navigation extends React.PureComponent<{}, NavigationState> {
-  public constructor(props: any) {
-    super(props);
-
-    this.state = { isOpen: false };
-  }
-
-  public render() {
-    return (
-      <StyledNav>
-        <Link href="/">
-          <a>
-            <LogoSFYT />
-          </a>
-        </Link>
-        <StyledButton onClick={this.toggleMenu}>
-          <Bar />
-        </StyledButton>
-        <StyledUl
-          style={{
-            maxHeight: this.state.isOpen ? "99999px" : "0",
-            display: this.state.isOpen ? "flex" : "none"
-          }}
-        >
-          <li>
-            <StyledLink href="/">Home</StyledLink>
-          </li>
-          <li>
-            <StyledLink href="/sauces">Sauces</StyledLink>
-          </li>
-          <li>
-            <StyledLink href="/sauce/add">Add Sauce</StyledLink>
-          </li>
-          {/* <li>
+  return (
+    <StyledNav>
+      <Link href="/">
+        <a>
+          <LogoSFYT />
+        </a>
+      </Link>
+      <StyledButton onClick={e => setIsOpen(!isOpen)}>
+        <Bar />
+      </StyledButton>
+      <StyledUl
+        style={{
+          maxHeight: isOpen ? "99999px" : "0",
+          display: isOpen ? "flex" : "none"
+        }}
+      >
+        <li>
+          <StyledLink href="/">Home</StyledLink>
+        </li>
+        <li>
+          <StyledLink href="/sauces">Sauces</StyledLink>
+        </li>
+        <li>
+          <StyledLink href="/sauce/add">Add Sauce</StyledLink>
+        </li>
+        {/* <li>
             <span>icon here</span>
           </li> */}
-        </StyledUl>
-      </StyledNav>
-    );
-  }
-
-  public toggleMenu = (event: React.MouseEvent<HTMLButtonElement>): void => {
-    const isOpen: boolean = this.state.isOpen;
-    this.setState({ isOpen: !isOpen });
-  };
-}
+      </StyledUl>
+    </StyledNav>
+  );
+};
 
 export default Navigation;
