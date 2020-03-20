e8ccc1b227375ec629f4af5efaf0218d886ae2d4
Kyle Bonar <kylewb2011@gmail.com>
2020-03-20 12:08:53 -0500

    Class component to FC, minor cleanups

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -12,7 +12,7 @@ interface TextInputProps {
   required?: boolean;
   label?: string;
   showLabel?: boolean;
-  type?: "text" | "textarea" | "password" | string; // have to allow 'string' or else styled components complains
+  type?: "text" | "password" | "email";
   value?: string | number;
   className?: string;
   disabled?: boolean;
@@ -24,74 +24,55 @@ interface TextInputProps {
   ): void;
 }
 
-interface TextInputState {
-  id: string;
-}
+const TextInput: React.FunctionComponent<TextInputProps> = props => {
+  // grab info from props and assign defaults if needed
+  const {
+    id = shortid.generate(),
+    type = "text",
+    disabled = false,
+    required = false,
+    showLabel = false,
+    label,
+    className,
+    placeholder,
+    requirementText,
+    onChange,
+    value,
+    name
+  } = props;
 
-class TextInput extends React.PureComponent<TextInputProps, TextInputState> {
-  public static defaultProps = {
-    showLabel: false,
-    required: false,
-    type: "text",
-    disabled: false
-  };
+  return (
+    <StyledDiv className={className}>
+      {showLabel && label && (
+        <Label htmlFor={id}>
+          {label}
+          {required ? <span style={{ color: "#B20000" }}>*</span> : ""}
+        </Label>
+      )}
 
-  state = { id: !this.props.id ? shortid.generate() : this.props.id };
+      <StyledInput
+        type={type}
+        id={id}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+        required={required}
+        aria-required={required}
+        disabled={disabled}
+        aria-disabled={disabled}
+      />
 
-  public render() {
-    return (
-      <StyledDiv className={this.props.className}>
-        {this.props.showLabel && this.props.label && (
-          <Label htmlFor={this.state.id}>
-            {this.props.label}
-            {this.props.required ? (
-              <span style={{ color: "#B20000" }}>*</span>
-            ) : (
-              ""
-            )}
-          </Label>
-        )}
-
-        {/* Make sure prop passed and is either 'text' or 'password' */}
-        {this.props.type && (
-          <StyledInput
-            type={this.props.type}
-            id={this.state.id}
-            name={this.props.name}
-            value={this.props.value}
-            placeholder={this.props.placeholder}
-            onChange={this.props.onChange}
-            required={this.props.required}
-            aria-required={this.props.required}
-            disabled={this.props.disabled}
-            aria-disabled={this.props.disabled}
-            style={{
-              marginBottom: this.props.requirementText && "0px"
-            }}
-          />
-        )}
-        {this.props.requirementText ? (
-          <p
-            style={{
-              fontSize: ".85rem",
-              marginTop: "1px",
-              marginBottom: "15px"
-            }}
-          >
-            {this.props.requirementText}
-          </p>
-        ) : (
-          ""
-        )}
-      </StyledDiv>
-    );
-  }
-}
+      {requirementText && <p>{requirementText}</p>}
+    </StyledDiv>
+  );
+};
 
 const StyledTextInput = styled(TextInput)`
   input {
     background-color: ${props => (props.disabled ? "#eee" : props.theme.white)};
     box-shadow: none;
+    margin-bottom: ${props => props.requirementText && "0px"};
 
     :hover,
     :focus {
