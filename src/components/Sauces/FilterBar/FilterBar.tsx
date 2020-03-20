67fd1b14621e382fcf6b1a92aa215da2113f1068
Kyle Bonar <kylewb2011@gmail.com>
2020-03-20 13:27:36 -0500

    Class component to FC

--- src/components/Sauces/FilterBar/FilterBar.tsx
+++ src/components/Sauces/FilterBar/FilterBar.tsx
@@ -1,4 +1,9 @@
 import * as React from "react";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
+
+import { AppState } from "../../../redux/configureStore";
+import useParamsFromPath from "../../../utils/hooks/useParamsFromPath";
 import {
   StyledFormContainer,
   StyledForm,
@@ -6,8 +11,6 @@ import {
   StyledInput,
   StyledButton
 } from "./FilterBarStyle";
-import { connect } from "react-redux";
-import { MyThunkDispatch, AppState } from "../../../redux/configureStore";
 
 export interface FilterBarProps {
   onSubmit: ({
@@ -21,194 +24,91 @@ export interface FilterBarProps {
     limit: string;
     srch: string;
   }) => void;
-  typeFromPath?: string;
-  orderFromPath?: string;
-  limitFromPath?: number;
-  srchFromPath?: string;
-  types?: { options: string[]; selected: string };
-  order?: { options: string[]; selected: string };
 }
 
-export interface FilterBarState {
-  types: { options: string[]; selected: string };
-  order: { options: string[]; selected: string };
-  limit: { options: string[]; selected: string };
-  srch: string;
-  [key: string]: { options: string[]; selected: string } | string;
-}
+const FilterBar: React.FunctionComponent<FilterBarProps> = props => {
+  // assign router and get params from the path
+  const router = useRouter();
+  const params = useParamsFromPath({ router });
 
-class FilterBar extends React.PureComponent<FilterBarProps, FilterBarState> {
-  public constructor(props: FilterBarProps) {
-    super(props);
+  // get info from redux
+  const { types, orders, limits } = useSelector((store: AppState) => {
+    // 1. Find types and create object
+    const _types = { options: store.sauces.types, selected: params.type };
 
-    this.state = {
-      types: { options: [], selected: this.props.typeFromPath || "" },
-      order: { options: [], selected: this.props.orderFromPath || "" },
-      limit: {
-        options: ["5", "10", "15", "25", "50"],
-        selected: this.props.limitFromPath
-          ? this.props.limitFromPath.toString()
-          : "15"
-      },
-      srch: ""
+    // 2. Find orders and create object
+    const _orders = { options: store.sauces.orders, selected: params.order };
+
+    // 3. Create options and rest object
+    const _limit = {
+      options: ["5", "10", "15", "25", "50"],
+      selected: params.limit
     };
-  }
 
-  public componentWillMount() {
-    // Grab info from props -- make sure we have it
-    const { types, order } = this.props;
-    if (!types || !order) {
-      return;
-    }
+    return { types: _types, orders: _orders, limits: _limit };
+  });
 
-    // Find srch if we have one or assign default val
-    const srch = this.props.srchFromPath || "";
+  // assign state
+  const [type, setType] = React.useState(types.selected);
+  const [order, setOrder] = React.useState(orders.selected);
+  const [limit, setLimit] = React.useState(limits.selected.toString());
 
-    // Assign to state
-    this.setState({ ...this.state, types, order, srch });
-  }
+  const [srch, setSrch] = React.useState(params.srch || "");
 
-  public componentWillReceiveProps(props: FilterBarProps) {
-    // Grab info from props -- make sure we have it
-    const { types, order } = props;
-    if (!types || !order) {
-      return;
-    }
+  return (
+    <StyledFormContainer>
+      <StyledForm onSubmit={onSubmit}>
+        <StyledDropDown
+          showLabel={true}
+          label={"Type"}
+          name={"types"}
+          options={types.options}
+          onSelect={e => setType(e.target.value)}
+          selectedValue={type}
+        />
 
-    // Find srch if we have one or assign default val
-    const srch = props.srchFromPath || "";
+        <StyledDropDown
+          showLabel={true}
+          label={"Order"}
+          name={"order"}
+          options={orders.options}
+          onSelect={e => setOrder(e.target.value)}
+          selectedValue={order}
+        />
 
-    this.setState({ ...this.state, types, order, srch });
-  }
+        <StyledDropDown
+          showLabel={true}
+          label={"Limit"}
+          name={"limit"}
+          options={limits.options}
+          onSelect={e => setLimit(e.target.value)}
+          selectedValue={limit}
+        />
 
-  public render() {
-    const { types, order, limit, srch } = this.state;
+        <StyledInput
+          showLabel={true}
+          label={"Name like..."}
+          onChange={e => setSrch(e.target.value)}
+          name={"srch"}
+        />
 
-    return (
-      <StyledFormContainer>
-        {types && order ? (
-          <StyledForm onSubmit={this.onSubmit}>
-            <StyledDropDown
-              showLabel={true}
-              label={"Type"}
-              name={"types"}
-              options={types.options}
-              onSelect={this.onDropDownChange}
-              selectedValue={types.selected}
-            />
+        <StyledButton type={"submit"}>Filter</StyledButton>
+      </StyledForm>
+    </StyledFormContainer>
+  );
 
-            <StyledDropDown
-              showLabel={true}
-              label={"Order"}
-              name={"order"}
-              options={order.options}
-              onSelect={this.onDropDownChange}
-              selectedValue={order.selected}
-            />
-
-            <StyledDropDown
-              showLabel={true}
-              label={"Limit"}
-              name={"limit"}
-              options={limit.options}
-              onSelect={this.onDropDownChange}
-              selectedValue={limit.selected}
-            />
-
-            <StyledInput
-              showLabel={true}
-              label={"Name like..."}
-              value={srch}
-              onChange={this.onTextChange}
-              name={"srch"}
-            />
-
-            <StyledButton type={"submit"}>Filter</StyledButton>
-          </StyledForm>
-        ) : (
-          "Loading..."
-        )}
-      </StyledFormContainer>
-    );
-  }
-
-  private onSubmit = (event: React.FormEvent): void => {
+  function onSubmit(event: React.FormEvent): void {
     // prevent default to stop form submission
     event.preventDefault();
 
     // Lift values to parent
-    this.props.onSubmit({
-      type: this.state.types.selected,
-      order: this.state.order.selected,
-      limit: this.state.limit.selected,
-      srch: this.state.srch
+    props.onSubmit({
+      type,
+      order,
+      limit,
+      srch
     });
-  };
-
-  private onTextChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const { name, value } = event.target;
-
-    this.setState({ ...this.state, [name]: value });
-  };
-
-  private onDropDownChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ): void => {
-    event.preventDefault();
-
-    const { name, value }: { name: string; value: string } = event.target;
-
-    // Make sure we are dealing with object
-    const dropdownObj = this.state[name];
-    if (typeof dropdownObj !== "object") return;
-
-    this.setState({
-      ...this.state,
-      [name]: { ...dropdownObj, selected: value }
-    });
-  };
-}
-
-function mapStateToProps(
-  state: AppState,
-  myProps: FilterBarProps
-): FilterBarProps {
-  // Grab submit
-  const { onSubmit } = myProps;
-
-  // Make sure we have types
-  const { types } = state.sauces;
-
-  if (types.length === 0 || !types) {
-    return { onSubmit };
   }
+};
 
-  // Make sure we have orders
-  const { orders } = state.sauces;
-  if (orders.length === 0 || !orders) {
-    return { onSubmit };
-  }
-
-  // Figure out which option should be selected. Do some massaging too.
-  let typeFromPath = myProps.typeFromPath || types[0];
-  typeFromPath = typeFromPath.toLowerCase();
-  let orderFromPath = myProps.orderFromPath || orders[0];
-  orderFromPath = orderFromPath.toLowerCase();
-
-  // Find srch if we have one or assign default val
-  const srchFromPath = myProps.srchFromPath || "";
-
-  return {
-    types: { options: types, selected: typeFromPath },
-    order: { options: orders, selected: orderFromPath },
-    onSubmit,
-    orderFromPath,
-    typeFromPath,
-    srchFromPath
-  };
-}
-
-// For TS w/ redux-thunk: https://github.com/reduxjs/redux-thunk/issues/213#issuecomment-428380685
-const mapDispatchToProps = (dispatch: MyThunkDispatch) => ({});
-
-export default connect(mapStateToProps, mapDispatchToProps)(FilterBar);
+export default FilterBar;
