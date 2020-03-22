92d50b0a34712706bfaf77332b722ceda69860a9
Kyle Bonar <kylewb2011@gmail.com>
2020-03-22 01:29:26 -0500

    Check if user is eligible to add a review to a sauce

---
+++ src/utils/hooks/useCanUserAddReview.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Auth from "../Auth/Auth";
+import { API } from "../api/API";
+import { IErrReturn } from "../Err/Err";
+
+export function useCanUserAddReview(): [boolean, string] {
+  // assign values
+  const [canUserAddReview, setCanUserAddReview] = React.useState(false);
+  const [error, setError] = React.useState("");
+
+  // assign router
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const slug = router.query.s;
+
+    // Sauce slug is whack, redirect user
+    if (!slug || Array.isArray(slug)) {
+      // Get user outta here
+      router.push("/sauces");
+
+      return;
+    }
+
+    // Make sure we are at top of page
+    window.scrollTo(0, 0);
+
+    // Make sure user can add review or not
+    const token = Auth.getToken();
+    if (!token || token.length === 0) {
+      // Redirect user to login w/ appropriate return address
+      router.replace(`/account/login?return=${router.asPath}`);
+
+      return;
+    }
+
+    // Construct data obj
+    const data = { user: { token }, sauce: { slug } };
+
+    // Find out if user is eligible to submit a review for this sauce or not
+    API.review
+      .canUserSubmit({ data })
+      .then(res => {
+        setCanUserAddReview(true);
+      })
+      .catch((err: IErrReturn) => {
+        // 401 === unauthorized, 403 === forbidden
+        if (
+          err.response.data.status === 401 ||
+          err.response.data.status === 403
+        ) {
+          // Disable form components and show flashmessage
+          setCanUserAddReview(false);
+          setError(err.response.data.msg);
+        } else {
+          // Redirect user to edit page
+          router.replace(`/review/edit?s=${slug}`);
+        }
+      });
+  }, []);
+
+  return [canUserAddReview, error];
+}
