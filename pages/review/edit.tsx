38554ff127b8f34c0eb65d4ac50f482edf558c51
Kyle Bonar <kylewb2011@gmail.com>
2020-03-22 17:26:19 -0500

    Call redux to edit review

--- pages/review/edit.tsx
+++ pages/review/edit.tsx
@@ -4,18 +4,35 @@ import ReviewForm from "../../src/components/ReviewForm/ReviewForm";
 import TopBar from "../../src/components/TopBar/TopBar";
 import Navigation from "../../src/components/Navigation/Navigation";
 import Footer from "../../src/components/Footer/Footer";
-
+import { IReviewToServer } from "../../src/redux/reviews/types";
+import { useDispatch } from "react-redux";
+import { reduxStore } from "../../src/redux/with-redux-store";
+import { editReview } from "../../src/redux/reviews/actions";
 interface ReviewAddPageProps {}
 
 const ReviewAddPage: React.FunctionComponent<ReviewAddPageProps> = props => {
+  // assign dispatch
+  const useThunkDispatch = useDispatch<typeof reduxStore.dispatch>();
+
   return (
     <>
       <TopBar />
       <Navigation />
-      <ReviewForm />
+      <ReviewForm onSubmit={onSubmit} />
       <Footer />
     </>
   );
+
+  async function onSubmit(data: IReviewToServer) {
+    try {
+      // add review
+      await useThunkDispatch(editReview(data));
+    } catch (err) {
+      // handle error in component
+    }
+
+    return null;
+  }
 };
 
 export default ReviewAddPage;
