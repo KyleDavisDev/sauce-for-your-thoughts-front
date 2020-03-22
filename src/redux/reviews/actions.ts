efd5354dab45b86ebfc5a856364e0cdaf0460e1e
Kyle Bonar <kylewb2011@gmail.com>
2020-03-22 17:26:51 -0500

    Updated JSDoc and params to use new interface

--- src/redux/reviews/actions.ts
+++ src/redux/reviews/actions.ts
@@ -86,21 +86,14 @@ export const addReview = (
 };
 
 /** @description Edit a single review
- *  @param {Object} data - all encompasing object
- *    @param {Object} data.user - holds user information
- *      @param {String} data.user.token - unique user identifier
- *    @param {Object} data.sauce - hold sauce information
- *      @param {String} data.sauce.token - unique sauce string
- *    @param {IReview} data.review
+ *  @param {IReviewToServer} data - all encompasing object
  *  @fires reviews#addedReview - add review to store
  *  @returns {Promise}
  *    @returns {NULL}
  */
-export const editReview = ({
-  data
-}: {
-  data: { user: { token: string }; review: IReview };
-}): MyThunkResult<Promise<null>> => async dispatch => {
+export const editReview = (
+  data: IReviewToServer
+): MyThunkResult<Promise<null>> => async dispatch => {
   // Add review
   await API.review.edit(data);
 
