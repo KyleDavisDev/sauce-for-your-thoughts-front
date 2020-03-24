c69449edaa1bccc2c3f79a3c4e3e608c41415ff1
Kyle Bonar <kylewb2011@gmail.com>
2020-03-24 14:09:47 -0500

    Move logic to child component, render ReviewEdit component

--- pages/review/edit.tsx
+++ pages/review/edit.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import ReviewForm from "../../src/components/ReviewForm/ReviewForm";
+import ReviewEdit from "../../src/components/ReviewEdit/ReviewEdit";
 import TopBar from "../../src/components/TopBar/TopBar";
 import Navigation from "../../src/components/Navigation/Navigation";
 import Footer from "../../src/components/Footer/Footer";
@@ -11,28 +11,14 @@ import { editReview } from "../../src/redux/reviews/actions";
 interface ReviewAddPageProps {}
 
 const ReviewAddPage: React.FunctionComponent<ReviewAddPageProps> = props => {
-  // assign dispatch
-  const useThunkDispatch = useDispatch<typeof reduxStore.dispatch>();
-
   return (
     <>
       <TopBar />
       <Navigation />
-      <ReviewForm onSubmit={onSubmit} />
+      <ReviewEdit />
       <Footer />
     </>
   );
-
-  async function onSubmit(data: IReviewToServer) {
-    try {
-      // add review
-      await useThunkDispatch(editReview(data));
-    } catch (err) {
-      // handle error in component
-    }
-
-    return null;
-  }
 };
 
 export default ReviewAddPage;
