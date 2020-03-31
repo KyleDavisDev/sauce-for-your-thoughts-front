483da77f47befe52bedcf7526d2af0f3ff67e23b
Kyle Bonar <kylewb2011@gmail.com>
2020-03-31 09:58:54 -0500

    Moved styles to own file

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,59 +1,15 @@
 import * as React from "react";
-import { Link } from "../Link/Link";
 import * as shortid from "shortid";
 
-import styled from "../../theme/styled-components";
-
-const StyledDiv = styled.div`
-  padding: 10px;
-`;
-StyledDiv.displayName = "StyledDiv";
-
-const StyledH5 = styled.h5`
-  margin: 0 0 0.5em 0;
-  font-weight: 400;
-  padding: 0;
-  text-transform: uppercase;
-  font-size: ${props => props.theme.scaleH5};
-`;
-StyledH5.displayName = "StyledH5";
-
-const StyledUl = styled.ul`
-  margin: 0px;
-  padding: 0px;
-  list-style: none;
-
-  li {
-    margin-top: 0.75rem;
-    margin-bottom: 0.75rem;
-    padding-left: 0.75rem;
-  }
-
-  @media (min-width: ${props => props.theme.smToMd}) {
-    li {
-      padding-left: 0;
-    }
-  }
-`;
-StyledUl.displayName = "StyledUl";
-
-const StyledLink = styled(Link)`
-  color: ${props => props.theme.primaryThemeColor};
-  &:hover,
-  &:focus {
-    color: ${props => props.theme.secondaryThemeColor};
-  }
-`;
-
-interface Item {
-  link?: string;
-  text: string;
-}
+import { StyledDiv, StyledH5, StyledLink, StyledUl } from "./ListStyle";
 
 interface ListProps {
   className?: string;
   title: string;
-  items: Item[];
+  items: Array<{
+    link?: string;
+    text: string;
+  }>;
 }
 
 class List extends React.PureComponent<ListProps> {
