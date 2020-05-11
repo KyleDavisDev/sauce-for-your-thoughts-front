d66f791fd37212109b8c0ef6441cffbf86c8bd3f
Kyle Bonar <kylewb2011@gmail.com>
2020-05-11 11:35:48 -0500

    On mount, refresh api token and user data if possible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,31 @@ import { Provider } from "react-redux";
 import Page from "../src/components/Page/Page";
 import withReduxStore from "../src/redux/with-redux-store";
 import Auth from "../src/utils/Auth/Auth";
-import { getPersonFromStorage } from "../src/redux/users/actions";
+import { userLoggedIn } from "../src/redux/users/actions";
 import "react-image-crop/dist/ReactCrop.css";
+import { API } from "../src/utils/api/API";
 
 class MyApp extends App<any, any> {
   constructor(props) {
     super(props);
-    // Check if we need to update our store with localStorage info
-    if (Auth.isUserAuthenticated()) {
-      this.props.reduxStore.dispatch(getPersonFromStorage());
+  }
+
+  public async componentDidMount() {
+    // 1) Update API token
+    const res = await API.user.refreshAPIToken();
+
+    // 2) If no problems, get user info
+    if (res.data.isGood) {
+      // 2.1) Get info about user
+      const getInfo: any = await API.user.getInfo();
+
+      // 2.2) Dispatch the login action to populate redux store
+      this.props.reduxStore.dispatch(
+        userLoggedIn({ token: res.data.token, ...getInfo })
+      );
     }
   }
+
   public render() {
     const { Component, pageProps, reduxStore } = this.props;
     return (
