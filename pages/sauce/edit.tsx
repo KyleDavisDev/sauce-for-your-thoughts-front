a450d3bbcb0becf0ada7718b5237c6e51db8b40f
Kyle Bonar <kylewb2011@gmail.com>
2020-05-26 16:40:33 -0500

    init commit

---
+++ pages/sauce/edit.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import SauceEdit from "../../src/components/SauceEdit/SauceEdit";
+
+interface SauceEditPageProps {}
+
+const SauceEditPage: React.FunctionComponent<SauceEditPageProps> = props => {
+  return <SauceEdit />;
+};
+
+export default SauceEditPage;
