25c94ea925fd0c3a3786c8a00908ebcf40839faa
Kyle Bonar <kylewb2011@gmail.com>
2020-05-26 10:26:43 -0500

    init comit

---
+++ src/components/ApproveSubmissions/ApproveSubmissionsStyles.tsx
@@ -0,0 +1,52 @@
+import styled from "../../theme/styled-components";
+
+export const StyledSauceContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-bottom: 2em;
+  background-color: white;
+  border-top: 6px solid ${x => x.theme.secondaryThemeColor};
+  visibility: visible;
+  max-height: 9999px;
+  padding: 1em;
+  box-shadow: 0 2px 5px #333;
+
+  transition: max-height 1s ease, padding 1s ease, all 1s ease;
+
+  &.hidden {
+    max-height: 0px;
+    overflow: hidden;
+    padding: 0px;
+    margin-bottom: 0px;
+  }
+`;
+
+export const StyledImageHolder = styled.div`
+  max-width: 33%;
+  width: 100%;
+  padding: 15px;
+  box-sizing: border-box;
+
+  img {
+    max-width: 100%;
+    width: 100%;
+  }
+`;
+
+export const StyledSauceContent = styled.div`
+  width: 66%;
+  padding: 15px;
+  box-sizing: border-box;
+`;
+
+export const StyledButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+
+  button {
+    margin: 0 15px;
+  }
+`;
