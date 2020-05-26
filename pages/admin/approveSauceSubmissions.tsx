115153746670b61a068ec9a59e10994c63cfb3fd
Kyle Bonar <kylewb2011@gmail.com>
2020-05-26 10:26:21 -0500

    init commit

---
+++ pages/admin/approveSauceSubmissions.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import ApproveSubmissions from "../../src/components/ApproveSubmissions/ApproveSubmissions";
+
+interface IApproveSauceSubmissionsProps {}
+
+const ApproveSauceSubmissions: React.FunctionComponent<IApproveSauceSubmissionsProps> = props => {
+  return <ApproveSubmissions />;
+};
+
+export default ApproveSauceSubmissions;
