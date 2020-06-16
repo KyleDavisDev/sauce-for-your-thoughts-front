7ce0e7baf6e4ab6811719f649dcc4e55ccbd6f58
Kyle Bonar <kylewb2011@gmail.com>
2020-06-16 16:44:55 -0500

    Init commit

---
+++ pages/account/reset/password.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import ResetPassword from "../../../src/components/ResetPassword/ResetPassword";
+
+export interface RegisterProps {}
+
+const Register: React.SFC<RegisterProps> = () => {
+  return <ResetPassword />;
+};
+
+export default Register;
