4495a7d09fa1b3c2c3c4fdf3beff3b295db0528e
Kyle Bonar <kylewb2011@gmail.com>
2020-06-16 23:54:52 -0500

    Removed unused code

--- src/components/ResetPassword/ResetPasswordStyle.tsx
+++ src/components/ResetPassword/ResetPasswordStyle.tsx
@@ -1,22 +1,9 @@
 import styled from "styled-components";
-import { Article } from "../../components/Article/Article";
+
 import { Button } from "../../components/Button/Button";
 
-export const StyledDiv = styled.div`
-  height: 100vh;
-`;
-
-export const StyledLogoContainer = styled.div`
-  max-width: 150px;
-  margin: 0 auto;
-  padding: 1em;
-`;
-
-export const StyledArticle = styled(Article)`
-  max-width: 600px;
-`;
 export const StyledFormContainer = styled.div`
-  background-color: ${props => props.theme.formContainerBackgroundColor};
+  border: ${props => props.theme.border};
   padding: 1.5rem;
   display: flex;
   flex-direction: row;
