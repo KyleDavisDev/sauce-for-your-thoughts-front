85341ebde32919c35db4c42eb0dca77a8f4f0d5b
Kyle Bonar <kylewb2011@gmail.com>
2020-07-05 16:32:07 -0500

    Moved some of RegisterUser component into this component

--- pages/account/register.tsx
+++ pages/account/register.tsx
@@ -1,10 +1,19 @@
 import * as React from "react";
 import RegisterUser from "../../src/components/RegisterUser/RegisterUser";
+import { Article } from "../../src/components/Article/Article";
+import HeaderSimple from "../../src/components/HeaderSimple/HeaderSimple";
 
 export interface RegisterProps {}
 
 const Register: React.SFC<RegisterProps> = () => {
-  return <RegisterUser />;
+  return (
+    <>
+      <HeaderSimple />
+      <Article size="sm">
+        <RegisterUser />
+      </Article>
+    </>
+  );
 };
 
 export default Register;
