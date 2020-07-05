f91edc11b7d11f7efcf9a5fa1dfbb9ed740adbe5
Kyle Bonar <kylewb2011@gmail.com>
2020-07-05 16:55:29 -0500

    Extending component to handle some stuff from LoginUser component

--- pages/account/login.tsx
+++ pages/account/login.tsx
@@ -1,10 +1,19 @@
 import * as React from "react";
 import LoginUser from "../../src/components/LoginUser/LoginUser";
+import HeaderSimple from "../../src/components/HeaderSimple/HeaderSimple";
+import { Article } from "../../src/components/Article/Article";
 
 export interface LoginProps {}
 
 const Login: React.SFC<LoginProps> = () => {
-  return <LoginUser />;
+  return (
+    <>
+      <HeaderSimple />
+      <Article size="sm">
+        <LoginUser />
+      </Article>
+    </>
+  );
 };
 
 export default Login;
