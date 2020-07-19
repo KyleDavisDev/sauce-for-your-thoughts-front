83c3a2ae2bbdff58bed4ff0caffc3529d14b683d
Kyle Bonar <kylewb2011@gmail.com>
2020-07-19 16:06:02 -0500

    init commit

---
+++ src/components/SetPassword/SetPasswordStyle.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+import { Button } from "../Button/Button";
+
+export const StyledFormContainer = styled.div`
+  border: ${props => props.theme.border};
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+export const StyledText = styled.p`
+  margin: 0 auto 1em;
+`;
+
+export const StyledButton = styled(Button)`
+  text-align: center;
+`;
