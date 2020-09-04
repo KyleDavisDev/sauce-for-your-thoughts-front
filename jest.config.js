68dbde4bdbd37bf5488b9f7ccf6976ee9a98d0a9
Kyle Bonar <kylewb2011@gmail.com>
2020-09-04 13:44:17 -0500

    Configuring tests

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  setupTestFrameworkScriptFile: require.resolve("./jest.setup.ts"),
-  snapshotSerializers: ["enzyme-to-json"],
-  roots: ["<rootDir>/src"],
+  setupFilesAfterEnv: [require.resolve("./jest.setup.ts")],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  roots: ["<rootDir>/src/components", "<rootDir>/src/utils"],
+  testEnvironment: "node",
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+      tsConfig: "tsconfig.test.json"
+    }
+  }
 };
