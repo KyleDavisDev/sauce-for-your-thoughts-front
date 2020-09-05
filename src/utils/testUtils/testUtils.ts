d4328b7fa6e1a332f3781cdc1e3d38ea4e73869c
Kyle Bonar <kylewb2011@gmail.com>
2020-09-05 15:53:43 -0500

    functions for creating fakeJSXElement, and for checking type of an element

--- src/utils/testUtils/testUtils.ts
+++ src/utils/testUtils/testUtils.ts
@@ -3,6 +3,9 @@ import casual from "casual";
 import { ReactWrapper } from "enzyme";
 import { MockLink, MockButton } from "./types";
 
+const ITERATION_SIZE = 8;
+const REACT_REGEX = /react(\d+)?./i;
+
 // seed it so we get consistent results
 casual.seed(777);
 
@@ -12,8 +15,15 @@ const fakeLink = (): MockLink => ({
   target: casual.boolean ? "_blank" : "_self"
 });
 
+const fakeJSXElement = (): JSX.Element => {
+  return casual.random_element([
+    React.createElement("div", null, casual.string),
+    React.createElement("span", null, casual.string)
+  ]);
+};
+
 const fakeButton = (): MockButton => ({
-  children: React.createElement("div", null, casual.string),
+  children: casual.random_element([fakeJSXElement(), casual.text]),
   displayType: casual.random_element(["outline", "solid"]),
   className: casual.string,
   type: casual.random_element(["button", "submit", "reset"]),
@@ -23,7 +33,8 @@ const fakeButton = (): MockButton => ({
 });
 
 const fakeLinks = () => [fakeLink(), fakeLink(), fakeLink()];
-const fakeButtons = () => [fakeButton(), fakeButton(), fakeButton()];
+const fakeButtons = (): MockButton[] =>
+  new Array(ITERATION_SIZE).fill(null).map(fakeButton);
 
 function simulateInputChange(
   wrapper: ReactWrapper,
@@ -35,4 +46,40 @@ function simulateInputChange(
     .simulate("change", { target: { name, value } });
 }
 
-export { fakeLinks, fakeButtons, simulateInputChange };
+const isClassComponent = (component): boolean => {
+  return (
+    typeof component === "function" &&
+    component.prototype &&
+    !!component.prototype.isReactComponent
+  );
+};
+
+// Ensure compatability with transformed code
+const isFunctionComponent = (component): boolean => {
+  return (
+    typeof component === "function" &&
+    String(component).includes("return") &&
+    !!String(component).match(REACT_REGEX) &&
+    String(component).includes(".createElement")
+  );
+};
+
+const isElement = (typeElement): boolean => {
+  return React.isValidElement(typeElement);
+};
+
+const isComponent = (component): boolean => {
+  return isClassComponent(component) || isFunctionComponent(component);
+};
+
+const isDOMTypeElement = (typeElement): boolean => {
+  return isElement(typeElement) && typeof typeElement.type === "string";
+};
+
+export {
+  fakeLinks,
+  fakeButtons,
+  simulateInputChange,
+  isDOMTypeElement,
+  isComponent
+};
