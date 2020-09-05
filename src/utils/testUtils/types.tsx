e174564a00caa3f420372fd2d7614624b756b80f
Kyle Bonar <kylewb2011@gmail.com>
2020-09-05 14:01:49 -0500

    Get MockButton interface from ButtonProps interface

--- src/utils/testUtils/types.tsx
+++ src/utils/testUtils/types.tsx
@@ -1,5 +1,9 @@
+import { ButtonProps } from "../../components/Button/Button";
+
 export interface MockLink {
   text: string;
   to: string;
   target: "_blank" | "_self";
 }
+
+export interface MockButton extends ButtonProps {}
