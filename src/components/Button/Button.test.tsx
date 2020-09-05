f6f3ffad098eb68c30678c35dfa87d2c1a51c58d
Kyle Bonar <kylewb2011@gmail.com>
2020-09-05 13:34:56 -0500

    Added snapshot test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -11,6 +11,14 @@ describe("<Button>", () => {
     expect(wrapper).toBeTruthy();
   });
 
+  it("matches snapshot", () => {
+    const wrapper = enzyme.shallow(
+      <Button onClick={() => {}}>Click me!</Button>
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
   it("renders correct text", () => {
     let wrapper = enzyme.render(<Button onClick={() => {}}>Text here</Button>);
     expect(wrapper.find("button").text()).toEqual("Text here");
