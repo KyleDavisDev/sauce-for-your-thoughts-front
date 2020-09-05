b6753f8a940f4e6b5a860ccc2392119a9693ab36
Kyle Bonar <kylewb2011@gmail.com>
2020-09-05 14:00:54 -0500

    Import mock buttons, compare mock buttons to snapshots

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,21 +2,24 @@ import * as React from "react";
 import * as enzyme from "enzyme";
 
 import { Button } from "./Button";
+import { fakeButtons } from "../../utils/testUtils/testUtils";
+
+const mockButtons = fakeButtons();
 
 describe("<Button>", () => {
   it("renders", () => {
-    const wrapper = enzyme.shallow(
-      <Button onClick={() => {}}>Click me!</Button>
-    );
+    const wrapper = enzyme.shallow(<Button>test</Button>);
     expect(wrapper).toBeTruthy();
   });
 
   it("matches snapshot", () => {
-    const wrapper = enzyme.shallow(
-      <Button onClick={() => {}}>Click me!</Button>
-    );
+    mockButtons.forEach(mockButton => {
+      const wrapper = enzyme.shallow(
+        <Button {...mockButton}>{mockButton.children}</Button>
+      );
 
-    expect(wrapper).toMatchSnapshot();
+      expect(wrapper).toMatchSnapshot();
+    });
   });
 
   it("renders correct text", () => {
