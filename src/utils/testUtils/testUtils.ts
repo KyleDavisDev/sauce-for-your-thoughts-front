f6c45c683b7b4a0de194f5fb90cc34211fb14c00
Kyle Bonar <kylewb2011@gmail.com>
2020-09-05 14:01:27 -0500

    Create fake button object and export set of fake buttons

--- src/utils/testUtils/testUtils.ts
+++ src/utils/testUtils/testUtils.ts
@@ -1,6 +1,7 @@
+import * as React from "react";
 import casual from "casual";
 import { ReactWrapper } from "enzyme";
-import { MockLink } from "./types";
+import { MockLink, MockButton } from "./types";
 
 // seed it so we get consistent results
 casual.seed(777);
@@ -11,7 +12,18 @@ const fakeLink = (): MockLink => ({
   target: casual.boolean ? "_blank" : "_self"
 });
 
+const fakeButton = (): MockButton => ({
+  children: React.createElement("div", null, casual.string),
+  displayType: casual.random_element(["outline", "solid"]),
+  className: casual.string,
+  type: casual.random_element(["button", "submit", "reset"]),
+  onClick: () => {},
+  style: undefined,
+  disabled: casual.boolean
+});
+
 const fakeLinks = () => [fakeLink(), fakeLink(), fakeLink()];
+const fakeButtons = () => [fakeButton(), fakeButton(), fakeButton()];
 
 function simulateInputChange(
   wrapper: ReactWrapper,
@@ -23,4 +35,4 @@ function simulateInputChange(
     .simulate("change", { target: { name, value } });
 }
 
-export { fakeLinks, simulateInputChange };
+export { fakeLinks, fakeButtons, simulateInputChange };
