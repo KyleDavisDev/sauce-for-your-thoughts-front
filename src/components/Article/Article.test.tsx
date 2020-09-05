cba744ead5d844bd8631368cc84ced22b0c8d63c
Kyle Bonar <kylewb2011@gmail.com>
2020-09-05 01:56:30 -0500

    Render passed children

--- src/components/Article/Article.test.tsx
+++ src/components/Article/Article.test.tsx
@@ -12,4 +12,22 @@ describe("<Article>", () => {
     );
     expect(wrapper).toBeTruthy();
   });
+
+  it("renders children", () => {
+    let wrapper = enzyme.shallow(
+      <Article>
+        <span>hello!</span>
+      </Article>
+    );
+    expect(wrapper.find("span")).toBeTruthy();
+    expect(wrapper.find("span").text()).toEqual("hello!");
+
+    wrapper = enzyme.shallow(
+      <Article>
+        <p>hello! Anoter test here</p>
+      </Article>
+    );
+    expect(wrapper.find("p")).toBeTruthy();
+    expect(wrapper.find("p").text()).toEqual("hello! Anoter test here");
+  });
 });
