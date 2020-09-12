3d7b7e384a37dc7689750d4503caf892c98f0262
Kyle Bonar <kylewb2011@gmail.com>
2020-09-12 18:14:32 -0500

    New MockLabel interface

--- src/utils/testUtils/types.tsx
+++ src/utils/testUtils/types.tsx
@@ -1,9 +1,12 @@
 import { LinkProps } from "../../components/Link/Link";
 import { ButtonProps } from "../../components/Button/Button";
 import { CardProps } from "../../components/Card/Card";
+import { LabelProps } from "../../components/Label/Label";
 
 export interface MockLink extends LinkProps {}
 
 export interface MockButton extends ButtonProps {}
 
 export interface MockCard extends CardProps {}
+
+export interface MockLabel extends LabelProps {}
