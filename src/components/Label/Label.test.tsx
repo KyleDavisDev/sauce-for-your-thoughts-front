7fb06d2a9856f2efaed6cd57c2ead1e211c4de9e
Kyle Bonar <kylewb2011@gmail.com>
2020-09-12 18:14:41 -0500

    Renders correct children

--- src/components/Label/Label.test.tsx
+++ src/components/Label/Label.test.tsx
@@ -2,11 +2,21 @@ import * as React from "react";
 import * as enzyme from "enzyme";
 
 import Label from "./Label";
+// import { fakeLabels } from "../../utils/testUtils/testUtils";
+// import { MockLabel } from "../../utils/testUtils/types";
+
+// const mockLabels = fakeLabels();
 
 describe("<Label />", () => {
   it("renders", () => {
     const wrapper = enzyme.shallow(<Label>""</Label>);
 
     expect(wrapper).toBeTruthy();
+  });
+
+  it("renders correct children", () => {
+    const wrapper = enzyme.shallow(<Label>abc</Label>);
+
+    expect(wrapper.props().children).toEqual("abc");
   });
 });
