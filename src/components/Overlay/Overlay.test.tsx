b14120c3a9ffc5ee7ebfe9f1261f02c08dd7e65c
Kyle Bonar <kylewb2011@gmail.com>
2020-09-15 10:38:32 -0500

    matches snapshot

--- src/components/Overlay/Overlay.test.tsx
+++ src/components/Overlay/Overlay.test.tsx
@@ -5,8 +5,14 @@ import { Overlay } from "./Overlay";
 
 describe("<Overlay />", () => {
   it("renders", () => {
-    const wrapper = enzyme.shallow(<Overlay></Overlay>);
+    const wrapper = enzyme.shallow(<Overlay>123</Overlay>);
 
     expect(wrapper).toBeTruthy();
+  });
+
+  it("matches snapshot", () => {
+    const wrapper = enzyme.shallow(<Overlay>123</Overlay>);
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
