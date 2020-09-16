549af32d440e335cbcb122fee7669c2fc333441e
Kyle Bonar <kylewb2011@gmail.com>
2020-09-16 08:37:50 -0500

    Check for tag amounts generated instead of content

--- src/components/Footer/components/About/About.test.tsx
+++ src/components/Footer/components/About/About.test.tsx
@@ -1,31 +1,26 @@
 import * as React from "react";
 import * as enzyme from "enzyme";
+
 import About from "./About";
 
-describe("<About>", () => {
-  const wrapper = enzyme.shallow(<About />);
-
+describe("<About />", () => {
   it("renders", () => {
+    const wrapper = enzyme.shallow(<About />);
     expect(wrapper).toBeTruthy();
   });
 
-  it("renders component's static title", () => {
-    expect(
-      wrapper
-        .find("StyledH5")
-        .render()
-        .text()
-    ).toEqual("About");
+  it("matches snapshot", () => {
+    const wrapper = enzyme.shallow(<About />);
+    expect(wrapper).toMatchSnapshot();
   });
 
-  it("renders component's static description", () => {
-    expect(
-      wrapper
-        .find("StyledP")
-        .render()
-        .text()
-    ).toEqual(
-      "Ever wonder what a sauce tastes like before buying it? Sauce For Your Thoughts can help! Add, or review, sauces and help out your fellow saucies! Never be blind-sides by a sauce again!"
-    );
+  it("renders single h5 tag", () => {
+    const wrapper = enzyme.shallow(<About />);
+    expect(wrapper.find("h5").length).toEqual(1);
+  });
+
+  it("renders single p tag", () => {
+    const wrapper = enzyme.shallow(<About />);
+    expect(wrapper.find("p").length).toEqual(1);
   });
 });
