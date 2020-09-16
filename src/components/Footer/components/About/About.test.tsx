c82f75e30d744a45609f839dff4da05a93406907
Kyle Bonar <kylewb2011@gmail.com>
2020-09-16 08:41:20 -0500

    concats classname to parent div

--- src/components/Footer/components/About/About.test.tsx
+++ src/components/Footer/components/About/About.test.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as enzyme from "enzyme";
 
 import About from "./About";
+import { casual } from "../../../../utils/testUtils/testUtils";
 
 describe("<About />", () => {
   it("renders", () => {
@@ -12,6 +13,13 @@ describe("<About />", () => {
   it("matches snapshot", () => {
     const wrapper = enzyme.shallow(<About />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("concats classname on parent div", () => {
+    const extraClasses = casual.string;
+    const wrapper = enzyme.shallow(<About className={extraClasses} />);
+
+    expect(wrapper.find("div").props().className).toContain(extraClasses);
   });
 
   it("renders single h5 tag", () => {
