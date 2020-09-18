f46704e7d7d5ad8a0fa8e05b477bae00be03fa3a
Kyle Bonar <kylewb2011@gmail.com>
2020-09-17 22:41:59 -0500

    Creat emock store, pass truthy test

--- src/components/Footer/components/Types/TypesOfSauces.test.tsx
+++ src/components/Footer/components/Types/TypesOfSauces.test.tsx
@@ -1,16 +1,22 @@
 import * as React from "react";
 import * as enzyme from "enzyme";
-import Types from "./TypesOfSauces";
-import { MemoryRouter } from "react-router-dom";
+import { Provider, useSelector } from "react-redux";
+
+import TypesOfSauces from "./TypesOfSauces";
+import {
+  mockStore,
+  ITERATION_SIZE
+} from "../../../../utils/testUtils/testUtils";
+
+const mockStores = new Array(ITERATION_SIZE).fill(null).map(mockStore);
 
 describe("<Types />", () => {
-  const wrapper = enzyme.render(
-    <MemoryRouter>
-      <Types />
-    </MemoryRouter>
-  );
-
   it("renders", () => {
+    const wrapper = enzyme.shallow(
+      <Provider store={mockStores[0]}>
+        <TypesOfSauces />
+      </Provider>
+    );
     expect(wrapper).toBeTruthy();
   });
 });
