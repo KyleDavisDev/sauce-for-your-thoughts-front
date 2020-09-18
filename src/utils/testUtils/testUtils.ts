60cb7576e45f718185c4e0eceed97188eacbd4eb
Kyle Bonar <kylewb2011@gmail.com>
2020-09-17 22:43:54 -0500

    Create fakeSauce, fakeSaucesState, fakeFlashMessage, mockStore

--- src/utils/testUtils/testUtils.ts
+++ src/utils/testUtils/testUtils.ts
@@ -1,13 +1,109 @@
 import * as React from "react";
 import casual from "casual";
 import { ReactWrapper } from "enzyme";
+import configureStore from "redux-mock-store";
+
 import { MockCard } from "./types";
+import { AppState } from "../../redux/configureStore";
+import { ISauce, ISaucesState } from "../../redux/sauces/types";
+import { FlashMessageProps } from "../../components/FlashMessage/FlashMessage";
+import Flatn from "../Flatn/Flatn";
 
 export const ITERATION_SIZE = 8;
 const REACT_REGEX = /react(\d+)?./i;
 
 // seed it so we get consistent results
 casual.seed(777);
+
+const relatedSauce = () => ({
+  name: casual.name,
+  slug: casual.uuid
+});
+
+export const fakeSauce = (): ISauce => ({
+  _id: casual.random_element([undefined, casual.integer(1, 500)]),
+  _addedToStore: casual.random_element([undefined, casual.unix_time]), // Unix time (in seconds) added to redux store
+  _full: casual.random_element([undefined, casual.boolean]), // Whether we have full sauce or partial
+  _related: casual.random_element([
+    undefined,
+    new Array(ITERATION_SIZE).fill(null).map(relatedSauce)
+  ]), // List of related sauces
+  isAdminApproved: casual.random_element([undefined, casual.boolean]),
+  name: casual.name,
+  ingredients: casual.string,
+  author: casual.name,
+  created: casual.unix_time, // Unix time (in seconds)
+  types: casual.array_of_words(),
+  maker: casual.name,
+  description: casual.string,
+  photo: casual.random_element([undefined, casual.url]),
+  shu: casual.random_element([
+    undefined,
+    casual.integer(1, 500),
+    casual.integer(1, 500).toString()
+  ]),
+  reviews: casual.random_element([
+    undefined,
+    new Array(ITERATION_SIZE).fill(null).map(x => casual.word)
+  ]),
+  city: casual.random_element([undefined, casual.city]),
+  state: casual.random_element([undefined, casual.state]),
+  country: casual.random_element([undefined, casual.country]),
+  slug: casual.random_element([undefined, casual.uuid])
+});
+
+export const fakeSaucesState = (): ISaucesState => {
+  const sauces: ISauce[] = new Array(casual.integer(1, 15))
+    .fill(null)
+    .map(fakeSauce);
+  const { allSlugs, bySlug } = Flatn.saucesArr({ sauces });
+
+  return {
+    allSlugs,
+    bySlug,
+    total: allSlugs.length,
+    query: undefined, // IQuery,
+    saucesWithNewestReviews: undefined, // Array<{ name: string; slug: string }>,
+    newest: undefined, //string[],
+    featured: undefined, //string[],
+    types: casual.array_of_words(),
+    orders: ["Newest", "Name", "Times Reviewed", "Avg Rating"]
+  };
+};
+
+export const fakeFlashMessage = (): FlashMessageProps => ({
+  className: casual.random_element([undefined, casual.string]),
+  type: casual.random_element([undefined, "success", "warning", "alert"]),
+  isVisible: casual.boolean,
+  slug: casual.random_element([undefined, casual.url]),
+  slugText: casual.random_element([undefined, casual.name]),
+  text: casual.random_element([undefined, casual.string]),
+  children: casual.random_element([undefined, casual.string])
+});
+
+export const mockStore = () => {
+  const storeConfig = configureStore([]);
+
+  const store: AppState = {
+    sauces: fakeSaucesState(),
+    users: {
+      self: {
+        token: casual.random_element([undefined, casual.uuid]),
+        displayName: casual.random_element([undefined, casual.name]),
+        avatarURL: casual.random_element([undefined, casual.url]),
+        isAdmin: casual.random_element([undefined, casual.boolean])
+      },
+      byDisplayName: undefined, // { [key: string]: IUser };
+      allDisplayNames: undefined //  string[];
+    },
+    reviews: {
+      byReviewID: undefined, //  { [key: string]: IReview };
+      allReviewIDs: undefined //  string[];
+    }2
+  };
+
+  return storeConfig(store);
+};
 
 export const fakeJSXElement = (): JSX.Element => {
   return casual.random_element([
