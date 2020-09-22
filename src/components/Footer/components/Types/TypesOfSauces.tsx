698b05d3809fda64f3044d41500d1094b6a068f1
Kyle Bonar <kylewb2011@gmail.com>
2020-09-22 14:57:58 -0500

    Make sure each id is unique

--- src/components/Footer/components/Types/TypesOfSauces.tsx
+++ src/components/Footer/components/Types/TypesOfSauces.tsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import List from "../../../List/List";
 import { AppState } from "../../../../redux/configureStore";
 import styled from "../../../../theme/styled-components";
+import { casual } from "../../../../utils/testUtils/testUtils";
 
 const StyledList = styled(List)`
   h5,
@@ -15,6 +16,7 @@ const StyledList = styled(List)`
     color: ${x => x.theme.secondaryThemeColor};
   }
 `;
+StyledList.displayName = "List";
 
 const TypesOfSauces: React.FC = () => {
   const items = useSelector((store: AppState) => store.sauces.types).map(
@@ -22,7 +24,7 @@ const TypesOfSauces: React.FC = () => {
       return {
         link: `/sauces?limit=15&order=newest&page=1&type=${type}`,
         text: type,
-        id: type
+        id: casual.uuid
       };
     }
   );
