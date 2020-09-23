396e56c832ef1cc178fab2064eacf1f5dc608c77
Kyle Bonar <kylewb2011@gmail.com>
2020-09-23 07:36:52 -0500

    Moved wrapper inside each test

--- src/components/Footer/components/Navigation/Navigation.test.tsx
+++ src/components/Footer/components/Navigation/Navigation.test.tsx
@@ -1,20 +1,16 @@
 import * as React from "react";
 import * as enzyme from "enzyme";
+
 import Navigation from "./Navigation";
-import { MemoryRouter } from "react-router-dom";
 
 describe("<Navigation />", () => {
-  const wrapper = enzyme.render(
-    <MemoryRouter>
-      <Navigation />
-    </MemoryRouter>
-  );
-
   it("renders", () => {
+    const wrapper = enzyme.render(<Navigation />);
     expect(wrapper).toBeTruthy();
   });
 
   it("has correct title", () => {
+    const wrapper = enzyme.render(<Navigation />);
     expect(wrapper.find("h5").text()).toEqual("Navigation");
   });
 });
