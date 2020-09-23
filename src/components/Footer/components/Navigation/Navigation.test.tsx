4631740b8bf472f6c65d921ca43b3696c81fc0c9
Kyle Bonar <kylewb2011@gmail.com>
2020-09-23 07:39:04 -0500

    Iterate over iteration, removed unneeded test

--- src/components/Footer/components/Navigation/Navigation.test.tsx
+++ src/components/Footer/components/Navigation/Navigation.test.tsx
@@ -2,15 +2,13 @@ import * as React from "react";
 import * as enzyme from "enzyme";
 
 import Navigation from "./Navigation";
+import { ITERATION_SIZE } from "../../../../utils/testUtils/testUtils";
 
 describe("<Navigation />", () => {
   it("renders", () => {
-    const wrapper = enzyme.render(<Navigation />);
-    expect(wrapper).toBeTruthy();
-  });
-
-  it("has correct title", () => {
-    const wrapper = enzyme.render(<Navigation />);
-    expect(wrapper.find("h5").text()).toEqual("Navigation");
+    new Array(ITERATION_SIZE).fill(null).forEach(() => {
+      const wrapper = enzyme.render(<Navigation />);
+      expect(wrapper).toBeTruthy();
+    });
   });
 });
