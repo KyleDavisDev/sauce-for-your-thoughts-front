2a036aee940579d014a17fc900b807d6356d2751
Kyle Bonar <kylewb2011@gmail.com>
2020-09-23 07:51:17 -0500

    Test over iteration

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -2,17 +2,22 @@ import * as React from "react";
 import * as enzyme from "enzyme";
 
 import Footer from "./Footer";
+import { ITERATION_SIZE } from "../../utils/testUtils/testUtils";
 
 describe("<Footer />", () => {
   it("renders", () => {
-    const wrapper = enzyme.shallow(<Footer />);
+    new Array(ITERATION_SIZE).fill(null).forEach(() => {
+      const wrapper = enzyme.shallow(<Footer />);
 
-    expect(wrapper).toBeTruthy();
+      expect(wrapper).toBeTruthy();
+    });
   });
 
   it("matches snapshot", () => {
-    const wrapper = enzyme.shallow(<Footer />);
+    new Array(ITERATION_SIZE).fill(null).forEach(() => {
+      const wrapper = enzyme.shallow(<Footer />);
 
-    expect(wrapper).toMatchSnapshot();
+      expect(wrapper).toMatchSnapshot();
+    });
   });
 });
