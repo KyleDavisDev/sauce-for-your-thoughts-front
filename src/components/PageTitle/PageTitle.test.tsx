e53c0f4a7bb9f5a4d06e22dd7476153d2688abee
Kyle Bonar <kylewb2011@gmail.com>
2020-09-26 11:13:48 -0500

    Create PageTitle mock, iterate over iteration, it renders

--- src/components/PageTitle/PageTitle.test.tsx
+++ src/components/PageTitle/PageTitle.test.tsx
@@ -1,44 +1,38 @@
 import * as React from "react";
 import * as enzyme from "enzyme";
-import PageTitle from "./PageTitle";
+
+import { casual, ITERATION_SIZE } from "../../utils/testUtils/testUtils";
+import PageTitle, { PageTitleProps } from "./PageTitle";
+
+const fakePageTitle = (): PageTitleProps => ({
+  children: casual.random_element([undefined, casual.string]),
+  className: casual.random_element([undefined, casual.string])
+});
+
+const mockPageTitles = new Array(ITERATION_SIZE).fill(null).map(fakePageTitle);
 
 describe("<PageTitle />", () => {
-  const children = ["child1", "different text", "Third rANDom text"];
-  const wrapper = enzyme.shallow(<PageTitle>{children[0]}</PageTitle>);
-
   it("renders", () => {
-    expect(wrapper).toBeTruthy();
+    mockPageTitles.forEach(mockPageTitle => {
+      const wrapper = enzyme.shallow(<PageTitle {...mockPageTitle} />);
+      expect(wrapper).toBeTruthy();
+    });
   });
 
-  it("renders correct tag", () => {
-    expect(wrapper.find("StyledH1")).toBeTruthy();
-    expect(wrapper.find("StyledH1").length).toEqual(1);
-  });
+  // it("renders correct tag", () => {
+  //   expect(wrapper.find("StyledH1")).toBeTruthy();
+  //   expect(wrapper.find("StyledH1").length).toEqual(1);
+  // });
 
-  it("renders correct text", () => {
-    expect(
-      wrapper
-        .find("StyledH1")
-        .render()
-        .text()
-    ).toEqual(children[0]);
+  // it("renders correct text", () => {
+  //   expect(wrapper.find("StyledH1").render().text()).toEqual(children[0]);
 
-    // update text, and test
-    wrapper.setProps({ children: children[1] });
-    expect(
-      wrapper
-        .find("StyledH1")
-        .render()
-        .text()
-    ).toEqual(children[1]);
+  //   // update text, and test
+  //   wrapper.setProps({ children: children[1] });
+  //   expect(wrapper.find("StyledH1").render().text()).toEqual(children[1]);
 
-    // update text, and test
-    wrapper.setProps({ children: children[2] });
-    expect(
-      wrapper
-        .find("StyledH1")
-        .render()
-        .text()
-    ).toEqual(children[2]);
-  });
+  //   // update text, and test
+  //   wrapper.setProps({ children: children[2] });
+  //   expect(wrapper.find("StyledH1").render().text()).toEqual(children[2]);
+  // });
 });
