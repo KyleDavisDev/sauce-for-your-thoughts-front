c8762a8af540373ac2a5a7e7d0fdf27f5c9be367
Kyle Bonar <kylewb2011@gmail.com>
2020-09-28 08:36:26 -0500

    It renders

---
+++ src/components/CheckBox/CheckBox.test.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import * as enzyme from "enzyme";
+
+import { casual } from "../../utils/testUtils/testUtils";
+import CheckBox, { CheckBoxProps } from "./CheckBox";
+
+const fakeCheckBox = (): CheckBoxProps => ({
+  checked: casual.boolean,
+  id: casual.uuid,
+  value: casual.string,
+  label: casual.string,
+  onClick: jest.fn(),
+  className: casual.random_element([undefined, casual.string])
+});
+
+describe("<CheckBox />", () => {
+  it("renders", () => {
+    const wrapper = enzyme.shallow(<CheckBox {...fakeCheckBox()} />);
+
+    expect(wrapper).toBeTruthy();
+  });
+});
