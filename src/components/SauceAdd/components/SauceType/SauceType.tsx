3c3a758abe64996e6d8bd871c420cc2bdd9c7c49
Kyle Bonar <kylewb2011@gmail.com>
2020-09-28 10:10:37 -0500

    Pure Component to FC

--- src/components/SauceAdd/components/SauceType/SauceType.tsx
+++ src/components/SauceAdd/components/SauceType/SauceType.tsx
@@ -16,48 +16,38 @@ export interface ISauceTypeProps {
   onCheckBoxClick: (event: React.MouseEvent<HTMLInputElement>) => void;
 }
 
-export default class SauceType extends React.PureComponent<
-  ISauceTypeProps,
-  {}
-> {
-  public componentWillReceiveProps(props: ISauceTypeProps) {
-    // grab types
-    const { typesOfSauces } = props;
+const SauceType: React.FC<ISauceTypeProps> = props => {
+  // grab types
+  const { typesOfSauces } = props;
 
-    // Sanity check
-    if (!typesOfSauces) return;
+  // Sanity check
+  if (!typesOfSauces) return null;
 
-    // update state w/ new sauces
-    this.setState(prevState => {
-      return { ...prevState, typesOfSauces };
-    });
-  }
+  return (
+    <StyledRow>
+      <StyledDescriptor title="Type">
+        What type of sauce is this? What is it primarily used for?
+      </StyledDescriptor>
+      <StyledRightSide>
+        <StyledDiv2>
+          <Label>Type of Sauce</Label>
 
-  public render() {
-    return (
-      <StyledRow>
-        <StyledDescriptor title="Type">
-          What type of sauce is this? What is it primarily used for?
-        </StyledDescriptor>
-        <StyledRightSide>
-          <StyledDiv2>
-            <Label>Type of Sauce</Label>
+          {Object.keys(typesOfSauces).map(type => {
+            return (
+              <CheckBox
+                id={typesOfSauces[type].key}
+                key={typesOfSauces[type].key}
+                value={typesOfSauces[type].value}
+                label={typesOfSauces[type].value}
+                checked={typesOfSauces[type].checked}
+                onClick={props.onCheckBoxClick}
+              />
+            );
+          })}
+        </StyledDiv2>
+      </StyledRightSide>
+    </StyledRow>
+  );
+};
 
-            {Object.keys(this.props.typesOfSauces).map(type => {
-              return (
-                <CheckBox
-                  id={this.props.typesOfSauces[type].key}
-                  key={this.props.typesOfSauces[type].key}
-                  value={this.props.typesOfSauces[type].value}
-                  label={this.props.typesOfSauces[type].value}
-                  checked={this.props.typesOfSauces[type].checked}
-                  onClick={this.props.onCheckBoxClick}
-                />
-              );
-            })}
-          </StyledDiv2>
-        </StyledRightSide>
-      </StyledRow>
-    );
-  }
-}
+export default SauceType;
