fa690c1347801fe683cd65ae1a07b73dcb000bb6
Kyle Bonar <kylewb2011@gmail.com>
2020-10-05 08:37:43 -0500

    matches snapshot

--- src/components/TextArea/TextArea.test.tsx
+++ src/components/TextArea/TextArea.test.tsx
@@ -8,8 +8,14 @@ let error = console.error;
 
 describe("<TextArea />", () => {
   it("renders", () => {
-    const wrapper = enzyme.render(<TextArea />);
+    const wrapper = enzyme.render(<TextArea onChange={() => {}} />);
 
     expect(wrapper).toBeTruthy();
+  });
+
+  it("matches snapshot", () => {
+    const wrapper = enzyme.render(<TextArea onChange={() => {}} />);
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
