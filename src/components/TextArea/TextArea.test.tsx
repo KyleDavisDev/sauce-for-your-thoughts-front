ceca277c66c364e2f3580a8d66a7a21a265d7c86
Kyle Bonar <kylewb2011@gmail.com>
2020-10-05 08:41:41 -0500

    Create fakeTextArea function

--- src/components/TextArea/TextArea.test.tsx
+++ src/components/TextArea/TextArea.test.tsx
@@ -2,9 +2,24 @@
 import * as React from "react";
 import * as enzyme from "enzyme";
 
-import TextArea from "./TextArea";
+import TextArea, { TextAreaProps } from "./TextArea";
+import { casual } from "../../utils/testUtils/testUtils";
 
-let error = console.error;
+const fakeTextArea = (): TextAreaProps => ({
+  id: casual.random_element([undefined, casual.string]),
+  name: casual.random_element([undefined, casual.string]),
+  placeholder: casual.random_element([undefined, casual.string]),
+  required: casual.random_element([undefined, casual.boolean]),
+  label: casual.random_element([undefined, casual.string]),
+  showLabel: casual.random_element([undefined, casual.boolean]),
+  value: casual.random_element([undefined, casual.string, casual.integer]),
+  className: casual.random_element([undefined, casual.string]),
+  disabled: casual.random_element([undefined, casual.boolean]),
+  readOnly: casual.random_element([undefined, casual.boolean]),
+  maxLength: casual.random_element([undefined, casual.integer(0, 5000)]),
+  requirementText: casual.string,
+  onChange: jest.fn()
+});
 
 describe("<TextArea />", () => {
   it("renders", () => {
