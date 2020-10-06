db1191f7863ef1d123ba1baa6ffb7866e91a5b11
Kyle Bonar <kylewb2011@gmail.com>
2020-10-06 09:34:28 -0500

    Split component into smaller pieces

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,18 +1,9 @@
 import * as React from "react";
 import { useSelector } from "react-redux";
-import ChevronDown from "../../images/icons/ChevronDown";
-import LoginIcon from "../../images/icons/LoginIcon";
-import UserIcon from "../../images/icons/UserIcon";
+
 import { AppState } from "../../redux/configureStore";
-import Menu from "./components/Menu/Menu";
-import {
-  StyledAvatar,
-  StyledDiv,
-  StyledDropDown,
-  StyledLink
-} from "./TopBarStyle";
-import Trigger from "./components/Trigger/Trigger";
-import Body from "./components/Body/Body";
+import LoggedInOptions from "./components/LoggedInOptions/LoggedInOptions";
+import DefaultOptions from "./components/DefaultOptions/DefaultOptions";
 
 const TopBar: React.FC = () => {
   const { self } = useSelector((state: AppState) => {
@@ -24,36 +15,9 @@ const TopBar: React.FC = () => {
   return (
     <header>
       {isLoggedIn && displayName && avatarURL ? (
-        <StyledDiv>
-          <StyledDropDown>
-            <Trigger>
-              <ChevronDown />
-              {displayName}
-              <StyledAvatar
-                src={avatarURL}
-                onError={e => {
-                  const elem = e.target as HTMLImageElement;
-                  elem.src =
-                    "https://res.cloudinary.com/foryourthoughts/image/upload/v1575867983/avatars/r0pnn1izbqm6wopt8lvq_lgq9q6.png";
-                }}
-              />
-            </Trigger>
-            <Body>
-              <Menu />
-            </Body>
-          </StyledDropDown>
-        </StyledDiv>
+        <LoggedInOptions displayName={displayName} avatarURL={avatarURL} />
       ) : (
-        <StyledDiv>
-          <StyledLink href="/account/register">
-            <UserIcon />
-            Register
-          </StyledLink>
-          <StyledLink href="/account/login">
-            <LoginIcon />
-            Log in
-          </StyledLink>
-        </StyledDiv>
+        <DefaultOptions />
       )}
     </header>
   );
