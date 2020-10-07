3f04b2049192fa1fbcaf9d8b83166685c24113e4
Kyle Bonar <kylewb2011@gmail.com>
2020-10-07 14:59:27 -0500

    Moved styled to relevant locations

--- src/components/TopBar/TopBarStyle.tsx
+++ src/components/TopBar/TopBarStyle.tsx
@@ -1,6 +1,4 @@
 import styled from "../../theme/styled-components";
-import { Link } from "../Link/Link";
-import { Dropdown } from "./components/Dropdown/Dropdown";
 
 export const StyledDiv = styled.div`
   background-color: ${props => props.theme.white};
@@ -10,31 +8,4 @@ export const StyledDiv = styled.div`
   width: 100%;
   padding: 0.5em 0em;
 `;
-
-export const StyledLink = styled(Link)`
-  text-decoration: none;
-  display: flex;
-  align-items: center;
-  margin-right: 2em;
-  color: ${x => x.theme.grey};
-  fill: ${x => x.theme.grey};
-
-  &:hover,
-  &:focus {
-    text-decoration: none;
-    color: ${x => x.theme.primaryThemeColor};
-    fill: ${x => x.theme.primaryThemeColor};
-  }
-`;
-StyledLink.displayName = "Link";
-
-export const StyledDropDown = styled(Dropdown)`
-  margin-right: 2em;
-`;
-StyledDropDown.displayName = "Dropdown";
-
-export const StyledAvatar = styled.img`
-  max-width: 35px;
-  margin-left: 5px;
-`;
-StyledAvatar.displayName = "img";
+StyledDiv.displayName = "div";
