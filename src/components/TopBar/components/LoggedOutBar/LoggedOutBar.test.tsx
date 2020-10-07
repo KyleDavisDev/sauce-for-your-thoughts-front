3c8987c96e05bdd078ec28ac9b70a5cb12ae9f78
Kyle Bonar <kylewb2011@gmail.com>
2020-10-07 09:00:52 -0500

    Loop over iteration size

--- src/components/TopBar/components/LoggedOutBar/LoggedOutBar.test.tsx
+++ src/components/TopBar/components/LoggedOutBar/LoggedOutBar.test.tsx
@@ -2,35 +2,46 @@ import * as React from "react";
 import * as enzyme from "enzyme";
 
 import LoggedOutBar from "./LoggedOutBar";
+import { ITERATION_SIZE } from "../../../../utils/testUtils/testUtils";
 
 describe("<LoggedOutBar />", () => {
   it("renders", () => {
-    const wrapper = enzyme.shallow(<LoggedOutBar />);
+    new Array(ITERATION_SIZE).fill(null).map(() => {
+      const wrapper = enzyme.shallow(<LoggedOutBar />);
 
-    expect(wrapper.exists()).toBeTruthy();
+      expect(wrapper.exists()).toBeTruthy();
+    });
   });
 
   it("matches snapshot", () => {
-    const wrapper = enzyme.shallow(<LoggedOutBar />);
+    new Array(ITERATION_SIZE).fill(null).map(() => {
+      const wrapper = enzyme.shallow(<LoggedOutBar />);
 
-    expect(wrapper).toMatchSnapshot();
+      expect(wrapper).toMatchSnapshot();
+    });
   });
 
   it("renders two Link components", () => {
-    const wrapper = enzyme.shallow(<LoggedOutBar />);
+    new Array(ITERATION_SIZE).fill(null).map(() => {
+      const wrapper = enzyme.shallow(<LoggedOutBar />);
 
-    expect(wrapper.find("Link").length).toEqual(2);
+      expect(wrapper.find("Link").length).toEqual(2);
+    });
   });
 
   it("first link is for registration", () => {
-    const wrapper = enzyme.shallow(<LoggedOutBar />);
+    new Array(ITERATION_SIZE).fill(null).map(() => {
+      const wrapper = enzyme.shallow(<LoggedOutBar />);
 
-    expect(wrapper.find("Link").first().prop("href")).toContain("register");
+      expect(wrapper.find("Link").first().prop("href")).toContain("register");
+    });
   });
 
   it("last link is for login", () => {
-    const wrapper = enzyme.shallow(<LoggedOutBar />);
+    new Array(ITERATION_SIZE).fill(null).map(() => {
+      const wrapper = enzyme.shallow(<LoggedOutBar />);
 
-    expect(wrapper.find("Link").last().prop("href")).toContain("login");
+      expect(wrapper.find("Link").last().prop("href")).toContain("login");
+    });
   });
 });
