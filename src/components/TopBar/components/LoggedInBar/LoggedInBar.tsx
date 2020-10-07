046a89ddb7580758d2727f8d61dbc6bd46cb5144
Kyle Bonar <kylewb2011@gmail.com>
2020-10-07 14:59:44 -0500

    Cleanup and simplication of component

--- src/components/TopBar/components/LoggedInBar/LoggedInBar.tsx
+++ src/components/TopBar/components/LoggedInBar/LoggedInBar.tsx
@@ -1,40 +1,21 @@
 import * as React from "react";
 
-import ChevronDown from "../../../../images/icons/ChevronDown";
-
 import Menu from "../Menu/Menu";
-import { StyledAvatar, StyledDiv, StyledDropDown } from "../../TopBarStyle";
-import Trigger from "../Trigger/Trigger";
-import Body from "../Body/Body";
 
-interface ILoggedInBarProps {
-  displayName: string;
-  avatarURL: string;
-}
+import DropDown from "../Dropdown/Dropdown";
+import Body from "../Body/Body";
+import Toggle from "../Toggle/Toggle";
+
+interface ILoggedInBarProps {}
 
 const LoggedInBar: React.FunctionComponent<ILoggedInBarProps> = props => {
-  const { displayName, avatarURL } = props;
-
   return (
-    <StyledDiv>
-      <StyledDropDown>
-        <Trigger>
-          <ChevronDown />
-          {displayName}
-          <StyledAvatar
-            src={avatarURL}
-            onError={e => {
-              const elem = e.target as HTMLImageElement;
-              elem.src =
-                "https://res.cloudinary.com/foryourthoughts/image/upload/v1575867983/avatars/r0pnn1izbqm6wopt8lvq_lgq9q6.png";
-            }}
-          />
-        </Trigger>
-        <Body>
-          <Menu />
-        </Body>
-      </StyledDropDown>
-    </StyledDiv>
+    <DropDown>
+      <Toggle />
+      <Body>
+        <Menu />
+      </Body>
+    </DropDown>
   );
 };
 
