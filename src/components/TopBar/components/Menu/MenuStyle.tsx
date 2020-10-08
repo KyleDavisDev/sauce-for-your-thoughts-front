8b5daaedc5091baa5bafb20ef0d07c15b277f6cc
Kyle Bonar <kylewb2011@gmail.com>
2020-10-08 10:06:34 -0500

    init commit

---
+++ src/components/TopBar/components/Menu/MenuStyle.tsx
@@ -0,0 +1,21 @@
+import styled from "../../../../theme/styled-components";
+
+export const StyledDiv = styled.div`
+  position: absolute;
+  z-index: 1000;
+  right: 0;
+`;
+StyledDiv.displayName = "div";
+
+export const StyledUL = styled.ul`
+  max-height: calc(100vh - 52px - 16px);
+  overflow-y: auto;
+  overflow-x: hidden;
+  width: 275px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
+  background-color: ${props => props.theme.white};
+  list-style: none;
+  padding: 0;
+  line-height: initial;
+`;
+StyledUL.displayName = "ul";
