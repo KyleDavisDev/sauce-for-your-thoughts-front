4dc46168fb87fccb5293882804be72c88ff87dff
Kyle Bonar <kylewb2011@gmail.com>
2020-10-08 10:06:55 -0500

    Abstract styles out, assign display name, add parent div

--- src/components/TopBar/components/Menu/Menu.tsx
+++ src/components/TopBar/components/Menu/Menu.tsx
@@ -8,44 +8,38 @@ import AccountMenuItem from "./components/AccountMenuItem/AccountMenuItem";
 import HelpMenuItem from "./components/HelpMenuItem/HelpMenuItem";
 import AdminMenuItem from "./components/AdminMenuItem/AdminMenuItem";
 import Item from "./components/Item/Item";
+import { StyledDiv, StyledUL } from "./MenuStyle";
 
-const StyledUL = styled.ul`
-  max-height: calc(100vh - 52px - 16px);
-  overflow-y: auto;
-  overflow-x: hidden;
-  width: 275px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
-  background-color: ${props => props.theme.white};
-  list-style: none;
-  padding: 0;
-  line-height: initial;
-`;
+export interface MenuProps {
+  className?: string;
+}
 
-export interface MenuProps {}
-
-const Menu: React.SFC<MenuProps> = props => {
+const Menu: React.FC<MenuProps> = props => {
   const isAdmin = useSelector((store: AppState) => store.users.self.isAdmin);
 
   return (
-    <StyledUL>
-      <li>
-        <ProfileMenuItem />
-      </li>
-      <li>
-        <AccountMenuItem />
-      </li>
-      <li>
-        <HelpMenuItem />
-      </li>
-      {isAdmin && (
+    <StyledDiv className={props.className}>
+      <StyledUL>
         <li>
-          <AdminMenuItem />
+          <ProfileMenuItem />
         </li>
-      )}
-      <hr style={{ margin: 0 }} />
-      <Item to="/account/logout">Logout</Item>
-    </StyledUL>
+        <li>
+          <AccountMenuItem />
+        </li>
+        <li>
+          <HelpMenuItem />
+        </li>
+        {isAdmin && (
+          <li>
+            <AdminMenuItem />
+          </li>
+        )}
+        <hr style={{ margin: 0 }} />
+        <Item to="/account/logout">Logout</Item>
+      </StyledUL>
+    </StyledDiv>
   );
 };
+Menu.displayName = "Menu";
 
 export default Menu;
