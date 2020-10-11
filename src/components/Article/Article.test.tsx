874a800253eee9a0c0286fbe1d6fdb4b79d79ecc
Kyle Bonar <kylewb2011@gmail.com>
2020-10-11 10:25:59 -0500

    Iterate over array

--- src/components/Article/Article.test.tsx
+++ src/components/Article/Article.test.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as enzyme from "enzyme";
 
-import { Article, ArticleProps } from "./Article";
+import  Article,{ ArticleProps } from "./Article";
 import {
   casual,
   fakeJSXElement,
@@ -27,30 +27,23 @@ describe("<Article>", () => {
   });
 
   it("matches snapshot", () => {
-    const wrapper = enzyme.shallow(
-      <Article>
-        <span>hello!</span>
-      </Article>
-    );
+    mockArticles.forEach(mockArticle => {
+      const wrapper = enzyme.shallow(
+        <Article {...mockArticle}>{mockArticle.children}</Article>
+      );
 
     expect(wrapper).toMatchSnapshot();
+    });
   });
 
-  it("renders children", () => {
-    let wrapper = enzyme.shallow(
-      <Article>
-        <span>hello!</span>
-      </Article>
-    );
-    expect(wrapper.find("span")).toBeTruthy();
-    expect(wrapper.find("span").text()).toEqual("hello!");
-
-    wrapper = enzyme.shallow(
-      <Article>
-        <p>hello! Anoter test here</p>
-      </Article>
-    );
-    expect(wrapper.find("p")).toBeTruthy();
-    expect(wrapper.find("p").text()).toEqual("hello! Anoter test here");
+  it("renders expected children", () => {
+    mockArticles.forEach(mockArticle => {
+      const wrapper = enzyme.shallow(
+        <Article {...mockArticle}>{mockArticle.children}</Article>
+      );
+      
+      expect(wrapper.find("article").prop("children")).toBeTruthy();
+      expect(wrapper.find("article").prop("children")).toEqual(mockArticle.children);
+    });
   });
 });
