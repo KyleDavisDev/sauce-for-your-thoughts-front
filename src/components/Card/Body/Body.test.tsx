85e943c845f9cb8748fc108c98151473e5251692
Kyle Bonar <kylewb2011@gmail.com>
2020-10-11 11:04:06 -0500

    Added mock setup

---
+++ src/components/Card/Body/Body.test.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import * as enzyme from "enzyme";
+
+import Body, { IBodyProps } from "./Body";
+import { casual, ITERATION_SIZE } from "../../../utils/testUtils/testUtils";
+
+const fakeBody = (): IBodyProps => ({
+  title: casual.random_element([undefined, casual.string]),
+  description: casual.random_element([undefined, casual.string])
+});
+
+describe("<Body />", () => {
+  const defaultText = "Loading ...";
+  it("renders", () => {
+    const wrapper = enzyme.shallow(<Body />);
+
+    expect(wrapper).toBeTruthy();
+  });
+});
