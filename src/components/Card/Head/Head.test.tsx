759666be407dc28f940fda5c91f7efcd255b0c13
Kyle Bonar <kylewb2011@gmail.com>
2020-10-11 11:33:00 -0500

    Setup mocks

--- src/components/Card/Head/Head.test.tsx
+++ src/components/Card/Head/Head.test.tsx
@@ -2,6 +2,16 @@ import * as React from "react";
 import * as enzyme from "enzyme";
 
 import Head, { IHeadProps } from "./Head";
+import { casual, ITERATION_SIZE } from "../../../utils/testUtils/testUtils";
+
+const fakeHead = (): IHeadProps => ({
+  showLink: casual.random_element([undefined, casual.boolean]),
+  to: casual.string,
+  imageLink: casual.random_element([undefined, casual.string]),
+  description: casual.random_element([undefined, casual.string])
+});
+
+const mockHead = new Array(ITERATION_SIZE).fill(null).map(fakeHead);
 
 describe("<Head />", () => {
   it("renders", () => {
