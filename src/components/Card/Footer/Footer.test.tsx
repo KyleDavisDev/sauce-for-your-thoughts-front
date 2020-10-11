614e66cfd6887041a33f7e335168045fffb97229
Kyle Bonar <kylewb2011@gmail.com>
2020-10-11 11:20:42 -0500

    It matches snapshot, setup mocks

--- src/components/Card/Footer/Footer.test.tsx
+++ src/components/Card/Footer/Footer.test.tsx
@@ -1,12 +1,27 @@
 import * as React from "react";
 import * as enzyme from "enzyme";
 
-import Footer from "./Footer";
+import Footer, { IFooterProps } from "./Footer";
+
+import { casual, ITERATION_SIZE } from "../../../utils/testUtils/testUtils";
+
+const fakeFooter = (): IFooterProps => ({
+  to: casual.string,
+  anchorText: casual.random_element([undefined, casual.string])
+});
+
+const mockFooters = new Array(ITERATION_SIZE).fill(null).map(fakeFooter);
 
 describe("<Footer />", () => {
   it("renders", () => {
     const wrapper = enzyme.shallow(<Footer to="" />);
 
     expect(wrapper).toBeTruthy();
+  });
+
+  it("matches snapshot", () => {
+    const wrapper = enzyme.shallow(<Footer to="" />);
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
