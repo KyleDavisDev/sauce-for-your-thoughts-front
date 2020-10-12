cfdf1a16b22d46d579b7d3acd9d15649a9119e5a
Kyle Bonar <kylewb2011@gmail.com>
2020-10-11 22:00:03 -0500

    Create mocks, iterate over mocks

--- src/components/SectionTitle/SectionTitle.test.tsx
+++ src/components/SectionTitle/SectionTitle.test.tsx
@@ -1,81 +1,56 @@
 import * as React from "react";
 import * as enzyme from "enzyme";
-import SectionTitle from "./SectionTitle";
+
+import SectionTitle, { SectionTitleProps } from "./SectionTitle";
+import { casual, ITERATION_SIZE } from "../../utils/testUtils/testUtils";
+
+const fakeSectionTitle = (): SectionTitleProps => ({
+  title: casual.string,
+  description: casual.string
+});
+
+const mockSectionTitles = new Array(ITERATION_SIZE)
+  .fill(null)
+  .map(fakeSectionTitle);
 
 describe("<SectionTitle />", () => {
-  const title = ["child1", "different text", "Third rANDom text"];
-  const description = [
-    "sample text here",
-    "more text",
-    "third string of text1231"
-  ];
-  const wrapper = enzyme.shallow(
-    <SectionTitle title={title[0]} description={description[0]} />
-  );
-
   it("renders", () => {
-    expect(wrapper).toBeTruthy();
+    mockSectionTitles.forEach(mockSectionTitle => {
+      const wrapper = enzyme.shallow(<SectionTitle {...mockSectionTitle} />);
+
+      expect(wrapper).toBeTruthy();
+    });
   });
 
   it("renders correct tags", () => {
-    expect(wrapper.find("StyledH2")).toBeTruthy();
-    expect(wrapper.find("StyledH2").length).toEqual(1);
+    mockSectionTitles.forEach(mockSectionTitle => {
+      const wrapper = enzyme.shallow(<SectionTitle {...mockSectionTitle} />);
 
-    expect(wrapper.find("StyledH6")).toBeTruthy();
-    expect(wrapper.find("StyledH6").length).toEqual(1);
+      expect(wrapper.find("h2")).toBeTruthy();
+      expect(wrapper.find("h2").length).toEqual(1);
+
+      expect(wrapper.find("h6")).toBeTruthy();
+      expect(wrapper.find("h6").length).toEqual(1);
+    });
   });
 
   it("renders correct h2 text", () => {
-    expect(
-      wrapper
-        .find("StyledH2")
-        .render()
-        .text()
-    ).toEqual(title[0]);
+    mockSectionTitles.forEach(mockSectionTitle => {
+      const wrapper = enzyme.shallow(<SectionTitle {...mockSectionTitle} />);
 
-    // Update title, retest
-    wrapper.setProps({ title: title[1] });
-    expect(
-      wrapper
-        .find("StyledH2")
-        .render()
-        .text()
-    ).toEqual(title[1]);
-
-    // Update title, retest
-    wrapper.setProps({ title: title[2] });
-    expect(
-      wrapper
-        .find("StyledH2")
-        .render()
-        .text()
-    ).toEqual(title[2]);
+      expect(wrapper.find("h2").render().text()).toEqual(
+        mockSectionTitle.title
+      );
+    });
   });
 
   it("renders correct h6 text", () => {
-    expect(
-      wrapper
-        .find("StyledH6")
-        .render()
-        .text()
-    ).toEqual(description[0]);
+    mockSectionTitles.forEach(mockSectionTitle => {
+      const wrapper = enzyme.shallow(<SectionTitle {...mockSectionTitle} />);
 
-    // Update description, retest
-    wrapper.setProps({ description: description[1] });
-    expect(
-      wrapper
-        .find("StyledH6")
-        .render()
-        .text()
-    ).toEqual(description[1]);
-
-    // Update description, retest
-    wrapper.setProps({ description: description[2] });
-    expect(
-      wrapper
-        .find("StyledH6")
-        .render()
-        .text()
-    ).toEqual(description[2]);
+      expect(wrapper.find("h6").render().text()).toEqual(
+        mockSectionTitle.description
+      );
+    });
   });
 });
