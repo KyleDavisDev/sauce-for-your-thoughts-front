1376e62f8cd5ba196fb8a8f33a036d8a16779195
Kyle Bonar <kylewb2011@gmail.com>
2020-10-11 23:16:31 -0500

    init commit

---
+++ src/components/LandingImage/Body/Body.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+
+import { AppState } from "../../../redux/configureStore";
+import {
+  HeroBody,
+  HeroTitle,
+  StyledDiv,
+  StyledSelect,
+  StyledInput,
+  StyledButton
+} from "./BodyStyle";
+
+interface IBodyProps {}
+
+const Body: React.FunctionComponent<IBodyProps> = props => {
+  const _defaultTitleText = "Find your perfect sauce";
+
+  const [search, setSearch] = React.useState("");
+  const [selectedValue, setSelectedValue] = React.useState("all");
+  const types = useSelector((store: AppState) => {
+    return store.sauces.types;
+  });
+
+  // assign router
+  const router = useRouter();
+
+  return (
+    <HeroBody>
+      <HeroTitle>{_defaultTitleText}</HeroTitle>
+      <form onSubmit={onSubmit}>
+        <StyledDiv>
+          <StyledSelect
+            id="types"
+            options={types}
+            selectedValue={selectedValue}
+            onSelect={e => setSelectedValue(e.target.value)}
+          />
+          <StyledInput
+            id="search"
+            type="text"
+            onChange={e => setSearch(e.target.value)}
+            placeholder="Search..."
+          />
+          <StyledButton type="submit">Search</StyledButton>
+        </StyledDiv>
+      </form>
+    </HeroBody>
+  );
+
+  async function onSubmit(event: React.FormEvent): Promise<null> {
+    event.preventDefault();
+
+    let query = "/sauces?";
+    if (search.length > 0) {
+      query += `srch=${search}`;
+    }
+
+    if (selectedValue.toLowerCase() !== "all") {
+      query += `&type=${selectedValue}`;
+    }
+
+    // take person to sauces page w/ prefilled search query
+    router.push(query);
+    return null;
+  }
+};
+
+export default Body;
---
+++ src/components/LandingImage/Body/BodyStyle.tsx
@@ -0,0 +1,80 @@
+import styled from "../../../theme/styled-components";
+import { Button } from "../../Button/Button";
+import Select from "../../Select/Select";
+import { TextInput } from "../../TextInput/TextInput";
+
+export const HeroBody = styled.div`
+  position: absolute;
+  z-index: 1;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  padding: 0 2em;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+`;
+HeroBody.displayName = "div";
+
+export const HeroTitle = styled.h1`
+  color: ${x => x.theme.landingHeroTextColor};
+  font-family: FuturaMedium;
+`;
+HeroTitle.displayName = "h1";
+
+export const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
+`;
+StyledDiv.displayName = "div";
+
+export const StyledSelect = styled(Select)`
+  min-height: 30px;
+  border: 0px;
+  box-sizing: border-box;
+
+  > div {
+    width: auto;
+
+    &:after {
+      top: 3px;
+    }
+  }
+
+  select {
+    width: auto;
+    border: 0;
+  }
+`;
+StyledSelect.displayName = "Select";
+
+export const StyledInput = styled(TextInput)`
+  flex-direction: row;
+  align-items: stretch;
+
+  input {
+    border: 0px;
+    margin-top: 0px;
+    margin-bottom: 0px;
+    width: 100%;
+    min-width: auto;
+    max-width: 100%;
+    padding: 0.66em;
+    box-sizing: border-box;
+    border: 0px;
+    font-size: 1em;
+
+    @media (min-width: ${props => props.theme.smToMd}) {
+      min-width: 20em;
+    }
+  }
+`;
+StyledInput.displayName = "input";
+
+export const StyledButton = styled(Button)`
+  display: flex;
+`;
+StyledButton.displayName = "button";
