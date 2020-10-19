73f427ce41426d62cfbecdddd9edabbddc998a2e
Kyle Bonar <kylewb2011@gmail.com>
2020-10-18 20:37:08 -0500

    Move style to own file

--- src/components/TopBar/components/LoggedInBar/components/Menu/components/Title/Title.tsx
+++ src/components/TopBar/components/LoggedInBar/components/Menu/components/Title/Title.tsx
@@ -1,20 +1,13 @@
 import * as React from "react";
-import styled from "styled-components";
 
-const StyledDiv = styled.div`
-  padding: 12px;
-  // background-color: ${x => x.theme.primaryLightThemeColor};
-  text-transform: uppercase;
-  font-size: 14px;
-  color: ${x => x.theme.black};
-`;
+import { StyledDiv } from "./TitleStyle";
 
 interface TitleProps {
   children: string;
   className?: string;
 }
 
-const Title: React.SFC<TitleProps> = props => {
+const Title: React.FC<TitleProps> = props => {
   return <StyledDiv className={props.className}>{props.children}</StyledDiv>;
 };
 
---
+++ src/components/TopBar/components/LoggedInBar/components/Menu/components/Title/TitleStyle.tsx
@@ -0,0 +1,9 @@
+import styled from "../../../../../../../../theme/styled-components";
+
+export const StyledDiv = styled.div`
+  padding: 12px 12px 0 12px;
+  text-transform: uppercase;
+  font-size: 14px;
+  color: ${x => x.theme.black};
+`;
+StyledDiv.displayName = "div";
