fb5f71d2d9a107ffb33ba13949008d46fd90c707
Kyle Bonar <kylewb2011@gmail.com>
2020-10-22 08:07:22 -0500

    Class to FC

--- src/components/TopBar/components/LoggedInBar/components/Menu/components/Profile/Profile.tsx
+++ src/components/TopBar/components/LoggedInBar/components/Menu/components/Profile/Profile.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 
 import {
   StyledLink,
@@ -10,43 +10,50 @@ import {
 import { Button } from "../../../../../../../Button/Button";
 import { AppState } from "../../../../../../../../redux/configureStore";
 
-interface ProfileProps {
-  displayName?: string;
-  avatarURL?: string;
-}
+interface ProfileProps {}
 
-class Profile extends React.PureComponent<ProfileProps, any> {
-  public render() {
-    return (
-      <StyledLink href="#">
-        <StyledHead>
-          <div>
-            <StyledImage
-              src={this.props.avatarURL}
-              alt={this.props.displayName}
-              height="50"
-              width="50"
-              onError={e => {
-                const elem = e.target as HTMLImageElement;
-                elem.src =
-                  "https://res.cloudinary.com/foryourthoughts/image/upload/v1575867983/avatars/r0pnn1izbqm6wopt8lvq_lgq9q6.png";
-              }}
-            />
-          </div>
+const Profile: React.FC<ProfileProps> = props => {
+  const _defaultDisplayName = "N/A";
+  const _defaultAvatarURL = "";
 
-          <div>
-            <h3>{this.props.displayName || "Me"}</h3>
-            <h4>Sauce Fanatic</h4>
-          </div>
-        </StyledHead>
+  const { self } = useSelector((state: AppState) => {
+    return state.users;
+  });
 
-        <StyledFoot>
-          <Button displayType="outline">View profile (Coming Soon)</Button>
-        </StyledFoot>
-      </StyledLink>
-    );
-  }
-}
+  const {
+    displayName = _defaultDisplayName,
+    avatarURL = _defaultAvatarURL
+  } = self ? self : {};
+
+  return (
+    <StyledLink href="#">
+      <StyledHead>
+        <div>
+          <StyledImage
+            src={avatarURL}
+            alt={displayName}
+            height="50"
+            width="50"
+            onError={e => {
+              const elem = e.target as HTMLImageElement;
+              elem.src =
+                "https://res.cloudinary.com/foryourthoughts/image/upload/v1575867983/avatars/r0pnn1izbqm6wopt8lvq_lgq9q6.png";
+            }}
+          />
+        </div>
+
+        <div>
+          <h3>{displayName || "Me"}</h3>
+          <h4>Sauce Fanatic</h4>
+        </div>
+      </StyledHead>
+
+      <StyledFoot>
+        <Button displayType="outline">View profile (Coming Soon)</Button>
+      </StyledFoot>
+    </StyledLink>
+  );
+};
 
 const mapState2Props = (state: AppState) => {
   const { self } = state.users;
@@ -61,4 +68,4 @@ const mapState2Props = (state: AppState) => {
   };
 };
 
-export default connect(mapState2Props)(Profile);
+export default Profile;
