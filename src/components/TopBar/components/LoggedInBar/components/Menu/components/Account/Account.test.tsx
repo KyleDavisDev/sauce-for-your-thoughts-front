a18e7e0733acf54fd938289a7b62aa691b167111
Kyle Bonar <kylewb2011@gmail.com>
2020-10-22 08:16:24 -0500

    It passes expected value to Title

--- src/components/TopBar/components/LoggedInBar/components/Menu/components/Account/Account.test.tsx
+++ src/components/TopBar/components/LoggedInBar/components/Menu/components/Account/Account.test.tsx
@@ -5,6 +5,8 @@ import Account from "./Account";
 import { ITERATION_SIZE } from "../../../../../../../../utils/testUtils/testUtils";
 
 describe("<Account />", () => {
+  const _title = "Account";
+
   it("renders", () => {
     new Array(ITERATION_SIZE).fill(null).map(() => {
       const wrapper = enzyme.shallow(<Account />);
@@ -26,6 +28,14 @@ describe("<Account />", () => {
       const wrapper = enzyme.shallow(<Account />);
 
       expect(wrapper.find("Title").exists()).toBeTruthy();
+    });
+  });
+
+  it("passes expected value to Title", () => {
+    new Array(ITERATION_SIZE).fill(null).map(() => {
+      const wrapper = enzyme.shallow(<Account />);
+
+      expect(wrapper.find("Title").prop("children")).toEqual(_title);
     });
   });
 });
