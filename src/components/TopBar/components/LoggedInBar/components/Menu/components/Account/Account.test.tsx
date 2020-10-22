8bda8507ac74855fd564d8a8a19b193b5acf2233
Kyle Bonar <kylewb2011@gmail.com>
2020-10-22 08:13:28 -0500

    Loop over iteration

--- src/components/TopBar/components/LoggedInBar/components/Menu/components/Account/Account.test.tsx
+++ src/components/TopBar/components/LoggedInBar/components/Menu/components/Account/Account.test.tsx
@@ -2,17 +2,22 @@ import * as React from "react";
 import * as enzyme from "enzyme";
 
 import Account from "./Account";
+import { ITERATION_SIZE } from "../../../../../../../../utils/testUtils/testUtils";
 
 describe("<Account />", () => {
   it("renders", () => {
-    const wrapper = enzyme.shallow(<Account />);
+    new Array(ITERATION_SIZE).fill(null).map(() => {
+      const wrapper = enzyme.shallow(<Account />);
 
-    expect(wrapper).toBeTruthy();
+      expect(wrapper).toBeTruthy();
+    });
   });
 
   it("matches snapshot", () => {
-    const wrapper = enzyme.shallow(<Account />);
+    new Array(ITERATION_SIZE).fill(null).map(() => {
+      const wrapper = enzyme.shallow(<Account />);
 
-    expect(wrapper).toMatchSnapshot();
+      expect(wrapper).toMatchSnapshot();
+    });
   });
 });
