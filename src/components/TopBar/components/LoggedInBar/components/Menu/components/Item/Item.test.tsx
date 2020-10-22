17eac095461820564839fba7e761a85049843afa
Kyle Bonar <kylewb2011@gmail.com>
2020-10-22 08:27:28 -0500

    It passes expected 'to' to Link component when provided

--- src/components/TopBar/components/LoggedInBar/components/Menu/components/Item/Item.test.tsx
+++ src/components/TopBar/components/LoggedInBar/components/Menu/components/Item/Item.test.tsx
@@ -16,6 +16,8 @@ const fakeItem = (): ItemProps => ({
 const mockItems = new Array(ITERATION_SIZE).fill(null).map(fakeItem);
 
 describe("<Item />", () => {
+  const _defaultPath = "#";
+
   it("renders", () => {
     mockItems.forEach(mockItem => {
       const wrapper = enzyme.shallow(
@@ -33,6 +35,18 @@ describe("<Item />", () => {
       );
 
       expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  it("passes expected 'to' to Link component when provided", () => {
+    mockItems.forEach(mockItem => {
+      if (!mockItem.to) return;
+
+      const wrapper = enzyme.shallow(
+        <Item {...mockItem}>{mockItem.children}</Item>
+      );
+
+      expect(wrapper.find("Link").prop("href")).toEqual(mockItem.to);
     });
   });
 });
