2246a83bb7805bf9d5ac3be6e65057c7bf663e7b
Kyle Bonar <kylewb2011@gmail.com>
2020-10-23 15:41:28 -0500

    Refactor suite for performance issues, it passes default src to img tag when needed

--- src/components/TopBar/components/LoggedInBar/components/Menu/components/Profile/Profile.test.tsx
+++ src/components/TopBar/components/LoggedInBar/components/Menu/components/Profile/Profile.test.tsx
@@ -15,58 +15,58 @@ const mockStores = new Array(ITERATION_SIZE).fill(null).map(fakeStore);
 describe("<Profile />", () => {
   const _defaultDisplayName = "N/A";
   const _defaultAvatarURL = "";
+  let wrappers: any = [];
+
+  mockStores.forEach(mockStore => {
+    let tmp = enzyme.mount(
+      <Provider store={mockStore}>
+        <Profile />
+      </Provider>
+    );
+    wrappers.push(tmp);
+  });
 
   it("renders", () => {
-    mockStores.forEach(mockStore => {
-      const wrapper = enzyme.shallow(
-        <Provider store={mockStore}>
-          <Profile />
-        </Provider>
-      );
-
+    wrappers.forEach(wrapper => {
       expect(wrapper).toBeTruthy();
     });
   });
 
   it("matches snapshot", () => {
-    mockStores.forEach(mockStore => {
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <Profile />
-        </Provider>
-      );
-
+    wrappers.forEach(wrapper => {
       expect(wrapper).toMatchSnapshot();
     });
   });
 
   it("renders an img tag", () => {
-    mockStores.forEach(mockStore => {
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <Profile />
-        </Provider>
-      );
-
+    wrappers.forEach(wrapper => {
       expect(wrapper.find("img").exists()).toBeTruthy();
     });
   });
 
   it("passes expected src to img tag when possible", () => {
-    mockStores.forEach(mockStore => {
+    wrappers.forEach((wrapper, ind) => {
       // grab avatarURL from store
-      const reduxState = mockStore.getState() as AppState;
+      const reduxState = mockStores[ind].getState() as AppState;
       const avatarURL = reduxState.users.self?.avatarURL;
       if (!avatarURL) return;
 
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <Profile />
-        </Provider>
-      );
-
       // compare component to redux store
       expect(wrapper.find("img").first().prop("src")).toEqual(avatarURL);
+    });
+  });
+
+  it("passes default src to img tag when needed", () => {
+    wrappers.forEach((wrapper, ind) => {
+      // grab avatarURL from store
+      const reduxState = mockStores[ind].getState() as AppState;
+      const avatarURL = reduxState.users.self?.avatarURL;
+      if (avatarURL) return;
+
+      // compare component to default
+      expect(wrapper.find("img").first().prop("src")).toEqual(
+        _defaultAvatarURL
+      );
     });
   });
 });
