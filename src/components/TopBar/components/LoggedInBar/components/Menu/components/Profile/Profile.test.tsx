d086c62fe21c61b0ed4f86abda4a3fc7bb8b6888
Kyle Bonar <kylewb2011@gmail.com>
2020-10-23 16:04:27 -0500

    Organized imports

--- src/components/TopBar/components/LoggedInBar/components/Menu/components/Profile/Profile.test.tsx
+++ src/components/TopBar/components/LoggedInBar/components/Menu/components/Profile/Profile.test.tsx
@@ -1,13 +1,13 @@
 import "jsdom-global/register";
 import * as React from "react";
 import * as enzyme from "enzyme";
+import { Provider } from "react-redux";
 
 import Profile from "./Profile";
 import {
   fakeStore,
   ITERATION_SIZE
 } from "../../../../../../../../utils/testUtils/testUtils";
-import { Provider } from "react-redux";
 import { AppState } from "../../../../../../../../redux/configureStore";
 
 const mockStores = new Array(ITERATION_SIZE).fill(null).map(fakeStore);
