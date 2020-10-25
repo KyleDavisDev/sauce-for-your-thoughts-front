584bd922ab70edadcd9a75ecc2517172da78f56a
Kyle Bonar <kylewb2011@gmail.com>
2020-10-25 15:03:37 -0500

    init commit

---
+++ src/components/UserSettings/components/Group/GroupStyle.tsx
@@ -0,0 +1,16 @@
+import styled from "../../../../theme/styled-components";
+import { Button } from "../../../Button/Button";
+
+export const StyledButton = styled(Button)`
+  display: inline-block;
+
+  > button {
+    color: #333;
+  }
+`;
+StyledButton.displayName = "button";
+
+export const StyledDiv = styled.div`
+  margin-bottom: 32px;
+`;
+StyledDiv.displayName = "div";
