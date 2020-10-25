4e1efd63d7a30f204176119fed75c95f3f68f8b5
Kyle Bonar <kylewb2011@gmail.com>
2020-10-25 16:26:46 -0500

    init commit

---
+++ src/components/UserSettings/components/ButtonRedirect/ButtonRedirect.test.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import * as enzyme from "enzyme";
+
+import ButtonRedirect, { IButtonRedirectProps } from "./ButtonRedirect";
+import { casual, ITERATION_SIZE } from "../../../../utils/testUtils/testUtils";
+
+const fakeButtonRedirect = (): IButtonRedirectProps => ({
+  name: casual.string,
+  href: casual.url
+});
+
+const mockButtonRedirects = new Array(ITERATION_SIZE)
+  .fill(null)
+  .map(fakeButtonRedirect);
+
+describe("<ButtonRedirect />", () => {
+  it("renders", () => {
+    mockButtonRedirects.forEach(mockButtonRedirect => {
+      const wrapper = enzyme.shallow(
+        <ButtonRedirect {...mockButtonRedirect} />
+      );
+
+      expect(wrapper).toBeTruthy();
+    });
+  });
+});
