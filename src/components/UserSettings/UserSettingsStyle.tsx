6d5c2aee44554f4457db6e1d6961e4f0bbd10e62
Kyle Bonar <kylewb2011@gmail.com>
2020-10-25 13:58:03 -0500

    Added displayNames

--- src/components/UserSettings/UserSettingsStyle.tsx
+++ src/components/UserSettings/UserSettingsStyle.tsx
@@ -5,12 +5,14 @@ export const StyledContainer = styled.div`
   border: ${props => props.theme.border};
   padding: 1.5rem;
 `;
+StyledContainer.displayName = "div";
 
 export const StyledText = styled.p`
   width: 80%;
   margin: 0 auto 1em;
   text-align: center;
 `;
+StyledText.displayName = "p";
 
 export const StyledButton = styled(Button)`
   display: inline-block;
@@ -19,7 +21,9 @@ export const StyledButton = styled(Button)`
     color: #333;
   }
 `;
+StyledButton.displayName = "button";
 
 export const StyledGroup = styled.div`
   margin-bottom: 32px;
 `;
+StyledGroup.displayName = "div";
