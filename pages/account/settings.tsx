aaefb73c29284701cbe82b8be5e9a3369d929003
Kyle Bonar <kylewb2011@gmail.com>
2020-10-25 14:00:53 -0500

    Adopt more styling responsibility from child component

--- pages/account/settings.tsx
+++ pages/account/settings.tsx
@@ -1,10 +1,21 @@
 import * as React from "react";
+import { Article } from "../../src/components/Article/Article";
+import HeaderSimple from "../../src/components/HeaderSimple/HeaderSimple";
+import PageTitle from "../../src/components/PageTitle/PageTitle";
 import UserSettings from "../../src/components/UserSettings/UserSettings";
 
 export interface SettingsPageProps {}
 
-const SettingsPage: React.SFC<SettingsPageProps> = () => {
-  return <UserSettings />;
+const SettingsPage: React.FC<SettingsPageProps> = () => {
+  return (
+    <>
+      <HeaderSimple />
+      <Article size="sm">
+        <PageTitle>Manage your account</PageTitle>
+        <UserSettings />
+      </Article>
+    </>
+  );
 };
 
 export default SettingsPage;
