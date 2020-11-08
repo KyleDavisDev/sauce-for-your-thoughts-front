17335772614357fb5ee660b44220d4979aa1ac2c
Kyle Bonar <kylewb2011@gmail.com>
2020-11-08 16:17:12 -0600

    Adopted formatting responsiblity

--- pages/account/update/password.tsx
+++ pages/account/update/password.tsx
@@ -1,10 +1,19 @@
 import * as React from "react";
+import { Article } from "../../../src/components/Article/Article";
+import HeaderSimple from "../../../src/components/HeaderSimple/HeaderSimple";
 import UpdatePassword from "../../../src/components/UpdatePassword/UpdatePassword";
 
 export interface PasswordPageProps {}
 
 const PasswordPage: React.SFC<PasswordPageProps> = () => {
-  return <UpdatePassword />;
+  return (
+    <>
+      <HeaderSimple />
+      <Article size="sm">
+        <UpdatePassword />
+      </Article>
+    </>
+  );
 };
 
 export default PasswordPage;
