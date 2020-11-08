6080b58658b3cd0d4f089a5e4a94a19ccb4be430
Kyle Bonar <kylewb2011@gmail.com>
2020-11-08 16:17:19 -0600

    Init commit, setup

---
+++ src/components/UpdatePassword/UpdatePassword.test.tsx
@@ -0,0 +1,97 @@
+import "jsdom-global/register";
+import * as React from "react";
+import * as enzyme from "enzyme";
+import { Provider } from "react-redux";
+import { act } from "react-dom/test-utils";
+
+import UpdatePassword from "./UpdatePassword";
+import {
+  casual,
+  fakeStore,
+  generateInValidPassword,
+  generateValidPassword,
+  ITERATION_SIZE,
+  simulateInputChange,
+  generateErr
+} from "../../utils/testUtils/testUtils";
+import { AppState } from "../../redux/configureStore";
+import { MockStoreEnhanced } from "redux-mock-store";
+
+// mock Next's router
+const mockPush = jest.fn();
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      push: mockPush
+    };
+  }
+}));
+
+// mock our API
+const mockAPICall = jest.fn(() => {
+  if (casual.boolean) {
+    return Promise.resolve({ isGood: true });
+  } else {
+    return Promise.reject(generateErr());
+  }
+});
+jest.mock("../../utils/api/API", () => {
+  return {
+    API: {
+      user: {
+        updatePassword: () => {
+          return mockAPICall();
+        }
+      }
+    }
+  };
+});
+
+describe("<UpdatePassword />", () => {
+  const MIN_PASSWORD_LENGTH = 8;
+  const _pageTitle = "Update Password";
+  const _redirectPath = "/account/login?return=/account/update/password";
+  const _defaultErrorMsg =
+    "There was a problem updating your password. Please verify network connection and try again.";
+
+  // May need to refer to these later so initializing out here
+  let wrappers: Array<enzyme.ReactWrapper<
+    any,
+    Readonly<{}>,
+    React.Component<{}, {}, any>
+  >> = [];
+  let mockStores: MockStoreEnhanced<unknown, {}>[] = [];
+
+  beforeAll(() => {
+    // add our mock stores
+    mockStores = new Array(ITERATION_SIZE).fill(null).map(fakeStore);
+
+    // add our mounts
+    wrappers = new Array(ITERATION_SIZE).fill(null).map((x, ind) => {
+      return enzyme.mount(
+        <Provider store={mockStores[ind]}>
+          <UpdatePassword />
+        </Provider>
+      );
+    });
+  });
+
+  afterEach(() => {
+    // clear all inputs
+    wrappers.forEach(wrapper => {
+      wrapper.find("TextInput input").forEach(input => {
+        input.simulate("change", { target: { value: "" } });
+      });
+    });
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders", () => {
+    wrappers.forEach(wrapper => {
+      expect(wrapper).toBeTruthy();
+    });
+  });
+});
