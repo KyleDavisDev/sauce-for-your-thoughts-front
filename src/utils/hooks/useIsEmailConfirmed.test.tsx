d932933f471fe464f761cebf8948260222bf9463
Kyle Bonar <kylewb2011@gmail.com>
2020-11-08 23:59:46 -0600

    Create mocks, It returns default loading when no token found

--- src/utils/hooks/useIsEmailConfirmed.test.tsx
+++ src/utils/hooks/useIsEmailConfirmed.test.tsx
@@ -1,44 +1,96 @@
+import "jsdom-global/register";
 import * as React from "react";
 import * as enzyme from "enzyme";
+import { Provider } from "react-redux";
+import { MockStoreEnhanced } from "redux-mock-store";
+import { act } from "react-dom/test-utils";
 
 import { FlashMessageProps } from "../../components/FlashMessage/FlashMessage";
 import { AppState } from "../../redux/configureStore";
-import { useIsEmailConfirmed } from "../../utils/hooks/useIsEmailConfirmed";
-import { MockStoreEnhanced } from "redux-mock-store";
-import { fakeStore, ITERATION_SIZE } from "../testUtils/testUtils";
-import { Provider } from "react-redux";
+import {
+  useIsEmailConfirmed,
+  IuseIsEmailConfirmed
+} from "../../utils/hooks/useIsEmailConfirmed";
+import {
+  casual,
+  fakeStore,
+  generateErr,
+  ITERATION_SIZE
+} from "../testUtils/testUtils";
+
+// mock our API
+const mockAPICall = jest.fn(() => {
+  if (casual.boolean) {
+    return Promise.resolve({ data: { isGood: true } });
+  } else {
+    const err = generateErr();
+    return Promise.reject(err);
+  }
+});
+jest.mock("../../utils/api/API", () => {
+  return {
+    API: {
+      user: {
+        isEmailConfirmed: () => {
+          return mockAPICall();
+        }
+      }
+    }
+  };
+});
 
 const HookWrapper = props => {
-  const hook = props.hook ? props.hook() : undefined;
+  const hook = props["data-hook"] ? props["data-hook"]() : undefined;
   return <div data-hook={hook} />;
 };
 
 describe("useIsEmailConfirmed", () => {
+  // init defaults
+  const _defaultIsLoading = false;
+  const _defaultIsEmailConfirmed = false;
+  const _defaultFlashState = { isVisible: false };
+
   // May need to refer to these later so initializing out here
-  let wrappers: enzyme.ShallowWrapper<
-    any,
-    Readonly<{}>,
-    React.Component<{}, {}, any>
-  >[] = [];
   let mockStores: MockStoreEnhanced<unknown, {}>[] = [];
 
   beforeAll(() => {
     // add our mock stores
     mockStores = new Array(ITERATION_SIZE).fill(null).map(fakeStore);
-
-    // add our mounts
-    wrappers = new Array(ITERATION_SIZE).fill(null).map((x, ind) => {
-      return enzyme.shallow(
-        <Provider store={mockStores[ind]}>
-          <HookWrapper data-hook={() => useIsEmailConfirmed()} />
-        </Provider>
-      );
-    });
   });
 
-  it("renders", () => {
-    wrappers.forEach(wrapper => {
-      expect(wrapper.exists()).toBeTruthy();
-    });
+  it("renders", async () => {
+    for (let i = 0, len = ITERATION_SIZE; i < len; i++) {
+      await act(async () => {
+        const wrapper = enzyme.mount(
+          <Provider store={mockStores[i]}>
+            <HookWrapper data-hook={() => useIsEmailConfirmed()} />
+          </Provider>
+        );
+
+        expect(wrapper.exists()).toBeTruthy();
+      });
+    }
+  });
+
+  it("returns default loading when no token found", async () => {
+    for (let i = 0, len = ITERATION_SIZE; i < len; i++) {
+      const reduxState = mockStores[i].getState() as AppState;
+      const token = reduxState.users.self?.token;
+      if (!token) return;
+
+      await act(async () => {
+        const wrapper = enzyme.mount(
+          <Provider store={mockStores[i]}>
+            <HookWrapper data-hook={() => useIsEmailConfirmed()} />
+          </Provider>
+        );
+
+        const hook: IuseIsEmailConfirmed = wrapper
+          .find("div")
+          .prop("data-hook");
+
+        expect(hook.loading).toEqual(_defaultIsLoading);
+      });
+    }
   });
 });
