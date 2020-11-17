2733a8c8db5d6ebe24b8432502f862b187daf825
Kyle Bonar <kylewb2011@gmail.com>
2020-11-17 12:44:57 -0600

    init commit

---
+++ src/components/RegisterUser/RegisterUser.test.tsx
@@ -0,0 +1,45 @@
+import "jsdom-global/register";
+import * as React from "react";
+import * as enzyme from "enzyme";
+import { Provider } from "react-redux";
+import { MockStoreEnhanced } from "redux-mock-store";
+
+import RegisterUser from "./RegisterUser";
+import {
+  casual,
+  ITERATION_SIZE,
+  fakeStore
+} from "../../utils/testUtils/testUtils";
+
+describe("<RegisterUser />", () => {
+  // mock scrollTo
+  window.scrollTo = jest.fn();
+
+  // May need to refer to these later so initializing out here
+  let wrappers: Array<enzyme.ReactWrapper<
+    any,
+    Readonly<{}>,
+    React.Component<{}, {}, any>
+  >> = [];
+  let mockStores: MockStoreEnhanced<unknown, {}>[] = [];
+
+  beforeAll(() => {
+    // add our mock stores
+    mockStores = new Array(ITERATION_SIZE).fill(null).map(fakeStore);
+
+    // add our mounts
+    wrappers = new Array(ITERATION_SIZE).fill(null).map((x, ind) => {
+      return enzyme.mount(
+        <Provider store={mockStores[ind]}>
+          <RegisterUser />
+        </Provider>
+      );
+    });
+  });
+
+  it("renders", () => {
+    wrappers.forEach(wrapper => {
+      expect(wrapper).toBeTruthy();
+    });
+  });
+});
