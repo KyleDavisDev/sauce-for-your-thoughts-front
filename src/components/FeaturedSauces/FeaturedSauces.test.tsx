9e4a89a69fb7c7c4a83a0e36589bdaf84f9dce6a
Kyle Bonar <kylewb2011@gmail.com>
2020-11-29 16:54:28 -0600

    init commit

---
+++ src/components/FeaturedSauces/FeaturedSauces.test.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as enzyme from "enzyme";
+
+import FeaturedSauces from "./FeaturedSauces";
+
+describe("<FeaturedSauces />", () => {
+  // it("renders", () => {
+  //   const wrapper = enzyme.
+  // })
+});
