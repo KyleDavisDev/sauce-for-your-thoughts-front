f7eff7105658cf0a42690559cd8fcc38e5c5d701
Kyle Bonar <kylewb2011@gmail.com>
2020-12-08 14:00:26 -0600

    Move styles to own file, absorb some more responsibility

--- src/components/SauceSpotlight/components/SauceReviews/SauceReviews.tsx
+++ src/components/SauceSpotlight/components/SauceReviews/SauceReviews.tsx
@@ -1,29 +1,13 @@
 import * as React from "react";
-import styled from "styled-components";
 
 import { IReview } from "../../../../redux/reviews/types";
 import { Button } from "../../../Button/Button";
-import { Link } from "../../../Link/Link";
 import SauceReviewBlock from "./components/SauceReviewBlock/SauceReviewBlock";
-
-const StyledContainer = styled.div`
-  font-family: AvenirNextReg;
-  padding: 1.5rem;
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  margin-bottom: 1.5em;
-
-  @media (min-width: ${props => props.theme.smToMd}) {
-    margin-bottom: 3em;
-  }
-`;
-
-const StyledLink = styled(Link)`
-  > div {
-    display: inline;
-  }
-`;
+import {
+  StyledContainer,
+  StyledDescriptor,
+  StyledLink
+} from "./SauceReviewsStyles";
 
 export interface SauceReviewsProps {
   slug?: string;
@@ -40,28 +24,36 @@ class SauceReviews extends React.PureComponent<SauceReviewsProps, any> {
     const { reviews, displayEditLink, slug } = this.props;
 
     return (
-      <div>
-        {reviews && reviews.length > 0 ? (
-          reviews.map(review => {
-            return <SauceReviewBlock review={review} key={review.reviewID} />;
-          })
-        ) : (
-          <StyledContainer>
-            <p style={{ marginTop: "0" }}>
-              <i>No reviews found! Have you tried this sauce? Add a review!</i>
-            </p>
-          </StyledContainer>
-        )}
-        {displayEditLink ? (
-          <StyledLink href={`/review/edit?s=${slug}`}>
-            <Button>Edit Your Review</Button>
-          </StyledLink>
-        ) : (
-          <StyledLink href={`/review/add?s=${slug}`}>
-            <Button>Add Review</Button>
-          </StyledLink>
-        )}
-      </div>
+      <>
+        {/* Reviews */}
+        <StyledDescriptor title={`Reviews (${reviews ? reviews.length : 0})`}>
+          The opinions expressed are soley those of the author.
+        </StyledDescriptor>
+        <div>
+          {reviews && reviews.length > 0 ? (
+            reviews.map(review => {
+              return <SauceReviewBlock review={review} key={review.reviewID} />;
+            })
+          ) : (
+            <StyledContainer>
+              <p style={{ marginTop: "0" }}>
+                <i>
+                  No reviews found! Have you tried this sauce? Add a review!
+                </i>
+              </p>
+            </StyledContainer>
+          )}
+          {displayEditLink ? (
+            <StyledLink href={`/review/edit?s=${slug}`}>
+              <Button>Edit Your Review</Button>
+            </StyledLink>
+          ) : (
+            <StyledLink href={`/review/add?s=${slug}`}>
+              <Button>Add Review</Button>
+            </StyledLink>
+          )}
+        </div>
+      </>
     );
   }
 }
