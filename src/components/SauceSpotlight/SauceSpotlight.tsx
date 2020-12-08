ade824ed743374a2e8fc0d40553d9a338beb2c0e
Kyle Bonar <kylewb2011@gmail.com>
2020-12-08 15:39:38 -0600

    Removed unused imports, render SauceRelated component

--- src/components/SauceSpotlight/SauceSpotlight.tsx
+++ src/components/SauceSpotlight/SauceSpotlight.tsx
@@ -1,16 +1,7 @@
 import * as React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useRouter } from "next/router";
 
-import { AppState } from "../../redux/configureStore";
-import { reduxStore } from "../../redux/with-redux-store";
-import { IReview } from "../../redux/reviews/types";
-import { getSauceBySlug } from "../../redux/sauces/actions";
-import { Link } from "../Link/Link";
-import { Button } from "../Button/Button";
-import List from "../List/List";
-import { FlashMessage } from "../FlashMessage/FlashMessage";
 import SauceHero from "./components/SauceHero/SauceHero";
+import SauceRelated from "./components/SauceRelated/SauceRelated";
 import SauceReviews from "./components/SauceReviews/SauceReviews";
 import {
   StyledLeftContainer,
@@ -29,21 +20,8 @@ const SauceSpotlight: React.FC<SauceSpotlightProps> = props => {
       </StyledLeftContainer>
 
       <StyledRightContainer>
-        {/* {sauce && sauce._related && sauce._related.length > 0 && (
-              <>
-                {sauce.slug && showAppropriateReviewButton(sauce.slug)}
-                <List
-                  items={sauce._related.map((x, ind) => {
-                    return {
-                      link: `/sauce/view?s=${x.slug}`,
-                      text: x.name,
-                      id: `${ind}-${x.name}`
-                    };
-                  })}
-                  title="Related Sauces"
-                />
-              </>
-            )} */}
+        <SauceRelated />
+
         {/* {saucesWithNewestReviews && saucesWithNewestReviews.length > 0 && (
               <List
                 items={saucesWithNewestReviews.map((x, ind) => {
