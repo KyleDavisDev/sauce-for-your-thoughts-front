dfeb9749654eea1472d2b3a19ef3a4cd784dd7ca
Kyle Bonar <kylewb2011@gmail.com>
2020-12-16 12:39:05 -0600

    Updated to match usage

--- src/redux/sauces/types.ts
+++ src/redux/sauces/types.ts
@@ -2,10 +2,10 @@ export interface ISauce {
   _id?: number;
   _addedToStore?: number; // Unix time (in seconds) added to redux store
   _full?: boolean; // Whether we have full sauce or partial
-  _related: Array<{ name: string; slug: string }>; // List of related sauces
+  _related?: Array<{ name: string; slug: string }>; // List of related sauces
   isAdminApproved?: boolean;
   name: string;
-  ingredients: string;
+  ingredients?: string;
   author: string;
   created: number; // Unix time (in seconds)
   types?: string[];
