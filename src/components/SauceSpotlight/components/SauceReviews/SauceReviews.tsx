c1ff25469c6735550559c5ae157edcfc41997172
Kyle Bonar <kylewb2011@gmail.com>
2020-12-21 13:27:40 -0600

    Get reviews from props, fixed typo, removed button display responsibility

--- src/components/SauceSpotlight/components/SauceReviews/SauceReviews.tsx
+++ src/components/SauceSpotlight/components/SauceReviews/SauceReviews.tsx
@@ -1,22 +1,18 @@
 import * as React from "react";
 
-import { useGetReviewsBySlug } from "../../../../utils/hooks/useGetReviewsBySlug/useGetReviewsBySlug";
-import { Button } from "../../../Button/Button";
 import SauceReviewBlock from "./components/SauceReviewBlock/SauceReviewBlock";
-import {
-  StyledContainer,
-  StyledDescriptor,
-  StyledLink
-} from "./SauceReviewsStyles";
+import { StyledContainer, StyledDescriptor } from "./SauceReviewsStyles";
+import { IReview } from "../../../../redux/reviews/types";
+import { FlashMessageProps } from "../../../FlashMessage/FlashMessage";
 
-export interface SauceReviewsProps {}
+export interface SauceReviewsProps {
+  reviews?: IReview[];
+  loading: boolean;
+  error: FlashMessageProps;
+}
 
-const SauceReviews: React.FC<SauceReviewsProps> = () => {
-  const { loading, reviews, error, getReviews } = useGetReviewsBySlug();
-
-  React.useEffect(() => {
-    if (!loading && reviews.length === 0) getReviews();
-  }, []);
+const SauceReviews: React.FC<SauceReviewsProps> = props => {
+  const { reviews, loading, error } = props;
 
   if (loading) {
     return <p>loading...</p>;
@@ -26,7 +22,7 @@ const SauceReviews: React.FC<SauceReviewsProps> = () => {
     return <p>{error.text}</p>;
   }
 
-  if (reviews.length === 0) {
+  if (!reviews || reviews.length === 0) {
     return (
       <StyledContainer>
         <p style={{ marginTop: "0" }}>
@@ -40,21 +36,12 @@ const SauceReviews: React.FC<SauceReviewsProps> = () => {
     <>
       {/* Reviews */}
       <StyledDescriptor title={`Reviews (${reviews ? reviews.length : 0})`}>
-        The opinions expressed are soley those of the author.
+        The opinions expressed are solely those of the author.
       </StyledDescriptor>
       <div>
         {reviews.map(review => {
           return <SauceReviewBlock review={review} key={review.reviewID} />;
         })}
-        {/* {displayEditLink ? (
-          <StyledLink href={`/review/edit?s=${slug}`}>
-            <Button>Edit Your Review</Button>
-          </StyledLink>
-        ) : (
-          <StyledLink href={`/review/add?s=${slug}`}>
-            <Button>Add Review</Button>
-          </StyledLink>
-        )} */}
       </div>
     </>
   );
