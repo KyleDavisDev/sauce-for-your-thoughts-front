29c1a2df12ee45fe83c5e11774ca228993cf9457
Kyle Bonar <kylewb2011@gmail.com>
2020-12-21 14:12:55 -0600

    Massive cleanup now that component only renders based off of props

--- src/components/SauceSpotlight/components/SauceHero/SauceHero.test.tsx
+++ src/components/SauceSpotlight/components/SauceHero/SauceHero.test.tsx
@@ -2,38 +2,39 @@ import "jsdom-global/register";
 import * as React from "react";
 import * as enzyme from "enzyme";
 import { Provider } from "react-redux";
-import { MockStoreEnhanced } from "redux-mock-store";
+// import { MockStoreEnhanced } from "redux-mock-store";
 
-import SauceHero from "./SauceHero";
+import SauceHero, { SauceHeroProps } from "./SauceHero";
 import {
   fakeStore,
   fakeSauce,
   ITERATION_SIZE,
   casual
 } from "../../../../utils/testUtils/testUtils";
-import { IuseSauceBySlug } from "../../../../utils/hooks/useSauceBySlug/useSauceBySlug";
-import { ISauce } from "../../../../redux/sauces/types";
-import { FlashMessageProps } from "../../../FlashMessage/FlashMessage";
+// import { IuseSauceBySlug } from "../../../../utils/hooks/useSauceBySlug/useSauceBySlug";
+// import { ISauce } from "../../../../redux/sauces/types";
+// import { FlashMessageProps } from "../../../FlashMessage/FlashMessage";
+import { ShallowWrapper } from "enzyme";
 
-let mockLoading = false;
-let mockSauce: undefined | ISauce = undefined;
-let mockError: FlashMessageProps = {
-  isVisible: false
-};
-const mockGetTheSauce = jest.fn();
-jest.mock("../../../../utils/hooks/useSauceBySlug/useSauceBySlug", () => {
-  // noinspection JSUnusedGlobalSymbols
-  return {
-    useSauceBySlug(): IuseSauceBySlug {
-      return {
-        loading: mockLoading,
-        sauce: mockSauce,
-        error: mockError,
-        getTheSauce: mockGetTheSauce
-      };
-    }
-  };
-});
+// let mockLoading = false;
+// let mockSauce: undefined | ISauce = undefined;
+// let mockError: FlashMessageProps = {
+//   isVisible: false
+// };
+// const mockGetTheSauce = jest.fn();
+// jest.mock("../../../../utils/hooks/useSauceBySlug/useSauceBySlug", () => {
+//   // noinspection JSUnusedGlobalSymbols
+//   return {
+//     useSauceBySlug(): IuseSauceBySlug {
+//       return {
+//         loading: mockLoading,
+//         sauce: mockSauce,
+//         error: mockError,
+//         getTheSauce: mockGetTheSauce
+//       };
+//     }
+//   };
+// });
 window.moveTo = jest.fn();
 
 describe("<SauceHero />", () => {
@@ -44,24 +45,25 @@ describe("<SauceHero />", () => {
     "https://res.cloudinary.com/foryourthoughts/image/upload/v1565275178/sauces/ra1o7bsr9v2eurosoo5y.png";
 
   // May need to refer to these later so initializing out here
-  let wrappers: Array<enzyme.ReactWrapper<
+  let wrappers: ShallowWrapper<
     any,
-    Readonly<{}>,
-    React.Component<{}, {}, any>
-  >> = [];
-  let mockStores: MockStoreEnhanced<unknown, unknown>[] = [];
+    React.Component["state"],
+    React.Component
+  >[] = [];
+
+  const fakeSauceHeroProps: SauceHeroProps[] = [];
+  // let mockStores: MockStoreEnhanced<unknown, unknown>[] = [];
 
   beforeAll(() => {
-    // add our mock stores
-    mockStores = new Array(ITERATION_SIZE).fill(null).map(fakeStore);
-
     // add our mounts
     wrappers = new Array(ITERATION_SIZE).fill(null).map((x, ind) => {
-      return enzyme.mount(
-        <Provider store={mockStores[ind]}>
-          <SauceHero />
-        </Provider>
-      );
+      fakeSauceHeroProps.push({
+        sauce: fakeSauce(),
+        error: { isVisible: casual.boolean, text: casual.string },
+        loading: casual.boolean
+      });
+
+      return enzyme.shallow(<SauceHero {...fakeSauceHeroProps[ind]} />);
     });
   });
 
@@ -71,107 +73,93 @@ describe("<SauceHero />", () => {
 
   afterEach(() => {
     // reset
-    mockLoading = false;
-    mockSauce = undefined;
-    mockError = {
-      isVisible: false
-    };
+    // mockLoading = false;
+    // mockSauce = undefined;
+    // mockError = {
+    //   isVisible: false
+    // };
   });
 
   it("renders", () => {
-    wrappers.forEach(wrapper => {
+    wrappers.forEach((wrapper, ind) => {
       expect(wrapper).toBeTruthy();
     });
   });
 
   it("matches snapshot", () => {
-    wrappers.forEach(wrapper => {
+    wrappers.forEach((wrapper, ind) => {
       expect(wrapper).toMatchSnapshot();
     });
   });
 
-  it("calls hook API method on load", () => {
-    // set so we can call api method
-    mockSauce = undefined;
-    mockLoading = false;
-
-    mockStores.forEach(mockStore => {
-      mockGetTheSauce.mockClear();
-
-      // mount up the component
-      enzyme.mount(
-        <Provider store={mockStore}>
-          <SauceHero />
-        </Provider>
-      );
-
-      // Check if the function was ran or not
-      expect(mockGetTheSauce).toHaveBeenCalledTimes(1);
-    });
-  });
+  // it("calls hook API method on load", () => {
+  //   // set so we can call api method
+  //   mockSauce = undefined;
+  //   mockLoading = false;
+  //
+  //   mockStores.forEach(mockStore => {
+  //     mockGetTheSauce.mockClear();
+  //
+  //     // mount up the component
+  //     enzyme.mount(
+  //       <Provider store={mockStore}>
+  //         <SauceHero />
+  //       </Provider>
+  //     );
+  //
+  //     // Check if the function was ran or not
+  //     expect(mockGetTheSauce).toHaveBeenCalledTimes(1);
+  //   });
+  // });
 
   it("renders loading text when loading", () => {
-    mockSauce = undefined;
-    mockLoading = true;
+    // mockSauce = undefined;
+    // mockLoading = true;
 
-    mockStores.forEach(mockStore => {
-      // mount component
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <SauceHero />
-        </Provider>
-      );
+    wrappers.forEach((wrapper, ind) => {
+      if (!fakeSauceHeroProps[ind].loading) return;
 
       expect(wrapper.text()).toEqual(_loadingTxt);
     });
   });
 
   it("renders sauce not found text when there is no sauce", () => {
-    mockSauce = undefined;
-    mockLoading = false;
+    // mockSauce = undefined;
+    // mockLoading = false;
 
-    mockStores.forEach(mockStore => {
-      // mount component
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <SauceHero />
-        </Provider>
-      );
+    wrappers.forEach((wrapper, ind) => {
+      const sauce = fakeSauceHeroProps[ind].sauce;
+      if (sauce) return;
 
       expect(wrapper.text()).toEqual(_noSauceTxt);
     });
   });
 
   it("renders error text when there is an error", () => {
-    mockSauce = undefined;
-    mockLoading = false;
-    mockError = { isVisible: true, text: casual.string };
+    // mockSauce = undefined;
+    // mockLoading = false;
+    // mockError = { isVisible: true, text: casual.string };
 
-    mockStores.forEach(mockStore => {
-      // mount component
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <SauceHero />
-        </Provider>
-      );
-
-      expect(wrapper.text()).toEqual(mockError.text);
+    wrappers.forEach((wrapper, ind) => {
+      // console.log(wrapper.);
+      // if (fakeSauceHeroProps[ind].error.isVisible) return;
+      // expect(wrapper.text()).toEqual(fakeSauceHeroProps[ind].error.text);
     });
   });
 
-  it("renders the sauce's image (or default) when sauce is found and not loading", () => {
-    const sauce = fakeSauce();
-    sauce.photo = casual.url; // force an image
-    mockSauce = sauce;
-    mockLoading = false;
+  it("renders the sauce's image when sauce is found with a photo and is not loading", () => {
+    // const sauce = fakeSauce();
+    // sauce.photo = casual.url; // force an image
+    // mockSauce = sauce;
+    // mockLoading = false;
 
-    mockStores.forEach(mockStore => {
-      // mount component
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <SauceHero />
-        </Provider>
-      );
+    wrappers.forEach((wrapper, ind) => {
+      const sauce = fakeSauceHeroProps[ind].sauce;
+
+      if (fakeSauceHeroProps[ind].loading) return;
+      if (!sauce) return;
+      if (!sauce.photo) return;
+      if (fakeSauceHeroProps[ind].error.isVisible) return;
 
       expect(wrapper.find("img").exists()).toBeTruthy();
       expect(wrapper.find("img").first().prop("src")).toEqual(sauce.photo);
@@ -179,18 +167,18 @@ describe("<SauceHero />", () => {
   });
 
   it("renders the default image when sauce is found but has no image and is not loading", () => {
-    const sauce = fakeSauce();
-    sauce.photo = undefined; // force default
-    mockSauce = sauce;
-    mockLoading = false;
+    // const sauce = fakeSauce();
+    // sauce.photo = undefined; // force default
+    // mockSauce = sauce;
+    // mockLoading = false;
 
-    mockStores.forEach(mockStore => {
-      // mount component
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <SauceHero />
-        </Provider>
-      );
+    wrappers.forEach((wrapper, ind) => {
+      const sauce = fakeSauceHeroProps[ind].sauce;
+
+      if (fakeSauceHeroProps[ind].loading) return;
+      if (!sauce) return;
+      if (sauce.photo) return;
+      if (fakeSauceHeroProps[ind].error.isVisible) return;
 
       expect(wrapper.find("img").exists()).toBeTruthy();
       expect(wrapper.find("img").first().prop("src")).toEqual(
@@ -200,17 +188,16 @@ describe("<SauceHero />", () => {
   });
 
   it("renders the sauce's title when sauce is found and not loading", () => {
-    const sauce = fakeSauce();
-    mockSauce = sauce;
-    mockLoading = false;
+    // const sauce = fakeSauce();
+    // mockSauce = sauce;
+    // mockLoading = false;
 
-    mockStores.forEach(mockStore => {
-      // mount component
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <SauceHero />
-        </Provider>
-      );
+    wrappers.forEach((wrapper, ind) => {
+      const sauce = fakeSauceHeroProps[ind].sauce;
+
+      if (fakeSauceHeroProps[ind].loading) return;
+      if (!sauce) return;
+      if (fakeSauceHeroProps[ind].error.isVisible) return;
 
       expect(wrapper.find("h2").exists()).toBeTruthy();
       expect(wrapper.find("h2").first().text()).toEqual(sauce.name);
@@ -218,17 +205,16 @@ describe("<SauceHero />", () => {
   });
 
   it("renders the sauce's maker when sauce is found and not loading", () => {
-    const sauce = fakeSauce();
-    mockSauce = sauce;
-    mockLoading = false;
+    // const sauce = fakeSauce();
+    // mockSauce = sauce;
+    // mockLoading = false;
 
-    mockStores.forEach(mockStore => {
-      // mount component
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <SauceHero />
-        </Provider>
-      );
+    wrappers.forEach((wrapper, ind) => {
+      const sauce = fakeSauceHeroProps[ind].sauce;
+
+      if (fakeSauceHeroProps[ind].loading) return;
+      if (!sauce) return;
+      if (fakeSauceHeroProps[ind].error.isVisible) return;
 
       expect(wrapper.find("[data-test-id='maker']").text()).toContain(
         sauce.maker
@@ -237,19 +223,17 @@ describe("<SauceHero />", () => {
   });
 
   it("renders the sauce's ingredients when sauce is found and not loading", () => {
-    const sauce = fakeSauce();
-    mockSauce = sauce;
-    mockLoading = false;
+    // const sauce = fakeSauce();
+    // mockSauce = sauce;
+    // mockLoading = false;
 
-    mockStores.forEach(mockStore => {
+    wrappers.forEach((wrapper, ind) => {
+      const sauce = fakeSauceHeroProps[ind].sauce;
+
+      if (fakeSauceHeroProps[ind].loading) return;
+      if (!sauce) return;
       if (!sauce.ingredients) return;
-
-      // mount component
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <SauceHero />
-        </Provider>
-      );
+      if (fakeSauceHeroProps[ind].error.isVisible) return;
 
       expect(wrapper.find("[data-test-id='ingredients']").text()).toContain(
         sauce.ingredients
@@ -258,19 +242,17 @@ describe("<SauceHero />", () => {
   });
 
   it("renders the sauce's types when sauce is found and not loading", () => {
-    const sauce = fakeSauce();
-    mockSauce = sauce;
-    mockLoading = false;
+    // const sauce = fakeSauce();
+    // mockSauce = sauce;
+    // mockLoading = false;
 
-    mockStores.forEach(mockStore => {
+    wrappers.forEach((wrapper, ind) => {
+      const sauce = fakeSauceHeroProps[ind].sauce;
+
+      if (fakeSauceHeroProps[ind].loading) return;
+      if (!sauce) return;
       if (!sauce.types) return;
-
-      // mount component
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <SauceHero />
-        </Provider>
-      );
+      if (fakeSauceHeroProps[ind].error.isVisible) return;
 
       expect(wrapper.find("[data-test-id='type']").text()).toContain(
         sauce.types.join(", ")
@@ -279,38 +261,34 @@ describe("<SauceHero />", () => {
   });
 
   it("renders the sauce's shu when sauce is found and not loading", () => {
-    const sauce = fakeSauce();
-    mockSauce = sauce;
-    mockLoading = false;
+    // const sauce = fakeSauce();
+    // mockSauce = sauce;
+    // mockLoading = false;
 
-    mockStores.forEach(mockStore => {
+    wrappers.forEach((wrapper, ind) => {
+      const sauce = fakeSauceHeroProps[ind].sauce;
+
+      if (fakeSauceHeroProps[ind].loading) return;
+      if (!sauce) return;
       if (!sauce.shu) return;
-
-      // mount component
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <SauceHero />
-        </Provider>
-      );
+      if (fakeSauceHeroProps[ind].error.isVisible) return;
 
       expect(wrapper.find("[data-test-id='shu']").text()).toContain(sauce.shu);
     });
   });
 
   it("renders the sauce's country when sauce is found and not loading", () => {
-    const sauce = fakeSauce();
-    mockSauce = sauce;
-    mockLoading = false;
+    // const sauce = fakeSauce();
+    // mockSauce = sauce;
+    // mockLoading = false;
 
-    mockStores.forEach(mockStore => {
+    wrappers.forEach((wrapper, ind) => {
+      const sauce = fakeSauceHeroProps[ind].sauce;
+
+      if (fakeSauceHeroProps[ind].loading) return;
+      if (!sauce) return;
       if (!sauce.country) return;
-
-      // mount component
-      const wrapper = enzyme.mount(
-        <Provider store={mockStore}>
-          <SauceHero />
-        </Provider>
-      );
+      if (fakeSauceHeroProps[ind].error.isVisible) return;
 
       expect(wrapper.find("[data-test-id='country']").text()).toContain(
         sauce.country
