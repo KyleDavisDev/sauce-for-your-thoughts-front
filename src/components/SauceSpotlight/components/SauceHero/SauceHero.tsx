5a7f4dc7b3e200fcf9310b1731e69f4bc6bd37cc
Kyle Bonar <kylewb2011@gmail.com>
2020-12-21 14:35:42 -0600

    minor cleanup

--- src/components/SauceSpotlight/components/SauceHero/SauceHero.tsx
+++ src/components/SauceSpotlight/components/SauceHero/SauceHero.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useSauceBySlug } from "../../../../utils/hooks/useSauceBySlug/useSauceBySlug";
+
 import FlashMessage, {
   FlashMessageProps
 } from "../../../FlashMessage/FlashMessage";
