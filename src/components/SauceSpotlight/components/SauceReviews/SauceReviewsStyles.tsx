e032ae8f7f58224a1ed21e60638b56a6e681fa5e
Kyle Bonar <kylewb2011@gmail.com>
2021-01-03 13:02:17 -0600

    Removed unused style

--- src/components/SauceSpotlight/components/SauceReviews/SauceReviewsStyles.tsx
+++ src/components/SauceSpotlight/components/SauceReviews/SauceReviewsStyles.tsx
@@ -1,18 +1,10 @@
 import styled from "../../../../theme/styled-components";
 import Descriptor from "../../../Descriptor/Descriptor";
-import { Link } from "../../../Link/Link";
 
 export const StyledContainer = styled.div`
   margin-bottom: 1.5em;
 `;
 StyledContainer.displayName = "div";
-
-export const StyledLink = styled(Link)`
-  > div {
-    display: inline;
-  }
-`;
-StyledLink.displayName = "Link";
 
 export const StyledDescriptor = styled(Descriptor)`
   > p {
