0cc517f8a6938db6ab781f7ed239cd984e1b2fbf
Kyle Bonar <kylewb2011@gmail.com>
2021-01-10 17:23:59 -0600

    It matches snapshot, iterate over many samples

--- src/components/TopBar/components/LoggedInBar/components/UserDropdown/UserDropdown.test.tsx
+++ src/components/TopBar/components/LoggedInBar/components/UserDropdown/UserDropdown.test.tsx
@@ -2,11 +2,26 @@ import * as React from "react";
 import * as enzyme from "enzyme";
 
 import UserDropdown from "./UserDropdown";
+import { ITERATION_SIZE } from "../../../../../../utils/testUtils/testUtils";
 
 describe("<UserDropdown />", () => {
-  it("renders", () => {
-    const wrapper = enzyme.shallow(<UserDropdown />);
+  let wrappers: any = [];
 
-    expect(wrapper.exists()).toBeTruthy();
+  beforeAll(() => {
+    wrappers = new Array(ITERATION_SIZE).fill(null).map(() => {
+      return enzyme.shallow(<UserDropdown />);
+    });
+  });
+
+  it("renders", () => {
+    wrappers.forEach(wrapper => {
+      expect(wrapper.exists()).toBeTruthy();
+    });
+  });
+
+  it("matches snapshot", () => {
+    wrappers.forEach(wrapper => {
+      expect(wrapper.exists()).toMatchSnapshot();
+    });
   });
 });
